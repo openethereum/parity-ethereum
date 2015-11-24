e335e7a8186836bca9808d6ac64a58168a0359a2
Gav Wood <i@gavwood.com>
2015-11-24 22:21:15 +0100

    Silly test function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-#[test]
-fn it_works() {
+#[macro_use] extern crate log;
+
+/// Silly function to return 69.
+///
+/// # Example
+///
+/// ```
+/// assert_eq!(ethcore::sixtynine(), 69);
+/// ```
+pub fn sixtynine() -> i32 {
+	debug!("Hello world!");
+	69
 }
+
