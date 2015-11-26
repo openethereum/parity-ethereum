8956d4cd5f8af43a395cf0e768ad54305fdcba6f
arkpar <arkady.paronyan@gmail.com>
2015-11-26 23:45:02 +0100

    Added rand and mio

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 extern crate rustc_serialize;
+extern crate mio;
+extern crate rand;
+#[macro_use]
+extern crate log;
 
 pub use std::str::FromStr;
 
@@ -8,6 +12,8 @@ pub mod uint;
 pub mod bytes;
 pub mod rlp;
 pub mod vector;
+
+//pub mod network;
 
 #[test]
 fn it_works() {
