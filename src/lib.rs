d516fca1653a2242b8b51b4adb4455c159c119ed
debris <marek.kotewicz@gmail.com>
2015-12-05 19:44:43 +0100

    added installation guide

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,74 @@
+//! Ethcore's ethereum implementation
+//! 
+//! ### Rust version
+//! - beta
+//! - nightly
+//!
+//! ### Supported platforms:
+//! - OSX
+//! - Linux/Ubuntu
+//! 
+//! ### Dependencies:
+//! - RocksDB 3.13
+//! - LLVM 3.7 (optional, required for `jit`)
+//! - evmjit (optional, required for `jit`)
+//!
+//! ### Dependencies Installation
+//! 
+//! - OSX
+//! 
+//!   - rocksdb
+//!   ```
+//!   brew install rocksdb
+//!   ```
+//!   
+//!   - llvm
+//!     
+//!       - download llvm 3.7 from http://llvm.org/apt/
+//!
+//!       ```
+//!       cd llvm-3.7.0.src
+//!       mkdir build && cd $_
+//!       cmake -G "Unix Makefiles" .. -DCMAKE_C_FLAGS_RELEASE= -DCMAKE_CXX_FLAGS_RELEASE= -DCMAKE_INSTALL_PREFIX=/usr/local/Cellar/llvm/3.7 -DCMAKE_BUILD_TYPE=Release 
+//!       make && make install
+//!       ```
+//!   - evmjit
+//!   
+//!       - download from https://github.com/debris/evmjit
+//!       
+//!       ```
+//!       cd evmjit
+//!       mkdir build && cd $_
+//!       cmake -DLLVM_DIR=/usr/local/lib/llvm-3.7/share/llvm/cmake ..
+//!       make && make install
+//!       ```
+//! 
+//! - Linux/Ubuntu
+//! 
+//!   - rocksdb
+//!
+//!     ```
+//!     wget https://github.com/facebook/rocksdb/archive/rocksdb-3.13.tar.gz
+//!     tar xvf rocksdb-3.13.tar.gz && cd rocksdb-rocksdb-3.13 && make shared_lib
+//!     sudo make install
+//!     ```
+//!   
+//!   - llvm
+//!   
+//!       - install using packages from http://llvm.org/apt/
+//!   
+//!   - evmjit
+//!   
+//!       - download from https://github.com/debris/evmjit
+//!       
+//!       ```
+//!       cd evmjit
+//!       mkdir build && cd $_
+//!       cmake .. && make
+//!       sudo make install
+//!       sudo ldconfig
+//!       ```
+
 #[macro_use] 
 extern crate log;
 extern crate ethcore_util;
