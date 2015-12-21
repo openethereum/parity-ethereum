07aef2499e5b1ed32b1645e8febfd9a689df9a17
Gav Wood <i@gavwood.com>
2015-12-21 21:08:42 +0000

    Spec can now read much from a JSON. Still todo builtins and params.

--- src/spec.rs
+++ src/spec.rs
@@ -1,17 +1,41 @@
-use std::collections::hash_map::*;
+use std::io::Read;
+use std::collections::HashMap;
 use std::cell::*;
 use std::str::FromStr;
+use rustc_serialize::base64::FromBase64;
+use rustc_serialize::json::Json;
+use rustc_serialize::hex::FromHex;
+use flate2::read::GzDecoder;
 use util::uint::*;
 use util::hash::*;
 use util::bytes::*;
 use util::triehash::*;
 use util::error::*;
 use util::rlp::*;
-use account::Account;
+use util::sha3::*;
+use account::*;
 use engine::Engine;
 use builtin::Builtin;
 use null_engine::NullEngine;
 use denominations::*;
+use header::*;
+
+/// Converts file from base64 gzipped bytes to json
+pub fn base_to_json(source: &[u8]) -> Json {
+	// there is probably no need to store genesis in based64 gzip,
+	// but that's what go does, and it was easy to load it this way
+	let data = source.from_base64().expect("Genesis block is malformed!");
+	let data_ref: &[u8] = &data;
+	let mut decoder = GzDecoder::new(data_ref).expect("Gzip is invalid");
+	let mut s: String = "".to_string();
+	decoder.read_to_string(&mut s).expect("Gzip is invalid");
+	Json::from_str(&s).expect("Json is invalid")
+}
+
+/// Convert JSON to a string->RLP map.
+pub fn json_to_rlp(_json: &Json) -> HashMap<String, Bytes> {
+	unimplemented!();
+}
 
 /// Parameters for a block chain; includes both those intrinsic to the design of the
 /// chain and those to be interpreted by the active chain engine.
@@ -43,6 +67,9 @@ pub struct Spec {
 }
 
 impl Spec {
+//	pub fn new(engine_name: String, engine_params: HashMap<String, Bytes>, builtins: HashMap<Address, Builtin>, genesis: Genesis) {
+//	}
+
 	/// Convert this object into a boxed Engine of the right underlying type.
 	// TODO avoid this hard-coded nastiness - use dynamic-linked plugin framework instead.
 	pub fn to_engine(self) -> Result<Box<Engine>, EthcoreError> {
@@ -62,13 +89,90 @@ impl Spec {
 
 	/// Compose the genesis block for this chain.
 	pub fn genesis_block(&self) -> Bytes {
-		// TODO
-		unimplemented!();
+		let empty_list = RlpStream::new_list(0).out();
+		let empty_list_sha3 = empty_list.sha3();
+		let header = Header {
+			parent_hash: self.parent_hash.clone(),
+			timestamp: self.timestamp.clone(),
+			number: U256::from(0u8),
+			author: self.author.clone(),
+			transactions_root: SHA3_EMPTY.clone(),
+			uncles_hash: empty_list_sha3.clone(),
+			extra_data: self.extra_data.clone(),
+			state_root: self.state_root().clone(),
+			receipts_root: SHA3_EMPTY.clone(),
+			log_bloom: H2048::new().clone(),
+			gas_used: self.gas_used.clone(),
+			gas_limit: self.gas_limit.clone(),
+			difficulty: self.difficulty.clone(),
+			seal: {
+				let seal = {
+					let mut s = RlpStream::new_list(self.seal_fields);
+					s.append_raw(&self.seal_rlp, self.seal_fields);
+					s.out()
+				};
+				let r = Rlp::new(&seal);
+				(0..self.seal_fields).map(|i| r.at(i).raw().to_vec()).collect()
+			},
+		};
+		let mut ret = RlpStream::new_list(3);
+		ret.append(&header);
+		ret.append_raw(&empty_list, 1);
+		ret.append_raw(&empty_list, 1);
+		ret.out()
 	}
 }
 
 
 impl Spec {
+	/// Loads genesis block from json file
+	pub fn from_json(json: Json) -> Spec {
+		// once we commit ourselves to some json parsing library (serde?)
+		// move it to proper data structure
+		let mut state = HashMap::new();
+		let accounts = json["alloc"].as_object().expect("Missing genesis state");
+		for (address, acc) in accounts.iter() {
+			let addr = Address::from_str(address).unwrap();
+			let o = acc.as_object().unwrap();
+			let balance = U256::from_dec_str(o["balance"].as_string().unwrap()).unwrap();
+			state.insert(addr, Account::new_basic(balance, U256::from(0)));
+		}
+
+		let builtins = {
+			// TODO: populate from json.
+			HashMap::new()
+		};
+
+		let (seal_fields, seal_rlp) = {
+			if json.find("mixhash").is_some() && json.find("nonce").is_some() {
+				let mut s = RlpStream::new();
+				s.append(&H256::from_str(&json["mixhash"].as_string().unwrap()[2..]).unwrap());
+				s.append(&H64::from_str(&json["nonce"].as_string().unwrap()[2..]).unwrap());
+				(2, s.out())
+			} else {
+				// backup algo that will work with sealFields/sealRlp (and without).
+				(usize::from_str(&json["sealFields"].as_string().unwrap_or("0x")[2..]).unwrap(), json["sealRlp"].as_string().unwrap_or("0x")[2..].from_hex().unwrap())
+			}
+		};
+
+		Spec {
+			engine_name: json["engineName"].as_string().unwrap().to_string(),
+			engine_params: json_to_rlp(&json["engineParams"]),
+			builtins: builtins,
+			parent_hash: H256::from_str(&json["parentHash"].as_string().unwrap()[2..]).unwrap(),
+			author: Address::from_str(&json["coinbase"].as_string().unwrap()[2..]).unwrap(),
+			difficulty: U256::from_str(&json["difficulty"].as_string().unwrap()[2..]).unwrap(),
+			gas_limit: U256::from_str(&json["gasLimit"].as_string().unwrap()[2..]).unwrap(),
+			gas_used: U256::from(0u8),
+			timestamp: U256::from_str(&json["timestamp"].as_string().unwrap()[2..]).unwrap(),
+			extra_data: json["extraData"].as_string().unwrap()[2..].from_hex().unwrap(),
+			genesis_state: state,
+			seal_fields: seal_fields,
+			seal_rlp: seal_rlp,
+			state_root_memo: RefCell::new(json["stateRoot"].as_string().map(|s| H256::from_str(&s[2..]).unwrap())),
+		}
+	}
+
 	pub fn olympic() -> Spec {
 		Spec {
 			engine_name: "Ethash".to_string(),
