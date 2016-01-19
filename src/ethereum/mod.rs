e82bcedf2e69c74af381b03ededa1db709a04b14
Gav Wood <gav@ethcore.io>
2016-01-19 11:57:10 +0100

    Fix again.

--- src/ethereum/mod.rs
+++ src/ethereum/mod.rs
@@ -40,7 +40,7 @@ mod tests {
 	fn ensure_db_good() {
 		let engine = new_morden().to_engine().unwrap();
 		let genesis_header = engine.spec().genesis_header();
-		let mut db = MemoryDB::new();
+		let mut db = JournalDB::new_temp();
 		engine.spec().ensure_db_good(&mut db);
 		let s = State::from_existing(db.clone(), genesis_header.state_root.clone(), engine.account_start_nonce());
 		assert_eq!(s.balance(&address_from_hex("0000000000000000000000000000000000000001")), U256::from(1u64));
