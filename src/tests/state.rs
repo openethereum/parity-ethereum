bb4e31b22438756a4ff9b8c9a91dba5fede91374
Tomusdrw <tomusdrw@gmail.com>
2016-01-22 08:04:52 +0100

    Marking test as heavy / ignored

--- src/tests/state.rs
+++ src/tests/state.rs
@@ -74,19 +74,19 @@ fn do_json_test(json_data: &[u8]) -> Vec<String> {
 declare_test!{StateTests_stBlockHashTest, "StateTests/stBlockHashTest"}
 declare_test!{StateTests_stCallCodes, "StateTests/stCallCodes"}
 declare_test!{ignore => StateTests_stCallCreateCallCodeTest, "StateTests/stCallCreateCallCodeTest"}	//<< Out of stack
-declare_test!{StateTests_stDelegatecallTest, "StateTests/stDelegatecallTest"}						//<< FAIL - gas too high
+declare_test!{StateTests_stDelegatecallTest, "StateTests/stDelegatecallTest"}
 declare_test!{StateTests_stExample, "StateTests/stExample"}
 declare_test!{StateTests_stInitCodeTest, "StateTests/stInitCodeTest"}
 declare_test!{StateTests_stLogTests, "StateTests/stLogTests"}
 declare_test!{StateTests_stMemoryStressTest, "StateTests/stMemoryStressTest"}
 declare_test!{StateTests_stMemoryTest, "StateTests/stMemoryTest"}
 declare_test!{StateTests_stPreCompiledContracts, "StateTests/stPreCompiledContracts"}
-declare_test!{heavy => StateTests_stQuadraticComplexityTest, "StateTests/stQuadraticComplexityTest"}	//<< Too long
-declare_test!{ignore => StateTests_stRecursiveCreate, "StateTests/stRecursiveCreate"}					//<< Out of stack 
+declare_test!{heavy => StateTests_stQuadraticComplexityTest, "StateTests/stQuadraticComplexityTest"} //<< Too long
+declare_test!{ignore => StateTests_stRecursiveCreate, "StateTests/stRecursiveCreate"} //<< Out of stack 
 declare_test!{StateTests_stRefundTest, "StateTests/stRefundTest"}
 declare_test!{StateTests_stSolidityTest, "StateTests/stSolidityTest"}
-declare_test!{ignore => StateTests_stSpecialTest, "StateTests/stSpecialTest"}							//<< Signal 11
-declare_test!{ignore => StateTests_stSystemOperationsTest, "StateTests/stSystemOperationsTest"}		//<< Signal 11
+declare_test!{ignore => StateTests_stSpecialTest, "StateTests/stSpecialTest"} //<< Out of Stack
+declare_test!{ignore => StateTests_stSystemOperationsTest, "StateTests/stSystemOperationsTest"} //<< Out of stack
 declare_test!{StateTests_stTransactionTest, "StateTests/stTransactionTest"}
 declare_test!{StateTests_stTransitionTest, "StateTests/stTransitionTest"}
 declare_test!{StateTests_stWalletTest, "StateTests/stWalletTest"}
