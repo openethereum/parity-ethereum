800154a8ae66a941c50a5adacccfe94195831e85
Nikolay Volf <nikvolf@gmail.com>
2016-01-26 14:39:49 +0400

    client test creating

---
+++ src/tests/client.rs
@@ -0,0 +1,20 @@
+use ethereum;
+use client::{BlockChainClient,Client};
+use std::env;
+use pod_state::*;
+
+
+#[test]
+fn test_client_is_created() {
+
+	let mut spec = ethereum::new_frontier_like_test();
+	spec.set_genesis_state(PodState::from_json(test.find("pre").unwrap()));
+	spec.overwrite_genesis(test.find("genesisBlockHeader").unwrap());
+
+	let mut dir = env::temp_dir();
+	dir.push(H32::random().hex());
+
+	let client_result = Client::new(spec, &dir, IOChannel::disconnected());
+
+	assert!(client_result.is_ok());
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,3 +4,4 @@ mod test_common;
 mod transaction;
 mod executive;
 mod state;
+mod client;
