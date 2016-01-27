f8568e10b7fe4ffbc525188ca360cd8e09fd55e8
debris <marek.kotewicz@gmail.com>
2016-01-27 19:03:12 +0100

    missing net_ methods

--- rpc/src/v1/traits/net.rs
+++ rpc/src/v1/traits/net.rs
@@ -10,11 +10,16 @@ pub trait Net: Sized + Send + Sync + 'static {
 	/// Returns number of peers connected to node.
 	fn peer_count(&self, _: Params) -> Result<Value, Error> { rpc_unimplemented!() }
 
+	/// Returns true if client is actively listening for network connections.
+	/// Otherwise false.
+	fn is_listening(&self, _: Params) -> Result<Value, Error> { rpc_unimplemented!() }
+
 	/// Should be used to convert object to io delegate.
 	fn to_delegate(self) -> IoDelegate<Self> {
 		let mut delegate = IoDelegate::new(Arc::new(self));
 		delegate.add_method("net_version", Net::version);
 		delegate.add_method("net_peerCount", Net::peer_count);
+		delegate.add_method("net_listening", Net::is_listening);
 		delegate
 	}
 }
