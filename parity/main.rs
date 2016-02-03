f2b8d6336af2d174c5b91cdd1a0003b33bd69e20
Gav Wood <gav@ethcore.io>
2016-02-04 00:48:36 +0100

    Fix default options.

--- parity/main.rs
+++ parity/main.rs
@@ -37,10 +37,10 @@ Usage:
 Options:
   -l --logging LOGGING     Specify the logging level.
   -j --jsonrpc             Enable the JSON-RPC API sever.
-  --jsonrpc-url URL        Specify URL for JSON-RPC API server (default: 127.0.0.1:8545).
+  --jsonrpc-url URL        Specify URL for JSON-RPC API server [default: 127.0.0.1:8545].
 
-  --cache-pref-size BYTES  Specify the prefered size of the blockchain cache in bytes (default: 16384).
-  --cache-max-size BYTES   Specify the maximum size of the blockchain cache in bytes (default: 262144).
+  --cache-pref-size BYTES  Specify the prefered size of the blockchain cache in bytes [default: 16384].
+  --cache-max-size BYTES   Specify the maximum size of the blockchain cache in bytes [default: 262144].
 
   -h --help                Show this screen.
 ", flag_cache_pref_size: usize, flag_cache_max_size: usize);
@@ -77,6 +77,8 @@ fn setup_rpc_server(_client: Arc<Client>, _sync: Arc<EthSync>, _url: &str) {
 
 fn main() {
 	let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+
+	println!("{:?}", args);
 
 	setup_log(&args.flag_logging);
 
