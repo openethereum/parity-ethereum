c9670b00322075b299e804934bb746d03c33abf9
debris <marek.kotewicz@gmail.com>
2016-02-03 17:35:25 +0100

    use env package version

--- rpc/src/v1/impls/web3.rs
+++ rpc/src/v1/impls/web3.rs
@@ -14,7 +14,7 @@ impl Web3Client {
 impl Web3 for Web3Client {
 	fn client_version(&self, params: Params) -> Result<Value, Error> {
 		match params {
-			Params::None => Ok(Value::String(format!("parity/0.9.0/{}/rust1.8-nightly", Target::os()).to_owned())),
+			Params::None => Ok(Value::String(format!("parity/{}/{}/rust1.8-nightly", env!("CARGO_PKG_VERSION"), Target::os()).to_owned())),
 			_ => Err(Error::invalid_params())
 		}
 	}
