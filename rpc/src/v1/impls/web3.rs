0b3acf7b13d3a4fb54fbbc03790981168cc5a913
Gav Wood <g@ethdev.com>
2016-02-03 17:58:22 +0100

    Update web3.rs

--- rpc/src/v1/impls/web3.rs
+++ rpc/src/v1/impls/web3.rs
@@ -14,7 +14,7 @@ impl Web3Client {
 impl Web3 for Web3Client {
 	fn client_version(&self, params: Params) -> Result<Value, Error> {
 		match params {
-			Params::None => Ok(Value::String(format!("parity/{}/{}/rust1.8-nightly", env!("CARGO_PKG_VERSION"), Target::os()).to_owned())),
+			Params::None => Ok(Value::String(format!("Parity/{}/{}-{}-{}", env!("CARGO_PKG_VERSION"), Target::arch(), Target::env(), Target::os()))),
 			_ => Err(Error::invalid_params())
 		}
 	}
