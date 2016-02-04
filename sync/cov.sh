e27081fe668b3ad5f2c8274789d91cd08bf7ba2f
Nikolay Volf <nikvolf@gmail.com>
2016-02-04 21:09:32 +0300

    filter out util/core

--- sync/cov.sh
+++ sync/cov.sh
@@ -5,5 +5,5 @@ fi
 
 cargo test --no-run || exit $?
 mkdir -p target/coverage
-kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/ethsync*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern sync/src --verify target/coverage target/debug/ethsync*
 xdg-open target/coverage/index.html
