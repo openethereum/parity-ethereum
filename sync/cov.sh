0c90b75c67f618aa48c6cf426ee26672ba62fcc0
Nikolay Volf <nikvolf@gmail.com>
2016-02-05 02:01:29 +0300

    excluded test code from coverage

--- sync/cov.sh
+++ sync/cov.sh
@@ -5,5 +5,5 @@ fi
 
 cargo test --no-run || exit $?
 mkdir -p target/coverage
-kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern sync/src --verify target/coverage target/debug/ethsync*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1,sync/src/tests --include-pattern sync/src --verify target/coverage target/debug/ethsync*
 xdg-open target/coverage/index.html
