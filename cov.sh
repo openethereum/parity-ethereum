f5b218ba89c0744acc0eae9c548f737d2d323cb3
Nikolay Volf <nikvolf@gmail.com>
2016-02-08 07:25:41 -0800

    making local coverage identical to CI

--- cov.sh
+++ cov.sh
@@ -15,7 +15,11 @@ if ! type kcov > /dev/null; then
 	exit 1
 fi
 
-cargo test --features ethcore/json-tests -p ethcore --no-run || exit $?
+cargo test --features ethcore/json-tests -p ethash -p ethcore-util -p ethcore -p ethsync -p ethcore-rpc -p parity --no-run || exit $?
 mkdir -p target/coverage
-kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethcore*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethcore-*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethash-*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethcore_util-*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethsync-*
+kcov --exclude-pattern ~/.multirust,rocksdb,secp256k1 --include-pattern src --verify target/coverage target/debug/deps/ethcore_rpc-*
 xdg-open target/coverage/index.html
