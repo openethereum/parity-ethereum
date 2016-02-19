f50bf528e6106960f8b8620eb59df195d1334a82
debris <marek.kotewicz@gmail.com>
2016-02-19 11:57:52 +0100

    fixed allow warnings in util

--- util/src/panics.rs
+++ util/src/panics.rs
@@ -72,7 +72,7 @@ impl PanicHandler {
 
 	/// Invoke closure and catch any possible panics.
 	/// In case of panic notifies all listeners about it.
-	#[allow(deprecated)]
+	#[cfg_attr(feature="dev", allow(deprecated))]
 	// TODO [todr] catch_panic is deprecated but panic::recover has different bounds (not allowing mutex)
 	pub fn catch_panic<G, R>(&self, g: G) -> thread::Result<R> where G: FnOnce() -> R + Send + 'static {
 		let guard = PanicGuard { handler: self };
