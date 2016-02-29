06623333d9de984c22eaa6adf0e5eed2d168070f
Nikolay Volf <nikvolf@gmail.com>
2016-02-29 15:23:43 +0300

    fix tabs

--- util/build.rs
+++ util/build.rs
@@ -7,7 +7,7 @@ use rustc_version::{version_meta, Channel};
 fn main() {
 	vergen(OutputFns::all()).unwrap();
 
-    if let Channel::Nightly = version_meta().channel {
+	if let Channel::Nightly = version_meta().channel {
 		println!("cargo:rustc-cfg=x64asm");
-    }
+	}
 }
