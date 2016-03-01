063020f5075f245b0c70c1a5c1918229efe25fe4
debris <marek.kotewicz@gmail.com>
2016-03-01 13:46:33 +0100

    added license headers

--- ethcore/src/blockchain/helpers/mod.rs
+++ ethcore/src/blockchain/helpers/mod.rs
@@ -1,3 +1,17 @@
-pub mod generators;
+// Copyright 2015, 2016 Ethcore (UK) Ltd.
+// This file is part of Parity.
 
-//pub use self::blockchain_builder::BlockChainBuilder;
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
+pub mod generators;
