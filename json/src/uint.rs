7a0dfec115c85c683d10ef166e5f25bd094a6e85
debris <marek.kotewicz@gmail.com>
2016-03-17 14:03:53 +0100

    added genesis method to ethjson blockchain

--- json/src/uint.rs
+++ json/src/uint.rs
@@ -22,7 +22,7 @@ use serde::de::Visitor;
 use util::numbers::{U256, Uint as U};
 
 /// Lenient uint json deserialization for test json files.
-#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub struct Uint(U256);
 
 impl Into<U256> for Uint {
