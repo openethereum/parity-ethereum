e942f86bd759d7918ebcea7f8c2c9f4a36a2a839
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2016-04-29 20:52:08 +0200

    Modules RPC (#1019)

--- rpc/src/v1/tests/rpc.rs
+++ rpc/src/v1/tests/rpc.rs
@@ -14,16 +14,25 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Ethcore rpc v1.
-//!
-//! Compliant with ethereum rpc.
+use std::collections::BTreeMap;
+use jsonrpc_core::IoHandler;
+use v1::{Rpc, RpcClient};
 
-pub mod traits;
-mod impls;
-mod types;
-mod helpers;
 
-pub mod tests;
+fn rpc_client() -> RpcClient {
+	let mut modules = BTreeMap::new();
+	modules.insert("rpc".to_owned(), "1.0".to_owned());
+	RpcClient::new(modules)
+}
 
-pub use self::traits::{Web3, Eth, EthFilter, Personal, Net, Ethcore, Rpc};
-pub use self::impls::*;
+#[test]
+fn rpc_modules() {
+	let rpc = rpc_client().to_delegate();
+	let io = IoHandler::new();
+	io.add_delegate(rpc);
+
+	let request = r#"{"jsonrpc": "2.0", "method": "modules", "params": [], "id": 1}"#;
+	let response = r#"{"jsonrpc":"2.0","result":{"eth": "1.0"},"id":1}"#;
+
+	assert_eq!(io.handle_request(request), Some(response.to_owned()));
+}
