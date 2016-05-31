d0b32f8d426398297bc5d07d859a27b6e7c5d80b
Nikolay Volf <nikvolf@gmail.com>
2016-05-31 13:02:53 +0200

    trait import from branch

--- db/src/traits.rs
+++ db/src/traits.rs
@@ -1,12 +1,29 @@
+// Copyright 2015, 2016 Ethcore (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
 //! Ethcore database trait
 
-use ipc::BinaryConvertable;
 use std::mem;
 use ipc::binary::BinaryConvertError;
 use std::collections::VecDeque;
+use std::cell::RefCell;
 
-pub type TransactionHandle = u32;
 pub type IteratorHandle = u32;
+
+pub const DEFAULT_CACHE_LEN: usize = 12288;
 
 #[derive(Binary)]
 pub struct KeyValue {
@@ -14,8 +31,8 @@ pub struct KeyValue {
 	pub value: Vec<u8>,
 }
 
-#[derive(Debug, Binary)]
-pub enum Error {
+	#[derive(Debug, Binary)]
+	pub enum Error {
 	AlreadyOpen,
 	IsClosed,
 	RocksDb(String),
@@ -28,12 +45,35 @@ pub enum Error {
 #[derive(Binary)]
 pub struct DatabaseConfig {
 	/// Optional prefix size in bytes. Allows lookup by partial key.
-	pub prefix_size: Option<usize>
+	pub prefix_size: Option<usize>,
+	/// write cache length
+	pub cache: usize,
 }
 
-pub trait DatabaseService {
+impl Default for DatabaseConfig {
+	fn default() -> DatabaseConfig {
+		DatabaseConfig {
+			prefix_size: None,
+			cache: DEFAULT_CACHE_LEN,
+		}
+	}
+}
+
+impl DatabaseConfig {
+	fn with_prefix(prefix: usize) -> DatabaseConfig {
+		DatabaseConfig {
+			prefix_size: Some(prefix),
+			cache: DEFAULT_CACHE_LEN,
+		}
+	}
+}
+
+	pub trait DatabaseService : Sized {
 	/// Opens database in the specified path
 	fn open(&self, config: DatabaseConfig, path: String) -> Result<(), Error>;
+
+	/// Opens database in the specified path with the default config
+	fn open_default(&self, path: String) -> Result<(), Error>;
 
 	/// Closes database
 	fn close(&self) -> Result<(), Error>;
@@ -43,18 +83,6 @@ pub trait DatabaseService {
 
 	/// Delete value by key.
 	fn delete(&self, key: &[u8]) -> Result<(), Error>;
-
-	/// Insert a key-value pair in the transaction. Any existing value value will be overwritten.
-	fn transaction_put(&self, transaction: TransactionHandle, key: &[u8], value: &[u8]) -> Result<(), Error>;
-
-	/// Delete value by key using transaction
-	fn transaction_delete(&self, transaction: TransactionHandle, key: &[u8]) -> Result<(), Error>;
-
-	/// Commit transaction to database.
-	fn write(&self, tr: TransactionHandle) -> Result<(), Error>;
-
-	/// Initiate new transaction on database
-	fn new_transaction(&self) -> TransactionHandle;
 
 	/// Get value by key.
 	fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Error>;
@@ -70,4 +98,35 @@ pub trait DatabaseService {
 
 	/// Next key-value for the the given iterator
 	fn iter_next(&self, iterator: IteratorHandle) -> Option<KeyValue>;
+
+	/// Dispose iteration that is no longer needed
+	fn dispose_iter(&self, handle: IteratorHandle) -> Result<(), Error>;
+
+	/// Write client transaction
+	fn write(&self, transaction: DBTransaction) -> Result<(), Error>;
+}
+
+#[derive(Binary)]
+pub struct DBTransaction {
+	pub writes: RefCell<Vec<KeyValue>>,
+	pub removes: RefCell<Vec<Vec<u8>>>,
+}
+
+impl DBTransaction {
+	pub fn new() -> DBTransaction {
+		DBTransaction {
+			writes: RefCell::new(Vec::new()),
+			removes: RefCell::new(Vec::new()),
+		}
+	}
+
+	pub fn put(&self, key: &[u8], value: &[u8]) {
+		let mut brw = self.writes.borrow_mut();
+		brw.push(KeyValue { key: key.to_vec(), value: value.to_vec() });
+	}
+
+	pub fn delete(&self, key: &[u8]) {
+		let mut brw = self.removes.borrow_mut();
+		brw.push(key.to_vec());
+	}
 }
