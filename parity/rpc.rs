6f4e01a93c08ab59baccf5ee36416b8f841e525a
NikVolf <nikvolf@gmail.com>
2016-06-11 01:51:32 +0300

    tweaks and fixes

--- parity/rpc.rs
+++ parity/rpc.rs
@@ -20,7 +20,6 @@ use std::sync::Arc;
 use std::net::SocketAddr;
 use util::panics::PanicHandler;
 use die::*;
-#[cfg(not(windows))]
 use jsonipc;
 use rpc_apis;
 use std::fmt;
@@ -122,14 +121,13 @@ pub fn setup_ipc_rpc_server(_dependencies: &Dependencies, _addr: &str, _apis: Ve
 	die!("Your Parity version has been compiled without JSON-RPC support.")
 }
 
-#[cfg(not(windows))]
 pub fn new_ipc(conf: IpcConfiguration, deps: &Dependencies) -> Option<jsonipc::Server> {
 	if !conf.enabled { return None; }
 	let apis = conf.apis.split(',').collect();
 	Some(setup_ipc_rpc_server(deps, &conf.socket_addr, apis))
 }
 
-#[cfg(all(feature = "rpc", not(windows)))]
+#[cfg(feature = "rpc")]
 pub fn setup_ipc_rpc_server(dependencies: &Dependencies, addr: &str, apis: Vec<&str>) -> jsonipc::Server {
 	let server = setup_rpc_server(apis, dependencies);
 	match server.start_ipc(addr) {
@@ -137,9 +135,4 @@ pub fn setup_ipc_rpc_server(dependencies: &Dependencies, addr: &str, apis: Vec<&
 		Err(any_error) => die!("RPC: {:?}", any_error),
 		Ok(server) => server
 	}
-}
-
-#[cfg(windows)]
-pub fn new_ipc(_conf: IpcConfiguration, _deps: &Dependencies) -> Option<()> {
-	Some(())
 }
