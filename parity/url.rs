91bb6c5d430fb94598c5f3752adffacfbb2b651c
Nikolay Volf <nikvolf@gmail.com>
2016-06-14 18:26:40 +0300

    final fix

--- parity/url.rs
+++ parity/url.rs
@@ -48,10 +48,12 @@ pub fn open(url: &str) {
 
 #[cfg(target_os="macos")]
 pub fn open(url: &str) {
+	use std;
 	let _ = std::process::Command::new("open").arg(url).output();
 }
 
 #[cfg(target_os="linux")]
 pub fn open(url: &str) {
+	use std;
 	let _ = std::process::Command::new("xdg-open").arg(url).output();
 }
