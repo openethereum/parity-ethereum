37260d7cf5f6f85495221e7e16e5541db02b49c7
NikVolf <nikvolf@gmail.com>
2016-06-14 18:16:41 +0300

    fix build and remove .cargo regs

--- parity/url.rs
+++ parity/url.rs
@@ -46,12 +46,12 @@ pub fn open(url: &str) {
 	}
 }
 
-#[cfg(macos)]
+#[cfg(target_os="macos")]
 pub fn open(url: &str) {
 	let _ = std::process::Command::new("open").arg(url).output();
 }
 
-#[cfg(linux)]
+#[cfg(target_os="linux")]
 pub fn open(url: &str) {
 	let _ = std::process::Command::new("xdg-open").arg(url).output();
 }
