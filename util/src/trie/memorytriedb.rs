cc50bf81bfdc96021939c2f4a72c283efcf42590
Robert Habermeier <rphmeier@gmail.com>
2016-06-22 17:58:25 +0200

    don't load whole trie into memory, lookup across memory and db

--- util/src/trie/memorytriedb.rs
+++ util/src/trie/memorytriedb.rs
@@ -26,11 +26,19 @@ use ::rlp::{Rlp, View};
 use std::collections::VecDeque;
 use std::ops::{Index, IndexMut};
 
-/// For lookups into the Node storage buffer.
-/// This is deliberately non-copyable.
+// For lookups into the Node storage buffer.
+// This is deliberately non-copyable.
 struct StorageHandle(usize);
 
-fn empty_children() -> [Option<StorageHandle>; 16] {
+// Handles to nodes in the trie.
+enum NodeHandle {
+	/// Loaded into memory.
+	InMemory(StorageHandle),
+	/// Either a hash or an inline node
+	Hash(H256),
+}
+
+fn empty_children() -> [Option<NodeHandle>; 16] {
 	[
 		None, None, None, None, None, None, None, None,
 		None, None, None, None, None, None, None, None,
@@ -49,31 +57,32 @@ enum Node {
 	/// The shared portion is encoded from a `NibbleSlice` meaning it contains
 	/// a flag indicating it is an extension.
 	/// The child node is always a branch.
-	Extension(Bytes, StorageHandle),
+	Extension(Bytes, NodeHandle),
 	/// A branch has up to 16 children and an optional value.
-	Branch([Option<StorageHandle>; 16], Option<Bytes>)
+	Branch([Option<NodeHandle>; 16], Option<Bytes>)
 }
 
 impl Node {
-	fn get_raw_or_lookup<'a>(node: &'a [u8], db: &'a HashDB) -> &'a [u8] {
-		// check if its sha3 + len
+	// load an inline node into memory or get the hash to do the lookup later.
+	fn inline_or_hash(node: &[u8], db: &HashDB, storage: &mut NodeStorage) -> NodeHandle {
 		let r = Rlp::new(node);
-		match r.is_data() && r.size() == 32 {
-			true => db.get(&r.as_val::<H256>()).expect("Not found!"),
-			false => node
+		if r.is_data() && r.size() < 32 {
+			let child = Node::from_rlp(r.data(), db, storage);
+			NodeHandle::InMemory(storage.alloc(child))
+		} else {
+			NodeHandle::Hash(r.as_val::<H256>())
 		}
 	}
-	// decode a node from rlp. Also loads any inline child nodes into
-	// storage.
+
+	// decode a node from rlp without getting its children.
 	fn from_rlp(rlp: &[u8], db: &HashDB, storage: &mut NodeStorage) -> Self {
 		match RlpNode::decoded(rlp) {
 			RlpNode::Empty => Node::Empty,
 			RlpNode::Leaf(k, v) => Node::Leaf(k.encoded(true), v.to_owned()),
 			RlpNode::Extension(k, v) => {
 				let key = k.encoded(false);
-				let child_rlp = Node::get_raw_or_lookup(v, db);
-				let child_node = Node::from_rlp(child_rlp, db, storage);
-				Node::Extension(key, storage.alloc(child_node))
+
+				Node::Extension(key, Self::inline_or_hash(v, db, storage))
 			}
 			RlpNode::Branch(children_rlp, v) => {
 				let val = v.map(|x| x.to_owned());
@@ -83,45 +92,11 @@ impl Node {
 					let raw = children_rlp[i];
 					let child_rlp = Rlp::new(raw);
 					if !child_rlp.is_empty()  {
-						let child_rlp = Node::get_raw_or_lookup(raw, db);
-						let child_node = Node::from_rlp(child_rlp, db, storage);
-						children[i] = Some(storage.alloc(child_node));
+						children[i] = Some(Self::inline_or_hash(raw, db, storage));
 					}
 				}
 
 				Node::Branch(children, val)
-			}
-		}
-	}
-
-	// walk the trie, attempting to find the key's node.
-	fn lookup<'a, 'key>(&'a self, partial: NibbleSlice<'key>, storage: &'a NodeStorage) -> Option<&'a [u8]> {
-		match *self {
-			Node::Empty => None,
-			Node::Leaf(ref key, ref value) => {
-				if NibbleSlice::from_encoded(key).0 == partial {
-					Some(value)
-				} else {
-					None
-				}
-			}
-			Node::Extension(ref slice, ref child) => {
-				let slice = NibbleSlice::from_encoded(slice).0;
-				if partial.starts_with(&slice) {
-					storage[child].lookup(partial.mid(slice.len()), storage)
-				} else {
-					None
-				}
-			}
-			Node::Branch(ref children, ref value) => {
-				if partial.is_empty() {
-					value.as_ref().map(|v| &v[..])
-				} else {
-					let idx = partial.at(0);
-					(&children[idx as usize]).as_ref().and_then(|child| {
-						storage[child].lookup(partial.mid(1), storage)
-					})
-				}
 			}
 		}
 	}
@@ -181,98 +156,6 @@ impl NodeStorage {
 
 		self.free_indices.push_back(idx);
 		::std::mem::replace(&mut self.nodes[idx], Node::Empty)
-	}
-
-	/// insert a key, value pair into the trie, creating new nodes if necessary.
-	fn insert(&mut self, handle: StorageHandle, partial: NibbleSlice, value: Bytes) -> StorageHandle {
-		match self.destroy(handle) {
-			Node::Branch(mut children, mut stored_value) => {
-				if partial.is_empty() {
-					stored_value = Some(value);
-				} else {
-					let index = partial.at(0) as usize;
-					if let Some(child_handle) = children[index].take() {
-						// original had something there. continue to insert.
-						let partial = partial.mid(1);
-						children[index] = Some(self.insert(child_handle, partial, value));
-					} else {
-						// original had empty slot. place a leaf there.
-						let leaf = Node::Leaf(partial.encoded(true), value);
-						children[index] = Some(self.alloc(leaf));
-					}
-				}
-
-				self.alloc(Node::Branch(children, stored_value))
-			}
-			Node::Leaf(encoded_key, stored_value) => {
-				let (existing_key, _) = NibbleSlice::from_encoded(&encoded_key);
-				let cp = partial.common_prefix(&existing_key);
-				if cp == partial.len() && cp == existing_key.len() {
-					// equivalent leaf: replace
-					self.alloc(Node::Leaf(encoded_key.clone(), value))
-				} else if cp == 0 {
-					// make a branch.
-					let mut children = empty_children();
-					let branch = if existing_key.is_empty() {
-						Node::Branch(children, Some(stored_value))
-					} else {
-						let index = existing_key.at(0) as usize;
-						let leaf = Node::Leaf(existing_key.mid(1).encoded(true), stored_value);
-						children[index] = Some(self.alloc(leaf));
-						Node::Branch(children, None)
-					};
-
-					let temp_handle = self.alloc(branch);
-					self.insert(temp_handle, partial, value)
-				} else if cp == existing_key.len() {
-					// fully shared prefix.
-					// transform to an extension + augmented version of onward node.
-					let stub_branch = self.alloc(Node::Branch(empty_children(), Some(stored_value)));
-					let downstream = self.insert(stub_branch, partial.mid(cp), value);
-
-					self.alloc(Node::Extension(existing_key.encoded(false), downstream))
-				} else {
-					// partially-shared prefix
-					let low = self.alloc(Node::Leaf(existing_key.mid(cp).encoded(true), stored_value));
-					let augmented_low = self.insert(low, partial.mid(cp), value);
-
-					self.alloc(Node::Extension(existing_key.encoded_leftmost(cp, false), augmented_low))
-				}
-			}
-			Node::Extension(encoded_key, child_branch) => {
-				let (existing_key, _) = NibbleSlice::from_encoded(&encoded_key);
-				let cp = partial.common_prefix(&existing_key);
-				if cp == 0 {
-					// make a branch.
-					assert!(!existing_key.is_empty()); // extension nodes may not have empty partial keys.
-					let mut children = empty_children();
-					let index = existing_key.at(0) as usize;
-					if existing_key.len() == 1 {
-						// direct extension
-						children[index] = Some(child_branch);
-					} else {
-						let extension = Node::Extension(existing_key.mid(1).encoded(false), child_branch);
-						children[index] = Some(self.alloc(extension));
-					}
-
-					let temp_branch = self.alloc(Node::Branch(children, None));
-					self.insert(temp_branch, partial, value)
-				} else if cp == existing_key.len() {
-					// fully-shared prefix.
-					let downstream = self.insert(child_branch, partial.mid(cp), value);
-					self.alloc(Node::Extension(existing_key.encoded(false), downstream))
-				} else {
-					// partially-shared prefix
-					let low = self.alloc(Node::Extension(existing_key.mid(cp).encoded(false), child_branch));
-					let augmented_low = self.insert(low, partial.mid(cp), value);
-
-					self.alloc(Node::Extension(existing_key.encoded_leftmost(cp, false), augmented_low))
-				}
-			}
-			Node::Empty => {
-				self.alloc(Node::Leaf(partial.encoded(true), value))
-			}
-		}
 	}
 }
 
@@ -335,6 +218,76 @@ impl<'a> MemoryTrieDB<'a> {
 			root_handle: StorageHandle(0),
 		})
 	}
+
+	// walk the trie, attempting to find the key's node.
+	fn lookup<'x, 'key>(&'x self, partial: NibbleSlice<'key>, handle: &NodeHandle) -> Option<&'x [u8]>
+	where 'x: 'key {
+		match *handle {
+			NodeHandle::Hash(ref hash) => self.do_db_lookup(hash, partial),
+			NodeHandle::InMemory(ref handle) => match self.storage[handle] {
+				Node::Empty => None,
+				Node::Leaf(ref key, ref value) => {
+					if NibbleSlice::from_encoded(key).0 == partial {
+						Some(value)
+					} else {
+						None
+					}
+				}
+				Node::Extension(ref slice, ref child) => {
+					let slice = NibbleSlice::from_encoded(slice).0;
+					if partial.starts_with(&slice) {
+						self.lookup(partial.mid(slice.len()), child)
+					} else {
+						None
+					}
+				}
+				Node::Branch(ref children, ref value) => {
+					if partial.is_empty() {
+						value.as_ref().map(|v| &v[..])
+					} else {
+						let idx = partial.at(0);
+						(&children[idx as usize]).as_ref().and_then(|child| self.lookup(partial.mid(1), child))
+					}
+				}
+			}
+		}
+	}
+
+	/// Return optional data for a key given as a `NibbleSlice`. Returns `None` if no data exists.
+	fn do_db_lookup<'x, 'key>(&'x self, hash: &H256, key: NibbleSlice<'key>) -> Option<&'x [u8]> where 'x: 'key {
+		let node_rlp = self.db.get(hash).expect("Not found!");
+		self.get_from_db_node(&node_rlp, key)
+	}
+
+	/// Recursible function to retrieve the value given a `node` and a partial `key`. `None` if no
+	/// value exists for the key.
+	///
+	/// Note: Not a public API; use Trie trait functions.
+	fn get_from_db_node<'x, 'key>(&'x self, node: &'x [u8], key: NibbleSlice<'key>) -> Option<&'x [u8]> where 'x: 'key {
+		match RlpNode::decoded(node) {
+			RlpNode::Leaf(ref slice, ref value) if &key == slice => Some(value),
+			RlpNode::Extension(ref slice, ref item) if key.starts_with(slice) => {
+				self.get_from_db_node(self.get_raw_or_lookup(item), key.mid(slice.len()))
+			},
+			RlpNode::Branch(ref nodes, value) => match key.is_empty() {
+				true => value,
+				false => self.get_from_db_node(self.get_raw_or_lookup(nodes[key.at(0) as usize]), key.mid(1))
+			},
+			_ => None
+		}
+	}
+
+	/// Given some node-describing data `node`, return the actual node RLP.
+	/// This could be a simple identity operation in the case that the node is sufficiently small, but
+	/// may require a database lookup.
+	fn get_raw_or_lookup<'x>(&'x self, node: &'x [u8]) -> &'x [u8] {
+		// check if its sha3 + len
+		let r = Rlp::new(node);
+		match r.is_data() && r.size() == 32 {
+			true => self.db.lookup(&r.as_val::<H256>()).expect("Not found!"),
+			false => node
+		}
+	}
 }
 
 impl<'a> Trie for MemoryTrieDB<'a> {
@@ -351,7 +304,8 @@ impl<'a> Trie for MemoryTrieDB<'a> {
 	}
 
 	fn get<'b, 'key>(&'b self, key: &'key [u8]) -> Option<&'b [u8]> where 'b: 'key {
-		self.storage.root().lookup(NibbleSlice::new(key), &self.storage)
+		let root_handle = StorageHandle(self.root_handle.0);
+		self.lookup(NibbleSlice::new(key), &NodeHandle::InMemory(root_handle))
 	}
 
 	fn contains(&self, key: &[u8]) -> bool {
@@ -361,10 +315,7 @@ impl<'a> Trie for MemoryTrieDB<'a> {
 
 impl<'a> TrieMut for MemoryTrieDB<'a> {
 	fn insert(&mut self, key: &[u8], value: &[u8]) {
-		// TODO [rob]: find a way to do this that doesn't subvert the ownership
-		// semantics of StorageHandle
-		let root_handle = StorageHandle(self.root_handle.0);
-		self.root_handle = self.storage.insert(root_handle, NibbleSlice::new(key), value.to_owned());
+		unimplemented!()
 	}
 
 	fn remove(&mut self, key: &[u8]) {
