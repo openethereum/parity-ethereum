aef1e362b9b8c95866e1497a302c32f2b68496ea
arkpar <arkady.paronyan@gmail.com>
2016-06-27 16:11:10 +0200

    Reduced IO messages; removed panics on IO notifications

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -359,7 +359,7 @@ impl<V> Client<V> where V: Verifier {
 					invalid: invalid_blocks,
 					enacted: enacted,
 					retracted: retracted,
-				})).unwrap();
+				})).unwrap_or_else(|e| warn!("Error sending IO notification: {:?}", e));
 			}
 		}
 
