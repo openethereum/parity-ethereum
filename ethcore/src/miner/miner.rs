aba1ddc55108133e2956865acd0d0458ab74f544
Tomasz Drwięga <tomasz@ethcore.io>
2016-07-08 19:00:55 +0200

    Uncommenting simple Miner tests

--- ethcore/src/miner/miner.rs
+++ ethcore/src/miner/miner.rs
@@ -729,8 +729,6 @@ mod tests {
 	use block::*;
 	use spec::Spec;
 
-	// TODO [ToDr] To uncomment` when TestBlockChainClient can actually return a ClosedBlock.
-	#[ignore]
 	#[test]
 	fn should_prepare_block_to_seal() {
 		// given
@@ -742,7 +740,6 @@ mod tests {
 		assert!(sealing_work.is_some(), "Expected closed block");
 	}
 
-	#[ignore]
 	#[test]
 	fn should_still_work_after_a_couple_of_blocks() {
 		// given
