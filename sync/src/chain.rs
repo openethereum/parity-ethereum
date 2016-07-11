966b31992c35c550061686bb4a766242616d7d78
NikVolf <nikvolf@gmail.com>
2016-07-11 15:46:22 +0200

    basic library re-layout

--- sync/src/chain.rs
+++ sync/src/chain.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
 ///
 /// `BlockChain` synchronization strategy.
 /// Syncs to peers and keeps up to date.
