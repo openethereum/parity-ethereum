d9aeafa40eafbc473f6d18931a612d67e7135c30
Tomasz Drwięga <tomasz@ethcore.io>
2016-07-11 19:11:42 +0200

    Removing --verify [ci skip]

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -10,30 +10,34 @@
 # $ cmake .. && make && sudo make install
 
 ### Running coverage
-
-KCOV_CMD=${1:-kcov}
-
-if ! type $KCOV_CMD > /dev/null; then
+if ! type kcov > /dev/null; then
    	echo "Install kcov first (details inside this file). Aborting."
 	exit 1
 fi
 
-. ./scripts/targets.sh
-
-cargo test $TARGETS --no-run || exit $?
+cargo test \
+	-p ethkey \
+	-p ethstore \
+	-p ethash \
+	-p ethcore-util \
+	-p ethcore \
+	-p ethsync \
+	-p ethcore-rpc \
+	-p parity \
+	-p ethcore-signer \
+	-p ethcore-dapps \
+	--no-run || exit $?
 rm -rf target/coverage
 mkdir -p target/coverage
 
-EXCLUDE="~/.multirust,rocksdb,secp256k1,/usr/,/.cargo,/root/.multirust,src/tests,util/json-tests,util/src/network/tests,sync/src/tests,ethcore/src/tests,ethcore/src/evm/tests,ethstore/tests target/kcov"
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethkey-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethstore-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethcore-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethash-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethcore_util-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethsync-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethcore_rpc-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethcore_signer-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethcore_dapps-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/deps/ethjson-*
-$KCOV_CMD --exclude-pattern $EXCLUDE --include-pattern src --verify target/coverage target/debug/parity-*
+EXCLUDE="~/.multirust,rocksdb,secp256k1,src/tests,util/json-tests,util/src/network/tests,sync/src/tests,ethcore/src/tests,ethcore/src/evm/tests,ethstore/tests"
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethkey-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethstore-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethcore-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethash-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethcore_util-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethsync-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethcore_rpc-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethcore_signer-*
+kcov --exclude-pattern $EXCLUDE --include-pattern src target/coverage target/debug/deps/ethcore_dapps-*
 xdg-open target/coverage/index.html
