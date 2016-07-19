889ab69991b400a8bc51f0264d6dc8972a62abb3
arkpar <arkady.paronyan@gmail.com>
2016-07-19 20:43:46 +0200

    Ping discovery nodes gradually

--- util/src/network/host.rs
+++ util/src/network/host.rs
@@ -419,7 +419,7 @@ impl Host {
 
 				self.nodes.write().add_node(n);
 				if let Some(ref mut discovery) = *self.discovery.lock() {
-					discovery.add_node(entry, true);
+					discovery.add_node(entry);
 				}
 			}
 		}
@@ -433,7 +433,7 @@ impl Host {
 		self.nodes.write().add_node(Node::new(entry.id.clone(), entry.endpoint.clone()));
 
 		if let Some(ref mut discovery) = *self.discovery.lock() {
-			discovery.add_node(entry, false);
+			discovery.add_node(entry);
 		}
 
 		Ok(())
@@ -549,9 +549,7 @@ impl Host {
 
 		if let Some(mut discovery) = discovery {
 			discovery.init_node_list(self.nodes.read().unordered_entries());
-			for n in self.nodes.read().unordered_entries() {
-				discovery.add_node(n.clone(), false);
-			}
+			discovery.add_node_list(self.nodes.read().unordered_entries());
 			*self.discovery.lock() = Some(discovery);
 			io.register_stream(DISCOVERY).expect("Error registering UDP listener");
 			io.register_timer(DISCOVERY_REFRESH, 7200).expect("Error registering discovery timer");
@@ -788,7 +786,7 @@ impl Host {
 								self.nodes.write().add_node(Node::new(entry.id.clone(), entry.endpoint.clone()));
 								let mut discovery = self.discovery.lock();
 								if let Some(ref mut discovery) = *discovery.deref_mut() {
-									discovery.add_node(entry, true);
+									discovery.add_node(entry);
 								}
 							}
 						}
