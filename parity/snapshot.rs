917d1f351a3ce15893913a2f4689e96f2103069c
Robert Habermeier <rphmeier@gmail.com>
2016-08-03 20:07:01 +0200

    instrument snapshot commands again

--- parity/snapshot.rs
+++ parity/snapshot.rs
@@ -16,43 +16,50 @@
 
 //! Snapshot and restoration commands.
 
-use std::str::{FromStr, from_utf8};
-use std::io::{BufReader, BufRead};
 use std::time::Duration;
-use std::thread::sleep;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use ethcore_logger::{setup_log, Config as LogConfig};
-use util::panics::{PanicHandler, ForwardPanic};
+use util::panics::PanicHandler;
 use ethcore::snapshot::{RestorationStatus, SnapshotService};
-use ethcore::snapshot::io::{SnapshotReader, PackedReader};
+use ethcore::snapshot::io::{SnapshotReader, PackedReader, PackedWriter};
 use ethcore::service::ClientService;
-use ethcore::client::{Mode, DatabaseCompactionProfile, Switch, VMType, BlockImportError, BlockChainClient, BlockID};
-use ethcore::error::ImportError;
+use ethcore::client::{Mode, DatabaseCompactionProfile, Switch, VMType};
 use ethcore::miner::Miner;
 use cache::CacheConfig;
-use informant::Informant;
 use params::{SpecType, Pruning};
 use helpers::{to_client_config, execute_upgrades};
 use dir::Directories;
 use fdlimit;
 
+/// Kinds of snapshot commands.
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Kind {
+	/// Take a snapshot.
+	Take,
+	/// Restore a snapshot.
+	Restore
+}
+
 /// Command for snapshot creation or restoration.
+#[derive(Debug, PartialEq)]
 pub struct SnapshotCommand {
+	pub cache_config: CacheConfig,
 	pub dirs: Directories,
 	pub spec: SpecType,
 	pub pruning: Pruning,
 	pub logger_config: LogConfig,
-	pub miner_options: MinerOptions,
 	pub mode: Mode,
 	pub tracing: Switch,
 	pub compaction: DatabaseCompactionProfile,
-	pub filename: PathBuf,
+	pub file_path: Option<String>,
+	pub wal: bool,
+	pub kind: Kind,
 }
 
 impl SnapshotCommand {
 	// shared portion of snapshot commands: start the client service
-	fn start_service(self) -> Result<(Arc<ClientService>, Arc<PanicHandler>), String> {
+	fn start_service(self) -> Result<(ClientService, Arc<PanicHandler>), String> {
 		// Setup panic handler
 		let panic_handler = PanicHandler::new_in_arc();
 
@@ -86,16 +93,16 @@ impl SnapshotCommand {
 			Arc::new(Miner::with_spec(try!(self.spec.spec())))
 		).map_err(|e| format!("Client service error: {:?}", e)));
 
-		(service, panic_handler)
+		Ok((service, panic_handler))
 	}
 
 	/// restore from a snapshot
 	pub fn restore(self) -> Result<(), String> {
-		let filename = self.filename.clone();
-		let (service, panic_handler) = try!(self.start_service());
+		let file = try!(self.file_path.clone().ok_or("No file path provided.".to_owned()));
+		let (service, _panic_handler) = try!(self.start_service());
 
 		let snapshot = service.snapshot_service();
-		let reader = PackedReader::new(&filename)
+		let reader = PackedReader::new(&Path::new(&file))
 			.map_err(|e| format!("Couldn't open snapshot file: {}", e))
 			.and_then(|x| x.ok_or("Snapshot file has invalid format.".into()));
 
@@ -105,12 +112,13 @@ impl SnapshotCommand {
 		// drop the client so we don't restore while it has open DB handles.
 		drop(service);
 
-		if !snapshot.begin_restoration(manifest.clone()) {
+		if !snapshot.begin_restore(manifest.clone()) {
 			return Err("Failed to begin restoration.".into());
 		}
 
 		let (num_state, num_blocks) = (manifest.state_hashes.len(), manifest.block_hashes.len());
 
+		let informant_handle = snapshot.clone();
 		::std::thread::spawn(move || {
  			while let RestorationStatus::Ongoing = informant_handle.status() {
  				let (state_chunks, block_chunks) = informant_handle.chunks_done();
@@ -128,24 +136,24 @@ impl SnapshotCommand {
  			}
 
  			let chunk = try!(reader.chunk(state_hash)
-				.ok_or_else(|| format!("Failed to read chunk {:?} from snapshot file.", state_hash)));
+				.map_err(|e| format!("Encountered error while reading chunk {:?}: {}", state_hash, e)));
  			snapshot.feed_state_chunk(state_hash, &chunk);
  		}
 
 		info!("Restoring blocks");
-		for &block_hash in &manifest.state_hashes {
-			if snapshot.status() ==RestorationStatus::Failed {
+		for &block_hash in &manifest.block_hashes {
+			if snapshot.status() == RestorationStatus::Failed {
 				return Err("Restoration failed".into());
-
-				let chunk = try!(reader.chunk(block_hash)
-					.ok_or_else(|| format!("Failed to read chunk {:?} from snapshot file.", block_hash)));
-				snapshot.feed_block_chunk(block_hash, &chunk);
 			}
+
+ 			let chunk = try!(reader.chunk(block_hash)
+				.map_err(|e| format!("Encountered error while reading chunk {:?}: {}", block_hash, e)));
+			snapshot.feed_block_chunk(block_hash, &chunk);
 		}
 
 		match snapshot.status() {
 			RestorationStatus::Ongoing => Err("Snapshot file is incomplete and missing chunks.".into()),
-			RestoratoinStatus::Failed => Err("Snapshot restoration failed.".into()),
+			RestorationStatus::Failed => Err("Snapshot restoration failed.".into()),
 			RestorationStatus::Inactive => {
 				info!("Restoration complete.");
 				Ok(())
@@ -155,17 +163,28 @@ impl SnapshotCommand {
 
 	/// Take a snapshot from the head of the chain.
 	pub fn take_snapshot(self) -> Result<(), String> {
-		let filename = self.filename.clone();
-		let (service, panic_handler) = try!(self.start_service());
+		let file_path = try!(self.file_path.clone().ok_or("No file path provided.".to_owned()));
+		let file_path: PathBuf = file_path.into();
+		let (service, _panic_handler) = try!(self.start_service());
 
-		let writer = try!(PackedWriter::new(&filename)
+		let writer = try!(PackedWriter::new(&file_path)
 			.map_err(|e| format!("Failed to open snapshot writer: {}", e)));
 
 		if let Err(e) = service.client().take_snapshot(writer) {
-			let _ = ::std::fs::remove(&filename);
+			let _ = ::std::fs::remove_file(&file_path);
 			return Err(format!("Encountered fatal error while creating snapshot: {}", e));
 		}
 
 		Ok(())
 	}
+}
+
+/// Execute this snapshot command.
+pub fn execute(cmd: SnapshotCommand) -> Result<String, String> {
+	match cmd.kind {
+		Kind::Take => try!(cmd.take_snapshot()),
+		Kind::Restore => try!(cmd.restore()),
+	}
+
+	Ok(String::new())
 }
