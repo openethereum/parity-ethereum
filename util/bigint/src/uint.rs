65a6732b2567a7ea76b9745a5bedb725b698759e
arkpar <arkady.paronyan@gmail.com>
2016-08-03 10:01:13 +0200

    Fixing build with stable

--- util/bigint/src/uint.rs
+++ util/bigint/src/uint.rs
@@ -36,10 +36,8 @@
 //! The functions here are designed to be fast.
 //!
 
-#[cfg(all(asm_available, target_arch="x86_64"))]
 use std::mem;
 use std::fmt;
-
 use std::str::{FromStr};
 use std::convert::From;
 use std::hash::Hash;
