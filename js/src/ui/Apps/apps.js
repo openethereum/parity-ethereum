a57a1dd5b6323d608034eee0cdcf23c6c0ea955f
Jaco Greeff <jaco@ethcore.io>
2016-08-12 11:01:12 +0200

    dapps & token placeholders uses same containers

--- js/src/ui/Apps/apps.js
+++ js/src/ui/Apps/apps.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+import { CardText } from 'material-ui/Card';
+
+import Container from '../Container';
+
 export default class Apps extends Component {
   render () {
     return (
-      <h2>managing contracts (adding, calling, etc) should go in here, next steps...</h2>
+      <Container>
+        <CardText>
+          managing contracts (adding, calling, etc) should go in here, next steps... almost there...
+        </CardText>
+      </Container>
     );
   }
 }
--- js/src/ui/Tokens/tokens.js
+++ js/src/ui/Tokens/tokens.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+import { CardText } from 'material-ui/Card';
+
+import Container from '../Container';
+
 export default class Tokens extends Component {
   render () {
     return (
-      <h2>the token dapp interface should go in here, next steps...</h2>
+      <Container>
+        <CardText>
+          the token dapp interface should go in here, we need a basic contract, deploy it and then go about playing and seeing what is the best way to pull everything together...
+        </CardText>
+      </Container>
     );
   }
 }
