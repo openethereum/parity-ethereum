207ce9abeaffc397801dad7d56be1073bf968e97
Jaco Greeff <jaco@ethcore.io>
2016-08-16 12:01:29 +0200

    update recipient address missing description

--- js/src/modals/Transfer/Details/details.js
+++ js/src/modals/Transfer/Details/details.js
@@ -16,7 +16,7 @@ const CHECK_STYLE = {
 };
 
 const ERRORS = {
-  requireRecipient: 'a recipient account is required for the transaction',
+  requireRecipient: 'a recipient network address is required for the transaction',
   invalidAddress: 'the supplied address is an invalid network address',
   invalidAmount: 'the supplied amount should be a valid positive number',
   largeAmount: 'the transaction total is higher than the available balance'
