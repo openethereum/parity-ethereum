8861431f37ae4bffa72b1ce4e3ae494654daf123
Nikolay Volf <nikvolf@gmail.com>
2016-08-18 13:52:19 +0300

    basic subscription

--- stratum/src/lib.rs
+++ stratum/src/lib.rs
@@ -17,9 +17,91 @@
 //! Stratum protocol implementation for parity ethereum/bitcoin clients
 
 extern crate json_tcp_server;
+extern crate jsonrpc_core;
+#[macro_use] extern crate log;
+
+#[cfg(test)]
+extern crate mio;
 
 use json_tcp_server::Server as JsonRpcServer;
+use jsonrpc_core::{IoHandler, Params, IoDelegate, to_value};
+use std::sync::{Arc, RwLock, RwLockReadGuard};
+use std::net::SocketAddr;
 
-struct Stratum {
-	rpc_server: JsonRpcServer
+pub struct Stratum {
+	rpc_server: JsonRpcServer,
+	handler: Arc<IoHandler>,
+	subscribers: RwLock<Vec<SocketAddr>>,
+}
+
+impl Stratum {
+	pub fn start(addr: &SocketAddr) -> Result<Arc<Stratum>, json_tcp_server::Error> {
+		let handler = Arc::new(IoHandler::new());
+		let server = try!(JsonRpcServer::new(addr, &handler));
+		let stratum = Arc::new(Stratum {
+			rpc_server: server,
+			handler: handler,
+			subscribers: RwLock::new(Vec::new()),
+		});
+
+		let mut delegate = IoDelegate::<Stratum>::new(stratum.clone());
+		delegate.add_method("miner.subscribe", Stratum::subscribe);
+		stratum.handler.add_delegate(delegate);
+
+		try!(stratum.rpc_server.run_async());
+
+		Ok(stratum)
+	}
+
+	fn subscribe(&self, _params: Params) -> std::result::Result<jsonrpc_core::Value, jsonrpc_core::Error> {
+		if let Some(context) = self.rpc_server.request_context() {
+			self.subscribers.write().unwrap().push(context.socket_addr);
+			trace!(target: "stratum", "Subscription request from {:?}", context.socket_addr);
+		}
+		Ok(try!(to_value(&0)))
+	}
+
+	pub fn subscribers(&self) -> RwLockReadGuard<Vec<SocketAddr>> {
+		self.subscribers.read().unwrap()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::str::FromStr;
+	use std::net::SocketAddr;
+
+	pub fn dummy_request(addr: &SocketAddr, buf: &[u8]) -> Vec<u8> {
+		use std::io::{Read, Write};
+		use mio::*;
+		use mio::tcp::*;
+
+		let mut poll = Poll::new().unwrap();
+		let mut sock = TcpStream::connect(addr).unwrap();
+		poll.register(&sock, Token(0), EventSet::writable(), PollOpt::edge() | PollOpt::oneshot()).unwrap();
+		poll.poll(Some(500)).unwrap();
+		sock.write_all(buf).unwrap();
+		poll.reregister(&sock, Token(0), EventSet::readable(), PollOpt::edge() | PollOpt::oneshot()).unwrap();
+		poll.poll(Some(500)).unwrap();
+
+		let mut buf = Vec::new();
+		sock.read_to_end(&mut buf).unwrap_or_else(|_| { 0 });
+		buf
+	}
+
+	#[test]
+	fn can_be_started() {
+		let stratum = Stratum::start(&SocketAddr::from_str("0.0.0.0:19980").unwrap());
+		assert!(stratum.is_ok());
+	}
+
+	#[test]
+	fn records_subscriber() {
+		let addr = SocketAddr::from_str("0.0.0.0:19985").unwrap();
+		let stratum = Stratum::start(&addr).unwrap();
+		let request = r#"{"jsonrpc": "2.0", "method": "miner.subscribe", "params": [], "id": 1}"#;
+		dummy_request(&addr, request.as_bytes());
+		assert_eq!(1, stratum.subscribers.read().unwrap().len());
+	}
 }
