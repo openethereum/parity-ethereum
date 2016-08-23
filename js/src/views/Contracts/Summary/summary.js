44e1bca81ddbdf8c733435d1e5dd7c581764af15
Jaco Greeff <jaco@ethcore.io>
2016-08-23 19:55:40 +0200

    move old Dapp/Dapps components to Contract/Contracts for future expansion

---
+++ js/src/views/Contracts/Summary/summary.js
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+
+import Container, { Title } from '../../../ui/Container';
+import IdentityIcon from '../../../ui/IdentityIcon';
+
+export default class Summary extends Component {
+  static contextTypes = {
+    api: React.PropTypes.object
+  }
+
+  static propTypes = {
+    contract: PropTypes.object.isRequired,
+    children: PropTypes.node
+  }
+
+  render () {
+    const contract = this.props.contract;
+
+    if (!contract) {
+      return null;
+    }
+
+    const viewLink = `/app/${contract.address}`;
+
+    return (
+      <Container>
+        <IdentityIcon
+          address={ contract.address } />
+        <Title
+          title={ <Link to={ viewLink }>{ contract.name || 'Unnamed' }</Link> }
+          byline={ contract.address } />
+        { this.props.children }
+      </Container>
+    );
+  }
+}
