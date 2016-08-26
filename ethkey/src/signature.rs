2cc2bd6518628ba74ccd3a3673f9d75dab1cad25
keorn <pczaban@gmail.com>
2016-08-26 10:40:00 +0200

    impl Hash for Signature

--- ethkey/src/signature.rs
+++ ethkey/src/signature.rs
@@ -18,6 +18,7 @@ use std::ops::{Deref, DerefMut};
 use std::cmp::PartialEq;
 use std::{mem, fmt};
 use std::str::FromStr;
+use std::hash::{Hash, Hasher};
 use secp256k1::{Message as SecpMessage, RecoverableSignature, RecoveryId, Error as SecpError};
 use secp256k1::key::{SecretKey, PublicKey};
 use rustc_serialize::hex::{ToHex, FromHex};
@@ -112,6 +113,12 @@ impl Default for Signature {
 	fn default() -> Self {
 		Signature([0; 65])
 	}
+}
+
+impl Hash for Signature {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+    	H520::from(self.0).hash(state);
+    }
 }
 
 impl From<[u8; 65]> for Signature {
