f7c640d2e119f0bff3f0c81bfad580c9cae95450
Jaco Greeff <jaco@ethcore.io>
2016-08-26 20:43:33 +0200

    signer via iframe (not working due to cross-domain restrictions)

--- js/src/views/Signer/signer.js
+++ js/src/views/Signer/signer.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 import Container from '../../ui/Container';
 
+import styles from './style.css';
+
 export default class Settings extends Component {
   render () {
     return (
       <Container>
-        <div>Signer...</div>
+        <iframe
+          className={ styles.iframe }
+          src='http://127.0.0.1:8180/'>
+        </iframe>
       </Container>
     );
   }
