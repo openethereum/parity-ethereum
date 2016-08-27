10a80c6e0411b54c363eb39cb8b8d1236f57da32
Jaco Greeff <jaco@ethcore.io>
2016-08-27 09:23:53 +0200

    basic constant queries of the registry

--- js/src/dapps/registry/Application/application.js
+++ js/src/dapps/registry/Application/application.js
@@ -1,12 +1,85 @@
-import React, { Component, PropTypes } from'react';
+import React, { Component, PropTypes } from 'react';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
+const muiTheme = getMuiTheme(lightBaseTheme);
+
+import registryAbi from '../abi/registry.json';
+import Loading from '../Loading';
+import Status from '../Status';
+
+const { Api } = window.parity;
+
+const api = new Api(new Api.Transport.Http('/rpc/'));
 
 export default class Application extends Component {
-  static propTypes = {
-  };
+  static childContextTypes = {
+    api: PropTypes.object,
+    instance: PropTypes.object,
+    muiTheme: PropTypes.object
+  }
+
+  state = {
+    address: null,
+    fee: null,
+    instance: null,
+    owner: null
+  }
+
+  componentDidMount () {
+    this.attachInterface();
+  }
 
   render () {
+    const { address, fee, owner } = this.state;
+
+    if (!address) {
+      return (
+        <Loading />
+      );
+    }
+
     return (
-      <div>hello</div>
+      <div>
+        <Status
+          address={ address }
+          fee={ fee }
+          owner={ owner } />
+      </div>
     );
+  }
+
+  getChildContext () {
+    return {
+      api,
+      instance: this.state.instance,
+      muiTheme
+    };
+  }
+
+  attachInterface = () => {
+    api.ethcore
+      .registryAddress()
+      .then((address) => {
+        const { instance } = api.newContract(registryAbi, address);
+
+        return Promise
+          .all([
+            instance.owner.call(),
+            instance.fee.call()
+          ])
+          .then(([owner, fee]) => {
+            this.setState({
+              address,
+              fee,
+              instance,
+              owner
+            });
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
---
+++ js/src/dapps/registry/Status/status.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+
+import styles from './style.css';
+
+const { Api } = window.parity;
+
+export default class Status extends Component {
+  static propTypes = {
+    address: PropTypes.string,
+    fee: PropTypes.object,
+    owner: PropTypes.string
+  };
+
+  render () {
+    const { address, fee, owner } = this.props;
+
+    return (
+      <div className={ styles.status }>
+        <div className={ styles.address }>Registry at { address }</div>
+        <div className={ styles.owner }>Owned by { owner }</div>
+        <div className={ styles.fee }>Registration fee { Api.format.fromWei(fee).toFixed(3) }ΞTH</div>
+      </div>
+    );
+  }
+}
