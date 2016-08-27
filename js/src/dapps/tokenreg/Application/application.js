9e9c6b6acd1aa16fd935470314ac79c7a7a9c35c
Jaco Greeff <jaco@ethcore.io>
2016-08-27 14:01:36 +0200

    display some very basic info, attach contract instance

--- js/src/dapps/tokenreg/Application/application.js
+++ js/src/dapps/tokenreg/Application/application.js
@@ -1,12 +1,107 @@
-import React, { Component, PropTypes } from'react';
+import React, { Component, PropTypes } from 'react';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
+import registryAbi from '../abi/registry.json';
+import tokenregAbi from '../abi/tokenreg.json';
+import Loading from '../Loading';
+import Status from '../Status';
+
+const { Api } = window.parity;
+
+const api = new Api(new Api.Transport.Http('/rpc/'));
+const muiTheme = getMuiTheme(lightBaseTheme);
 
 export default class Application extends Component {
-  static propTypes = {
-  };
+  static childContextTypes = {
+    api: PropTypes.object,
+    instance: PropTypes.object,
+    muiTheme: PropTypes.object
+  }
+
+  state = {
+    address: null,
+    instance: null,
+    loading: true
+  }
+
+  componentDidMount () {
+    this.attachInterface();
+  }
 
   render () {
+    const { address, fee, loading, owner } = this.state;
+
+    if (loading) {
+      return (
+        <Loading />
+      );
+    }
+
     return (
-      <div>hello</div>
+      <div>
+        <Status
+          address={ address }
+          fee={ fee }
+          owner={ owner } />
+      </div>
     );
+  }
+
+  getChildContext () {
+    return {
+      api,
+      instance: this.state.instance,
+      muiTheme
+    };
+  }
+
+  onNewBlockNumber = (blockNumber) => {
+    const { instance } = this.state;
+
+    instance.fee
+      .call()
+      .then((fee) => {
+        this.setState({
+          fee
+        });
+      });
+  }
+
+  attachInterface = () => {
+    api.ethcore
+      .registryAddress()
+      .then((registryAddress) => {
+        console.log(`registry found at ${registryAddress}`);
+        const registry = api.newContract(registryAbi, registryAddress).instance;
+
+        return registry.getAddress.call({}, [Api.format.sha3('tokenreg'), 'A']);
+      })
+      .then((address) => {
+        console.log(`tokenreg was found at ${address}`);
+        const { instance } = api.newContract(tokenregAbi, address);
+
+        return Promise
+          .all([
+            instance.owner.call(),
+            instance.fee.call()
+          ])
+          .then(([owner, fee]) => {
+            console.log(`owner as ${owner}, fee set at ${fee.toFormat()}`);
+            this.setState({
+              address,
+              fee,
+              instance,
+              loading: false,
+              owner
+            });
+
+            api.events.subscribe('eth.blockNumber', this.onNewBlockNumber);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
