dabd951eacf1d0af131bff1b5817fac872055dc0
Jaco Greeff <jaco@ethcore.io>
2016-08-30 13:47:00 +0200

    dialogs for add & edit (not populated, but there)

---
+++ js/src/modals/AddressBook/EditEntry/editEntry.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+
+import { FlatButton } from 'material-ui';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+
+import Modal from '../../../ui/Modal';
+
+export default class EditEntry extends Component {
+  static propTypes = {
+    onClose: PropTypes.func
+  };
+
+  state = {
+  };
+
+  render () {
+    return (
+      <Modal
+        visible
+        actions={ this.renderDialogActions() }>
+        { this.renderPage() }
+      </Modal>
+    );
+  }
+
+  renderDialogActions () {
+    return ([
+      <FlatButton
+        icon={ <ContentClear /> }
+        label='Cancel'
+        primary
+        onTouchTap={ this.onClose } />,
+      <FlatButton
+        icon={ <ContentAdd /> }
+        label='Save Entry'
+        primary
+        onTouchTap={ this.onAdd } />
+    ]);
+  }
+
+  renderPage () {
+    return <div>content</div>;
+  }
+}
--- js/src/modals/AddressBook/addressBook.js
+++ js/src/modals/AddressBook/addressBook.js
@@ -1,9 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 
 import { FlatButton } from 'material-ui';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import ContentClear from 'material-ui/svg-icons/content/clear';
 
 import Modal from '../../ui/Modal';
+
+import AddEntry from './AddEntry';
+import EditEntry from './EditEntry';
 
 export default class AddressBook extends Component {
   static propTypes = {
@@ -11,6 +15,8 @@ export default class AddressBook extends Component {
   };
 
   state = {
+    showAdd: false,
+    showEdit: false
   };
 
   render () {
@@ -18,23 +24,66 @@ export default class AddressBook extends Component {
       <Modal
         visible
         actions={ this.renderDialogActions() }>
+        { this.renderModals() }
         { this.renderPage() }
       </Modal>
     );
   }
 
   renderDialogActions () {
-    return (
+    return ([
+      <FlatButton
+        icon={ <ContentAdd /> }
+        label='Add Entry'
+        primary
+        onTouchTap={ this.onAdd } />,
       <FlatButton
         icon={ <ContentClear /> }
         label='Close'
         primary
         onTouchTap={ this.onClose } />
-    );
+    ]);
   }
 
   renderPage () {
     return <div>content</div>;
+  }
+
+  renderModals () {
+    const { showAdd, showEdit } = this.state;
+
+    if (showAdd) {
+      return (
+        <AddEntry
+          onClose={ this.onCloseAdd } />
+      );
+    } else if (showEdit) {
+      return (
+        <EditEntry
+          onClose={ this.onCloseEdit } />
+      );
+    }
+
+    return null;
+  }
+
+  onAdd = () => {
+    console.log('onAdd');
+    this.setState({
+      showAdd: true
+    });
+  }
+
+  onCloseAdd = () => {
+    this.setState({
+      showAdd: false
+    });
+  }
+
+  onCloseEdit = () => {
+    this.setState({
+      showAdd: false
+    });
   }
 
   onClose = () => {
