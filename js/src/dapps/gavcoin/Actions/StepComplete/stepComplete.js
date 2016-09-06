f5368a824c4b16365542afb34a9b7a8c15dab554
Jaco Greeff <jaco@ethcore.io>
2016-09-06 10:53:41 +0200

    update the signer link to embedded signer

--- js/src/dapps/gavcoin/Actions/StepComplete/stepComplete.js
+++ js/src/dapps/gavcoin/Actions/StepComplete/stepComplete.js
@@ -6,7 +6,7 @@ export default class StepComplete extends Component {
   render () {
     return (
       <div className={ styles.dialogtext }>
-        Your transaction has been posted. Please visit the <a href='http://127.0.0.1:8180/' className={ styles.link } target='_blank'>Parity Signer</a> to authenticate the transfer.
+        Your transaction has been posted. Please visit the <a href='http://127.0.0.1:8080/#/signer' className={ styles.link } target='_blank'>Parity Signer</a> to authenticate the transfer.
       </div>
     );
   }
