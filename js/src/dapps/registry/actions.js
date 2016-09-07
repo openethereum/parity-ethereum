9920fb1d2a0bbf9acebb4552d6e716eda6c45b28
Jannis R <mail@jannisr.de>
2016-09-07 11:50:01 +0200

    move fetch logic into actions
    Haven't read the `redux-thunk` docs properly tbh. It makes sense to put the async (impure) stuff into actions, as reducers are supposed to be pure.

--- js/src/dapps/registry/actions.js
+++ js/src/dapps/registry/actions.js
@@ -1,8 +1,43 @@
-export const fetchContract = () => ({ type: 'fetch contract' });
+import registryAbi from './abi/registry.json';
+const { api } = window.parity;
+
 export const setContract = (contract) => ({ type: 'set contract', contract });
 
-export const fetchFee = () => ({ type: 'fetch fee' });
+export const fetchContract = () => (dispatch) =>
+  api.ethcore.registryAddress()
+  .then((address) => {
+    const contract = api.newContract(registryAbi, address);
+    dispatch(setContract(contract));
+    dispatch(fetchFee());
+    dispatch(fetchOwner());
+  })
+  .catch((err) => {
+    console.error('could not fetch contract');
+    if (err) console.error(err.stack);
+  });
+
 export const setFee = (fee) => ({ type: 'set fee', fee });
 
-export const fetchOwner = () => ({ type: 'fetch owner' });
+const fetchFee = () => (dispatch, getState) => {
+  const { contract } = getState();
+  if (!contract) return;
+  contract.instance.fee.call()
+  .then((fee) => dispatch(setFee(fee)))
+  .catch((err) => {
+    console.error('could not fetch fee');
+    if (err) console.error(err.stack);
+  });
+};
+
 export const setOwner = (owner) => ({ type: 'set owner', owner });
+
+export const fetchOwner = () => (dispatch, getState) => {
+  const { contract } = getState();
+  if (!contract) return;
+  contract.instance.owner.call()
+  .then((owner) => dispatch(setOwner(owner)))
+  .catch((err) => {
+    console.error('could not fetch owner');
+    if (err) console.error(err.stack);
+  });
+};
--- js/src/dapps/registry/reducers.js
+++ js/src/dapps/registry/reducers.js
@@ -1,53 +1,16 @@
-import {
-  setContract,
-  fetchFee, setFee,
-  fetchOwner, setOwner
-} from './actions';
-import registryAbi from './abi/registry.json';
-const { api } = window.parity;
-
 const initialState = {
   contract: null,
   fee: null,
   owner: null
 };
 
-const onFetchContract = (state, action) => (dispatch) =>
-  api.ethcore.registryAddress().then(
-    (address) => {
-      const contract = api.newContract(registryAbi, address);
-      dispatch(setContract(contract));
-      dispatch(fetchFee());
-      dispatch(fetchOwner());
-    },
-    () => console.error('could not fetch contract')
-  );
-
-const onFetchFee = (state, action) => (dispatch) =>
-  state.contract.fee.call().then(
-    (fee) => dispatch(setFee(fee)),
-    () => console.error('could not fetch fee')
-  );
-
-const onFetchOwner = (state, action) => (dispatch) =>
-  state.contract.owner.call().then(
-    (owner) => dispatch(setOwner(owner)),
-    () => console.error('could not fetch owner')
-  );
-
 export default (state = initialState, action) => {
-  if (action.type === 'fetch contract')
-    return onFetchContract(state, action);
   if (action.type === 'set contract')
     return { ...state, contract: action.contract };
 
-  if (action.type === 'fetch fee')
-    return onFetchFee(state, action);
   if (action.type === 'set fee')
     return { ...state, fee: action.fee };
 
-  if (action.type === 'fetch owner')
-    return onFetchOwner(state, action);
   if (action.type === 'set owner')
     return { ...state, owner: action.owner };
 
