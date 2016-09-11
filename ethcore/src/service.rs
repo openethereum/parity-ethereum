dcfd7eab6d44922017c65e184d02a3368056d40d
Robert Habermeier <rphmeier@gmail.com>
2016-09-11 14:05:59 +0200

    Local snapshot restore (#2058)
    * restore from local snapshot
    
    * update status with chunks done
    
    * rework local restore trigger

--- ethcore/src/service.rs
+++ ethcore/src/service.rs
@@ -186,7 +186,7 @@ impl IoHandler<ClientIoMessage> for ClientIoHandler {
 			ClientIoMessage::BlockVerified => { self.client.import_verified_blocks(); }
 			ClientIoMessage::NewTransactions(ref transactions) => { self.client.import_queued_transactions(transactions); }
 			ClientIoMessage::BeginRestoration(ref manifest) => {
-				if let Err(e) = self.snapshot.init_restore(manifest.clone()) {
+				if let Err(e) = self.snapshot.init_restore(manifest.clone(), true) {
 					warn!("Failed to initialize snapshot restoration: {}", e);
 				}
 			}
