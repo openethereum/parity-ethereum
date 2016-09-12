7b932d5246598ed1a7fb396c16a69736a3e97e96
Jaco Greeff <jaco@ethcore.io>
2016-09-12 15:39:56 +0200

    make tests run (not all passing again, need to fix)

--- js/src/views/Status/components/Call/Call.spec.js
+++ js/src/views/Status/components/Call/Call.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import '../../env-specific/tests';
+import '../../../../environment/tests';
 
 import Call from './Call';
 
