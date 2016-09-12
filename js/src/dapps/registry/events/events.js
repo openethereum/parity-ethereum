adbcca124d7c28edb2320fc3387b8e13afd70d63
Jannis R <mail@jannisr.de>
2016-09-12 11:20:49 +0200

    component for registry events

---
+++ js/src/dapps/registry/events/events.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+
+import styles from './events.css';
+import bytesToHex from '../../../api/util/bytes-array-to-hex';
+
+const renderReserved = (e) => (
+  <p key={ e.key } className={ styles.reserved }>
+    <code>{ e.parameters.owner }</code>
+    { ' ' }
+    <abbr title={ e.transaction }>registered</abbr>
+    { ' ' }
+    <code>{ bytesToHex(e.parameters.name) }</code>
+  </p>
+);
+
+export default class Events extends Component {
+
+  static propTypes = {
+    actions: PropTypes.object,
+    events: PropTypes.array
+  }
+  componentDidMount () {
+    // TODO remove this
+    this.props.actions.subscribe('Reserved', 0, 'latest');
+  }
+
+  render () {
+    return (
+      <Card className={ styles.events }>
+        <CardHeader title="Stuff Happening" />
+        <CardText>{
+          this.props.events
+            .filter((e) => e.state === 'mined')
+            .map(renderReserved)
+        }</CardText>
+      </Card>
+    );
+  }
+}
