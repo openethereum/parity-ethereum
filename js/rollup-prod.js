1aef264cd13dad239533d6edba9e3b4a88928542
Jaco Greeff <jaco@ethcore.io>
2016-09-13 22:01:48 +0200

    tweak rollup scripts naming slightly

--- js/rollup-prod.js
+++ js/rollup-prod.js
@@ -1,7 +1,7 @@
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 
-import config from './rollup.dev';
+import config from './rollup-dev';
 
 config.dest = 'dist/app.min.js';
 config.plugins[3] = replace({
