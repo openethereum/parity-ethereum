1460e0d0731648cc56de3797e17b0c3228567fc3
Jaco Greeff <jaco@ethcore.io>
2016-09-13 17:45:46 +0200

    ensure address is always in lowercase

--- js/src/api/util/identity.js
+++ js/src/api/util/identity.js
@@ -2,7 +2,7 @@ import blockies from 'blockies';
 
 export function createIdentityImg (address, scale = 7) {
   return blockies({
-    seed: address,
+    seed: (address || '').toLowerCase(),
     size: 8,
     scale
   }).toDataURL();
