126e24747f714a52335136724827d88d60330473
Jaco Greeff <jaco@ethcore.io>
2016-09-13 21:47:27 +0200

    export all library interfaces as part of parity.js

--- js/src/parity.js
+++ js/src/parity.js
@@ -1,13 +1,17 @@
 import 'isomorphic-fetch';
 import es6Promise from 'es6-promise';
 
+import Abi from './abi';
 import Api from './api';
+import JsonRpc from './jsonrpc';
 
 es6Promise.polyfill();
 
 const api = new Api(new Api.Transport.Http('/rpc/'));
 
 window.parity = {
+  Abi,
   Api,
+  JsonRpc,
   api
 };
