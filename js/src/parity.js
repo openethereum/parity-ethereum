c29981d401d8f3dff5e8f77f71f6e7a902a758d4
Jaco Greeff <jaco@ethcore.io>
2016-09-13 21:48:58 +0200

    re-org imports (polyfills first)

--- js/src/parity.js
+++ js/src/parity.js
@@ -1,11 +1,11 @@
 import 'isomorphic-fetch';
+
 import es6Promise from 'es6-promise';
+es6Promise.polyfill();
 
 import Abi from './abi';
 import Api from './api';
 import JsonRpc from './jsonrpc';
-
-es6Promise.polyfill();
 
 const api = new Api(new Api.Transport.Http('/rpc/'));
 
