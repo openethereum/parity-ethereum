8ba6ed6432ae535097bdb6ad2380b2c16d3e13ab
Jaco Greeff <jaco@ethcore.io>
2016-09-13 21:43:00 +0200

    remove React exports, not quire ready for it as of yet

--- js/src/parity.js
+++ js/src/parity.js
@@ -2,7 +2,6 @@ import 'isomorphic-fetch';
 import es6Promise from 'es6-promise';
 
 import Api from './api';
-import { IdentityIcon } from './ui';
 
 es6Promise.polyfill();
 
@@ -10,8 +9,5 @@ const api = new Api(new Api.Transport.Http('/rpc/'));
 
 window.parity = {
   Api,
-  api,
-  react: {
-    IdentityIcon
-  }
+  api
 };
