faccd7404a60fd805b8bfb08fb9ada0e225537fe
Jaco Greeff <jaco@ethcore.io>
2016-09-15 16:30:22 +0200

    don't show tokens.value === 0 in transfer

--- js/src/redux/providers/balances.js
+++ js/src/redux/providers/balances.js
@@ -164,7 +164,7 @@ export default class Balances {
           this._tokens.forEach((token, tidx) => {
             balance.tokens.push({
               token,
-              value: balanceOf[tidx].toString()
+              value: balanceOf[tidx]
             });
           });
         });
