b10dcc43f6e279b67f417b3108dcd693574c7619
Jaco Greeff <jaco@ethcore.io>
2016-09-15 22:18:38 +0200

    describe blocks reflect new files location

--- js/src/api/util/sha3.spec.js
+++ js/src/api/util/sha3.spec.js
@@ -16,7 +16,7 @@
 
 import { sha3 } from './sha3';
 
-describe('api/format/sha3', () => {
+describe('api/util/sha3', () => {
   describe('sha3', () => {
     it('constructs a correct sha3 value', () => {
       expect(sha3('jacogr')).to.equal('0x2f4ff4b5a87abbd2edfed699db48a97744e028c7f7ce36444d40d29d792aa4dc');
