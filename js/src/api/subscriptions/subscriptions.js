7d8719c1ecb491852c34e655690239f93b4454b2
Jaco Greeff <jaco@ethcore.io>
2016-09-18 11:26:19 +0200

    fix typo

--- js/src/api/subscriptions/subscriptions.js
+++ js/src/api/subscriptions/subscriptions.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { isError } from '../uitl/types';
+import { isError } from '../util/types';
 
 import Eth from './eth';
 import Logging from './logging';
