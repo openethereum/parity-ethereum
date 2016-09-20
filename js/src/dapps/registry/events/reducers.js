d3be336c70eeb64dbb5eb5e8b534100299db7052
Jannis R <mail@jannisr.de>
2016-09-20 17:51:54 +0200

    registry: show latest events first

--- js/src/dapps/registry/events/reducers.js
+++ js/src/dapps/registry/events/reducers.js
@@ -11,8 +11,8 @@ const initialState = {
 };
 
 const sortEvents = (a, b) => {
-  const d = a.block.minus(b.block).toFixed(0);
-  if (d === 0) return a.index.minus(b.index).toFixed(0);
+  const d = b.block.minus(a.block).toFixed(0);
+  if (d === 0) return b.index.minus(a.index).toFixed(0);
   return d;
 };
 
