ea3cda0b5829de5d95990994c3b25f7dedc20cc4
Jaco Greeff <jaco@ethcore.io>
2016-09-22 00:28:24 +0200

    fix address selection dropdown

--- js/src/ui/Form/InputAddressSelect/inputAddressSelect.js
+++ js/src/ui/Form/InputAddressSelect/inputAddressSelect.js
@@ -27,6 +27,7 @@ import styles from './inputAddressSelect.css';
 class InputAddressSelect extends Component {
   static propTypes = {
     accounts: PropTypes.object,
+    contacts: PropTypes.object,
     disabled: PropTypes.bool,
     error: PropTypes.string,
     label: PropTypes.string,
@@ -71,11 +72,12 @@ class InputAddressSelect extends Component {
   }
 
   renderSelect () {
-    const { accounts, disabled, error, hint, label, value } = this.props;
+    const { accounts, contacts, disabled, error, hint, label, value } = this.props;
 
     return (
       <AddressSelect
         accounts={ accounts }
+        contacts={ contacts }
         disabled={ disabled }
         label={ label }
         hint={ hint }
@@ -106,7 +108,8 @@ function mapStateToProps (state) {
   const { accounts, contacts } = state.personal;
 
   return {
-    entries: Object.values(accounts).concat(Object.values(contacts))
+    accounts,
+    contacts
   };
 }
 
