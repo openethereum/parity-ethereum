07ffb6d13aff1e90730f9bd07a656971dfd32c83
Nikolay Volf <nikvolf@gmail.com>
2016-09-26 22:49:44 +0300

    fix name

--- ethcore/src/state.rs
+++ ethcore/src/state.rs
@@ -262,7 +262,7 @@ impl State {
 	/// Add `incr` to the balance of account `a`.
 	pub fn add_balance(&mut self, a: &Address, incr: &U256) {
 		trace!(target: "state", "add_balance({}, {}): {}", a, incr, self.balance(a));
-		self.db.note_account_bloom(contract);
+		self.db.note_account_bloom(a);
 		self.require(a, false).add_balance(incr);
 	}
 
