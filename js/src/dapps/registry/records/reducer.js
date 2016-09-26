4f5c509ce164b124a4ae292e21658dbfea5f8254
Jannis R <mail@jannisr.de>
2016-09-26 14:44:30 +0200

    registry: reducer returned invalid state
    `state.hasAccount` was missing.

--- js/src/dapps/registry/records/reducer.js
+++ js/src/dapps/registry/records/reducer.js
@@ -11,6 +11,7 @@ export default (state = initialState, action) => {
 
   if (action.type === 'records update start') {
     return {
+      ...state,
       pending: true,
       name: action.name, type: action.entry, value: action.value
     };
@@ -18,6 +19,7 @@ export default (state = initialState, action) => {
 
   if (action.type === 'records update error' && action.type === 'records update success') {
     return {
+      ...state,
       pending: false,
       name: initialState.name, type: initialState.type, value: initialState.value
     };
