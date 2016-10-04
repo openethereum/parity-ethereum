dbe83ae59c61f44fec595bd3f0f049a803d05f3d
Jaco Greeff <jaco@ethcore.io>
2016-10-04 18:06:10 +0200

    bring in events

--- js/src/dapps/basiccoin/Events/events.js
+++ js/src/dapps/basiccoin/Events/events.js
@@ -16,12 +16,91 @@
 
 import React, { Component } from 'react';
 
+import { loadAllTokens, subscribeEvents, unsubscribeEvents } from '../services';
 import Container from '../Container';
 
+import styles from './events.css';
+
 export default class Deploy extends Component {
+  state = {
+    subscriptionId: 0,
+    loading: true,
+    events: [],
+    pendingEvents: [],
+    minedEvents: []
+  }
+
+  componentDidMount () {
+    loadAllTokens()
+      .then((tokens) => {
+        const addresses = tokens.map((token) => token.address);
+
+        return subscribeEvents(addresses, this.eventCallback);
+      })
+      .then((subscriptionId) => {
+        this.setState({ subscriptionId, loading: false });
+      })
+      .catch((error) => {
+        console.error('componentDidMount', error);
+      });
+  }
+
+  componentWillUnmount () {
+    const { subscriptionId } = this.state;
+
+    if (subscriptionId) {
+      unsubscribeEvents(subscriptionId);
+    }
+  }
+
   render () {
+    const { loading } = this.state;
+
+    return loading
+      ? this.renderLoading()
+      : this.renderEvents();
+  }
+
+  renderLoading () {
     return (
-      <Container>todo ...</Container>
+      <Container>
+        <div className={ styles.statusHeader }>
+          Attaching events
+        </div>
+      </Container>
     );
+  }
+
+  renderEvents () {
+    const { events } = this.state;
+
+    return events.length
+      ? this.renderEventsList()
+      : this.renderEventsNone();
+  }
+
+  renderEventsNone () {
+    return (
+      <Container>
+        <div className={ styles.statusHeader }>
+          There are currently no events available
+        </div>
+      </Container>
+    );
+  }
+
+  renderEventsList () {
+    return (
+      <Container>events go here ...</Container>
+    );
+  }
+
+  eventCallback = (error, events) => {
+    if (error) {
+      console.error('eventCallback', error);
+      return;
+    }
+
+    console.log(events);
   }
 }
