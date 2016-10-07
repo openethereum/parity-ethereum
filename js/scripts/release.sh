58b486f4b95a90bb00b45a70018cb3eaa972fea7
Jaco Greeff <jaco@ethcore.io>
2016-10-07 15:40:18 +0200

    release push does the trick

--- js/scripts/release.sh
+++ js/scripts/release.sh
@@ -2,13 +2,30 @@
 
 # change into the js directory (one down from scripts)
 pushd `dirname $0`
-cd ..
+cd ../build
 
-# run release
-EXICCODE=0
+# variables
+UTCDATE=`date -u "+%Y%m%d-%H%M%S"`
+
+# init git
+rm -rf ./.git
+git init
+
+# our user details
+git config push.default simple
+git config user.email "jaco+gitlab@ethcore.io"
+git config user.name "GitLab Build Bot"
+
+# add local files and send it up
+git remote add origin https://${GITHUB_JS_PRECOMPILED}:@github.com/ethcore/js-precompiled.git
+git fetch
+git checkout master
+git add .
+git commit -m "$UTCDATE"
+git push origin master --force
 
 # back to root
 popd
 
 # exit with exit code
-exit $EXITCODE
+exit 0
