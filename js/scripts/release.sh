39bfde4fad91b57cda4fbc8e1a6ca24737c8159b
Jaco Greeff <jaco@ethcore.io>
2016-10-07 13:53:46 +0200

    first stab at making the build build

--- js/scripts/release.sh
+++ js/scripts/release.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-set -o errexit
+# change into the js directory (one down from scripts)
+pushd `dirname $0`
+cd ..
 
-if [[ ("$TRAVIS_PULL_REQUEST" != "false") || ("$TRAVIS_BRANCH" != "master") ]]; then
-  exit 0
-fi
+# run release
+EXICCODE=0
 
-git config --global user.email "admin@travis-ci.org"
-git config --global user.name "Travis CI"
-git config --global push.default simple
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-git checkout master
+# back to root
+popd
 
-DATE=`date`
-
-npm run build
-echo "/* ${DATE} */" >> ./index.js
-
-git add --force index.js
-git commit --message "[CI skip] ${DATE}"
-
-npm version patch --message "[CI skip] ${DATE} %s"
-git push
-git push --tags
-
-echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
-npm publish
+# exit with exit code
+exit $EXITCODE
