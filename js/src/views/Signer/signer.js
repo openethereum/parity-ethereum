9f85eb2540ee7491b7fc14216f593e5bc7e35d52
Jaco Greeff <jaco@ethcore.io>
2016-10-07 21:05:54 +0200

    cleanup signer after connection changes

--- js/src/views/Signer/signer.js
+++ js/src/views/Signer/signer.js
@@ -14,59 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
 
 import { Actionbar, Page } from '../../ui';
-
-import LoadingPage from './containers/LoadingPage';
 import RequestsPage from './containers/RequestsPage';
 
 import styles from './signer.css';
 
-export class Signer extends Component {
-  static propTypes = {
-    signer: PropTypes.shape({
-      isLoading: PropTypes.bool.isRequired
-    }).isRequired
-  };
-
+export default class Signer extends Component {
   render () {
     return (
       <div className={ styles.signer }>
         <Actionbar
           title='Trusted Signer' />
         <Page>
-          { this.renderPage() }
+          <RequestsPage />
         </Page>
       </div>
     );
   }
-
-  renderPage () {
-    const { isLoading } = this.props.signer;
-
-    if (isLoading) {
-      return (
-        <LoadingPage />
-      );
-    }
-
-    return (
-      <RequestsPage />
-    );
-  }
 }
-
-function mapStateToProps (state) {
-  return state;
-}
-
-function mapDispatchToProps (dispatch) {
-  return {};
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Signer);
