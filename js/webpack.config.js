6caac6b2316fa70e33f5db6099859269933aa73d
Jaco Greeff <jaco@ethcore.io>
2016-10-12 14:08:50 +0200

    don't inline woff files

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -86,10 +86,6 @@ module.exports = {
         loader: 'file-loader'
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
         test: /\.(woff(2)|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader'
       }
