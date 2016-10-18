10fc3faf0ef769f38a1d9dc289d9fb8a8a66e710
Jannis R <mail@jannisr.de>
2016-10-18 11:29:08 +0200

    signer: remove reject counter

--- js/src/views/Signer/components/TransactionPendingForm/TransactionPendingFormReject/TransactionPendingFormReject.js
+++ js/src/views/Signer/components/TransactionPendingForm/TransactionPendingFormReject/TransactionPendingFormReject.js
@@ -18,35 +18,16 @@ import React, { Component, PropTypes } from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
 
-import { REJECT_COUNTER_TIME } from '../../constants/constants';
 import styles from './TransactionPendingFormReject.css';
 
 export default class TransactionPendingFormReject extends Component {
 
   static propTypes = {
     onReject: PropTypes.func.isRequired,
-    className: PropTypes.string,
-    rejectCounterTime: PropTypes.number
+    className: PropTypes.string
   };
-
-  static defaultProps = {
-    rejectCounterTime: REJECT_COUNTER_TIME
-  };
-
-  state = {
-    rejectCounter: this.props.rejectCounterTime
-  }
-
-  componentWillMount () {
-    this.onInitCounter();
-  }
-
-  componentWillUnmount () {
-    this.onResetCounter();
-  }
 
   render () {
-    const { rejectCounter } = this.state;
     const { onReject } = this.props;
 
     return (
@@ -58,36 +39,10 @@ export default class TransactionPendingFormReject extends Component {
         <RaisedButton
           onClick={ onReject }
           className={ styles.rejectButton }
-          disabled={ rejectCounter > 0 }
           fullWidth
-          label={ `Reject Transaction ${this.renderCounter()}` }
+          label={ 'Reject Transaction' }
         />
       </div>
     );
-  }
-
-  renderCounter () {
-    const { rejectCounter } = this.state;
-    if (!rejectCounter) {
-      return '';
-    }
-    return `(${rejectCounter})`;
-  }
-
-  onInitCounter () {
-    this.rejectInterval = setInterval(() => {
-      let { rejectCounter } = this.state;
-      if (rejectCounter === 0) {
-        return clearInterval(this.rejectInterval);
-      }
-      this.setState({ rejectCounter: rejectCounter - 1 });
-    }, 1000);
-  }
-
-  onResetCounter () {
-    clearInterval(this.rejectInterval);
-    this.setState({
-      rejectCounter: this.props.rejectCounterTime
-    });
   }
 }
