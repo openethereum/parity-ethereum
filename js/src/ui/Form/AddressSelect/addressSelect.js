0a39be642faa314c2d53ae88fd79963e2f6b3286
Nicolas Gotchac <ngotchac@gmail.com>
2016-10-19 19:55:37 +0200

    Use Autocomplete to make address selection searchable (#2141)

--- js/src/ui/Form/AddressSelect/addressSelect.js
+++ js/src/ui/Form/AddressSelect/addressSelect.js
@@ -15,11 +15,10 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import React, { Component, PropTypes } from 'react';
-import { MenuItem } from 'material-ui';
+import { MenuItem, AutoComplete } from 'material-ui';
 
 import IdentityIcon from '../../IdentityIcon';
 import IdentityName from '../../IdentityName';
-import Select from '../Select';
 
 import styles from './addressSelect.css';
 
@@ -36,31 +35,75 @@ export default class AddressSelect extends Component {
     onChange: PropTypes.func.isRequired
   }
 
+  state = {
+    entries: {}
+  }
+
+  componentWillMount () {
+    const { accounts, contacts } = this.props;
+    const entries = Object.assign({}, accounts || {}, contacts || {});
+    this.setState({ entries });
+  }
+
+  componentWillReceiveProps (newProps) {
+    const { accounts, contacts } = newProps;
+    const entries = Object.assign({}, accounts || {}, contacts || {});
+    this.setState({ entries });
+  }
+
   render () {
-    const { disabled, error, hint, label, value } = this.props;
+    const { disabled, error, hint, label } = this.props;
 
     return (
-      <Select
-        disabled={ disabled }
-        label={ label }
-        hint={ hint }
-        error={ error }
-        value={ value }
-        onChange={ this.onChange }>
-        { this.renderSelectEntries() }
-      </Select>
+      <div>
+        <AutoComplete
+          className={ error ? '' : styles.paddedInput }
+          disabled={ disabled }
+          floatingLabelText={ label }
+          hintText={ `search for ${hint}` }
+          errorText={ error }
+          onNewRequest={ this.onChange }
+          searchText={ this.getSearchText() }
+          onBlur={ this.onBlur }
+          onFocus={ this.onFocus }
+          onUpdateInput={ this.onUpdateInput }
+
+          filter={ this.handleFilter }
+          openOnFocus
+          fullWidth
+          floatingLabelFixed
+          dataSource={ this.renderSelectEntries() }
+        />
+
+        { this.renderIdentityIcon() }
+      </div>
+    );
+  }
+
+  renderIdentityIcon () {
+    if (this.props.error) return null;
+
+    const { value } = this.props;
+
+    return (
+      <IdentityIcon
+        className={ styles.icon }
+        inline center
+        address={ value } />
     );
   }
 
   renderSelectEntries () {
-    const { accounts, contacts } = this.props;
-    const entries = Object.values(Object.assign({}, accounts || {}, contacts || {}));
+    const values = Object.values(this.state.entries);
 
-    if (!entries.length) {
+    if (!values.length) {
       return null;
     }
 
-    return entries.map(this.renderSelectEntry);
+    return values.map(entry => ({
+      text: entry.address,
+      value: this.renderSelectEntry(entry)
+    }));
   }
 
   renderSelectEntry = (entry) => {
@@ -89,7 +132,45 @@ export default class AddressSelect extends Component {
     );
   }
 
-  onChange = (event, idx, value) => {
-    this.props.onChange(event, value);
+  getSearchText () {
+    const { value } = this.props;
+    const entries = this.state.entries;
+
+    if (!value) return '';
+
+    const entry = entries[value];
+    if (!entry) return '';
+
+    return entry.name ? entry.name.toUpperCase() : '';
+  }
+
+  handleFilter = (searchText, address) => {
+    const entry = this.state.entries[address];
+    const lowCaseSearch = searchText.toLowerCase();
+
+    return [ entry.name, entry.address ]
+      .some(text => text.toLowerCase().indexOf(lowCaseSearch) !== -1);
+  }
+
+  onChange = (item, idx) => {
+    if (idx === -1) {
+      this.props.onChange(null, '');
+      return null;
+    }
+
+    const entries = Object.values(this.state.entries);
+    const address = entries[idx].address;
+
+    this.props.onChange(null, address);
+  }
+
+  onFocus = () => {
+    this.props.onChange(null, '');
+  }
+
+  onBlur = () => {
+    if (this.props.error) {
+      this.props.onChange(null, '');
+    }
   }
 }
