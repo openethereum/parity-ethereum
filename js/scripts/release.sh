e71c7522100be8a6e2d8a2b47bc6422243016907
Jaco Greeff <jacogr@gmail.com>
2016-10-25 17:39:19 +0200

    Output git fetch/push to log files (#2862)
    * add -q options for fetch & push
    
    * Store log in .gitignored location
    
    * update GITLOG command location
    
    * Remove test branch

--- js/scripts/release.sh
+++ js/scripts/release.sh
@@ -10,7 +10,9 @@ function setup_git_user {
 }
 
 # change into the build directory
-pushd `dirname $0`
+BASEDIR=`dirname $0`
+GITLOG=./.git/gitcommand.log
+pushd $BASEDIR
 cd ../.build
 
 # variables
@@ -33,12 +35,12 @@ git init
 # add local files and send it up
 setup_git_user
 git remote add origin https://${GITHUB_JS_PRECOMPILED}:@github.com/ethcore/js-precompiled.git
-git fetch origin
+git fetch origin 2>$GITLOG
 git checkout -b $CI_BUILD_REF_NAME
 git add .
 git commit -m "$UTCDATE [compiled]"
 git merge origin/$CI_BUILD_REF_NAME -X ours --commit -m "$UTCDATE [release]"
-git push origin $CI_BUILD_REF_NAME
+git push origin $CI_BUILD_REF_NAME 2>$GITLOG
 
 # back to root
 popd
@@ -49,10 +51,10 @@ cargo update -p parity-ui-precompiled
 # add to git and push
 setup_git_user
 git remote set-url origin https://${GITHUB_JS_PRECOMPILED}:@github.com/ethcore/parity.git
-git fetch origin
+git fetch origin 2>$GITLOG
 git add . || true
 git commit -m "[ci skip] js-precompiled $UTCDATE" || true
-git push origin || true
+git push origin $CI_BUILD_REF_NAME 2>$GITLOG || true
 
 # exit with exit code
 exit 0
