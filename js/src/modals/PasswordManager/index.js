20431afaf6fe99bba0b2794afad2391980defa5d
Nicolas Gotchac <ngotchac@gmail.com>
2016-10-25 16:37:30 +0200

    Added modal in Account Page to test & modify password (#2389)

--- js/src/modals/PasswordManager/index.js
+++ js/src/modals/PasswordManager/index.js
@@ -14,26 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import AddAddress from './AddAddress';
-import AddContract from './AddContract';
-import CreateAccount from './CreateAccount';
-import DeployContract from './DeployContract';
-import EditMeta from './EditMeta';
-import ExecuteContract from './ExecuteContract';
-import FirstRun from './FirstRun';
-import Shapeshift from './Shapeshift';
-import Transfer from './Transfer';
-import PasswordManager from './PasswordManager';
-
-export {
-  AddAddress,
-  AddContract,
-  CreateAccount,
-  DeployContract,
-  EditMeta,
-  ExecuteContract,
-  FirstRun,
-  Shapeshift,
-  Transfer,
-  PasswordManager
-};
+export default from './passwordManager.js';
