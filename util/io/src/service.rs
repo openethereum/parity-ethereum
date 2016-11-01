a33e2f2e0d262382b6cc2bb77863c7d21cfb34b5
keorn <pczaban@gmail.com>
2016-11-01 17:52:52 +0000

    fix deadlock after merge

--- util/io/src/service.rs
+++ util/io/src/service.rs
@@ -249,7 +249,8 @@ impl<Message> Handler for IoManager<Message> where Message: Send + Clone + Sync 
 		let handler_index  = token.0  / TOKENS_PER_HANDLER;
 		let token_id  = token.0  % TOKENS_PER_HANDLER;
 		if let Some(handler) = self.handlers.read().get(handler_index) {
-			if let Some(timer) = self.timers.read().get(&token.0) {
+			let maybe_timer = self.timers.read().get(&token.0).cloned();
+			if let Some(timer) = maybe_timer {
 				if timer.once {
 					self.timers.write().remove(&token_id);
 					event_loop.clear_timeout(&timer.timeout);
