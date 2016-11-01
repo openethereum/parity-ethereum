374c2b3e4c7659bfb17045798e62100078b6abcb
Jaco Greeff <jaco@ethcore.io>
2016-11-01 13:59:50 +0100

    Really fix boolean display

--- js/src/views/Contract/Queries/queries.js
+++ js/src/views/Contract/Queries/queries.js
@@ -108,10 +108,12 @@ export default class Queries extends Component {
   }
 
   renderValue (value) {
-    if (!value) return null;
+    if (typeof value === 'undefined') {
+      return null;
+    }
 
     const { api } = this.context;
-    let valueToDisplay = value.toString();
+    let valueToDisplay = null;
 
     if (api.util.isInstanceOf(value, BigNumber)) {
       valueToDisplay = value.toFormat(0);
@@ -119,6 +121,8 @@ export default class Queries extends Component {
       valueToDisplay = api.util.bytesToHex(value);
     } else if (typeof value === 'boolean') {
       valueToDisplay = value ? 'true' : 'false';
+    } else {
+      valueToDisplay = value.toString();
     }
 
     return (
