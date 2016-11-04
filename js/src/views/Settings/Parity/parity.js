48217b8b6bca33940edec2279d54cacff4903b1c
Jaco Greeff <jaco@ethcore.io>
2016-11-04 17:13:09 +0100

    Mode selector

--- js/src/views/Settings/Parity/parity.js
+++ js/src/views/Settings/Parity/parity.js
@@ -14,17 +14,31 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { MenuItem } from 'material-ui';
 
-import { Container, ContainerTitle } from '../../../ui';
+import { Select, Container, ContainerTitle } from '../../../ui';
 
 import layout from '../layout.css';
 
+const MODES = {
+  'active': 'active',
+  'passive': 'passive',
+  'dark': 'dark',
+  'off': 'off'
+};
+
 export default class Parity extends Component {
-  static propTypes = {
+  static contextTypes = {
+    api: PropTypes.object.isRequired
   }
 
   state = {
+    mode: 'active'
+  }
+
+  componentWillMount () {
+    this.loadMode();
   }
 
   render () {
@@ -33,12 +47,59 @@ export default class Parity extends Component {
         <ContainerTitle title='Parity' />
         <div className={ layout.layout }>
           <div className={ layout.overview }>
-            <div>The proxy setup allows you to access Parity and all associated decentralized applications via memororable addresses.</div>
+            <div>Control the Parity node settings via this interface.</div>
           </div>
           <div className={ layout.details }>
+            { this.renderModes() }
           </div>
         </div>
       </Container>
     );
+  }
+
+  renderModes () {
+    const modes = Object
+      .keys(MODES)
+      .map((mode) => {
+        const description = MODES[mode];
+
+        return (
+          <MenuItem
+            key={ mode }
+            value={ mode }
+            label={ description }>
+            { description }
+          </MenuItem>
+        );
+      });
+
+    const { mode } = this.state;
+
+    return (
+      <Select
+        label='mode of operation'
+        hint='the syning mode for the Parity node'
+        value={ mode }
+        onChange={ this.onChangeMode }>
+        { modes }
+      </Select>
+    );
+  }
+
+  onChangeMode = (event, mode) => {
+    this.setState({ mode });
+  }
+
+  loadMode () {
+    const { api } = this.context;
+
+    api.ethcore
+      .mode()
+      .then((mode) => {
+        this.setMode({ mode });
+      })
+      .catch((error) => {
+        console.warn('loadMode', error);
+      });
   }
 }
