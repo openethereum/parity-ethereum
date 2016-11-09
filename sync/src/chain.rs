96f76522c5cd34cc9790f6c98c13688d6c8c1aea
arkpar <arkady.paronyan@gmail.com>
2016-11-09 16:40:17 +0100

    Additional snapshot sync checks

--- sync/src/chain.rs
+++ sync/src/chain.rs
@@ -457,7 +457,10 @@ impl ChainSync {
 		if self.state != SyncState::WaitingPeers {
 			return;
 		}
-		let best_block = io.chain().chain_info().best_block_number;
+		// Make sure the snapshot block is not too far away from our best block, network best block and
+		// fork detection block
+		let mut best_block = max(io.chain().chain_info().best_block_number, self.highest_block.unwrap_or(0));
+		best_block = max(best_block, self.fork_block.as_ref().map(|&(n, _)| n).unwrap_or(0));
 
 		let (best_hash, max_peers, snapshot_peers) = {
 			//collect snapshot infos from peers
