90ff810e367c438d3c2943e60eaa849307af9ce3
Jaco Greeff <jacogr@gmail.com>
2016-11-09 20:05:08 +0100

    Handle redirects from /api/content on manifest.json gracefully (#3315)
    * Add redirect follow mode to manifest.json
    
    * Remove (now) unused count parameter
    
    * autoRewrite: true for dev mode proxy redirects

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -198,7 +198,8 @@ module.exports = {
     proxy: {
       '/api/*': {
         target: 'http://127.0.0.1:8080',
-        changeOrigin: true
+        changeOrigin: true,
+        autoRewrite: true
       },
       '/app/*': {
         target: 'http://127.0.0.1:8080',
