1823ebd7e23526687dddc51f15f7dfa51cafcfe6
Nicolas Gotchac <ngotchac@gmail.com>
2016-11-10 14:58:49 +0100

    Fix lint

--- js/src/modals/LoadContract/loadContract.js
+++ js/src/modals/LoadContract/loadContract.js
@@ -79,16 +79,16 @@ export default class LoadContract extends Component {
     const contractsTab = Object.keys(contracts).length === 0
       ? null
       : (
-        <Tab label='Local' >
-          { this.renderEditor() }
+      <Tab label='Local' >
+        { this.renderEditor() }
 
-          <SelectableList
-            onChange={ this.onClickContract }
-          >
-            <Subheader>Saved Contracts</Subheader>
-            { this.renderContracts(contracts) }
-          </SelectableList>
-        </Tab>
+        <SelectableList
+          onChange={ this.onClickContract }
+        >
+          <Subheader>Saved Contracts</Subheader>
+          { this.renderContracts(contracts) }
+        </SelectableList>
+      </Tab>
       );
 
     return (
