0e4ef539fc441dfc663a36a517bc63cff3196bc2
Nicolas Gotchac <ngotchac@gmail.com>
2016-11-11 15:00:04 +0100

    Solidity Compiler in UI (#3279)
    * Added new Deploy Contract page // Use Brace in React #2276
    
    * Adding Web Wrokers WIP
    
    * Compiling Solidity code // Getting mandatory params #2276
    
    * Working editor and deployment #2276
    
    * WIP : displaying source code
    
    * Added Solidity hightling, editor component in UI
    
    * Re-adding the standard Deploy Modal #2276
    
    * Using MobX in Contract Edition // Save to Localstorage #2276
    
    * User select Solidity version #2276
    
    * Loading Solidity versions and closing worker properly #2276
    
    * Adds export to solidity editor #2276
    
    * Adding Import to Contract Editor #2276
    
    * Persistent Worker => Don't load twice Solidity Code #2276
    
    * UI Fixes
    
    * Editor tweaks
    
    * Added Details with ABI in Contract view
    
    * Adds Save capabilities to contract editor // WIP on Load #3279
    
    * Working Load and Save contracts... #3231
    
    * Adding loader of Snippets // Export with name #3279
    
    * Added snippets / Importing from files and from URL
    
    * Fix wrong ID in saved Contract
    
    * Fix lint
    
    * Fixed Formal errors as warning #3279
    
    * Fixing lint issues
    
    * Use NPM Module for valid URL (fixes linting issue too)
    
    * Don't clobber tests.

--- js/src/redux/providers/compilerReducer.js
+++ js/src/redux/providers/compilerReducer.js
@@ -14,16 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-export Balances from './balances';
-export Personal from './personal';
-export Signer from './signer';
-export Status from './status';
+import { handleActions } from 'redux-actions';
 
-export apiReducer from './apiReducer';
-export balancesReducer from './balancesReducer';
-export imagesReducer from './imagesReducer';
-export personalReducer from './personalReducer';
-export signerReducer from './signerReducer';
-export statusReducer from './statusReducer';
-export blockchainReducer from './blockchainReducer';
-export compilerReducer from './compilerReducer';
+const initialState = {
+  worker: null
+};
+
+export default handleActions({
+  setWorker (state, action) {
+    const { worker } = action;
+
+    return Object.assign({}, state, { worker });
+  }
+}, initialState);
