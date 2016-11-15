36244abbe86c4620fbfd258a382be819cae4145c
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2016-11-15 19:48:15 +0100

    Update test client with max code size

--- ethcore/src/client/test_client.rs
+++ ethcore/src/client/test_client.rs
@@ -315,7 +315,7 @@ pub fn get_temp_state_db() -> GuardedTempResult<StateDB> {
 
 impl MiningBlockChainClient for TestBlockChainClient {
 	fn latest_schedule(&self) -> Schedule {
-		Schedule::new_post_eip150(23999, true, true, true)
+		Schedule::new_post_eip150(24576, true, true, true)
 	}
 
 	fn prepare_open_block(&self, author: Address, gas_range_target: (U256, U256), extra_data: Bytes) -> OpenBlock {
