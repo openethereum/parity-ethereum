bfcc8d33d6fb5493ac8bed8805656eafe9a9cca8
Jannis R <mail@jannisr.de>
2016-11-15 13:45:27 +0100

    sms verification: make Done component dumb

--- js/src/modals/SMSVerification/Done/done.js
+++ js/src/modals/SMSVerification/Done/done.js
@@ -14,20 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import SuccessIcon from 'material-ui/svg-icons/navigation/check';
 
 import styles from './done.css';
 
 export default class Done extends Component {
-  static propTypes = {
-    onSuccess: PropTypes.func.isRequired
-  }
-
-  componentWillMount () {
-    this.props.onSuccess();
-  }
-
   render () {
     return (
       <div className={ styles.container }>
