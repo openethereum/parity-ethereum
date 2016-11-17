6fd17540f3ad3272e225cc0565ef35cead149677
Nicolas Gotchac <ngotchac@gmail.com>
2016-11-17 15:54:13 +0100

    Use the right webpack file // Export to window.Parity #3471

--- js/src/parity.js
+++ js/src/parity.js
@@ -14,34 +14,19 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import 'babel-polyfill/dist/polyfill.js';
+import 'babel-polyfill';
+import 'whatwg-fetch';
+
 import es6Promise from 'es6-promise';
 es6Promise.polyfill();
 
-const isNode = typeof global !== 'undefined' && typeof global !== 'undefined';
-const isBrowser = typeof self !== 'undefined' && typeof self.window !== 'undefined';
-
-if (isBrowser) {
-  require('whatwg-fetch');
-}
-
-if (isNode) {
-  global.fetch = require('node-fetch');
-}
-
 import Api from './api';
+
 import './dev.parity.html';
 
-// commonjs
-module.exports = { Api };
-// es6 default export compatibility
-module.exports.default = module.exports;
+const api = new Api(new Api.Transport.Http('/rpc/'));
 
-if (isBrowser) {
-  const api = new Api(new Api.Transport.Http('/rpc/'));
-
-  self.window.parity = {
-    Api,
-    api
-  };
-}
+window.parity = {
+  Api,
+  api
+};
