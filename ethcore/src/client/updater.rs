cd770490fffc1e6f0f5d46830d84b0417f28d1cb
Gav Wood <gavin@ethcore.io>
2016-11-18 19:22:47 +0800

    Remove unneeded file.

--- ethcore/src/client/updater.rs
+++ ethcore/src/client/updater.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-//use util::{U256, H256, Address, H2048, Uint, FixedHash};
 use std::sync::Weak;
 use util::misc::code_hash;
 use util::Address;
