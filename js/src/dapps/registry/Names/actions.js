42964e9a8e167c7c8d36aa75fca41ea70a7b7c05
Jaco Greeff <jaco@ethcore.io>
2016-11-21 15:48:46 +0100

    lint fixes

--- js/src/dapps/registry/Names/actions.js
+++ js/src/dapps/registry/Names/actions.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { sha3, toWei } from '../parity.js';
+import { sha3 } from '../parity.js';
 
 const alreadyQueued = (queue, action, name) =>
   !!queue.find((entry) => entry.action === action && entry.name === name);
