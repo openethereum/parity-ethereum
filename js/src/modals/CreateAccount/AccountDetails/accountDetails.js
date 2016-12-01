08a6be5d62325c5e358f4d737ebe6119bacb13ac
Jannis R <mail@jannisr.de>
2016-12-01 12:11:03 +0100

    recovery phrase: move print button to modal actions

--- js/src/modals/CreateAccount/AccountDetails/accountDetails.js
+++ js/src/modals/CreateAccount/AccountDetails/accountDetails.js
@@ -15,15 +15,8 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import React, { Component, PropTypes } from 'react';
-import PrintIcon from 'material-ui/svg-icons/action/print';
 
 import { Form, Input, InputAddress } from '../../../ui';
-import Button from '../../../ui/Button';
-
-import { createIdentityImg } from '../../../api/util/identity';
-import print from './print';
-import recoveryPage from './recovery-page.ejs';
-import ParityLogo from '../../../../assets/images/parity-logo-black-no-text.svg';
 
 export default class AccountDetails extends Component {
   static propTypes = {
@@ -49,7 +42,6 @@ export default class AccountDetails extends Component {
           label='address'
           value={ address } />
         { this.renderPhrase() }
-        { this.renderPhraseCopyButton() }
       </Form>
     );
   }
@@ -69,27 +61,5 @@ export default class AccountDetails extends Component {
         label='owner recovery phrase (keep private and secure, it allows full and unlimited access to the account)'
         value={ phrase } />
     );
-  }
-
-  renderPhraseCopyButton () {
-    const { phrase } = this.props;
-    if (!phrase) {
-      return null;
-    }
-
-    return (
-      <Button
-        icon={ <PrintIcon /> }
-        label={ 'print recovery phrase' }
-        onClick={ this.printPhrase }
-      />
-    );
-  }
-
-  printPhrase = () => {
-    const { address, phrase, name } = this.props;
-    const identity = createIdentityImg(address);
-
-    print(recoveryPage({ phrase, name, identity, address, logo: ParityLogo }));
   }
 }
