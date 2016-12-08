3379e61246cde635c296d31322b71e63395a5cd4
Jaco Greeff <jaco@ethcore.io>
2016-12-08 10:04:52 +0100

    Move UI components into UI

--- js/src/modals/SMSVerification/GatherData/gatherData.js
+++ js/src/modals/SMSVerification/GatherData/gatherData.js
@@ -25,7 +25,7 @@ import { fromWei } from '~/api/util/wei';
 import { Form, Input } from '~/ui';
 import { nullableProptype } from '~/util/proptypes';
 
-import { termsOfService } from '../../../3rdparty/sms-verification';
+import termsOfService from './tos';
 import styles from './gatherData.css';
 
 export default class GatherData extends Component {
