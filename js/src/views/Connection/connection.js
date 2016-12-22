19af9cfe8d9a3df9c7d9d3a273a947731cb4aba6
Jaco Greeff <jaco@ethcore.io>
2016-12-22 14:33:38 +0100

    .trim() on token

--- js/src/views/Connection/connection.js
+++ js/src/views/Connection/connection.js
@@ -144,7 +144,8 @@ class Connection extends Component {
     );
   }
 
-  onChangeToken = (event, token) => {
+  onChangeToken = (event, _token) => {
+    const token = _token.trim();
     const validToken = /[a-zA-Z0-9]{4}(-)?[a-zA-Z0-9]{4}(-)?[a-zA-Z0-9]{4}(-)?[a-zA-Z0-9]{4}/.test(token);
 
     this.setState({ token, validToken }, () => {
