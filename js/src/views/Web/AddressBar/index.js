c7c309d1524cffef82c1689a4370e63c54527d88
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2016-12-27 11:15:02 +0100

    Web Based Dapps (#3956)
    * Dapps web
    
    Conflicts:
    	dapps/src/apps/fetcher.rs
    	dapps/src/handlers/fetch.rs
    
    * Rewriting fetch
    
    * Parity-wide fetch service
    
    * Obey the limits and support cancellation.
    
    * Removing temporary files.
    
    * Actually use Fetch for dapps
    
    * Re-implementing file fetching to avoid temporary files.
    
    * Serde to 0.8.19
    
    * Fixing content & dapps fetch
    
    * Serving web content and injecting scripts
    
    * Don't wait for old content, start a new download
    
    * Supporting timeouts and query
    
    * Simple GUI for the browser
    
    * Proxy tokens validation
    
    * Recovering from invalid web-based requests
    
    * Remember last visisted URL
    
    * Removing unused variables
    
    * Addressing review comments
    
    * Setting default account in web3
    
    * Adding WebBrowser dapp to the list
    
    * Actually prune old entries when generating new token

--- js/src/views/Web/AddressBar/index.js
+++ js/src/views/Web/AddressBar/index.js
@@ -14,22 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import Web3 from 'web3';
-import web3extensions from './util/web3.extensions';
-
-import './dev.web3.html';
-
-const http = new Web3.providers.HttpProvider('/rpc/');
-const web3 = new Web3(http);
-// set default account
-web3.eth.getAccounts((err, accounts) => {
-  if (err || !accounts || !accounts[0]) {
-    return;
-  }
-
-  web3.eth.defaultAccount = accounts[0];
-});
-
-web3extensions(web3).map((extension) => web3._extend(extension));
-
-global.web3 = web3;
+export default from './addressBar';
