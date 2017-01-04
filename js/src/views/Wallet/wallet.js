fbb52ac58bc65939e2d4c4c496e1e36e0fd6efe5
Nicolas Gotchac <ngotchac@gmail.com>
2017-01-04 16:25:39 +0100

    Fix wallet in main net (#4038)

--- js/src/views/Wallet/wallet.js
+++ js/src/views/Wallet/wallet.js
@@ -180,7 +180,7 @@ class Wallet extends Component {
     const { address, isTest, wallet } = this.props;
     const { owners, require, confirmations, transactions } = wallet;
 
-    if (!isTest || !owners || !require) {
+    if (!owners || !require) {
       return (
         <div style={ { marginTop: '4em' } }>
           <Loading size={ 4 } />
