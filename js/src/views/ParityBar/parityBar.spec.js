63b9fdf3d9782719e09284cec6b4a696c26d5d58
Jaco Greeff <jaco@ethcore.io>
2017-01-05 16:14:16 +0100

    ParityBar verification

--- js/src/views/ParityBar/parityBar.spec.js
+++ js/src/views/ParityBar/parityBar.spec.js
@@ -44,9 +44,59 @@ function render (props = {}, state = {}) {
   return component;
 }
 
-describe.only('views/ParityBar', () => {
+describe('views/ParityBar', () => {
   it('renders defaults', () => {
     expect(render()).to.be.ok;
+  });
+
+  describe('renderBar', () => {
+    let bar;
+
+    beforeEach(() => {
+      render({ dapp: true });
+      bar = shallow(instance.renderBar());
+    });
+
+    it('renders nothing when not overlaying a dapp', () => {
+      render({ dapp: false });
+      expect(instance.renderBar()).to.be.null;
+    });
+
+    it('renders when overlaying a dapp', () => {
+      expect(bar.find('div')).not.to.have.length(0);
+    });
+
+    it('includes the ParityBackground', () => {
+      expect(bar.find('Connect(ParityBackground)')).to.have.length(1);
+    });
+
+    it('renders the Parity button', () => {
+      const label = shallow(bar.find('Button').first().props().label);
+      expect(label.find('FormattedMessage').props().id).to.equal('parityBar.label.parity');
+    });
+
+    it('renders the Signer button', () => {
+      const label = shallow(bar.find('Button').last().props().label);
+
+      expect(label.find('FormattedMessage').props().id).to.equal('parityBar.label.signer');
+    });
+  });
+
+  describe('renderExpanded', () => {
+    let expanded;
+
+    beforeEach(() => {
+      render();
+      expanded = shallow(instance.renderExpanded());
+    });
+
+    it('includes the ParityBackground', () => {
+      expect(expanded.find('Connect(ParityBackground)')).to.have.length(1);
+    });
+
+    it('includes the Signer', () => {
+      expect(expanded.find('Connect(Embedded)')).to.have.length(1);
+    });
   });
 
   describe('renderLabel', () => {
@@ -64,21 +114,48 @@ describe.only('views/ParityBar', () => {
   });
 
   describe('renderSignerLabel', () => {
+    let label;
+
     beforeEach(() => {
       render();
+      label = shallow(instance.renderSignerLabel());
     });
 
     it('renders the signer label', () => {
-      expect(shallow(instance.renderSignerLabel()).find('FormattedMessage').props().id).to.equal('parityBar.label.parity');
+      expect(label.find('FormattedMessage').props().id).to.equal('parityBar.label.signer');
     });
 
-    it('does not render a badge when no pendings', () => {
-      expect(shallow(instance.renderSignerLabel()).find('Badge')).to.have.length(0);
+    it('does not render a badge when no pending requests', () => {
+      expect(label.find('Badge')).to.have.length(0);
     });
 
-    it('renders a badge when with pendings', () => {
+    it('renders a badge when pending requests', () => {
       render({}, { signer: { pending: ['123', '456'] } });
       expect(shallow(instance.renderSignerLabel()).find('Badge').props().value).to.equal(2);
+    });
+  });
+
+  describe('opened state', () => {
+    beforeEach(() => {
+      render({ dapp: true });
+
+      sinon.spy(instance, 'renderBar');
+      sinon.spy(instance, 'renderExpanded');
+    });
+
+    afterEach(() => {
+      instance.renderBar.restore();
+      instance.renderExpanded.restore();
+    });
+
+    it('renders the bar on with opened === false', () => {
+      expect(component.find('Link[to="/apps"]')).to.have.length(1);
+    });
+
+    it('renders expanded with opened === true', () => {
+      expect(instance.renderExpanded).not.to.have.been.called;
+      instance.setState({ opened: true });
+      expect(instance.renderExpanded).to.have.been.called;
     });
   });
 });
