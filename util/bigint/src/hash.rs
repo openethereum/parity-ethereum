7b98e6adec7273fd5f8403f424b6b62edc10ee4a
arkpar <arkady.paronyan@gmail.com>
2017-01-12 12:58:22 +0100

    Optimize hash comparison

--- util/bigint/src/hash.rs
+++ util/bigint/src/hash.rs
@@ -62,6 +62,8 @@ pub fn clean_0x(s: &str) -> &str {
 	}
 }
 
+extern { fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32; }
+
 macro_rules! impl_hash {
 	($from: ident, $size: expr) => {
 		#[repr(C)]
@@ -214,25 +216,16 @@ macro_rules! impl_hash {
 
 		impl PartialEq for $from {
 			fn eq(&self, other: &Self) -> bool {
-				for i in 0..$size {
-					if self.0[i] != other.0[i] {
-						return false;
-					}
-				}
-				true
+				unsafe { memcmp(self.0.as_ptr() as *const i8, other.0.as_ptr() as *const i8, $size) == 0 }
 			}
 		}
 
 		impl Ord for $from {
 			fn cmp(&self, other: &Self) -> Ordering {
-				for i in 0..$size {
-					if self.0[i] > other.0[i] {
-						return Ordering::Greater;
-					} else if self.0[i] < other.0[i] {
-						return Ordering::Less;
-					}
-				}
-				Ordering::Equal
+				let r = unsafe { memcmp(self.0.as_ptr() as *const i8, other.0.as_ptr() as *const i8, $size) };
+				if r < 0 { return Ordering::Less }
+				if r > 0 { return Ordering::Greater }
+				return Ordering::Equal;
 			}
 		}
 
