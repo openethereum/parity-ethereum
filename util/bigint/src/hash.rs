d5f4b5188902394aad5d51a8089503931db425b1
arkpar <arkady.paronyan@gmail.com>
2017-01-12 14:37:53 +0100

    Use libc

--- util/bigint/src/hash.rs
+++ util/bigint/src/hash.rs
@@ -26,6 +26,7 @@ use rand::Rng;
 use rand::os::OsRng;
 use rustc_serialize::hex::{FromHex, FromHexError};
 use bigint::{Uint, U256};
+use libc::{c_void, memcmp};
 
 /// Trait for a fixed-size byte array to be used as the output of hash functions.
 pub trait FixedHash: Sized {
@@ -61,8 +62,6 @@ pub fn clean_0x(s: &str) -> &str {
 		s
 	}
 }
-
-extern { fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32; }
 
 macro_rules! impl_hash {
 	($from: ident, $size: expr) => {
@@ -216,13 +215,13 @@ macro_rules! impl_hash {
 
 		impl PartialEq for $from {
 			fn eq(&self, other: &Self) -> bool {
-				unsafe { memcmp(self.0.as_ptr() as *const i8, other.0.as_ptr() as *const i8, $size) == 0 }
+				unsafe { memcmp(self.0.as_ptr() as *const c_void, other.0.as_ptr() as *const c_void, $size) == 0 }
 			}
 		}
 
 		impl Ord for $from {
 			fn cmp(&self, other: &Self) -> Ordering {
-				let r = unsafe { memcmp(self.0.as_ptr() as *const i8, other.0.as_ptr() as *const i8, $size) };
+				let r = unsafe { memcmp(self.0.as_ptr() as *const c_void, other.0.as_ptr() as *const c_void, $size) };
 				if r < 0 { return Ordering::Less }
 				if r > 0 { return Ordering::Greater }
 				return Ordering::Equal;
