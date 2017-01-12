cc69ab56685aed11096ea2cdee6ecef92f67b3d6
Robert Habermeier <rphmeier@gmail.com>
2017-01-12 16:49:40 +0100

    refactor shared history pruning code in client

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -206,19 +206,6 @@ impl Client {
 			config.history
 		};
 
-		if let (Some(earliest), Some(latest)) = (state_db.journal_db().earliest_era(), state_db.journal_db().latest_era()) {
-			if latest > earliest && latest - earliest > history {
-				for era in earliest..(latest - history + 1) {
-					if state_db.journal_db().mem_used() <= config.history_mem { break }
-
-					trace!("Removing era {}", era);
-					let mut batch = DBTransaction::new(&db);
-					state_db.mark_canonical(&mut batch, era, &chain.block_hash(era).expect("Old block not found in the database"))?;
-					db.write(batch).map_err(ClientError::Database)?;
-				}
-			}
-		}
-
 		if !chain.block_header(&chain.best_block_hash()).map_or(true, |h| state_db.journal_db().contains(h.state_root())) {
 			warn!("State root not found for block #{} ({})", chain.best_block_number(), chain.best_block_hash().hex());
 		}
@@ -259,6 +246,13 @@ impl Client {
 			on_mode_change: Mutex::new(None),
 			registrar: Mutex::new(None),
 		});
+
+		{
+			let state_db = client.state_db.lock().boxed_clone();
+			let chain = client.chain.read();
+			client.prune_ancient(state_db, &chain)?;
+		}
+
 		if let Some(reg_addr) = client.additional_params().get("registrar").and_then(|s| Address::from_str(s).ok()) {
 			trace!(target: "client", "Found registrar at {}", reg_addr);
 			let weak = Arc::downgrade(&client);
@@ -555,26 +549,6 @@ impl Client {
 		let mut state = block.drain();
 
 		state.journal_under(&mut batch, number, hash).expect("DB commit failed");
-
-		// prune all ancient eras until we're below the memory target,
-		// but have at least the minimum number of states.
-		loop {
-			if state.journal_db().mem_used() <= self.config.history_mem { break }
-			match state.journal_db().earliest_era() {
-				Some(era) if era + self.history <= number => {
-					trace!(target: "client", "Pruning state for ancient era {}", era);
-					match chain.block_hash(era) {
-						Some(ancient_hash) => {
-							state.mark_canonical(&mut batch, era, &ancient_hash).expect("DB commit failed");
-						}
-						None =>
-							debug!(target: "client", "Missing expected hash for block {}", era),
-					}
-				}
-				_ => break, // means that every era is kept, no pruning necessary.
-			}
-		}
-
 		let route = chain.insert_block(&mut batch, block_data, receipts);
 		self.tracedb.read().import(&mut batch, TraceImportRequest {
 			traces: traces.into(),
@@ -590,7 +564,47 @@ impl Client {
 		self.db.read().write_buffered(batch);
 		chain.commit();
 		self.update_last_hashes(&parent, hash);
+
+		if let Err(e) = self.prune_ancient(state, &chain) {
+			warn!("Failed to prune ancient state data: {}", e);
+		}
+
 		route
+	}
+
+	// prune ancient states until below the memory limit or only the minimum amount remain.
+	fn prune_ancient(&self, mut state_db: StateDB, chain: &BlockChain) -> Result<(), ClientError> {
+		let number = match state_db.journal_db().latest_era() {
+			Some(n) => n,
+			None => return Ok(()),
+		};
+
+		// prune all ancient eras until we're below the memory target,
+		// but have at least the minimum number of states.
+		loop {
+			let needs_pruning = state_db.journal_db().is_pruned() &&
+				state_db.journal_db().mem_used() >= self.config.history_mem;
+
+			if !needs_pruning { break }
+			match state_db.journal_db().earliest_era() {
+				Some(era) if era + self.history <= number => {
+					trace!(target: "client", "Pruning state for ancient era {}", era);
+					match chain.block_hash(era) {
+						Some(ancient_hash) => {
+							let mut batch = DBTransaction::new(&self.db.read());
+							state_db.mark_canonical(&mut batch, era, &ancient_hash)?;
+							self.db.read().write_buffered(batch);
+							state_db.journal_db().flush();
+						}
+						None =>
+							debug!(target: "client", "Missing expected hash for block {}", era),
+					}
+				}
+				_ => break, // means that every era is kept, no pruning necessary.
+			}
+		}
+
+		Ok(())
 	}
 
 	fn update_last_hashes(&self, parent: &H256, hash: &H256) {
