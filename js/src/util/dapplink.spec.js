6a1395d5ba3357978992444e1ee4c88be6755009
Jaco Greeff <jaco@ethcore.io>
2017-01-18 12:26:00 +0100

    s/ethlink/dapplink/

--- js/src/util/dapplink.spec.js
+++ js/src/util/dapplink.spec.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { BASE_URL, decode, encode } from './ethlink';
+import { BASE_URL, decode, encode } from './dapplink';
 
 const TEST_TOKEN = 'token';
 const TEST_URL = 'https://parity.io';
