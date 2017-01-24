c1ab35e9418b7722c7df0c7b7cd2f15d4b8498c7
maciejhirsz <maciej.hirsz@gmail.com>
2017-01-24 18:25:54 +0100

    Check if interfaces are synced in JS spec tests

--- js/src/jsonrpc/index.spec.js
+++ js/src/jsonrpc/index.spec.js
@@ -14,10 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+import fs from 'fs';
+import path from 'path';
 import interfaces from './';
 import { Address, BlockNumber, Data, Hash, Integer, Quantity } from './types';
-
-const flatlist = {};
 
 function verifyType (obj) {
   if (typeof obj !== 'string') {
@@ -36,15 +36,63 @@ function verifyType (obj) {
   }
 }
 
+// Get a list of JSON-RPC from Rust trait source code
+function parseMethodsFromRust (source) {
+  // Matching the custom `rpc` attribute
+  const regex = /#\[rpc\((?:async\s*,\s*)?name\s*=\s*"([^"]+)"\)]/g;
+
+  const methods = [];
+
+  source.toString().replace(regex, (match, method) => {
+    methods.push(method);
+
+    return match;
+  });
+
+  return methods;
+}
+
+// Get a list of all JSON-RPC methods from all defined traits
+function getMethodsFromRustTraits () {
+  const traitsDir = path.join(__dirname, '../../../rpc/src/v1/traits');
+
+  return fs.readdirSync(traitsDir)
+    .filter((name) => (name !== 'mod.rs' && /\.rs$/.test(name)))
+    .map((name) => fs.readFileSync(path.join(traitsDir, name)))
+    .map(parseMethodsFromRust)
+    .reduce((a, b) => a.concat(b));
+}
+
+const rustMethods = {};
+
+getMethodsFromRustTraits().forEach((method) => {
+  const [group, name] = method.split('_');
+
+  rustMethods[group] = rustMethods[group] || {};
+  rustMethods[group][name] = true;
+});
+
+describe('Rust defined JSON-RPC methods', () => {
+  Object.keys(rustMethods).forEach((group) => {
+    describe(group, () => {
+      Object.keys(rustMethods[group]).forEach((name) => {
+        describe(`${group}_${name}`, () => {
+          it('has a defined JS interface', () => {
+            expect(rustMethods[group][name]).to.be.true;
+          });
+        });
+      });
+    });
+  });
+});
+
 describe('jsonrpc/interfaces', () => {
   Object.keys(interfaces).forEach((group) => {
     describe(group, () => {
       Object.keys(interfaces[group]).forEach((name) => {
         const method = interfaces[group][name];
 
-        flatlist[`${group}_${name}`] = true;
-
-        describe(name, () => {
+        describe(`${group}_${name}`, () => {
           it('has the correct interface', () => {
             expect(method.desc).to.be.a('string');
             expect(method.params).to.be.an('array');
