a58fad06a7955247e4f01dd98cd97bab2d82f71f
Maciej Hirsz <maciej.hirsz@gmail.com>
2017-01-24 22:02:52 +0100

    JSON-RPC interfaces with documentation (#4276)
    * Extended Markdown generator
    
    * Synced and extended all JSON-RPC interfaces
    
    * Fix linter errors
    
    * Format `parity_listAccounts` output in API
    
    * typo

--- js/src/jsonrpc/interfaces/web3.js
+++ js/src/jsonrpc/interfaces/web3.js
@@ -15,6 +15,7 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Data } from '../types';
+import { withComment } from '../helpers';
 
 export default {
   clientVersion: {
@@ -22,21 +23,24 @@ export default {
     params: [],
     returns: {
       type: String,
-      desc: 'The current client version'
+      desc: 'The current client version',
+      example: 'Parity//v1.5.0-unstable-9db3f38-20170103/x86_64-linux-gnu/rustc1.14.0'
     }
   },
 
   sha3: {
-    desc: 'Returns Keccak-256 (*not* the standardized SHA3-256) of the given data.',
+    desc: 'Returns Keccak-256 (**not** the standardized SHA3-256) of the given data.',
     params: [
       {
         type: String,
-        desc: 'The data to convert into a SHA3 hash'
+        desc: 'The data to convert into a SHA3 hash.',
+        example: withComment('0x68656c6c6f20776f726c64', '"hello world"')
       }
     ],
     returns: {
       type: Data,
-      desc: 'The SHA3 result of the given string'
+      desc: 'The Keccak-256 hash of the given string.',
+      example: '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad'
     }
   }
 };
