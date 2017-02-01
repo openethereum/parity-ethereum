04fb2afba1151d0be32b1262a8ed03d2e1a05eba
Jaco Greeff <jacogr@gmail.com>
2017-02-01 14:52:15 +0100

    Add parity_defaultAccount RPC (with subscription) (#4383)
    * Add parity_defaultAccount RPC (with subscription)
    
    * Add jsonrpc interface

--- js/src/api/subscriptions/personal.spec.js
+++ js/src/api/subscriptions/personal.spec.js
@@ -18,18 +18,20 @@ import sinon from 'sinon';
 
 import Personal from './personal';
 
+const TEST_DEFAULT = '0xfa64203C044691aA57251aF95f4b48d85eC00Dd5';
 const TEST_INFO = {
-  '0xfa64203C044691aA57251aF95f4b48d85eC00Dd5': {
+  [TEST_DEFAULT]: {
     name: 'test'
   }
 };
-const TEST_LIST = ['0xfa64203C044691aA57251aF95f4b48d85eC00Dd5'];
+const TEST_LIST = [TEST_DEFAULT];
 
 function stubApi (accounts, info) {
   const _calls = {
     accountsInfo: [],
     allAccountsInfo: [],
-    listAccounts: []
+    listAccounts: [],
+    defaultAccount: []
   };
 
   return {
@@ -45,6 +47,12 @@ function stubApi (accounts, info) {
         const stub = sinon.stub().resolves(info || TEST_INFO)();
 
         _calls.allAccountsInfo.push(stub);
+        return stub;
+      },
+      defaultAccount: () => {
+        const stub = sinon.stub().resolves(Object.keys(info || TEST_INFO)[0])();
+
+        _calls.defaultAccount.push(stub);
         return stub;
       }
     },
@@ -107,9 +115,10 @@ describe('api/subscriptions/personal', () => {
       });
 
       it('updates subscribers', () => {
-        expect(cb.firstCall).to.have.been.calledWith('eth_accounts', null, TEST_LIST);
-        expect(cb.secondCall).to.have.been.calledWith('parity_accountsInfo', null, TEST_INFO);
-        expect(cb.thirdCall).to.have.been.calledWith('parity_allAccountsInfo', null, TEST_INFO);
+        expect(cb).to.have.been.calledWith('parity_defaultAccount', null, TEST_DEFAULT);
+        expect(cb).to.have.been.calledWith('eth_accounts', null, TEST_LIST);
+        expect(cb).to.have.been.calledWith('parity_accountsInfo', null, TEST_INFO);
+        expect(cb).to.have.been.calledWith('parity_allAccountsInfo', null, TEST_INFO);
       });
     });
 
@@ -122,6 +131,10 @@ describe('api/subscriptions/personal', () => {
 
       it('sets the started status', () => {
         expect(personal.isStarted).to.be.true;
+      });
+
+      it('calls parity_defaultAccount', () => {
+        expect(api._calls.defaultAccount.length).to.be.ok;
       });
 
       it('calls personal_accountsInfo', () => {
