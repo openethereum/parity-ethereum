67b293217b5b9b7d80ee75cd71fed7d9bf97dadd
Jaco Greeff <jaco@ethcore.io>
2017-02-07 08:21:19 +0100

    Re-add opening correct url in tab

--- js/src/views/Home/Urls/urls.js
+++ js/src/views/Home/Urls/urls.js
@@ -127,7 +127,7 @@ export default class Urls extends Component {
     this.props.store.gotoUrl(url);
 
     if (extensionStore.hasExtension) {
-      window.open(url, '_blank');
+      window.open(this.props.store.currentUrl, '_blank');
     } else {
       router.push('/web');
     }
