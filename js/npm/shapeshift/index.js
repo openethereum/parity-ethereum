0760a85aeaabfb60459535f90e486dfa0a21a1a9
Jannis R <mail@jannisr.de>
2017-02-09 16:17:00 +0100

    export correctly under ES5, remove unnecessary deps

--- js/npm/shapeshift/index.js
+++ js/npm/shapeshift/index.js
@@ -14,4 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-module.exports = require('./dist');
+module.exports = require('./dist').default;
