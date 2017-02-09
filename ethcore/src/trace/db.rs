2e1f67f8f23515bb36181d227f61ec86ad91202d
Tomasz Drwięga <tomasz@parity.io>
2017-02-09 11:00:52 +0100

    VecDeque::from

--- ethcore/src/trace/db.rs
+++ ethcore/src/trace/db.rs
@@ -305,7 +305,7 @@ impl<T> TraceDatabase for TraceDB<T> where T: DatabaseExtras {
 	}
 
 	fn trace(&self, block_number: BlockNumber, tx_position: usize, trace_position: Vec<usize>) -> Option<LocalizedTrace> {
-		let trace_position_deq = trace_position.into_iter().collect::<VecDeque<usize>>();
+		let trace_position_deq = VecDeque::from(trace_position);
 		self.extras.block_hash(block_number)
 			.and_then(|block_hash| self.transactions_traces(&block_hash)
 				.and_then(|traces| traces.into_iter().nth(tx_position))
