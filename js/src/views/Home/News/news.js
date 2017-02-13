595861b9c0d7a5dcba6f3c52a069b655cd9125e0
Jaco Greeff <jaco@ethcore.io>
2017-02-13 16:29:57 +0100

    Add missing observer

--- js/src/views/Home/News/news.js
+++ js/src/views/Home/News/news.js
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 
@@ -25,6 +26,7 @@ import styles from './news.css';
 
 const VERSION_ID = '1';
 
+@observer
 export default class News extends Component {
   store = Store.get();
 
