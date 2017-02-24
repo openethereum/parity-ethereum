38cf7d2639de49b2c8f25dc767f83fb14361fbdc
Jaco Greeff <jacogr@gmail.com>
2017-02-24 12:44:38 +0100

    Linting updates (#4662)

--- js/src/i18n/nl/accounts.js
+++ js/src/i18n/nl/accounts.js
@@ -27,5 +27,5 @@ export default {
   tooltip: {
     actions: `voor de huidige weergave zijn koppelingen beschikbaar op de werkbalk voor snelle toegang: het uitvoeren van acties of het creëren van een nieuw item`,
     overview: `hier vind je een overzichtelijke weergave van je accounts, waarin je meta informatie kunt bewerken en transacties kunt uitvoeren en bekijken`
-   }
+  }
 };
