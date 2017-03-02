f59aa210499f873c7a716f00992f5c475f08d592
Svyatoslav Nikolsky <svyatonik@gmail.com>
2017-03-02 17:32:32 +0300

    added licenses

--- secret_store/src/key_server_cluster/encryption_session.rs
+++ secret_store/src/key_server_cluster/encryption_session.rs
@@ -1,3 +1,19 @@
+// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
 use std::collections::{BTreeSet, BTreeMap, VecDeque};
 use std::fmt::{Debug, Formatter, Error as FmtError};
 use std::sync::Arc;
@@ -1174,4 +1190,6 @@ mod tests {
 			assert_eq!(document_secret_plain, document_secret_decrypted);
 		}
 	}
+
+	// TODO: add test where some nodes are disqualified from session
 }
