9047b4b487e5e67ac5fbaf20ba79ad6476a31456
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2017-03-10 10:25:40 +0100

    Supress USB error message (#4839)

--- ethcore/src/account_provider/mod.rs
+++ ethcore/src/account_provider/mod.rs
@@ -152,7 +152,7 @@ impl AccountProvider {
 					manager.set_key_path(if settings.hardware_wallet_classic_key { KeyPath::EthereumClassic } else { KeyPath::Ethereum });
 					hardware_store = Some(manager)
 				},
-				Err(e) => warn!("Error initializing hardware wallets: {}", e),
+				Err(e) => debug!("Error initializing hardware wallets: {}", e),
 			}
 		}
 		AccountProvider {
