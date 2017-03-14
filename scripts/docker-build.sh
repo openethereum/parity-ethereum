1959f0ae125d401e6b0e33db3c26fdbd577f6388
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2017-03-14 20:21:51 +0300

    fix docker-build
    [ci skip]

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd docker/hub
-docker build --build-arg BUILD_TAG=$1 --no-cache=true --tag ethcore/parity:$1 .
+if [ "$1" == "latest" ]; then DOCKER_BUILD_TAG="beta-release"; fi
+docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag ethcore/parity:$1 .
 docker push ethcore/parity:$1
