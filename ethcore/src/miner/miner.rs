131aa14afa3f7417e38799010c22ad8f7938e45f
NikVolf <nikvolf@gmail.com>
2017-03-16 03:37:50 +0300

    ensure sealing work enabled if notifier registed

--- ethcore/src/miner/miner.rs
+++ ethcore/src/miner/miner.rs
@@ -234,7 +234,8 @@ pub struct Miner {
 impl Miner {
 	/// Push notifier that will handle new jobs
 	pub fn push_notifier(&self, notifier: Box<NotifyWork>) {
-		self.notifiers.write().push(notifier)
+		self.notifiers.write().push(notifier);
+		self.sealing_work.lock().enabled = true;
 	}
 
 	/// Creates new instance of miner Arc.
