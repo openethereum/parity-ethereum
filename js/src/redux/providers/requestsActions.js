a9ad911df3ca0e4274a98031792a91681e0baefa
Nicolas Gotchac <ngotchac@gmail.com>
2017-03-21 14:05:01 +0100

    Linting

--- js/src/redux/providers/requestsActions.js
+++ js/src/redux/providers/requestsActions.js
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import BigNumber from 'bignumber.js';
 import store from 'store';
 
 import { outTransaction } from '~/api/format/output';
