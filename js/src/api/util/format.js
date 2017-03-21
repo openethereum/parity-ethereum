3128b3f00647c45c06371d9f30adf4c2e6e8c8f6
Jaco Greeff <jaco@ethcore.io>
2017-03-21 13:17:08 +0100

    cleanupValue

--- js/src/api/util/format.js
+++ js/src/api/util/format.js
@@ -20,6 +20,38 @@ export function bytesToHex (bytes) {
   return '0x' + bytes.map((b) => ('0' + b.toString(16)).slice(-2)).join('');
 }
 
+export function cleanupValue (value, type) {
+  // TODO: make work with arbitrary depth arrays
+  if (value instanceof Array && type.match(/bytes[0-9]+/)) {
+    // figure out if it's an ASCII string hiding in there:
+    let ascii = '';
+
+    for (let index = 0, ended = false; index < value.length && ascii !== null; ++index) {
+      const val = value[index];
+
+      if (val === 0) {
+        ended = true;
+      } else {
+        ascii += String.fromCharCode(val);
+      }
+
+      if ((ended && val !== 0) || (!ended && (val < 32 || val >= 128))) {
+        ascii = null;
+      }
+    }
+
+    value = ascii === null
+      ? bytesToHex(value)
+      : ascii;
+  }
+
+  if (type.substr(0, 4) === 'uint' && +type.substr(4) <= 48) {
+    value = +value;
+  }
+
+  return value;
+}
+
 export function hexToBytes (hex) {
   const raw = toHex(hex).slice(2);
   const bytes = [];
