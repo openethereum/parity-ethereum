428aaf5fb7309fb59e9875aa22009df0269bdafb
Svyatoslav Nikolsky <svyatonik@gmail.com>
2017-03-24 10:47:12 +0300

    fixed compilation

--- parity/secretstore.rs
+++ parity/secretstore.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::sync::Arc;
 use dir::default_data_path;
 use ethcore::client::Client;
 use helpers::replace_home;
