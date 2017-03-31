e5e05972b0ed4f6d3fd3dfb4cf6e42c67a415344
Jaco Greeff <jaco@parity.io>
2017-03-31 11:01:21 +0200

    Add tests

--- js/src/dapps/dappreg/utils.js
+++ js/src/dapps/dappreg/utils.js
@@ -19,7 +19,7 @@ import { api } from './parity';
 export const INVALID_URL_HASH = '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-function isContentHash (url) {
+export function isContentHash (url) {
   return /^0x[0-9a-f]{64}/.test(url);
 }
 
