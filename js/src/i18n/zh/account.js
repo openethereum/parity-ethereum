f91f195f771210ca4af084bf0f6c488e099a8d87
Shaoping <shaopingzhang4310@gmail.com>
2017-04-02 00:21:54 +0800

    Update account.js

--- js/src/i18n/zh/account.js
+++ js/src/i18n/zh/account.js
@@ -16,24 +16,24 @@
 
 export default {
   button: {
-    delete: `delete`,
-    edit: `edit`,
-    faucet: `Kovan ETH`,
-    password: `password`,
+    delete: `delete删除`,
+    edit: `edit编辑`,
+    faucet: `Kovan ETH Kovan测试网络ETH`,
+    password: `password密码`,
     shapeshift: `shapeshift`,
-    transfer: `transfer`,
-    verify: `verify`
+    transfer: `transfer转账`,
+    verify: `verify确认`
   },
   hardware: {
-    confirmDelete: `Are you sure you want to remove the following hardware address from your account list?`
+    confirmDelete: `Are you sure you want to remove the following hardware address from your account list?你确定从你的账户列表中移除下面的硬件地址吗？`
   },
   header: {
-    outgoingTransactions: `{count} outgoing transactions`,
+    outgoingTransactions: `{count} outgoing transactions笔正在发生的转账`,
     uuid: `uuid: {uuid}`
   },
-  title: `Account Management`,
+  title: `Account Management账户管理`,
   transactions: {
-    poweredBy: `Transaction list powered by {etherscan}`,
-    title: `transactions`
+    poweredBy: `Transaction list powered by {etherscan}提供的交易列表`,
+    title: `transactions交易`
   }
 };
