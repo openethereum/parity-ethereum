47f2db25e2e90d7fd90c228217b89673c0921f22
Shaoping <shaopingzhang4310@gmail.com>
2017-04-09 20:25:50 +0800

    translated firstRun

--- js/src/i18n/zh/firstRun.js
+++ js/src/i18n/zh/firstRun.js
@@ -16,34 +16,37 @@
 
 export default {
   button: {
-    close: `Close`,
-    create: `Create`,
-    next: `Next`,
-    print: `Print Phrase`,
-    skip: `Skip`
+    close: `关闭`,//Close
+    create: `创建`,//Create
+    next: `下一步`,//Next
+    print: `打印词组`,//Print Phrase
+    skip: `跳过`//Skip
   },
   completed: {
-    congrats: `Congratulations! Your node setup has been completed successfully and you are ready to use the application.`,
-    next: `Next you will receive a walk-through of the available functions and the general application interface to get you up and running in record time.`
+    congrats: `恭喜！你的节点设置已经完成，你现在可以使用应用了。`,
+    //Congratulations! Your node setup has been completed successfully and you are ready to use the application.
+    next: `下面你将浏览可用的功能和通用的应用界面，让你最快速地使用Parity。`
+    //Next you will receive a walk-through of the available functions and the general application interface to get you up and running in record time.
   },
   title: {
-    completed: `completed`,
-    newAccount: `new account`,
-    recovery: `recovery`,
-    terms: `terms`,
-    welcome: `welcome`
+    completed: `完成`,//completed
+    newAccount: `新账户`,//new account
+    recovery: `恢复`,//recovery
+    terms: `条款`,//terms
+    welcome: `欢迎`//welcome
   },
   tnc: {
-    accept: `I accept these terms and conditions`
+    accept: `我接受这些条款和条件`//I accept these terms and conditions
   },
   welcome: {
-    description: `As part of a new installation, the next few steps will guide you through the process of setting up you Parity instance and your associated accounts. Our aim is to make it as simple as possible and to get you up and running in record-time, so please bear with us. Once completed you will have -`,
-    greeting: `Welcome to Parity, the fastest and simplest way to run your node.`,
-    next: `Click Next to continue your journey.`,
+    description: `作为初次安装的一部分，下面的几个步骤将指导你如何设置你的Parity和相关账户。我们的目标是使得使用Parity变得尽可能的简单，让你成功运行，所以请有点耐心。 一旦完成，你将拥有`,
+    //As part of a new installation, the next few steps will guide you through the process of setting up you Parity instance and your associated accounts.Our aim is to make it as simple as possible and to get you up and running in record-time, so please bear with us.Once completed you will have -
+    greeting: `欢迎使用Parity，这是运行以太坊节点的最快和最简单的方式。`,//Welcome to Parity, the fastest and simplest way to run your node.
+    next: `点击下一步继续`,//Click Next to continue your journey.
     step: {
-      account: `Created your first Parity account`,
-      privacy: `Understood our privacy policy & terms of operation`,
-      recovery: `Have the ability to recover your account`
+      account: `创建你的第一个Parity账户`,//Created your first Parity account
+      privacy: `理解你的隐私政策和运作条款`,//Understood our privacy policy & terms of operation
+      recovery: `有能力可以恢复你的账户`//Have the ability to recover your account
     }
   }
 };
