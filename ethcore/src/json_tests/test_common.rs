17da31754453bc1a90b90a3e11858cddedbda5af
arkpar <arkady.paronyan@gmail.com>
2017-04-10 16:21:57 +0200

    Allow dir name for a test

--- ethcore/src/json_tests/test_common.rs
+++ ethcore/src/json_tests/test_common.rs
@@ -15,9 +15,35 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 pub use util::*;
-use std::fs::File;
+use std::fs::{File, read_dir};
+use std::path::Path;
+use std::ffi::OsString;
 
-pub fn run_test_file(path: &str, runner: fn (json_data: &[u8]) -> Vec<String>) {
+pub fn run_test_path(p: &Path, skip: &[&'static str], runner: fn (json_data: &[u8]) -> Vec<String>) {
+	let path = Path::new(p);
+	let s: HashSet<OsString> = skip.iter().map(|s| {
+		let mut os: OsString = s.into();
+		os.push(".json");
+		os
+	}).collect();
+	if path.is_dir() {
+		for p in read_dir(path).unwrap().filter_map(|e| {
+			let e = e.unwrap();
+			if s.contains(&e.file_name()) {
+				None
+			} else {
+				Some(e.path())
+			}}) {
+			run_test_path(&p, skip, runner)
+		}
+	} else {
+		let mut path = p.to_path_buf();
+		path.set_extension("json");
+		run_test_file(&path, runner)
+	}
+}
+
+pub fn run_test_file(path: &Path, runner: fn (json_data: &[u8]) -> Vec<String>) {
 	let mut data = Vec::new();
 	let mut file = File::open(&path).expect("Error opening test file");
 	file.read_to_end(&mut data).expect("Error reading test file");
@@ -26,19 +52,26 @@ pub fn run_test_file(path: &str, runner: fn (json_data: &[u8]) -> Vec<String>) {
 }
 
 macro_rules! test {
-	($name: expr) => {
-		::json_tests::test_common::run_test_file(concat!("res/ethereum/tests/", $name, ".json"), do_json_test);
+	($name: expr, $skip: expr) => {
+		::json_tests::test_common::run_test_path(::std::path::Path::new(concat!("res/ethereum/tests/", $name)), &$skip, do_json_test);
 	}
 }
 
 #[macro_export]
 macro_rules! declare_test {
+	(skip => $arr: expr, $id: ident, $name: expr) => {
+		#[test]
+		#[allow(non_snake_case)]
+		fn $id() {
+			test!($name, $arr);
+		}
+	};
 	(ignore => $id: ident, $name: expr) => {
 		#[ignore]
 		#[test]
 		#[allow(non_snake_case)]
 		fn $id() {
-			test!($name);
+			test!($name, []);
 		}
 	};
 	(heavy => $id: ident, $name: expr) => {
@@ -46,14 +79,14 @@ macro_rules! declare_test {
 		#[test]
 		#[allow(non_snake_case)]
 		fn $id() {
-			test!($name);
+			test!($name, []);
 		}
 	};
 	($id: ident, $name: expr) => {
 		#[test]
 		#[allow(non_snake_case)]
 		fn $id() {
-			test!($name);
+			test!($name, []);
 		}
 	}
 }
