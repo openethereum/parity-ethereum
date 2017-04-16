e43d33a20e9a485ee240bcbdb0a94a2bb46c228d
Shaoping <shaopingzhang4310@gmail.com>
2017-04-16 17:06:29 +0800

    translated accounts.js

--- js/src/i18n/zh/accounts.js
+++ js/src/i18n/zh/accounts.js
@@ -25,7 +25,7 @@ export default {
   },
   title: `账户总览`,//Accounts Overview
   tooltip: {
-    actions: ``,
+    actions: `与当前视窗有关的操作可以在工具栏中快速被找到，不论是执行操作还是创建新项`,
     //actions relating to the current view are available on the toolbar for quick access, be it for performing actions or creating a new item
     overview: `你的账户很容易使用，使你可以编辑元信息、转账、查看交易和向账户充值`
     //your accounts are visible for easy access, allowing you to edit the meta information, make transfers, view transactions and fund the account
