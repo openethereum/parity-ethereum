4568300d9bda79692384ffd1706b0882f8e75d8a
Jaco Greeff <jaco@parity.io>
2017-04-20 10:34:00 +0200

    Use @parity/abi

--- js/src/api/contract/contract.js
+++ js/src/api/contract/contract.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import Abi from '~/abi';
+import Abi from '@parity/abi';
 
 let nextSubscriptionId = 0;
 
