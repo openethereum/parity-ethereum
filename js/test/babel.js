75d9fa3fe94ab044b799bc3ef7c6822d2913d9c4
Jaco Greeff <jaco@parity.io>
2017-04-20 11:56:13 +0200

    mocha babel ignores (@parity modules)

--- js/test/babel.js
+++ js/test/babel.js
@@ -14,4 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-require('babel-register')();
+require('babel-register')({
+  ignore: /node_modules\/(?!@parity\/(abi|api|jsonrpc))/
+});
