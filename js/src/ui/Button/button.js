5216c28356357674b2af590a2bdf445501aeaf4a
oconnorct1 <oconnorct1@gmail.com>
2017-04-24 23:32:33 -0400

    buttttooooonnnnnnnnnn

--- js/src/ui/Button/button.js
+++ js/src/ui/Button/button.js
@@ -15,9 +15,10 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import React, { Component, PropTypes } from 'react';
-import { FlatButton } from 'material-ui';
 
 import { nodeOrStringProptype } from '~/util/proptypes';
+
+import styles from './button.css';
 
 export default class Button extends Component {
   static propTypes = {
@@ -27,7 +28,9 @@ export default class Button extends Component {
     icon: PropTypes.node,
     label: nodeOrStringProptype(),
     onClick: PropTypes.func,
-    primary: PropTypes.bool
+    primary: PropTypes.bool,
+    before: nodeOrStringProptype(),
+    after: nodeOrStringProptype()
   }
 
   static defaultProps = {
@@ -35,18 +38,54 @@ export default class Button extends Component {
   }
 
   render () {
-    const { className, backgroundColor, disabled, icon, label, primary, onClick } = this.props;
+    let {
+      className,
+      backgroundColor,
+      disabled,
+      icon,
+      label,
+      onClick,
+      before,
+      after
+    } = this.props;
+
+    let style = { backgroundColor };
+
+    if (className) {
+      className = `${className} ${styles.customButton}`;
+    } else {
+      className = styles.customButton;
+    }
+    if (disabled) {
+      style = {
+        userSelect: 'none',
+        pointerEvents: 'none',
+        color: 'rgba(255, 255, 255, 0.298039)',
+        cursor: 'default'
+      };
+    }
+    if (icon) {
+      before = icon;
+    }
+    if (before && (!label)) {
+      label = before;
+      before = null;
+    }
 
     return (
-      <FlatButton
-        className={ className }
-        backgroundColor={ backgroundColor }
-        disabled={ disabled }
-        icon={ icon }
-        label={ label }
-        primary={ primary }
-        onTouchTap={ onClick }
-      />
+      <div className={ className } style={ style } onClick={ onClick }>
+        <div>
+          <span id={ styles.positionBefore }>
+            { before }
+          </span>
+          <span id={ styles.content }>
+            { label }
+          </span>
+          <span id={ styles.positionAfter }>
+            { after }
+          </span>
+        </div>
+      </div>
     );
   }
 }
