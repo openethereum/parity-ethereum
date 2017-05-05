da85a65e5637d041ea533b6b26bbb42f5c6784c5
Gav Wood <gavin@parity.io>
2017-05-05 17:35:05 +0200

    Update mod.rs

--- rpc/src/v1/impls/mod.rs
+++ rpc/src/v1/impls/mod.rs
@@ -42,7 +42,6 @@ pub use self::parity_accounts::ParityAccountsClient;
 pub use self::parity_set::ParitySetClient;
 pub use self::personal::PersonalClient;
 pub use self::pubsub::PubSubClient;
-pub use self::rpc::RpcClient;
 pub use self::signer::SignerClient;
 pub use self::signing::SigningQueueClient;
 pub use self::signing_unsafe::SigningUnsafeClient;
