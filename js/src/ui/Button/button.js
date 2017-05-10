66843a349d906af9ca6ed0dde1f62b5b7d7c7193
oconnorct1 <oconnorct1@gmail.com>
2017-05-09 21:08:05 -0400

    SemanticButton

--- js/src/ui/Button/button.js
+++ js/src/ui/Button/button.js
@@ -15,13 +15,13 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 import React, { PropTypes } from 'react';
-import { Button as ButtonUI } from 'semantic-ui-react';
+import { Button as SemanticButton } from 'semantic-ui-react';
 
 import { nodeOrStringProptype } from '@parity/shared/util/proptypes';
 
 export default function Button ({ active, animated, basic, className, color, disabled, fullWidth, icon, label, onClick, primary, size, toggle }) {
   return (
-    <ButtonUI
+    <SemanticButton
       active={ active }
       animated={ animated }
       basic={ basic }
