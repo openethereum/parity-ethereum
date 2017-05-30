fd3c8bced7c3ac43d7408704a8b9960ec378031d
Jaco Greeff <jacogr@gmail.com>
2017-05-30 13:41:30 +0200

    UI2 fixes for Signer popup (#5711)
    * Fix new origin shape
    
    * Pass input types through (<input /> tag)
    
    * Update build

--- js/src/shell/Signer/components/RequestOrigin/requestOrigin.js
+++ js/src/shell/Signer/components/RequestOrigin/requestOrigin.js
@@ -29,7 +29,10 @@ export default class RequestOrigin extends Component {
   static propTypes = {
     origin: PropTypes.shape({
       type: PropTypes.oneOf(['unknown', 'dapp', 'rpc', 'ipc', 'signer']),
-      details: PropTypes.string.isRequired
+      details: PropTypes.shape({
+        dapp: PropTypes.string,
+        session: PropTypes.string
+      }).isRequired
     }).isRequired
   };
 
@@ -126,7 +129,7 @@ export default class RequestOrigin extends Component {
     }
 
     if (origin.type === 'signer') {
-      return this.renderSigner(origin.details);
+      return this.renderSigner(origin.details.session || origin.details);
     }
   }
 
