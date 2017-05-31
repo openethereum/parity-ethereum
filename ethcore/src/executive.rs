6c0b1656333f8833b7ca5f43087d972a6d79a512
NikVolf <nikvolf@gmail.com>
2017-05-31 17:11:29 +0300

    fix wasm magic header

--- ethcore/src/executive.rs
+++ ethcore/src/executive.rs
@@ -34,6 +34,8 @@ pub use types::executed::{Executed, ExecutionResult};
 /// Maybe something like here: `https://github.com/ethereum/libethereum/blob/4db169b8504f2b87f7d5a481819cfb959fc65f6c/libethereum/ExtVM.cpp`
 const STACK_SIZE_PER_DEPTH: usize = 24*1024;
 
+const WASM_MAGIC_NUMBER: [u8; 4] = [0x00, 0x61, 0x73, 0x6d];
+
 /// Returns new address created from address, nonce, and code hash
 pub fn contract_address(address_scheme: CreateContractAddress, sender: &Address, nonce: &U256, code_hash: &H256) -> Address {
 	use rlp::RlpStream;
@@ -73,7 +75,7 @@ pub struct TransactOptions {
 pub fn executor<E>(engine: &E, vm_factory: &Factory, params: &ActionParams) 
 	-> Box<evm::Evm> where E: Engine + ?Sized
 {
-	if engine.supports_wasm() && params.code.as_ref().map_or(false, |code| code.len() > 4 && &code[0..4] == b"wasm") {
+	if engine.supports_wasm() && params.code.as_ref().map_or(false, |code| code.len() > 4 && &code[0..4] == &WASM_MAGIC_NUMBER) {
 		Box::new(
 			wasm::WasmInterpreter::new()
 				// prefer to fail fast
