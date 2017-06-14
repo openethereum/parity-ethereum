f75c75ec633f1dd527c5d365b7d8e974bcb378f2
Jaco Greeff <jaco@parity.io>
2017-06-14 16:46:13 +0200

    Lenient provider checks

--- js/src/api/api.spec.js
+++ js/src/api/api.spec.js
@@ -22,18 +22,6 @@ import util from './util';
 import Api from './api';
 
 describe('api/Api', () => {
-  describe('constructor', () => {
-    it('requires defined/non-null provider object', () => {
-      expect(() => new Api()).to.throw(/Api needs provider/);
-      expect(() => new Api(null)).to.throw(/Api needs provider/);
-    });
-
-    it('requires an send function on the transport object', () => {
-      expect(() => new Api({})).to.throw(/Api needs provider/);
-      expect(() => new Api({ send: true })).to.throw(/Api needs provider/);
-    });
-  });
-
   describe('interface', () => {
     const api = new Api(new Api.Provider.Http(TEST_HTTP_URL, -1));
 
