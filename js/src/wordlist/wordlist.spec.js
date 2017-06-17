df5f19915e94ef14cd9c8c1b89159450dadc51a2
Craig OConnor <oconnorct1@gmail.com>
2017-06-17 14:35:29 -0600

    linting

--- js/src/wordlist/wordlist.spec.js
+++ js/src/wordlist/wordlist.spec.js
@@ -3,10 +3,10 @@ import { expect } from 'chai';
 import { randomBytes, randomNumber, randomWord, randomPhrase } from './';
 
 describe('wordlist', () => {
-
   describe('Random Bytes', () => {
     it('creates random bytes of specified length', () => {
       const bytes = randomBytes(12);
+
       expect(bytes.length).to.equal(12);
     });
   });
@@ -14,6 +14,7 @@ describe('wordlist', () => {
   describe('Random number', () => {
     it('creates random number', () => {
       const number = randomNumber();
+
       expect(typeof number === 'number').to.be.true;
     });
   });
@@ -21,15 +22,18 @@ describe('wordlist', () => {
   describe('Random word', () => {
     it('creates random bytes', () => {
       const word = randomWord();
+
       expect(typeof word === 'string').to.be.true;
     });
   });
 
   describe('Random phrase', () => {
     const phrase = randomPhrase(12).split(' ');
+
     it('creates random phrase of specified length', () => {
       expect(phrase.length).to.equal(12);
     });
+    
     it('creates random phrase that was converted to array', () => {
       expect(Array.isArray(phrase)).to.be.true;
     });
