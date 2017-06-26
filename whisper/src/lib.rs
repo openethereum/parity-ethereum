5c8a37434388630b386bb90d49985942fe058864
Robert Habermeier <rphmeier@gmail.com>
2017-06-26 20:34:18 +0200

    rallying and message logic

--- whisper/src/lib.rs
+++ whisper/src/lib.rs
@@ -33,7 +33,7 @@ use std::sync::mpsc;
 use std::time::{self, Duration, SystemTime};
 
 use bigint::hash::{H256, H512};
-use network::{NetworkContext, NetworkError, PeerId, TimerToken};
+use network::{NetworkContext, NetworkError, NodeId, PeerId, TimerToken};
 use parking_lot::{Mutex, RwLock};
 use rlp::{DecoderError, RlpStream, UntrustedRlp};
 use smallvec::SmallVec;
@@ -41,6 +41,11 @@ use tiny_keccak::keccak256;
 
 const RALLY_TOKEN: TimerToken = 1;
 const RALLY_TIMEOUT_MS: u64 = 750; // supposed to be at least once per second.
+
+const PROTOCOL_VERSION: usize = 2;
+
+// maximum tolerated delay between messages packets.
+const MAX_TOLERATED_DELAY_MS: u64 = 2000;
 
 mod packet {
 	pub const STATUS: u8 = 0;
@@ -52,8 +57,28 @@ mod packet {
 struct Topic([u8; 4]);
 
 impl Topic {
-	fn bloom(&self) -> H512 {
-		unimplemented!()
+	// set up to three bits in the 64-byte bloom passed.
+	//
+	// this takes 3 sets of 9 bits, treating each as an index in the range
+	// 0..512 into the bloom and setting the corresponding bit in the bloom to 1.
+	fn bloom_into(&self, bloom: &mut H512) {
+		let mut set_bit = |idx: usize| {
+			let idx = idx & 511;
+			bloom[idx / 8] |= 1 << idx % 8;
+		};
+
+		let data = &self.0;
+		let mut combined = ((data[0] as usize) << 24) |
+			((data[1] as usize) << 16) |
+			((data[2] as usize) << 8) |
+			data[3] as usize;
+
+		// take off the last 5 bits as we only use 27.
+		combined >>= 5;
+
+		set_bit(combined);
+		set_bit(combined >> 9);
+		set_bit(combined >> 18);
 	}
 }
 
@@ -119,9 +144,41 @@ struct Message {
 	envelope: Envelope,
 	bloom: H512,
 	hash: H256,
+	encoded_size: usize,
 }
 
 impl Message {
+	// decode and check for validity.
+	fn decode(rlp: UntrustedRlp) -> Result<Self, Error> {
+		let envelope: Envelope = rlp.as_val()?;
+		if envelope.expiry <= envelope.ttl { return Err(Error::LivesTooLong) }
+
+		// other validity checks?
+		let mut bloom = H512::default();
+		for topic in &envelope.topics {
+			topic.bloom_into(&mut bloom);
+		}
+
+		let hash = keccak256(rlp.as_raw());
+
+		Ok(Message {
+			envelope: envelope,
+			bloom: bloom,
+			hash: H256(hash),
+			encoded_size: rlp.as_raw().len(),
+		})
+	}
+
+	// get a reference to the envelope.
+	fn envelope(&self) -> &Envelope {
+		&self.envelope
+	}
+
+	// Get the encoded size of the envelope.
+	fn encoded_size(&self) -> usize {
+		self.encoded_size
+	}
+
 	fn expiry(&self) -> SystemTime {
 		time::UNIX_EPOCH + Duration::from_secs(self.envelope.expiry)
 	}
@@ -134,6 +191,9 @@ enum Error {
 	LivesTooLong,
 	Network(NetworkError),
 	UnknownPacket(u8),
+	UnknownPeer(PeerId),
+	ProtocolVersionMismatch(usize),
+	UnexpectedMessage,
 }
 
 impl From<DecoderError> for Error {
@@ -155,6 +215,10 @@ impl fmt::Display for Error {
 			Error::LivesTooLong => write!(f, "Message claims to be issued before the unix epoch."),
 			Error::Network(ref err) => write!(f, "Network error: {}", err),
 			Error::UnknownPacket(ref id) => write!(f, "Unknown packet kind: {}", id),
+			Error::UnknownPeer(ref id) => write!(f, "Message received from unknown peer: {}", id),
+			Error::ProtocolVersionMismatch(ref proto) =>
+				write!(f, "Unknown protocol version: {}", proto),
+			Error::UnexpectedMessage => write!(f, "Unexpected message."),
 		}
 	}
 }
@@ -199,8 +263,9 @@ impl Messages {
 	}
 
 	// prune expired messages.
-	fn prune_expired(&mut self, now: &SystemTime) {
+	fn prune_expired(&mut self, now: &SystemTime) -> Vec<Message> {
 		let mut expired_times = Vec::new();
+		let mut expired_messages = Vec::new();
 
 		{
 			let expired = self.by_expiry.iter()
@@ -213,16 +278,69 @@ impl Messages {
 			for expired_id in expired {
 				let message = self.slab.remove(expired_id).expect("only live ids are kept; qed");
 				self.known.remove(&message.hash);
+
+				expired_messages.push(message);
 			}
 		}
 
 		for expired_time in expired_times {
 			self.by_expiry.remove(&expired_time);
 		}
+
+		expired_messages
+	}
+
+	fn iter(&self) -> ::slab::Iter<Message, usize> {
+		self.slab.iter()
 	}
 }
 
-struct Peer;
+enum State {
+	Unconfirmed(SystemTime), // awaiting status packet.
+	TheirTurn(SystemTime), // it has been their turn to send since stored time.
+	OurTurn,
+}
+
+struct Peer {
+	node_key: NodeId,
+	state: State,
+	known_messages: HashSet<H256>,
+	topic_filter: Option<H512>,
+}
+
+impl Peer {
+	// note that a message has been evicted from the queue.
+	fn note_evicted(&mut self, messages: &[Message]) {
+		for message in messages {
+			self.known_messages.remove(&message.hash);
+		}
+	}
+
+	// whether this peer will accept the message.
+	fn will_accept(&self, message: &Message) -> bool {
+		let known = self.known_messages.contains(&message.hash);
+
+		let matches_bloom = self.topic_filter.as_ref()
+			.map_or(true, |topic| topic & &message.bloom == message.bloom);
+
+		!known && matches_bloom
+	}
+
+	fn note_known(&mut self, message: &Message) {
+		self.known_messages.insert(message.hash.clone());
+	}
+
+	fn set_topic_filter(&mut self, topic: H512) {
+		self.topic_filter = Some(topic);
+	}
+
+	fn can_send_messages(&self) -> bool {
+		match self.state {
+			State::Unconfirmed(_) | State::OurTurn => false,
+			State::TheirTurn(_) => true,
+		}
+	}
+}
 
 /// The whisper network protocol handler.
 pub struct Handler {
@@ -233,7 +351,10 @@ pub struct Handler {
 }
 
 impl Handler {
-	fn rally(&self) {
+	fn rally(&self, io: &NetworkContext) {
+		// cannot be greater than 16MB (protocol limitation)
+		const MAX_MESSAGES_PACKET_SIZE: usize = 8 * 1024 * 1024;
+
 		// accumulate incoming messages.
 		let incoming_messages: Vec<_> = self.incoming.lock().try_iter().collect();
 		let mut messages = self.messages.lock();
@@ -244,40 +365,165 @@ impl Handler {
 			messages.insert(message);
 		}
 
-		// 1. prune expired messages.
-		// 2. loop over peers and send messages that they haven't seen yet
-		unimplemented!()
+		let now = SystemTime::now();
+		let pruned_messages = messages.prune_expired(&now);
+
+		let peers = self.peers.read();
+
+		// send each peer a packet with new messages it may find relevant.
+		for (peer_id, peer) in peers.iter() {
+			let mut peer_data = peer.lock();
+			peer_data.note_evicted(&pruned_messages);
+
+			let punish_timeout = |last_activity: &SystemTime| {
+				if *last_activity + Duration::from_millis(MAX_TOLERATED_DELAY_MS) <= now {
+					debug!(target: "whisper", "Disconnecting peer {} due to excessive timeout.", peer_id);
+					io.disconnect_peer(*peer_id);
+				}
+			};
+
+			// check timeouts and skip peers who we can't send a rally to.
+			match peer_data.state {
+				State::Unconfirmed(ref time) | State::TheirTurn(ref time) => {
+					punish_timeout(time);
+					continue;
+				}
+				State::OurTurn => {}
+			}
+
+			// construct packet, skipping messages the peer won't accept.
+			let mut stream = RlpStream::new();
+			stream.begin_unbounded_list();
+
+			for message in messages.iter() {
+				if !peer_data.will_accept(message) { continue }
+
+				if stream.estimate_size(message.encoded_size()) > MAX_MESSAGES_PACKET_SIZE {
+					break;
+				}
+
+				peer_data.note_known(message);
+				stream.append(message.envelope());
+			}
+			stream.complete_unbounded_list();
+
+			peer_data.state = State::TheirTurn(SystemTime::now());
+			if let Err(e) = io.send(*peer_id, packet::MESSAGES, stream.out()) {
+				debug!(target: "whisper", "Failed to send messages packet to peer {}: {}", peer_id, e);
+				io.disconnect_peer(*peer_id);
+			}
+		}
 	}
 
 	// handle status packet from peer.
 	fn on_status(&self, io: &NetworkContext, peer: &PeerId, status: UntrustedRlp)
 		-> Result<(), Error>
 	{
+		let proto: usize = status.as_val()?;
+		if proto != PROTOCOL_VERSION { return Err(Error::ProtocolVersionMismatch(proto)) }
+
+		// TODO: compare peer node ID to ours to determine who should begin
+		// rallying.
+		let peers = self.peers.read();
+		match peers.get(peer) {
+			Some(peer) =>
+				peer.lock().state = State::TheirTurn(SystemTime::now()),
+			None => {
+				debug!(target: "whisper", "Received message from unknown peer.");
+				return Err(Error::UnknownPeer(*peer));
+			}
+		}
+
 		Ok(())
 	}
 
-	fn on_messages(&self, io: &NetworkContext, peer: &PeerId, messages: UntrustedRlp)
+	fn on_messages(&self, peer: &PeerId, messages: UntrustedRlp)
 		-> Result<(), Error>
 	{
 		let sender = self.async_sender.lock().clone();
 
-		// decode messages packet and put to message store.
-		// check for messages that match our "listeners"
-		// broadcast using async sender.
+		let peers = self.peers.read();
+		let peer = match peers.get(peer) {
+			Some(peer) => peer,
+			None => {
+				debug!(target: "whisper", "Received message from unknown peer.");
+				return Err(Error::UnknownPeer(*peer));
+			}
+		};
+
+		let mut peer = peer.lock();
+
+		if !peer.can_send_messages() {
+			return Err(Error::UnexpectedMessage);
+		}
+
+		peer.state = State::OurTurn;
+
+		for message_rlp in messages.iter() {
+			let message = Message::decode(message_rlp)?;
+			peer.note_known(&message);
+
+			// TODO: check whether the message matches our local filter criteria
+			// and pass payload to handler.
+
+			sender.send(message).expect("receiver always kept alive; qed");
+		}
+
 
 		Ok(())
 	}
 
-	fn on_topic_filter(&self, io: &NetworkContext, peer: &PeerId, filter: UntrustedRlp)
+	fn on_topic_filter(&self, peer: &PeerId, filter: UntrustedRlp)
 		-> Result<(), Error>
 	{
+		let peers = self.peers.read();
+		match peers.get(peer) {
+			Some(peer) => {
+				let mut peer = peer.lock();
+
+				if let State::Unconfirmed(_) = peer.state {
+					return Err(Error::UnexpectedMessage);
+				}
+
+				peer.set_topic_filter(filter.as_val()?)
+			}
+			None => {
+				debug!(target: "whisper", "Received message from unknown peer.");
+				return Err(Error::UnknownPeer(*peer));
+			}
+		}
+
 		Ok(())
 	}
 
 	fn on_connect(&self, io: &NetworkContext, peer: &PeerId) {
+		trace!(target: "whisper", "Connecting peer {}", peer);
+
+		let node_key = match io.session_info(*peer).and_then(|info| info.id) {
+			Some(node_key) => node_key,
+			None => {
+				debug!(target: "whisper", "Disconnecting peer {}, who has no node key.", peer);
+				io.disable_peer(*peer);
+				return;
+			}
+		};
+
+		self.peers.write().insert(*peer, Mutex::new(Peer {
+			node_key: node_key,
+			state: State::Unconfirmed(SystemTime::now()),
+			known_messages: HashSet::new(),
+			topic_filter: None,
+		}));
+
+		if let Err(e) = io.send(*peer, packet::STATUS, ::rlp::encode(&PROTOCOL_VERSION).to_vec()) {
+			debug!(target: "whisper", "Error sending status: {}", e);
+			io.disconnect_peer(*peer);
+		}
 	}
 
 	fn on_disconnect(&self, io: &NetworkContext, peer: &PeerId) {
+		trace!(target: "whisper", "Disconnecting peer {}", peer);
+		let _ = self.peers.write().remove(peer);
 	}
 }
 
@@ -292,8 +538,8 @@ impl ::network::NetworkProtocolHandler for Handler {
 		let rlp = UntrustedRlp::new(data);
 		let res = match packet_id {
 			packet::STATUS => self.on_status(io, peer, rlp),
-			packet::MESSAGES => self.on_messages(io, peer, rlp),
-			packet::TOPIC_FILTER => self.on_topic_filter(io, peer, rlp),
+			packet::MESSAGES => self.on_messages(peer, rlp),
+			packet::TOPIC_FILTER => self.on_topic_filter(peer, rlp),
 			other => Err(Error::UnknownPacket(other)),
 		};
 
@@ -305,13 +551,19 @@ impl ::network::NetworkProtocolHandler for Handler {
 
 	fn connected(&self, io: &NetworkContext, peer: &PeerId) {
 		// peer with higher ID should begin rallying.
+		self.on_connect(io, peer)
 	}
 
 	fn disconnected(&self, io: &NetworkContext, peer: &PeerId) {
+		self.on_disconnect(io, peer)
 	}
 
 	fn timeout(&self, io: &NetworkContext, timer: TimerToken) {
 		// rally with each peer and handle timeouts.
+		match timer {
+			RALLY_TOKEN => self.rally(io),
+			other => debug!(target: "whisper", "Timout triggered on unknown token {}", other),
+		}
 	}
 }
 
