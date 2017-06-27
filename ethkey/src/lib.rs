32575c6d79a3213b1162d945e6e7333c37b9ae19
Robert Habermeier <rphmeier@gmail.com>
2017-06-27 19:18:33 +0200

    associated error type for ethkey generators and `OsRng` generator

--- ethkey/src/lib.rs
+++ ethkey/src/lib.rs
@@ -38,10 +38,16 @@ lazy_static! {
 	pub static ref SECP256K1: secp256k1::Secp256k1 = secp256k1::Secp256k1::new();
 }
 
+/// Uninstantiatable error type for infallible generators.
+#[derive(Debug)]
+pub enum Void {}
+
 /// Generates new keypair.
 pub trait Generator {
+	type Error;
+
 	/// Should be called to generate new keypair.
-	fn generate(self) -> Result<KeyPair, Error>;
+	fn generate(self) -> Result<KeyPair, Self::Error>;
 }
 
 pub mod math;
