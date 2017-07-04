f0526ddf52815b243168514f93b3aae1cea7ca91
Jaco Greeff <jaco@ethcore.io>
2017-07-04 14:13:45 +0200

    Allow display of boolean/false values

--- js/src/ui/Form/Input/input.js
+++ js/src/ui/Form/Input/input.js
@@ -113,6 +113,9 @@ export default class Input extends Component {
   render () {
     const { children, className, defaultValue, disabled, error, hint, label, max, min, onClick, readOnly, step, style, tabIndex, type } = this.props;
     const { value } = this.state;
+    const displayValue = typeof value !== 'undefined'
+      ? value
+      : defaultValue;
 
     return (
       <LabelWrapper
@@ -141,12 +144,10 @@ export default class Input extends Component {
           step={ step }
           style={ style }
           tabIndex={ tabIndex }
-          value={ parseI18NString(this.context, value || defaultValue) }
+          value={ parseI18NString(this.context, `${displayValue}`) }
         >
           { this.renderCopyButton() }
-          <input
-            type={ type }
-          />
+          <input type={ type } />
           { children }
         </SemanticInput>
       </LabelWrapper>
