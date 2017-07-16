1ce8ac14429b24efbefc0d05aa8043d1099ccfe7
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2017-07-16 03:21:54 +0200

    User_defaults now contains serialization for is_first_launch parameter; Solved the "TraceDB resync required" issue when trying to use "--tracing on" option just after a DB kill operation;

--- parity/user_defaults.rs
+++ parity/user_defaults.rs
@@ -41,6 +41,7 @@ impl Serialize for UserDefaults {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
 	where S: Serializer {
 		let mut map: BTreeMap<String, Value> = BTreeMap::new();
+		map.insert("is_first_launch".into(), Value::Bool(self.is_first_launch));
 		map.insert("pruning".into(), Value::String(self.pruning.as_str().into()));
 		map.insert("tracing".into(), Value::Bool(self.tracing));
 		map.insert("fat_db".into(), Value::Bool(self.fat_db));
