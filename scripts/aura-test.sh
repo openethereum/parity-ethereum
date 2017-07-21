1585c1adc5cfec9f05eaae4043d8a50c298d7950
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2017-07-21 19:55:47 +0300

    Update aura-test.sh

--- scripts/aura-test.sh
+++ scripts/aura-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 rm -rf parity-import-tests/
+rm -rf /tmp/aura-test-data/
 cargo build -j $(nproc) --release
 git clone https://github.com/paritytech/parity-import-tests
 target/release/parity -v
 echo "Start Aura test"
-target/release/parity import parity-import-tests/aura/blocks.rlp --chain parity-import-tests/aura/chain.json
+target/release/parity import parity-import-tests/aura/blocks.rlp --chain parity-import-tests/aura/chain.json -d /tmp/aura-test-data
 if [ $? -eq 0 ]
 then
   echo "Import test passed"
@@ -12,7 +13,7 @@ else
   echo "Import test failed" >&2
   exit 1
 fi
-target/release/parity restore parity-import-tests/aura/snap --chain parity-import-tests/aura/chain.json
+target/release/parity restore parity-import-tests/aura/snap --chain parity-import-tests/aura/chain.json -d /tmp/aura-test-data
 if [ $? -eq 0 ]
 then
   echo "Restore test passed"
