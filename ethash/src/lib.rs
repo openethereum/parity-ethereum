0fa9c93f6f6ca6e3be580da8bb3e03f6315d6022
Vurich <jackefransham@hotmail.co.uk>
2017-07-22 14:49:02 +0200

    Style reungrumble

--- ethash/src/lib.rs
+++ ethash/src/lib.rs
@@ -14,10 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-#![cfg_attr(feature = "benches", feature(test))]
-
 //! Ethash implementation
 //! See https://github.com/ethereum/wiki/wiki/Ethash
+
+#![cfg_attr(feature = "benches", feature(test))]
+
 extern crate primal;
 extern crate sha3;
 extern crate parking_lot;
