17e6d2d51cec9e349768bf82125dbcb6e20b6038
Joseph Mark <joseph@mark.gd>
2017-07-22 11:09:43 +0700

    Descriptive error for invalid preset (#4673)

--- parity/cli/presets/mod.rs
+++ parity/cli/presets/mod.rs
@@ -23,6 +23,6 @@ pub fn preset_config_string(arg: &str) -> Result<&'static str, Error> {
         "non-standard-ports" => Ok(include_str!("./config.non-standard-ports.toml")),
         "insecure" => Ok(include_str!("./config.insecure.toml")),
         "dev-insecure" => Ok(include_str!("./config.dev-insecure.toml")),
-        _ => Err(Error::new(ErrorKind::InvalidInput, arg.clone()))
+        _ => Err(Error::new(ErrorKind::InvalidInput, "Config doesn't match any presets [dev, mining, non-standard-ports, insecure, dev-insecure]"))
     }
 }
