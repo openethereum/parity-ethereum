648cc035cec26589970a14e8d12ae4b25776004d
Jaco Greeff <jaco@parity.io>
2017-07-26 17:55:14 +0200

    Enable request of new token from app

--- js/packages/api/provider/postMessage.js
+++ js/packages/api/provider/postMessage.js
@@ -18,14 +18,28 @@ export default class PostMessage {
   id = 0;
   _messages = {};
 
-  constructor (token, destination) {
+  constructor (appId, token, destination) {
+    this._appId = appId;
     this._token = token;
-    this._destination = destination;
+    this._destination = destination || window.parent;
 
     window.addEventListener('message', this.receiveMessage, false);
   }
 
   addMiddleware () {
+  }
+
+  requestNewToken () {
+    return new Promise((resolve, reject) => {
+      this.send('shell_requestNewToken', [this._appId], (error, token) => {
+        if (error) {
+          reject(error);
+        } else {
+          this._token = token;
+          resolve(token);
+        }
+      });
+    });
   }
 
   _send (data) {
@@ -38,9 +52,10 @@ export default class PostMessage {
     this._messages[id] = { callback };
     this._send({
       id,
-      from: this._token,
+      from: this._appId,
       method,
       params,
+      to: 'shell',
       token: this._token
     });
   }
@@ -53,9 +68,10 @@ export default class PostMessage {
       this._messages[id] = { callback, resolve, reject, subscription: true, initial: true };
       this._send({
         id,
-        from: this._token,
+        from: this._appId,
         api,
         params,
+        to: 'shell',
         token: this._token
       });
     });
@@ -68,8 +84,9 @@ export default class PostMessage {
       this._messages[id] = { callback: (e, v) => e ? reject(e) : resolve(v) };
       this._send({
         id,
-        from: this._token,
+        from: this._appId,
         subId,
+        to: 'shell',
         token: this._token
       });
     });
@@ -79,8 +96,12 @@ export default class PostMessage {
     return this.unsubscribe('*');
   }
 
-  receiveMessage = ({ data: { id, error, from, token, result }, origin, source }) => {
-    if (from !== 'shell' || token !== this._token) {
+  receiveMessage = ({ data: { id, error, from, to, token, result }, origin, source }) => {
+    const isTokenValid = token
+      ? token === this._token
+      : to === this._appId;
+
+    if (from !== 'shell' || to !== this._appId || !isTokenValid) {
       return;
     }
 
