3ea78bbbd68062d09873a4280fe221cf0a4f326f
fro <frol.rage@gmail.com>
2017-07-28 15:56:08 +0300

    fix path

--- ethcore/evm/src/wasm/tests.rs
+++ ethcore/evm/src/wasm/tests.rs
@@ -25,7 +25,7 @@ use byteorder::{LittleEndian, ByteOrder};
 
 macro_rules! load_sample {
 	($name: expr) => {
-		include_bytes!(concat!("../../../../../wasm-tests/compiled/", $name)).to_vec()
+		include_bytes!(concat!("../../../res/wasm-tests/compiled/", $name)).to_vec()
 	}
 }
 
