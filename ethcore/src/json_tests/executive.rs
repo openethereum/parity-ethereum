48f28fe29c180a8fe634bd0695650f0b9dc90fe9
debris <marek.kotewicz@gmail.com>
2017-07-29 23:19:33 +0200

    fixed json_tests

--- ethcore/src/json_tests/executive.rs
+++ ethcore/src/json_tests/executive.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::sync::Arc;
 use super::test_common::*;
 use evm::action_params::ActionParams;
 use state::{Backend as StateBackend, State, Substate};
