ffd53edf89fba14a1c5b2ea5c12c02a490f130bc
Jaco Greeff <jaco@parity.io>
2017-08-02 11:15:36 +0200

    Update build

--- js/src/FirstRun/firstRun.js
+++ js/src/FirstRun/firstRun.js
@@ -27,10 +27,10 @@ import { Button, Portal } from '@parity/ui';
 import { CheckIcon, DoneIcon, NextIcon, PrintIcon, ReplayIcon } from '@parity/ui/Icons';
 
 import ParityLogo from '@parity/shared/assets/images/parity-logo-black-no-text.svg';
-import { NewAccount, AccountDetails } from '@parity/dapp-accounts/CreateAccount';
-import print from '@parity/dapp-accounts/CreateAccount/print';
-import recoveryPage from '@parity/dapp-accounts/CreateAccount/recoveryPage.ejs';
-import CreateStore from '@parity/dapp-accounts/CreateAccount/store';
+import { NewAccount, AccountDetails } from '@parity/dapp-accounts/src/CreateAccount';
+import print from '@parity/dapp-accounts/src/CreateAccount/print';
+import recoveryPage from '@parity/dapp-accounts/src/CreateAccount/recoveryPage.ejs';
+import CreateStore from '@parity/dapp-accounts/src/CreateAccount/store';
 
 import Completed from './Completed';
 import TnC from './TnC';
