44a7c3d0b672a19cda6c6ab6026afac33f14075a
Jaco Greeff <jaco@parity.io>
2017-08-04 13:50:51 +0200

    Re-add modules path

--- js/webpack/app.js
+++ js/webpack/app.js
@@ -145,6 +145,9 @@ module.exports = {
   },
 
   resolve: {
+    modules: [
+      path.join(__dirname, '../node_modules')
+    ],
     extensions: ['.json', '.js', '.jsx'],
     unsafeCache: true
   },
