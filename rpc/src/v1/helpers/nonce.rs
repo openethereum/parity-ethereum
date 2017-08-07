5cdf4297cef3d1ed04cb3178ddd5a078ee662453
Joseph Mark <joseph@mark.gd>
2017-08-07 13:00:33 +0700

    Single thread nonce tests

--- rpc/src/v1/helpers/nonce.rs
+++ rpc/src/v1/helpers/nonce.rs
@@ -15,11 +15,11 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::sync::Arc;
-use futures::{future, Future, BoxFuture, Poll, Async};
+use futures::{Future, Poll, Async};
 use util::{U256, RwLock};
 use std::collections::{VecDeque};
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum NonceError {
     Dropped,
     InvalidPoll,
@@ -66,6 +66,7 @@ impl Future for Nonce {
                     while reserved.contains(&self.value) { // reserved by another thread
                         self.value = self.value + U256::one(); // increment prospective nonce
                     }
+                    drop(reserved);
                     // prospective nonce is available
                     if let Some(mut reserved) = self.reserved.try_write() {
                         reserved.push_back(self.value);
@@ -141,5 +142,55 @@ impl Nonce {
             NonceState::Dispatch => Ok(Async::Ready(self.value)),
             _ => Err(NonceError::InvalidPoll),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reserve() {
+        let mut reserved_inner = VecDeque::new();
+        reserved_inner.push_back(U256::from(0));
+        reserved_inner.push_back(U256::from(1));
+        let reserved = Arc::new(RwLock::new(reserved_inner));
+
+        let mut nonce_a = Nonce::start_with(U256::from(0), reserved.clone());
+        let mut nonce_b = Nonce::start_with(U256::from(3), reserved.clone());
+        let mut nonce_e = Nonce::start_with(U256::from(3), reserved.clone());
+        let mut nonce_l = Nonce::start_with(U256::from(3), reserved.clone());
+
+        let reserve_a = nonce_a.poll_reserve().ok().unwrap();
+        let reserve_b = nonce_b.poll_reserve().ok().unwrap();
+        let reserve_e = nonce_e.poll_dispatch().err().unwrap();
+
+        let read = reserved.read();
+        let reserve_l = nonce_l.poll_reserve().ok().unwrap();
+
+        assert_eq!(reserve_a, Async::Ready(U256::from(2)));
+        assert_eq!(reserve_b, Async::Ready(U256::from(3)));
+        assert_eq!(reserve_e, NonceError::InvalidPoll);
+        assert_eq!(reserve_l, Async::NotReady);
+    }
+
+    #[test]
+    fn dispatch() {
+        let mut reserved = Arc::new(RwLock::new(VecDeque::new()));
+        let mut nonce = Nonce::start_with(U256::from(55), reserved.clone());
+        let mut nonce_l = Nonce::start_with(U256::from(55), reserved.clone());
+        
+        let _ = nonce.poll_reserve();
+        let _ = nonce_l.poll_reserve();
+
+        let dispatch = nonce.poll_dispatch().ok().unwrap();
+        let dispatch_e = nonce.poll_reserve().err().unwrap();
+
+        let read = reserved.read();
+        let dispatch_l = nonce_l.poll_dispatch().ok().unwrap();
+
+        assert_eq!(dispatch, Async::Ready(U256::from(55)));
+        assert_eq!(dispatch_e, NonceError::InvalidPoll);
+        assert_eq!(dispatch_l, Async::NotReady);
     }
 }
