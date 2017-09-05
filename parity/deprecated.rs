be745f711f069a629b5a9dc76f05b8a166de0eec
Axel Chalon <tnf_chordmaster@hotmail.fr>
2017-09-05 13:30:22 +0200

    Refactor and port CLI from Docopt to Clap (#2066) (#6356)
    * Refactor and port CLI from Docopt to Clap (#2066)
    
    * Add --can-restart and --force-direct to help
    
    * Add flag support to subc & move import/export options to subcommand
    
    * Reorder subcommand args (put positional args last in CLI help message)

--- parity/deprecated.rs
+++ parity/deprecated.rs
@@ -65,40 +65,40 @@ pub fn find_deprecated(args: &Args) -> Vec<Deprecated> {
 		result.push(Deprecated::Replaced("--ipc-off", "--no-ipc"));
 	}
 
-	if args.flag_etherbase.is_some() {
+	if args.arg_etherbase.is_some() {
 		result.push(Deprecated::Replaced("--etherbase", "--author"));
 	}
 
-	if args.flag_extradata.is_some() {
+	if args.arg_extradata.is_some() {
 		result.push(Deprecated::Replaced("--extradata", "--extra-data"));
 	}
 
 	// Removed in 1.7
-	if args.flag_dapps_port.is_some() {
+	if args.arg_dapps_port.is_some() {
 		result.push(Deprecated::Replaced("--dapps-port", "--jsonrpc-port"));
 	}
 
-	if args.flag_dapps_interface.is_some() {
+	if args.arg_dapps_interface.is_some() {
 		result.push(Deprecated::Replaced("--dapps-interface", "--jsonrpc-interface"));
 	}
 
-	if args.flag_dapps_hosts.is_some() {
+	if args.arg_dapps_hosts.is_some() {
 		result.push(Deprecated::Replaced("--dapps-hosts", "--jsonrpc-hosts"));
 	}
 
-	if args.flag_dapps_cors.is_some() {
+	if args.arg_dapps_cors.is_some() {
 		result.push(Deprecated::Replaced("--dapps-cors", "--jsonrpc-cors"));
 	}
 
-	if args.flag_dapps_user.is_some() {
+	if args.arg_dapps_user.is_some() {
 		result.push(Deprecated::Removed("--dapps-user"));
 	}
 
-	if args.flag_dapps_pass.is_some() {
+	if args.arg_dapps_pass.is_some() {
 		result.push(Deprecated::Removed("--dapps-pass"));
 	}
 
-	if args.flag_dapps_apis_all.is_some() {
+	if args.flag_dapps_apis_all {
 		result.push(Deprecated::Replaced("--dapps-apis-all", "--jsonrpc-apis"));
 	}
 
@@ -124,15 +124,15 @@ mod tests {
 			args.flag_dapps_off = true;
 			args.flag_ipcdisable = true;
 			args.flag_ipc_off = true;
-			args.flag_etherbase = Some(Default::default());
-			args.flag_extradata = Some(Default::default());
-			args.flag_dapps_port = Some(Default::default());
-			args.flag_dapps_interface = Some(Default::default());
-			args.flag_dapps_hosts = Some(Default::default());
-			args.flag_dapps_cors = Some(Default::default());
-			args.flag_dapps_user = Some(Default::default());
-			args.flag_dapps_pass = Some(Default::default());
-			args.flag_dapps_apis_all = Some(Default::default());
+			args.arg_etherbase = Some(Default::default());
+			args.arg_extradata = Some(Default::default());
+			args.arg_dapps_port = Some(Default::default());
+			args.arg_dapps_interface = Some(Default::default());
+			args.arg_dapps_hosts = Some(Default::default());
+			args.arg_dapps_cors = Some(Default::default());
+			args.arg_dapps_user = Some(Default::default());
+			args.arg_dapps_pass = Some(Default::default());
+			args.flag_dapps_apis_all = true;
 			args
 		}), vec![
 			Deprecated::DoesNothing("--jsonrpc"),
