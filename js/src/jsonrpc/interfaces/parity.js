e8d519daf31a85db790f0459b6da9cfe599cdc85
5chdn <5chdn@users.noreply.github.com>
2017-09-20 14:34:25 +0200

    Add musicoin to the wallet

--- js/src/jsonrpc/interfaces/parity.js
+++ js/src/jsonrpc/interfaces/parity.js
@@ -1746,7 +1746,7 @@ export default {
     params: [
       {
         type: String,
-        desc: 'Chain spec name, one of: "foundation", "ropsten", "morden", "kovan", "olympic", "classic", "dev", "expanse" or a filename.',
+        desc: 'Chain spec name, one of: "foundation", "ropsten", "morden", "kovan", "olympic", "classic", "dev", "expanse", "musicoin" or a filename.',
         example: 'foundation'
       }
     ],
