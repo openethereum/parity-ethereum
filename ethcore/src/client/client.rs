87f50fe23b823e9306c7d622d50e9ad20e2c7c48
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2017-09-21 10:11:53 +0200

    Fixed receipt serialization and RPC (#6555)

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -1978,7 +1978,7 @@ fn transaction_receipt(engine: &Engine, mut tx: LocalizedTransaction, mut receip
 			log_index: no_of_logs + i,
 		}).collect(),
 		log_bloom: receipt.log_bloom,
-		state_root: receipt.state_root,
+		outcome: receipt.outcome,
 	}
 }
 
@@ -2023,7 +2023,7 @@ mod tests {
 		use super::transaction_receipt;
 		use ethkey::KeyPair;
 		use log_entry::{LogEntry, LocalizedLogEntry};
-		use receipt::{Receipt, LocalizedReceipt};
+		use receipt::{Receipt, LocalizedReceipt, TransactionOutcome};
 		use transaction::{Transaction, LocalizedTransaction, Action};
 		use util::Hashable;
 		use tests::helpers::TestEngine;
@@ -2035,7 +2035,7 @@ mod tests {
 
 		let block_number = 1;
 		let block_hash = 5.into();
-		let state_root = Some(99.into());
+		let state_root = 99.into();
 		let gas_used = 10.into();
 		let raw_tx = Transaction {
 			nonce: 0.into(),
@@ -2063,14 +2063,12 @@ mod tests {
 			data: vec![],
 		}];
 		let receipts = vec![Receipt {
-			state_root: state_root,
-			status_code: None,
+			outcome: TransactionOutcome::StateRoot(state_root),
 			gas_used: 5.into(),
 			log_bloom: Default::default(),
 			logs: vec![logs[0].clone()],
 		}, Receipt {
-			state_root: state_root,
-			status_code: None,
+			outcome: TransactionOutcome::StateRoot(state_root),
 			gas_used: gas_used,
 			log_bloom: Default::default(),
 			logs: logs.clone(),
@@ -2106,7 +2104,7 @@ mod tests {
 				log_index: 2,
 			}],
 			log_bloom: Default::default(),
-			state_root: state_root,
+			outcome: TransactionOutcome::StateRoot(state_root),
 		});
 	}
 }
--- ethcore/src/state/mod.rs
+++ ethcore/src/state/mod.rs
@@ -22,7 +22,7 @@
 use std::cell::{RefCell, RefMut};
 use std::collections::hash_map::Entry;
 
-use receipt::Receipt;
+use receipt::{Receipt, TransactionOutcome};
 use engines::Engine;
 use evm::env_info::EnvInfo;
 use error::Error;
@@ -655,21 +655,19 @@ impl<B: Backend> State<B> {
 			eip658 ||
 			(env_info.number >= engine.params().eip98_transition && env_info.number >= engine.params().validate_receipts_transition);
 
-		let state_root = if no_intermediate_commits {
-			None
+		let outcome = if no_intermediate_commits {
+			if eip658 {
+				TransactionOutcome::StatusCode(if e.exception.is_some() { 0 } else { 1 })
+			} else {
+				TransactionOutcome::Unknown
+			}
 		} else {
 			self.commit()?;
-			Some(self.root().clone())
-		};
-
-		let status_byte = if eip658 {
-			Some(if e.exception.is_some() { 0 } else { 1 })
-		} else {
-			None
+			TransactionOutcome::StateRoot(self.root().clone())
 		};
 
 		let output = e.output;
-		let receipt = Receipt::new(state_root, status_byte, e.cumulative_gas_used, e.logs);
+		let receipt = Receipt::new(outcome, e.cumulative_gas_used, e.logs);
 		trace!(target: "state", "Transaction receipt: {:?}", receipt);
 
 		Ok(ApplyOutcome {
