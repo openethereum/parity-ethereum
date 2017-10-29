4b56f2530d561405d2f4cab899989f957aa38f03
arkpar <arkady.paronyan@gmail.com>
2017-10-29 13:35:12 +0100

    Don't create IPC dir on windows

--- parity/rpc.rs
+++ parity/rpc.rs
@@ -304,9 +304,11 @@ pub fn new_ipc<D: rpc_apis::Dependencies>(
 	let handler = setup_apis(conf.apis, dependencies);
 	let remote = dependencies.remote.clone();
 	let path = PathBuf::from(&conf.socket_addr);
-	if let Some(dir) = path.parent() {
-		::std::fs::create_dir_all(&dir)
-			.map_err(|err| format!("Unable to create IPC directory at {}: {}", dir.display(), err))?;
+	if !cfg!(windows) {
+		if let Some(dir) = path.parent() {
+			::std::fs::create_dir_all(&dir)
+				.map_err(|err| format!("Unable to create IPC directory at {}: {}", dir.display(), err))?;
+		}
 	}
 
 	match rpc::start_ipc(&conf.socket_addr, handler, remote, rpc::RpcExtractor) {
