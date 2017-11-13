0f2bfda8302dc352df786849374d0aafd5d2e561
Jaco Greeff <jacogr@gmail.com>
2017-11-13 11:52:34 +0100

    Fix v1 precompiled (#7033)
    * Combine cargo updates into one
    
    * Update log tag

--- js-old/scripts/push-precompiled.sh
+++ js-old/scripts/push-precompiled.sh
@@ -5,7 +5,6 @@ set -e
 UTCDATE=`date -u "+%Y%m%d-%H%M%S"`
 PRECOMPILED_BRANCH="v1"
 GIT_JS_PRECOMPILED="https://${GITHUB_JS_PRECOMPILED}:@github.com/paritytech/js-precompiled.git"
-GIT_PARITY="https://${GITHUB_JS_PRECOMPILED}:@github.com/paritytech/parity.git"
 
 # setup the git user defaults for the current repo
 function setup_git_user {
@@ -22,17 +21,17 @@ pushd $BASEDIR
 cd ../.dist
 
 # add local files and send it up
-echo "*** [v1] Setting up GitHub config for js-precompiled"
+echo "*** [v1 precompiled] Setting up GitHub config for js-precompiled"
 rm -rf ./.git
 git init
 setup_git_user
 
-echo "*** [v1] Checking out $PRECOMPILED_BRANCH branch"
+echo "*** [v1 precompiled] Checking out $PRECOMPILED_BRANCH branch"
 git remote add origin $GIT_JS_PRECOMPILED
 git fetch origin 2>$GITLOG
 git checkout -b $PRECOMPILED_BRANCH
 
-echo "*** [v1] Committing compiled files for $UTCDATE"
+echo "*** [v1 precompiled] Committing compiled files for $UTCDATE"
 mv build ../build.new
 git add .
 git commit -m "$UTCDATE [update]"
@@ -44,33 +43,10 @@ mv ../build.new build
 git add .
 git commit -m "$UTCDATE [release]"
 
-echo "*** [v1] Merging remote"
+echo "*** [v1 precompiled] Merging remote"
 git push origin HEAD:refs/heads/$PRECOMPILED_BRANCH 2>$GITLOG
-PRECOMPILED_HASH=`git rev-parse HEAD`
 
 # move to root
-cd ../..
-
-echo "*** [v1] Setting up GitHub config for parity"
-setup_git_user
-git remote set-url origin $GIT_PARITY
-git reset --hard origin/$BRANCH 2>$GITLOG
-
-echo "*** [v1] Updating cargo parity-ui-old-precompiled#$PRECOMPILED_HASH"
-git submodule update
-# Not needed since $BRANCH is hardcoded
-# sed -i "/^parity-ui-old-precompiled/ { s/branch = \".*\"/branch = \"$BRANCH\"/g; }" dapps/ui/Cargo.toml
-cargo update -p parity-ui-old-precompiled
-# --precise "$PRECOMPILED_HASH"
-
-echo "*** [v1] Committing updated files"
-git add dapps/ui/Cargo.toml
-git add Cargo.lock
-git commit -m "[ci skip] js-precompiled $UTCDATE"
-git push origin HEAD:refs/heads/$BRANCH 2>$GITLOG
-
-# back to root
-echo "*** [v1] Release completed"
 popd
 
 # exit with exit code
---
+++ js/scripts/push-precompiled.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+# variables
+UTCDATE=`date -u "+%Y%m%d-%H%M%S"`
+BRANCH=$CI_BUILD_REF_NAME
+GIT_JS_PRECOMPILED="https://${GITHUB_JS_PRECOMPILED}:@github.com/paritytech/js-precompiled.git"
+
+# setup the git user defaults for the current repo
+function setup_git_user {
+  git config push.default simple
+  git config merge.ours.driver true
+  git config user.email "$GITHUB_EMAIL"
+  git config user.name "GitLab Build Bot"
+}
+
+# change into the build directory
+BASEDIR=`dirname $0`
+GITLOG=./.git/gitcommand.log
+pushd $BASEDIR
+cd ../.dist
+
+# add local files and send it up
+echo "*** [v2 precompiled] Setting up GitHub config"
+rm -rf ./.git
+git init
+setup_git_user
+
+echo "*** [v2 precompiled] Checking out $BRANCH branch"
+git remote add origin $GIT_JS_PRECOMPILED
+git fetch origin 2>$GITLOG
+git checkout -b $BRANCH
+
+echo "*** [v2 precompiled] Committing compiled files for $UTCDATE"
+mv build ../build.new
+git add .
+git commit -m "$UTCDATE [update]"
+git merge origin/$BRANCH -X ours --commit -m "$UTCDATE [merge]"
+git rm -r build
+rm -rf build
+git commit -m "$UTCDATE [cleanup]"
+mv ../build.new build
+git add .
+git commit -m "$UTCDATE [release]"
+
+echo "*** [v2 precompiled] Merging remote"
+git push origin HEAD:refs/heads/$BRANCH 2>$GITLOG
+
+popd
+
+# exit with exit code
+exit 0
