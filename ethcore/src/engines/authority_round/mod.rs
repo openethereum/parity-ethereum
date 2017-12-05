3284020b04b6e3d7ba9d15800165045d6a362777
Takehiro Miyao <takehiro-miyao@gmo.jp>
2017-12-06 00:06:04 +0900

    mistake comment in calc difficulty (#7154)

--- ethcore/src/engines/authority_round/mod.rs
+++ ethcore/src/engines/authority_round/mod.rs
@@ -456,7 +456,6 @@ impl Engine<EthereumMachine> for AuthorityRound {
 	}
 
 	fn populate_from_parent(&self, header: &mut Header, parent: &Header) {
-		// Chain scoring: total weight is sqrt(U256::max_value())*height - step
 		let new_difficulty = U256::from(U128::max_value()) + header_step(parent).expect("Header has been verified; qed").into() - self.step.load().into();
 		header.set_difficulty(new_difficulty);
 	}
