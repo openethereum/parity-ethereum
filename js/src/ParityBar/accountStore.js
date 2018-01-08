07b1e4d9b484dd28f22306e06f2543b680435031
Amaury Martiny <amaury.martiny@gmail.com>
2018-01-08 14:45:48 +0100

    Show all accounts in the top bar

--- js/src/ParityBar/accountStore.js
+++ js/src/ParityBar/accountStore.js
@@ -14,14 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { action, observable, transaction } from 'mobx';
+import { action, computed, observable, transaction } from 'mobx';
 
 let instance;
 
 export default class AccountStore {
-  @observable accounts = [];
+  @observable allAccounts = [];
   @observable defaultAccount = null;
   @observable isLoading = false;
+
+  whitelist = [];
 
   constructor (api) {
     this._api = api;
@@ -31,13 +33,24 @@ export default class AccountStore {
       .then(() => this.loadAccounts());
   }
 
+  @computed get accounts () {
+    return this.allAccounts.filter(account => this.whitelist.includes(account.address));
+  }
+
+  /**
+   * For backwards compatibility
+   */
   @action setAccounts = (accounts) => {
-    this.accounts = accounts;
+    return this.setAllAccounts(accounts);
+  }
+
+  @action setAllAccounts = (allAccounts) => {
+    this.allAccounts = allAccounts;
   }
 
   @action setDefaultAccount = (defaultAccount) => {
     transaction(() => {
-      this.accounts = this.accounts.map((account) => {
+      this.allAccounts = this.allAccounts.map((account) => {
         account.checked = account.address === defaultAccount;
 
         return account;
@@ -70,6 +83,8 @@ export default class AccountStore {
         this._api.parity.allAccountsInfo()
       ])
       .then(([whitelist, allAccounts]) => {
+        this.whitelist = whitelist;
+
         transaction(() => {
           const accounts = Object
             .keys(allAccounts)
@@ -77,9 +92,8 @@ export default class AccountStore {
               const account = allAccounts[address];
               const isAccount = account.uuid;
               const isExternal = account.meta && (account.meta.external || account.meta.hardware);
-              const isWhitelisted = !whitelist || whitelist.includes(address);
 
-              return (isAccount || isExternal) && isWhitelisted;
+              return (isAccount || isExternal);
             })
             .map((address) => {
               return {
@@ -90,7 +104,7 @@ export default class AccountStore {
             });
 
           this.setLoading(false);
-          this.setAccounts(accounts);
+          this.setAllAccounts(accounts);
         });
       })
       .catch((error) => {
@@ -119,7 +133,7 @@ export default class AccountStore {
         }
       });
 
-    return Promise.all([ promiseDefaultAccount, promiseEthAccounts, promiseAccountsInfo ]);
+    return Promise.all([promiseDefaultAccount, promiseEthAccounts, promiseAccountsInfo]);
   }
 
   static get (api) {
