76301e332a8d26b06c4d8d99117a40d1c2871faa
Tomasz Drwięga <tomasz@parity.io>
2018-01-29 02:50:30 +0100

    Revert "revert to #7677 #7679"
    This reverts commit 568dc33a02d9e8a35e57b23b6b723a9e83ec7b48.

--- rpc/src/v1/impls/light/parity.rs
+++ rpc/src/v1/impls/light/parity.rs
@@ -208,7 +208,12 @@ impl Parity for ParityClient {
 	}
 
 	fn registry_address(&self) -> Result<Option<H160>> {
-		Err(errors::light_unimplemented(None))
+		let reg = self.light_dispatch.client.engine().params().registrar;
+		if reg == Default::default() {
+			Ok(None)
+		} else {
+			Ok(Some(reg.into()))
+		}
 	}
 
 	fn rpc_settings(&self) -> Result<RpcSettings> {
