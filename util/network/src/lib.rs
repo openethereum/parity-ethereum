5fd9640acf7d64e59e3998248e1b6154ceffa417
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2018-01-31 09:50:01 +0100

    Filter-out nodes.json (#7716)
    * Filter-out nodes.json
    
    * network: sort node table nodes by failure ratio
    
    * network: fix node table tests
    
    * network: fit node failure percentage into buckets of 5%
    
    * network: consider number of attempts in sorting of node table
    
    * network: fix node table grumbles

--- util/network/src/lib.rs
+++ util/network/src/lib.rs
@@ -80,14 +80,16 @@ extern crate path;
 extern crate ethcore_logger;
 extern crate ipnetwork;
 extern crate keccak_hash as hash;
+extern crate serde;
 extern crate serde_json;
 extern crate snappy;
 
 #[macro_use]
 extern crate error_chain;
-
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate serde_derive;
 
 #[cfg(test)]
 extern crate ethcore_devtools as devtools;
@@ -213,4 +215,3 @@ pub enum AllowIP {
     /// Block all addresses
     None,
 }
-
