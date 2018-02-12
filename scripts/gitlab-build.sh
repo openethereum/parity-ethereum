78f39e909048ba583eff7d56a52db94c63abae80
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2018-02-12 12:54:15 +0300

    Update gitlab-build.sh (#7855)
    fix build ```version``` after https://github.com/paritytech/parity/pull/7723

--- scripts/gitlab-build.sh
+++ scripts/gitlab-build.sh
@@ -9,7 +9,7 @@ ARC=$3
 CC=$4
 CXX=$5
 IDENT=$6
-VER="$(grep -m 1 version Cargo.toml | awk '{print $3}' | tr -d '"' | tr -d "\n")"
+VER="$(grep -m 1 "version =" Cargo.toml | awk '{print $3}' | tr -d '"' | tr -d "\n")"
 S3WIN=""
 echo "--------------------"
 echo "Build for platform: " $BUILD_PLATFORM
