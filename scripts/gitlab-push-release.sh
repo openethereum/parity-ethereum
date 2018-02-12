1f68e4eb99ba1a7b8718dd1a8885a20213a7c046
Tomasz Drwięga <tomasz@parity.io>
2018-02-12 16:00:46 +0100

    Add some logs.

--- scripts/gitlab-push-release.sh
+++ scripts/gitlab-push-release.sh
@@ -4,5 +4,9 @@ set -e # fail on any error
 set -u # treat unset variables as error
 
 DATA="secret=$RELEASES_SECRET"
+
+echo "Pushing release to Mainnet"
 source scripts/safe_curl.sh $DATA "http://localhost:1337/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
+
+echo "Pushing release to Kovan"
 source scripts/safe_curl.sh $DATA "http://localhost:1338/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
