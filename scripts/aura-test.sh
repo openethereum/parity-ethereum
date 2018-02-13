00aac4042d97c28bf335e0c06b1239d102b9d19c
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2018-02-13 18:18:11 +0300

    Update aura-test.sh

--- scripts/aura-test.sh
+++ scripts/aura-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e # fail on any error
+set -u # treat unset variables as error
+
 cargo build -j $(nproc) --release --features final $CARGOFLAGS
 git clone https://github.com/paritytech/parity-import-tests
 cp target/release/parity parity-import-tests/aura/parity
