857d7daf379a209e743f903b387d9e9389dfee69
Afri Schoedon <5chdn@users.noreply.github.com>
2018-02-14 12:39:08 +0100

    Backport master ci prs

--- scripts/aura-test.sh
+++ scripts/aura-test.sh
@@ -7,6 +7,6 @@ git clone https://github.com/paritytech/parity-import-tests
 cp target/release/parity parity-import-tests/aura/parity
 cd parity-import-tests/aura
 echo "Start Aura test"
-parity import blocks.rlp --chain chain.json
-parity restore snap --chain chain.json
+./parity import blocks.rlp --chain chain.json
+./parity restore snap --chain chain.json
 echo "Aura test complete"
