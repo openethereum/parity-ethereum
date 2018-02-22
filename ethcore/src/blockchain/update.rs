b933bd0ceed7650a14c5f9f18b790de2e4993544
Marek Kotewicz <marek.kotewicz@gmail.com>
2018-02-22 11:22:56 +0100

    fixed broken logs (#7934)
    * fixed broken logs
    
    * bring back old lock order
    
    * removed bloom groups from blockchain
    
    * revert unrelated changes
    
    * simplify blockchain_block_blooms

--- ethcore/src/blockchain/update.rs
+++ ethcore/src/blockchain/update.rs
@@ -2,8 +2,7 @@ use std::collections::HashMap;
 use bigint::hash::H256;
 use header::BlockNumber;
 use blockchain::block_info::BlockInfo;
-use blooms::BloomGroup;
-use super::extras::{BlockDetails, BlockReceipts, TransactionAddress, LogGroupPosition};
+use blockchain::extras::{BlockDetails, BlockReceipts, TransactionAddress};
 
 /// Block extras update info.
 pub struct ExtrasUpdate<'a> {
@@ -19,8 +18,6 @@ pub struct ExtrasUpdate<'a> {
 	pub block_details: HashMap<H256, BlockDetails>,
 	/// Modified block receipts.
 	pub block_receipts: HashMap<H256, BlockReceipts>,
-	/// Modified blocks blooms.
-	pub blocks_blooms: HashMap<LogGroupPosition, BloomGroup>,
 	/// Modified transaction addresses (None signifies removed transactions).
 	pub transactions_addresses: HashMap<H256, Option<TransactionAddress>>,
 }
