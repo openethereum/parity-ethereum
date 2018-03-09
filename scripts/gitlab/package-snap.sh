1fdee1a21753c430625139da8b1ffe37268ac4bd
Kirill Pimenov <kirill@parity.io>
2018-03-09 16:31:18 +0100

    Regularize architectures

--- scripts/gitlab/package-snap.sh
+++ scripts/gitlab/package-snap.sh
@@ -2,12 +2,6 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-apt install -y rhash
-MD5_BIN="rhash --md5"
-SHA256_BIN="rhash --sha256"
-
-snapcraft clean
-
 case $RELEASE_TRACK in
     stable) export CHANNEL="stable" ;;
     beta)   export CHANNEL="beta" ;;
@@ -16,6 +10,7 @@ esac
 
 SNAP_PACKAGE="parity_"$VERSION"_"$BUILD_ARCH".snap"
 
+snapcraft clean
 cat scripts/gitlab/templates/snapcraft.template.yaml | envsubst '$VERSION,$CHANNEL,$BUILD_ARCH' > snapcraft.yaml
 snapcraft build -d
 
@@ -24,5 +19,5 @@ mkdir -p artifacts
 mv $SNAP_PACKAGE "artifacts/"
 
 cd artifacts
-$MD5_BIN $SNAP_PACKAGE -o $SNAP_PACKAGE".md5"
-$SHA256_BIN $SNAP_PACKAGE > $SNAP_PACKAGE".sha256"
+rhash --md5 $SNAP_PACKAGE -o $SNAP_PACKAGE".md5"
+rhash --sha256 $SNAP_PACKAGE -o $SNAP_PACKAGE".sha256"
