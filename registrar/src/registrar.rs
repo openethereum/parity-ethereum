cd9e3035b121ed39e01c4688025ef45b7e78e11d
niklasad1 <niklasadolfsson1@gmail.com>
2018-03-12 21:18:52 +0100

    address review feedback

--- registrar/src/registrar.rs
+++ registrar/src/registrar.rs
@@ -1,3 +1,19 @@
+// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
 use futures::{Future, future, IntoFuture};
 use ethabi::{Address, Bytes};
 use std::sync::Arc;
@@ -5,7 +21,7 @@ use keccak_hash::keccak;
 
 use_contract!(registry, "Registry", "res/registrar.json");
 
-// Maps a domain name to IPv4 address
+// Maps a domain name to an Ethereum address
 const DNS_A_RECORD: &'static str = "A";
 
 pub type Asynchronous = Box<Future<Item=Bytes, Error=String> + Send>;
