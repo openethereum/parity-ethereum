6dc14f16dbfd0e0769e1606d1be30d315e913063
Wei Tang <hi@that.world>
2018-04-11 21:25:50 +0800

    Directly wait for future to resolve in a threadpool

--- secret_store/src/listener/http_listener.rs
+++ secret_store/src/listener/http_listener.rs
@@ -105,7 +105,7 @@ impl KeyServerHttpListener {
 				});
 
 				// TODO: Change this to tokio::spawn once hyper is Send.
-				current_thread::spawn(serve);
+				serve.wait();
 				future::ok(())
 			});
 
