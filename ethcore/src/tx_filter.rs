caf8b89a8402824c58e1bc287e04eba0e95cefe2
Vlad Lupashevskyi <vlad@lupashevskyi.com>
2018-04-18 17:08:42 +0300

    Introduced test for the new tx permission contract version + old test renamed as deprecated

--- ethcore/src/tx_filter.rs
+++ ethcore/src/tx_filter.rs
@@ -115,16 +115,138 @@ mod test {
 	use spec::Spec;
 	use client::{BlockChainClient, Client, ClientConfig, BlockId};
 	use miner::Miner;
-	use ethereum_types::Address;
+	use ethereum_types::{U256, Address};
 	use io::IoChannel;
 	use ethkey::{Secret, KeyPair};
 	use super::TransactionFilter;
 	use transaction::{Transaction, Action};
 	use tempdir::TempDir;
 
-	/// Contract code: https://gist.github.com/arkpar/38a87cb50165b7e683585eec71acb05a
+	/// Contract code: https://gist.github.com/VladLupashevskyi/a157d8162db85de67de522eeb7c8ee85
 	#[test]
 	fn transaction_filter() {
+		let spec_data = r#"
+		{
+			"name": "TestNodeFilterContract",
+			"engine": {
+				"authorityRound": {
+					"params": {
+						"stepDuration": 1,
+						"startStep": 2,
+						"validators": {
+							"contract": "0x0000000000000000000000000000000000000000"
+						}
+					}
+				}
+			},
+			"params": {
+				"accountStartNonce": "0x0",
+				"maximumExtraDataSize": "0x20",
+				"minGasLimit": "0x1388",
+				"networkID" : "0x69",
+				"gasLimitBoundDivisor": "0x0400",
+				"transactionPermissionContract": "0x0000000000000000000000000000000000000005"
+			},
+			"genesis": {
+				"seal": {
+					"generic": "0xc180"
+				},
+				"difficulty": "0x20000",
+				"author": "0x0000000000000000000000000000000000000000",
+				"timestamp": "0x00",
+				"parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+				"extraData": "0x",
+				"gasLimit": "0x222222"
+			},
+			"accounts": {
+				"0000000000000000000000000000000000000001": { "balance": "1", "builtin": { "name": "ecrecover", "pricing": { "linear": { "base": 3000, "word": 0 } } } },
+				"0000000000000000000000000000000000000002": { "balance": "1", "builtin": { "name": "sha256", "pricing": { "linear": { "base": 60, "word": 12 } } } },
+				"0000000000000000000000000000000000000003": { "balance": "1", "builtin": { "name": "ripemd160", "pricing": { "linear": { "base": 600, "word": 120 } } } },
+				"0000000000000000000000000000000000000004": { "balance": "1", "builtin": { "name": "identity", "pricing": { "linear": { "base": 15, "word": 3 } } } },
+				"0000000000000000000000000000000000000005": {
+					"balance": "1",
+					"constructor": "608060405234801561001057600080fd5b506104c3806100206000396000f300608060405260043610610062576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063469ab1e31461006757806375d0c0dc1461009a578063a0a8e4601461012a578063d4b03ee014610155575b600080fd5b34801561007357600080fd5b5061007c6101e2565b60405180826000191660001916815260200191505060405180910390f35b3480156100a657600080fd5b506100af610253565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100ef5780820151818401526020810190506100d4565b50505050905090810190601f16801561011c5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b34801561013657600080fd5b5061013f610290565b6040518082815260200191505060405180910390f35b34801561016157600080fd5b506101c0600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610299565b604051808263ffffffff1663ffffffff16815260200191505060405180910390f35b60006101ec610253565b6040518082805190602001908083835b60208310151561022157805182526020820191506020810190506020830392506101fc565b6001836020036101000a0380198251168184511680821785525050505050509050019150506040518091039020905090565b60606040805190810160405280601681526020017f54585f5045524d495353494f4e5f434f4e545241435400000000000000000000815250905090565b60006002905090565b6000737e5f4552091a69125d5dfcb7b8c2659029395bdf8473ffffffffffffffffffffffffffffffffffffffff1614156102d95763ffffffff9050610490565b732b5ad5c4795c026514f8317c7a215e218dccd6cf8473ffffffffffffffffffffffffffffffffffffffff1614156103175760026001179050610490565b736813eb9362372eef6200f3b1dbc3f819671cba698473ffffffffffffffffffffffffffffffffffffffff1614156103525760019050610490565b73e1ab8145f7e55dc933d51a18c793f901a3a0b2768473ffffffffffffffffffffffffffffffffffffffff1614801561038b5750600082145b1561039c5763ffffffff9050610490565b73e57bfe9f44b819898f47bf37e5af72a0783e11418473ffffffffffffffffffffffffffffffffffffffff161480156103fe575073d41c057fd1c78805aac12b0a94a405c0461a6fbb8373ffffffffffffffffffffffffffffffffffffffff16145b1561040c5760019050610490565b73d41c057fd1c78805aac12b0a94a405c0461a6fbb8473ffffffffffffffffffffffffffffffffffffffff1614801561046e575073e57bfe9f44b819898f47bf37e5af72a0783e11418373ffffffffffffffffffffffffffffffffffffffff16145b801561047a5750600082145b1561048b5763ffffffff9050610490565b600090505b93925050505600a165627a7a72305820ea35352692061875f689bdefc5e0d0ec86be33cdcf729bb0908f76b1da038b560029"
+				}
+			}
+		}
+		"#;
+
+		let tempdir = TempDir::new("").unwrap();
+		let spec = Spec::load(&tempdir.path(), spec_data.as_bytes()).unwrap();
+		let client_db = Arc::new(::kvdb_memorydb::create(::db::NUM_COLUMNS.unwrap_or(0)));
+
+		let client = Client::new(
+			ClientConfig::default(),
+			&spec,
+			client_db,
+			Arc::new(Miner::with_spec(&spec)),
+			IoChannel::disconnected(),
+		).unwrap();
+		let key1 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000001")).unwrap();
+		let key2 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000002")).unwrap();
+		let key3 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000003")).unwrap();
+		let key4 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000004")).unwrap();
+		let key5 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000005")).unwrap();
+		let key6 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000006")).unwrap();
+		let key7 = KeyPair::from_secret(Secret::from("0000000000000000000000000000000000000000000000000000000000000007")).unwrap();
+
+		let filter = TransactionFilter::from_params(spec.params()).unwrap();
+		let mut basic_tx = Transaction::default();
+		basic_tx.action = Action::Call(Address::from("d41c057fd1c78805aac12b0a94a405c0461a6fbb"));
+		let create_tx = Transaction::default();
+		let mut call_tx = Transaction::default();
+		call_tx.action = Action::Call(Address::from("0000000000000000000000000000000000000005"));
+
+		let mut basic_tx_with_ether_and_to_key7 = Transaction::default();
+		basic_tx_with_ether_and_to_key7.action = Action::Call(Address::from("d41c057fd1c78805aac12b0a94a405c0461a6fbb"));
+		basic_tx_with_ether_and_to_key7.value = U256::from(123123);
+		let mut call_tx_with_ether = Transaction::default();
+		call_tx_with_ether.action = Action::Call(Address::from("0000000000000000000000000000000000000005"));
+		call_tx_with_ether.value = U256::from(123123);
+
+
+		let mut basic_tx_to_key6 = Transaction::default();
+		basic_tx_to_key6.action = Action::Call(Address::from("e57bfe9f44b819898f47bf37e5af72a0783e1141"));
+		let mut basic_tx_with_ether_and_to_key6 = Transaction::default();
+		basic_tx_with_ether_and_to_key6.action = Action::Call(Address::from("e57bfe9f44b819898f47bf37e5af72a0783e1141"));
+		basic_tx_with_ether_and_to_key6.value = U256::from(123123);
+
+		let genesis = client.block_hash(BlockId::Latest).unwrap();
+
+		assert!(filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key1.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &create_tx.clone().sign(key1.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &call_tx.clone().sign(key1.secret(), None), &*client));
+
+		assert!(filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key2.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &create_tx.clone().sign(key2.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &call_tx.clone().sign(key2.secret(), None), &*client));
+
+		assert!(filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key3.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &create_tx.clone().sign(key3.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &call_tx.clone().sign(key3.secret(), None), &*client));
+
+		assert!(!filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key4.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &create_tx.clone().sign(key4.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &call_tx.clone().sign(key4.secret(), None), &*client));
+
+		assert!(filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key1.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &create_tx.clone().sign(key1.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &call_tx.clone().sign(key1.secret(), None), &*client));
+
+		assert!(!filter.transaction_allowed(&genesis, &basic_tx_with_ether_and_to_key7.clone().sign(key5.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &call_tx_with_ether.clone().sign(key5.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &basic_tx.clone().sign(key6.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &basic_tx_with_ether_and_to_key7.clone().sign(key6.secret(), None), &*client));
+		assert!(filter.transaction_allowed(&genesis, &basic_tx_to_key6.clone().sign(key7.secret(), None), &*client));
+		assert!(!filter.transaction_allowed(&genesis, &basic_tx_with_ether_and_to_key6.clone().sign(key7.secret(), None), &*client));
+
+
+
+	}
+
+	/// Contract code: https://gist.github.com/arkpar/38a87cb50165b7e683585eec71acb05a
+	#[test]
+	fn transaction_filter_deprecated() {
 		let spec_data = r#"
 		{
 			"name": "TestNodeFilterContract",
