e2f3f01ca9d7393ca9d81d752bd79a7f2087ebd3
Wei Tang <hi@that.world>
2018-04-19 21:12:23 +0800

    Use saturating_sub to avoid potential overflow

--- ethcore/src/executive.rs
+++ ethcore/src/executive.rs
@@ -344,7 +344,7 @@ impl<'a, B: 'a + StateBackend> Executive<'a, B> {
 		vm_tracer: &mut V
 	) -> vm::Result<FinalizationResult> where T: Tracer, V: VMTracer {
 		let local_stack_size = ::io::LOCAL_STACK_SIZE.with(|sz| sz.get());
-		let depth_threshold = (local_stack_size - STACK_SIZE_ENTRY_OVERHEAD) / STACK_SIZE_PER_DEPTH;
+		let depth_threshold = local_stack_size.saturating_sub(STACK_SIZE_ENTRY_OVERHEAD) / STACK_SIZE_PER_DEPTH;
 		let static_call = params.call_type == CallType::StaticCall;
 
 		// Ordinary execution - keep VM in same thread
@@ -361,7 +361,7 @@ impl<'a, B: 'a + StateBackend> Executive<'a, B> {
 			let vm_factory = self.state.vm_factory();
 			let mut ext = self.as_externalities(OriginInfo::from(&params), unconfirmed_substate, output_policy, tracer, vm_tracer, static_call);
 
-			scope.builder().stack_size(::std::cmp::max((schedule.max_depth - depth_threshold) * STACK_SIZE_PER_DEPTH, local_stack_size)).spawn(move || {
+			scope.builder().stack_size(::std::cmp::max(schedule.max_depth.saturating_sub(depth_threshold) * STACK_SIZE_PER_DEPTH, local_stack_size)).spawn(move || {
 				let mut vm = vm_factory.create(&params, &schedule);
 				vm.exec(params, &mut ext).finalize(ext)
 			}).expect("Sub-thread creation cannot fail; the host might run out of resources; qed")
