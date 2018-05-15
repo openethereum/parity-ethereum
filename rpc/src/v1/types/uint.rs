78e8b0e231bafd181e3068997fa3c0eed160f766
Andrew Jones <ascjones@gmail.com>
2018-05-15 17:18:00 +0100

    Prefix uint fmt with `0x` with alternate flag

--- rpc/src/v1/types/uint.rs
+++ rpc/src/v1/types/uint.rs
@@ -55,7 +55,13 @@ macro_rules! impl_uint {
 
 		impl fmt::LowerHex for $name {
 			fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-				write!(f, "{:#x}", self.0)
+				// TODO: remove this once updated to new version of primitives
+				// including https://github.com/paritytech/primitives/pull/33
+				// replace with `::core::fmt::LowerHex::fmt(self.0, f)`
+				if f.alternate() {
+					write!(f, "0x");
+				}
+				write!(f, "{:x}", self.0)
 			}
 		}
 
@@ -102,19 +108,19 @@ impl_uint!(U64, u64, 1);
 
 impl serde::Serialize for U128 {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
-		serializer.serialize_str(&format!("0x{:x}", self.0))
+		serializer.serialize_str(&format!("{:#x}", self))
 	}
 }
 
 impl serde::Serialize for U256 {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
-		serializer.serialize_str(&format!("0x{:x}", self.0))
+		serializer.serialize_str(&format!("{:#x}", self))
 	}
 }
 
 impl serde::Serialize for U64 {
 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
-		serializer.serialize_str(&format!("0x{:x}", self.0))
+		serializer.serialize_str(&format!("{:#x}", self))
 	}
 }
 
