9143d846bff3220300726ae8937f351c1a5c941b
niklasad1 <niklasadolfsson1@gmail.com>
2018-05-19 20:01:18 +0200

    Update `add_license` script

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+PAT="^// Copyright.*If not, see <http://www.gnu.org/licenses/>\.$"
+
 for f in $(find . -name '*.rs'); do
- 	cat license_header $f > $f.new
-	mv $f.new $f
+	HEADER=$(head -16 $f)
+	if [[ $HEADER =~ $PAT ]]; then
+		BODY=$(tail -n +17 $f)
+		cat license_header > temp
+		echo "$BODY" >> temp
+		mv temp $f
+	else
+		echo "$f was missing header" 
+		cat license_header $f > temp
+		mv temp $f
+	fi
 done
