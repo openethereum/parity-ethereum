b1b14eba1177a4b2d1dcbf70f5fc36ef6047d3cb
niklasad1 <niklasadolfsson1@gmail.com>
2018-05-20 15:28:29 +0200

    don't append `gpl license` in files with other lic

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-PAT="^// Copyright.*If not, see <http://www.gnu.org/licenses/>\.$"
+PAT_GPL="^// Copyright.*If not, see <http://www.gnu.org/licenses/>\.$"
+PAT_OTHER"^// Copyright"
 
 for f in $(find . -name '*.rs'); do
 	HEADER=$(head -16 $f)
-	if [[ $HEADER =~ $PAT ]]; then
+	if [[ $HEADER =~ $PAT_GPL ]]; then
 		BODY=$(tail -n +17 $f)
 		cat license_header > temp
 		echo "$BODY" >> temp
 		mv temp $f
+	elif [[ $HEADER =~ $PAT_OTHER ]]; then
+		echo "Other license was found do nothing"
 	else
 		echo "$f was missing header" 
 		cat license_header $f > temp
