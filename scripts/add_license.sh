7924e0b89a306ee1c81586aff4463e4ba4aa3afa
niklasad1 <niklasadolfsson1@gmail.com>
2018-05-20 15:52:10 +0200

    include c and cpp files too

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -3,7 +3,7 @@
 PAT_GPL="^// Copyright.*If not, see <http://www.gnu.org/licenses/>\.$"
 PAT_OTHER="^// Copyright"
 
-for f in $(find . -name '*.rs'); do
+for f in $(find . -type f | egrep '\.(c|cpp|rs)$'); do
 	HEADER=$(head -16 $f)
 	if [[ $HEADER =~ $PAT_GPL ]]; then
 		BODY=$(tail -n +17 $f)
