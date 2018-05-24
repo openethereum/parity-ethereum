c832c4f99221ea39c9f2f510cceee63e81259280
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-05-24 04:39:52 +0200

    Use impl Future in the light client RPC helpers (#8628)

--- rpc/src/v1/impls/light/eth.rs
+++ rpc/src/v1/impls/light/eth.rs
@@ -538,7 +538,7 @@ impl<T: LightChainClient + 'static> Filterable for EthClient<T> {
 	}
 
 	fn logs(&self, filter: EthcoreFilter) -> BoxFuture<Vec<Log>> {
-		self.fetcher().logs(filter)
+		Box::new(self.fetcher().logs(filter)) as BoxFuture<_>
 	}
 
 	fn pending_logs(&self, _block_number: u64, _filter: &EthcoreFilter) -> Vec<Log> {
