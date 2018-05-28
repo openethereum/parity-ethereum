484376824a4b65f696d673f923b25ee4cae6f42f
David <dvdplm@gmail.com>
2018-05-28 17:10:29 +0200

    Fix compilation error on nightly rust (#8707)
    On nightly rust passing `public_url` works but that breaks on stable. This works for both.

--- util/network-devp2p/src/service.rs
+++ util/network-devp2p/src/service.rs
@@ -32,7 +32,7 @@ impl IoHandler<NetworkIoMessage> for HostHandler {
 		if let NetworkIoMessage::NetworkStarted(ref public_url) = *message {
 			let mut url = self.public_url.write();
 			if url.as_ref().map_or(true, |uref| uref != public_url) {
-				info!(target: "network", "Public node URL: {}", Colour::White.bold().paint(public_url.as_ref()));
+				info!(target: "network", "Public node URL: {}", Colour::White.bold().paint(AsRef::<str>::as_ref(public_url)));
 			}
 			*url = Some(public_url.to_owned());
 		}
