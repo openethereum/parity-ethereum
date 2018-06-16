f57565c9a503581192f209184b6f8ebce990fac4
David Palm <dvdplm@gmail.com>
2018-06-16 20:47:45 +0200

    Add keccak-hasher crate that implements Hasher with the Keccak-256 hash

---
+++ util/keccak-hasher/src/lib.rs
@@ -0,0 +1,42 @@
+// Copyright 2015-2018 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Hasher implementation for the Keccak-256 hash
+extern crate hashdb;
+extern crate ethereum_types;
+extern crate tiny_keccak;
+
+use hashdb::Hasher;
+use ethereum_types::H256;
+use tiny_keccak::Keccak;
+
+#[derive(Default, Debug, Clone, PartialEq)]
+pub struct KeccakHasher;
+impl Hasher for KeccakHasher {
+	type Out = H256;
+    // TODO: this is a problem: we do not want the Hasher to make assumptions about the encoding scheme.
+	const HASHED_NULL_RLP: H256 = H256( [0x56, 0xe8, 0x1f, 0x17, 0x1b, 0xcc, 0x55, 0xa6, 0xff, 0x83, 0x45, 0xe6, 0x92, 0xc0, 0xf8, 0x6e, 0x5b, 0x48, 0xe0, 0x1b, 0x99, 0x6c, 0xad, 0xc0, 0x01, 0x62, 0x2f, 0xb5, 0xe3, 0x63, 0xb4, 0x21] );
+	const LENGTH: usize = 32;
+	fn hash(x: &[u8]) -> Self::Out {
+		let mut out = [0;32];
+		Keccak::keccak256(x, &mut out);
+		out.into()
+	}
+}
+
+// Move concrete impl to own "façade" crate?
+// /// Convenience type for crates that need a `HashDB` with Keccak hashes
+// pub type KeccakHashDB = HashDB<KeccakHasher>;
