d5136ab98eed2d53f72d49d4d998e66b53b31bd5
Afri Schoedon <5chdn@users.noreply.github.com>
2018-06-22 17:48:36 +0200

    scripts: fix docker build tag on latest using master (#8952)

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,6 +2,10 @@
 cd docker/hub
 DOCKER_BUILD_TAG=$1
 echo "Docker build tag: " $DOCKER_BUILD_TAG
-docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+if [[ "$DOCKER_BUILD_TAG" = "latest" ]]; then
+	docker build --build-arg BUILD_TAG="master" --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+else
+	docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+fi
 docker run -it parity/parity:$DOCKER_BUILD_TAG -v
 docker push parity/parity:$DOCKER_BUILD_TAG
