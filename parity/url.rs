f6108ec0d39b153967576c2e9a51cd80c626d0e8
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-07-04 16:38:17 +0200

    Some work towards iOS build

--- parity/url.rs
+++ parity/url.rs
@@ -79,7 +79,7 @@ pub fn open(url: &str) -> Result<(), Error> {
 	Ok(())
 }
 
-#[cfg(target_os="android")]
+#[cfg(any(target_os="android", target_os="ios"))]
 pub fn open(_url: &str) -> Result<(), Error> {
 	// TODO: While it is generally always bad to leave a function implemented, there is not much
 	//		 more we can do here. This function will eventually be removed when we compile Parity
