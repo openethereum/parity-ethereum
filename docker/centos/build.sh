1d486cba32a29be417ba09b13f4814e67ec49766
Nikola Jokic <nikola@kip.investments>
2018-08-01 00:03:58 +0200

    append git sha to the builder image, update readme

--- docker/centos/build.sh
+++ docker/centos/build.sh
@@ -3,16 +3,15 @@
 # The image name
 PARITY_IMAGE_REPO=${PARITY_IMAGE_REPO:-parity/parity}
 # The tag to be used for builder image
-PARITY_BUILDER_IMAGE_TAG=${PARITY_BUILDER_IMAGE_TAG:-build-latest}
+PARITY_BUILDER_IMAGE_TAG=${PARITY_BUILDER_IMAGE_TAG:-build}
 # The tag to be used for runner image
 PARITY_RUNNER_IMAGE_TAG=${PARITY_RUNNER_IMAGE_TAG:-latest}
 
-echo Building $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG from $(git log -1 --format="%H")
+echo Building $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG-$(git log -1 --format="%H")
+docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG-$(git log -1 --format="%H") . -f docker/centos/build.Dockerfile
 
-docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG . -f docker/centos/build.Dockerfile
-
-echo Creating $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG, extracting binary
-docker create --name extract $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG 
+echo Creating $PARITY_BUILDER_IMAGE_TAG-$(git log -1 --format="%H"), extracting binary
+docker create --name extract $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG-$(git log -1 --format="%H") 
 mkdir docker/centos/parity
 docker cp extract:/build/parity-ethereum/target/release/parity docker/centos/parity
 
@@ -25,4 +24,5 @@ docker rm -f extract
 
 echo Echoing Parity version:
 docker run $PARITY_IMAGE_REPO:$PARITY_RUNNER_IMAGE_TAG --version
+
 echo Done.
