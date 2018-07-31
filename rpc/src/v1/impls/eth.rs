e84e6937eed2b99075cb324f19f73fdd117966db
André Silva <andre.beat@gmail.com>
2018-07-31 11:07:03 +0100

    rpc: avoid calling queue_info twice on eth_getWork

--- rpc/src/v1/impls/eth.rs
+++ rpc/src/v1/impls/eth.rs
@@ -744,9 +744,11 @@ impl<C, SN: ?Sized, S: ?Sized, M, EM, T: StateInfo + 'static> Eth for EthClient<
 
 		// check if we're still syncing and return empty strings in that case
 		{
-			let status = self.sync.status();
-			let client = &self.client;
-			if is_major_importing(Some(status.state), client.queue_info()) || self.client.queue_info().total_queue_size() > MAX_QUEUE_SIZE_TO_MINE_ON {
+			let sync_status = self.sync.status();
+			let queue_info = self.client.queue_info();
+			let total_queue_size = queue_info.total_queue_size();
+
+			if is_major_importing(Some(sync_status.state), queue_info) || total_queue_size > MAX_QUEUE_SIZE_TO_MINE_ON {
 				trace!(target: "miner", "Syncing. Cannot give any work.");
 				return Err(errors::no_work());
 			}
