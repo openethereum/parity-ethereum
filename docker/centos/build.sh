ceff3e1d65aa030553bbe885d53c93968b694281
Nikola Jokic <nikola@kip.investments>
2018-07-31 15:25:24 +0200

    Using ADD instead of git clone

--- docker/centos/build.sh
+++ docker/centos/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# The tag used when doing git checkout
-PARITY_BUILD_TAG=${PARITY_BUILD_TAG:-master}
-# The repo to pull
-PARITY_BUILD_REPO=${PARITY_BUILD_REPO:-https://github.com/paritytech/parity-ethereum}
 # The image name
 PARITY_IMAGE_REPO=${PARITY_IMAGE_REPO:-parity/parity}
 # The tag to be used for builder image
@@ -12,18 +8,18 @@ PARITY_BUILDER_IMAGE_TAG=${PARITY_BUILDER_IMAGE_TAG:-build-latest}
 PARITY_RUNNER_IMAGE_TAG=${PARITY_RUNNER_IMAGE_TAG:-latest}
 
 echo Building $PARITY_BUILDER_IMAGE_TAG from $PARITY_BUILD_REPO:$PARITY_BUILD_TAG
-docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG . -f build.Dockerfile
+docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG . -f docker/centos/build.Dockerfile
 
 echo Creating $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG, extracting binary
 docker create --name extract $PARITY_IMAGE_REPO:$PARITY_BUILDER_IMAGE_TAG 
-mkdir parity
-docker cp extract:/build/parity-ethereum/target/release/parity ./parity
+mkdir docker/centos/parity
+docker cp extract:/build/parity-ethereum/target/release/parity docker/centos/parity
 
 echo Building $PARITY_IMAGE_REPO:$PARITY_RUNNER_IMAGE_TAG
-docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_RUNNER_IMAGE_TAG .
+docker build --no-cache -t $PARITY_IMAGE_REPO:$PARITY_RUNNER_IMAGE_TAG docker/centos/Dockerfile
 
 echo Cleaning up ...
-rm -rf ./parity
+rm -rf docker/centos/parity
 docker rm -f extract
 
 echo Done.
