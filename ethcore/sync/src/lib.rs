b87c7cac5468649973fc8672f120052357062385
Marek Kotewicz <marek.kotewicz@gmail.com>
2018-08-24 11:53:31 +0200

    block view! removal in progress (#9397)

--- ethcore/sync/src/lib.rs
+++ ethcore/sync/src/lib.rs
@@ -27,7 +27,6 @@ extern crate ethcore_network_devp2p as devp2p;
 extern crate parity_bytes as bytes;
 extern crate ethcore_io as io;
 extern crate ethcore_transaction as transaction;
-#[cfg_attr(test, macro_use)]
 extern crate ethcore;
 extern crate ethereum_types;
 extern crate env_logger;
