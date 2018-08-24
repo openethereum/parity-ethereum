df25c598711c80827a0e82db805d71cea4b9446c
5chdn <5chdn@users.noreply.github.com>
2018-08-24 12:12:10 -0400

    parity: fix configuration tests

--- parity/params.rs
+++ parity/params.rs
@@ -84,7 +84,7 @@ impl str::FromStr for SpecType {
 impl fmt::Display for SpecType {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		f.write_str(match *self {
-			SpecType::Foundation => "ethereum",
+			SpecType::Foundation => "foundation",
 			SpecType::Classic => "classic",
 			SpecType::Poanet => "poanet",
 			SpecType::Tobalaba => "tobalaba",
