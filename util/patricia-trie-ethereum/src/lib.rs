1cfb1ac2b6571e475c2eb726dadeb0eb7a78425c
David Palm <dvdplm@gmail.com>
2018-08-24 18:15:47 +0200

    Add doc tests from `patricia-trie` to `patricia-trie-ethereum` Fix/update a few deps

--- util/patricia-trie-ethereum/src/lib.rs
+++ util/patricia-trie-ethereum/src/lib.rs
@@ -36,6 +36,36 @@ use rlp::DecoderError;
 pub type RlpCodec = RlpNodeCodec<KeccakHasher>;
 
 /// Convenience type alias to instantiate a Keccak/Rlp-flavoured `TrieDB`
+///
+/// Use it as a `Trie` trait object. You can use `db()` to get the backing database object.
+/// Use `get` and `contains` to query values associated with keys in the trie.
+///
+/// # Example
+/// ```
+/// extern crate patricia_trie as trie;
+/// extern crate patricia_trie_ethereum as ethtrie;
+/// extern crate hashdb;
+/// extern crate keccak_hasher;
+/// extern crate memorydb;
+/// extern crate ethereum_types;
+///
+/// use trie::*;
+/// use hashdb::*;
+/// use keccak_hasher::KeccakHasher;
+/// use memorydb::*;
+/// use ethereum_types::H256;
+/// use ethtrie::{TrieDB, TrieDBMut};
+///
+///
+/// fn main() {
+///   let mut memdb = MemoryDB::<KeccakHasher>::new();
+///   let mut root = H256::new();
+///   TrieDBMut::new(&mut memdb, &mut root).insert(b"foo", b"bar").unwrap();
+///   let t = TrieDB::new(&memdb, &root).unwrap();
+///   assert!(t.contains(b"foo").unwrap());
+///   assert_eq!(t.get(b"foo").unwrap().unwrap(), DBValue::from_slice(b"bar"));
+/// }
+/// ```
 pub type TrieDB<'db> = trie::TrieDB<'db, KeccakHasher, RlpCodec>;
 
 /// Convenience type alias to instantiate a Keccak/Rlp-flavoured `SecTrieDB`
@@ -45,6 +75,41 @@ pub type SecTrieDB<'db> = trie::SecTrieDB<'db, KeccakHasher, RlpCodec>;
 pub type FatDB<'db> = trie::FatDB<'db, KeccakHasher, RlpCodec>;
 
 /// Convenience type alias to instantiate a Keccak/Rlp-flavoured `TrieDBMut`
+///
+/// Use it as a `TrieMut` trait object. You can use `db()` to get the backing database object.
+/// Note that changes are not committed to the database until `commit` is called.
+/// Querying the root or dropping the trie will commit automatically.
+
+/// # Example
+/// ```
+/// extern crate patricia_trie as trie;
+/// extern crate patricia_trie_ethereum as ethtrie;
+/// extern crate hashdb;
+/// extern crate keccak_hash;
+/// extern crate keccak_hasher;
+/// extern crate memorydb;
+/// extern crate ethereum_types;
+///
+/// use keccak_hash::KECCAK_NULL_RLP;
+/// use ethtrie::{TrieDBMut, trie::TrieMut};
+/// use hashdb::DBValue;
+/// use keccak_hasher::KeccakHasher;
+/// use memorydb::*;
+/// use ethereum_types::H256;
+///
+/// fn main() {
+///   let mut memdb = MemoryDB::<KeccakHasher>::new();
+///   let mut root = H256::new();
+///   let mut t = TrieDBMut::new(&mut memdb, &mut root);
+///   assert!(t.is_empty());
+///   assert_eq!(*t.root(), KECCAK_NULL_RLP);
+///   t.insert(b"foo", b"bar").unwrap();
+///   assert!(t.contains(b"foo").unwrap());
+///   assert_eq!(t.get(b"foo").unwrap().unwrap(), DBValue::from_slice(b"bar"));
+///   t.remove(b"foo").unwrap();
+///   assert!(!t.contains(b"foo").unwrap());
+/// }
+/// ```
 pub type TrieDBMut<'db> = trie::TrieDBMut<'db, KeccakHasher, RlpCodec>;
 
 /// Convenience type alias to instantiate a Keccak/Rlp-flavoured `SecTrieDBMut`
