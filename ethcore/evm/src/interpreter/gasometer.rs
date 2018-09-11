05f5e48b3f6d8c88b31fae2a14029233acc0da0b
cheme <emericchevalier.pro@gmail.com>
2018-09-11 17:47:26 +0200

    In create memory calculation is the same for create2 because the additional parameter was popped before. (#9522)

--- ethcore/evm/src/interpreter/gasometer.rs
+++ ethcore/evm/src/interpreter/gasometer.rs
@@ -233,11 +233,7 @@ impl<Gas: evm::CostType> Gasometer<Gas> {
 			},
 			instructions::CREATE | instructions::CREATE2 => {
 				let gas = Gas::from(schedule.create_gas);
-				let mem = match instruction {
-					instructions::CREATE => mem_needed(stack.peek(1), stack.peek(2))?,
-					instructions::CREATE2 => mem_needed(stack.peek(2), stack.peek(3))?,
-					_ => unreachable!("instruction can only be CREATE/CREATE2 checked above; qed"),
-				};
+				let mem = mem_needed(stack.peek(1), stack.peek(2))?;
 
 				Request::GasMemProvide(gas, mem, None)
 			},
