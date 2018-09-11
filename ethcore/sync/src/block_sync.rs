b4033f3861f5696e47fdfd9b55e5a4ddca664bbb
Andrew Jones <ascjones@gmail.com>
2018-09-11 17:22:11 +0100

    Add comment to explain subchain heads filter

--- ethcore/sync/src/block_sync.rs
+++ ethcore/sync/src/block_sync.rs
@@ -282,8 +282,14 @@ impl BlockDownloader {
 		match self.state {
 			State::ChainHead => {
 				if !headers.is_empty() {
-					let is_subchain_heads = headers.len() == 1 ||
-						headers.len() > 1 && headers[0].header.number() - headers[1].header.number() > 1;
+					// When the round starts, subchain headers are requested
+					// with gaps to be filled. However if the round is reset
+					// while in State::Blocks then its possible to receive
+					// stale responses for the subchain heads in the gap. In
+					// this case the headers will have consecutive numbers so can be ignored here.
+					let is_subchain_heads = headers.len() == 1
+						|| headers.len() > 1
+						&& headers[0].header.number() - headers[1].header.number() > 1;
 
 					if is_subchain_heads {
 						trace!(target: "sync", "Received {} subchain heads, proceeding to download", headers.len());
