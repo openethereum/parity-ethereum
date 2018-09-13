4ddd69cc551da5bb5903525ffbb59bc56619a4b6
Nicolas Gotchac <ngotchac@gmail.com>
2018-09-13 12:58:49 +0200

    Multithreaded snapshot creation (#9239)
    * Add Progress to Snapshot Secondary chunks creation
    
    * Use half of CPUs to multithread snapshot creation
    
    * Use env var to define number of threads
    
    * info to debug logs
    
    * Add Snapshot threads as CLI option
    
    * Randomize chunks per thread
    
    * Remove randomness, add debugging
    
    * Add warning
    
    * Add tracing
    
    * Use parity-common fix seek branch
    
    * Fix log
    
    * Fix tests
    
    * Fix tests
    
    * PR Grumbles
    
    * PR Grumble II
    
    * Update Cargo.lock
    
    * PR Grumbles
    
    * Default snapshot threads to half number of CPUs
    
    * Fix default snapshot threads // min 1

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -1150,7 +1150,8 @@ impl Client {
 			},
 		};
 
-		snapshot::take_snapshot(&*self.engine, &self.chain.read(), start_hash, db.as_hashdb(), writer, p)?;
+		let processing_threads = self.config.snapshot.processing_threads;
+		snapshot::take_snapshot(&*self.engine, &self.chain.read(), start_hash, db.as_hashdb(), writer, p, processing_threads)?;
 
 		Ok(())
 	}
