5aff9f582391d58ea6cfa59bb1cb11ec4bb5a70e
gabriel <gabriel@parity.io>
2018-09-14 12:16:39 +0200

    remove check stage again

--- scripts/gitlab/test.sh
+++ scripts/gitlab/test.sh
@@ -5,14 +5,40 @@ set -e # fail on any error
 set -u # treat unset variables as error
 
 
+set -x # full command output for development
+git log --graph --oneline --all --decorate=short -n 10
+
+
+case $CI_COMMIT_REF_NAME in
+  (master|beta|stable)
+    export GIT_COMPARE=$CI_COMMIT_REF_NAME~
+    ;;
+  (*)
+    export GIT_COMPARE=master
+  ;;
+esac
+
+
+export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^test.sh -e ^scripts/ | wc -l)"
+echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
+
+
+if [ "${RUST_FILES_MODIFIED}" = "0" ]
+then
+  echo "__________Skipping Rust tests since no Rust files modified__________";
+  exit 0
+fi
+
+
 rustup default $1
 
 git submodule update --init --recursive
 rustup show
 
-./test.sh || exit $?
+exec ./test.s
 
 # if [[ "$CI_COMMIT_REF_NAME" == "nightly" ]];
 #   ### @TODO re-enable fail after https://github.com/paritytech/parity-import-tests/issues/3
 #   then sh scripts/aura-test.sh; # || exit $?;
 # fi
+
