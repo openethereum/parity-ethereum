870ec89e9a89dabedde055407df5208691eb2984
gabriel klawitter <gabreal@users.noreply.github.com>
2018-09-14 15:55:14 +0200

    Simultaneous platform tests WIP (#9557)
    * look into commit changes
    
    * look into commit changes ii
    
    * all on test
    
    * build only_releaseable_branches for platforms
    
    * allow failure for check during development
    
    * windows test typo
    
    * fix sh for windows
    
    * remove check stage again
    
    * debug macos platform

--- scripts/gitlab/test.sh
+++ scripts/gitlab/test.sh
@@ -4,25 +4,41 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-rustup default $1
 
-if [[ "$CI_COMMIT_REF_NAME" = "master" || "$CI_COMMIT_REF_NAME" = "beta" || "$CI_COMMIT_REF_NAME" = "stable" ]]; then
-  export GIT_COMPARE=$CI_COMMIT_REF_NAME~;
-else
-  export GIT_COMPARE=master;
+set -x # full command output for development
+git log --graph --oneline --all --decorate=short -n 10
+
+
+case $CI_COMMIT_REF_NAME in
+  (master|beta|stable)
+    export GIT_COMPARE=$CI_COMMIT_REF_NAME~
+    ;;
+  (*)
+    export GIT_COMPARE=master
+  ;;
+esac
+
+
+export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^test.sh -e ^scripts/ | wc -l | tr -d ' ')"
+echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
+
+
+if [ "${RUST_FILES_MODIFIED}" = "0" ]
+then
+  echo "__________Skipping Rust tests since no Rust files modified__________";
+  exit 0
 fi
 
-export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^test.sh -e ^windows/ -e ^scripts/ -e ^mac/ -e ^nsis/ | wc -l)"
-echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
+
+rustup default $1
 
 git submodule update --init --recursive
 rustup show
-if [[ "${RUST_FILES_MODIFIED}" == "0" ]];
-then echo "__________Skipping Rust tests since no Rust files modified__________";
-else ./test.sh || exit $?;
-fi
+
+exec ./test.sh
 
 # if [[ "$CI_COMMIT_REF_NAME" == "nightly" ]];
 #   ### @TODO re-enable fail after https://github.com/paritytech/parity-import-tests/issues/3
 #   then sh scripts/aura-test.sh; # || exit $?;
 # fi
+
