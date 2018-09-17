371a1a699545f8bad20457c9ac29afd2aee333cb
cheme <emericchevalier.pro@gmail.com>
2018-09-17 18:20:05 +0200

    Put skipping 'under issue' test behind a feature.

--- ethcore/src/json_tests/skip.rs
+++ ethcore/src/json_tests/skip.rs
@@ -18,12 +18,18 @@
 
 use ethjson;
 
+
+#[cfg(feature="ci-skip-issue")]
 lazy_static!{
-
-
 	pub static ref SKIP_TEST_STATE: ethjson::test::SkipStates = {
 		let skip_data = include_bytes!("../../res/ethereum/tests-issues/currents.json");
 		ethjson::test::SkipStates::load(&skip_data[..]).expect("No invalid json allowed")
 	};
+}
 
+#[cfg(not(feature="ci-skip-issue"))]
+lazy_static!{
+	pub static ref SKIP_TEST_STATE: ethjson::test::SkipStates = {
+		ethjson::test::SkipStates::empty()
+	};
 }
