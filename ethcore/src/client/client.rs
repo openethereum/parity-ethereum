b1cabf4ce21bfdf4263dd4cab110584dd0e9378e
Niklas Adofsson <niklas@parity.io>
2018-09-28 09:51:56 +0200

    grumbles(trace) : remove incorrect trace

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -2374,7 +2374,6 @@ impl ProvingBlockChainClient for Client {
 	}
 
 	fn prove_transaction(&self, transaction: SignedTransaction, id: BlockId) -> Option<(Bytes, Vec<DBValue>)> {
-		trace!(target: "pip_provider", "prove_transaction: {:?}", transaction);
 		let (header, mut env_info) = match (self.block_header(id), self.env_info(id)) {
 			(Some(s), Some(e)) => (s, e),
 			_ => return None,
