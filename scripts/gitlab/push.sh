782923615f503c309314d671a36e5d39dbd66631
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2018-09-30 13:42:49 +0200

    Calculate sha3 instead of sha256 for push-release. (#9673)
    * Calculate sha3 instead of sha256 for push-release.
    
    * Add pushes to the script.

--- scripts/gitlab/push.sh
+++ scripts/gitlab/push.sh
@@ -2,11 +2,17 @@
 
 set -e # fail on any error
 set -u # treat unset variables as error
-updater_push_release () {
-  echo "push release"
-  # Mainnet
 
-}
+
+echo "__________Register Release__________"
+DATA="secret=$RELEASES_SECRET"
+
+echo "Pushing release to Mainnet"
+../../scripts/gitlab/safe_curl.sh $DATA "http://update.parity.io:1337/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
+
+echo "Pushing release to Kovan"
+../../scripts/gitlab/safe_curl.sh $DATA "http://update.parity.io:1338/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
+
 echo "__________Set ENVIROMENT__________"
 DESCRIPTION="$(cat CHANGELOG.md)"
 RELEASE_TABLE="$(cat scripts/gitlab/templates/release-table.md)"
@@ -35,9 +41,10 @@ do
   for binary in $(ls parity.sha256)
   do
     sha256=$(cat $binary | awk '{ print $1}' )
+    sha3=$(cat ${binary/sha256/sha3} | awk '{ print $1}' )
     case $DIR in
       x86_64* )
-        DATA="commit=$CI_BUILD_REF&sha3=$sha256&filename=parity$WIN&secret=$RELEASES_SECRET"
+        DATA="commit=$CI_BUILD_REF&sha3=$sha3&filename=parity$WIN&secret=$RELEASES_SECRET"
         ../../scripts/gitlab/safe_curl.sh $DATA "http://update.parity.io:1337/push-build/$CI_BUILD_REF_NAME/$DIR"
         # Kovan
         ../../scripts/gitlab/safe_curl.sh $DATA "http://update.parity.io:1338/push-build/$CI_BUILD_REF_NAME/$DIR"
