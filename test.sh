f3b806b471e55925464e09c71a77bd25d1611906
gabriel klawitter <gabreal@users.noreply.github.com>
2018-10-01 11:55:17 +0100

    test.sh: use cargo --target for platforms other than linux, win or mac (#9650)
    * test.sh: use cargo --target for platforms other than linux, win or mac
    
    * drying test.sh script
    
    * run tests only when not cross-compiling
    
    * quote variable value

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@
 FEATURES="json-tests,ci-skip-issue"
 OPTIONS="--release"
 VALIDATE=1
+THREADS=8
 
 case $1 in
   --no-json)
@@ -29,31 +30,63 @@ esac
 
 set -e
 
-if [ "$VALIDATE" -eq "1" ]; then
-# Validate --no-default-features build
-echo "________Validate build________"
-time cargo check --no-default-features
-time cargo check --manifest-path util/io/Cargo.toml --no-default-features
-time cargo check --manifest-path util/io/Cargo.toml --features "mio"
 
-# Validate chainspecs
-echo "________Validate chainspecs________"
-time ./scripts/validate_chainspecs.sh
+validate () {
+  if [ "$VALIDATE" -eq "1" ]
+  then
+    echo "________Validate build________"
+    time cargo check $@ --no-default-features
+    time cargo check $@ --manifest-path util/io/Cargo.toml --no-default-features
+    time cargo check $@ --manifest-path util/io/Cargo.toml --features "mio"
+  
+    # Validate chainspecs
+    echo "________Validate chainspecs________"
+    time ./scripts/validate_chainspecs.sh
+  else
+    echo "# not validating due to \$VALIDATE!=1"
+  fi
+}
+
+cpp_test () {
+  # Running the C++ example
+  echo "________Running the C++ example________"
+  cd parity-clib-examples/cpp && \
+    mkdir -p build && \
+    cd build && \
+    cmake .. && \
+    make -j $THREADS && \
+    ./parity-example && \
+    cd .. && \
+    rm -rf build && \
+    cd ../..
+}
+
+cargo_test () {
+  echo "________Running Parity Full Test Suite________"
+  git submodule update --init --recursive
+  time cargo test $OPTIONS --features "$FEATURES" --all $@ -- --test-threads $THREADS
+}
+
+
+if [ "$CARGO_TARGET" ]
+then
+  validate --target $CARGO_TARGET
+else
+  validate
 fi
 
-# Running the C++ example
-echo "________Running the C++ example________"
-cd parity-clib-examples/cpp && \
-  mkdir -p build && \
-  cd build && \
-  cmake .. && \
-  make -j 8 && \
-  ./parity-example && \
-  cd .. && \
-  rm -rf build && \
-  cd ../..
+test "${RUN_TESTS}" = "true" && cpp_test
 
-# Running tests
-echo "________Running Parity Full Test Suite________"
-git submodule update --init --recursive
-time cargo test  $OPTIONS --features "$FEATURES" --all $1 -- --test-threads 8
+if [ "$CARGO_TARGET" ]
+then
+
+  if [ "${RUN_TESTS}" = "true" ]
+  then
+    cargo_test --target $CARGO_TARGET $@
+  else
+    cargo_test --no-run --target $CARGO_TARGET $@
+  fi
+else
+  cargo_test $@
+fi
+
