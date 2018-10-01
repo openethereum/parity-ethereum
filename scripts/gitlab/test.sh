ffadc7f8a27488992ae4bc00fef25728ac8f0ad8
5chdn <5chdn@users.noreply.github.com>
2018-10-01 12:48:17 +0100

    ci: remove github release script

--- scripts/gitlab/test.sh
+++ scripts/gitlab/test.sh
@@ -4,10 +4,7 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-
-set -x # full command output for development
 git log --graph --oneline --all --decorate=short -n 10
-
 
 case $CI_COMMIT_REF_NAME in
   (master|beta|stable)
@@ -18,10 +15,8 @@ case $CI_COMMIT_REF_NAME in
   ;;
 esac
 
-
 export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^CHANGELOG.md -e ^test.sh -e ^scripts/ -e ^docs/ | wc -l | tr -d ' ')"
 echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
-
 
 if [ "${RUST_FILES_MODIFIED}" = "0" ]
 then
@@ -29,16 +24,9 @@ then
   exit 0
 fi
 
-
 rustup default $1
 
 git submodule update --init --recursive
 rustup show
 
 exec ./test.sh
-
-# if [[ "$CI_COMMIT_REF_NAME" == "nightly" ]];
-#   ### @TODO re-enable fail after https://github.com/paritytech/parity-import-tests/issues/3
-#   then sh scripts/aura-test.sh; # || exit $?;
-# fi
-
