f749b0e050332c0a2c81197663750d54e873eee2
5chdn <5chdn@users.noreply.github.com>
2018-10-01 22:30:22 +0100

    ci: fix git compare for nightly builds

--- scripts/gitlab/test-all.sh
+++ scripts/gitlab/test-all.sh
@@ -7,12 +7,15 @@ set -u # treat unset variables as error
 git log --graph --oneline --all --decorate=short -n 10
 
 case $CI_COMMIT_REF_NAME in
-  (master|beta|stable)
+  (beta|stable)
     export GIT_COMPARE=$CI_COMMIT_REF_NAME~
+    ;;
+  (master|nightly)
+    export GIT_COMPARE=master~
     ;;
   (*)
     export GIT_COMPARE=master
-  ;;
+    ;;
 esac
 
 export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^CHANGELOG.md -e ^test.sh -e ^scripts/ -e ^docs/ -e ^docker/ -e ^snap/ | wc -l | tr -d ' ')"
