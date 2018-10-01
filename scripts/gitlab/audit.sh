f0f123c82312f2475b04caa896272c270c5840e4
5chdn <5chdn@users.noreply.github.com>
2018-10-01 14:58:25 +0100

    ci: simplify audit script

--- scripts/gitlab/audit.sh
+++ scripts/gitlab/audit.sh
@@ -3,10 +3,5 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-rustup default stable
-
-git submodule update --init --recursive
-
-rustup show
-cargo install cargo-audit
-cargo audit
+cargo +stable install cargo-audit
+cargo +stable audit
