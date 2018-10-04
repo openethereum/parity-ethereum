1556356c55c3360583f96dd8f67cb6700aebcf27
Wei Tang <accounts@that.world>
2018-10-04 20:50:18 +0800

    Implement CREATE2 gas changes and fix some potential overflowing (#9694)
    * Implement CREATE2 gas changes and fix some potential overflowing
    
    * Ignore create2 state tests
    
    * Split CREATE and CREATE2 in gasometer
    
    * Generalize rounding (x + 31) / 32 to to_word_size

--- ethcore/evm/src/evm.rs
+++ ethcore/evm/src/evm.rs
@@ -57,7 +57,7 @@ impl Finalize for Result<GasLeft> {
 
 /// Cost calculation type. For low-gas usage we calculate costs using usize instead of U256
 pub trait CostType: Sized + From<usize> + Copy
-	+ ops::Mul<Output=Self> + ops::Div<Output=Self> + ops::Add<Output=Self> +ops::Sub<Output=Self>
+	+ ops::Mul<Output=Self> + ops::Div<Output=Self> + ops::Add<Output=Self> + ops::Sub<Output=Self>
 	+ ops::Shr<usize, Output=Self> + ops::Shl<usize, Output=Self>
 	+ cmp::Ord + fmt::Debug {
 	/// Converts this cost into `U256`
