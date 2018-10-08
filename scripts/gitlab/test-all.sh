89d9d32334423245144304889a76fc3559bbb077
Denis_P <denis.pisarev@parity.io>
2018-10-08 18:33:29 +0200

    ci2 to build evrtng, minus scripts to check Rustfile changes

--- scripts/gitlab/test-all.sh
+++ scripts/gitlab/test-all.sh
@@ -6,26 +6,26 @@ set -u # treat unset variables as error
 
 git log --graph --oneline --all --decorate=short -n 10
 
-case $CI_COMMIT_REF_NAME in
-  (beta|stable)
-    export GIT_COMPARE=$CI_COMMIT_REF_NAME~
-    ;;
-  (master|nightly)
-    export GIT_COMPARE=master~
-    ;;
-  (*)
-    export GIT_COMPARE=master
-    ;;
-esac
+# case $CI_COMMIT_REF_NAME in
+#   (beta|stable)
+#     export GIT_COMPARE=$CI_COMMIT_REF_NAME~
+#     ;;
+#   (master|nightly)
+#     export GIT_COMPARE=master~
+#     ;;
+#   (*)
+#     export GIT_COMPARE=master
+#     ;;
+# esac
 
-export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^CHANGELOG.md -e ^test.sh -e ^scripts/ -e ^docs/ -e ^docker/ -e ^snap/ | wc -l | tr -d ' ')"
-echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
+# export RUST_FILES_MODIFIED="$(git --no-pager diff --name-only $GIT_COMPARE...$CI_COMMIT_SHA | grep -v -e ^\\. -e ^LICENSE -e ^README.md -e ^CHANGELOG.md -e ^test.sh -e ^scripts/ -e ^docs/ -e ^docker/ -e ^snap/ | wc -l | tr -d ' ')"
+# echo "RUST_FILES_MODIFIED: $RUST_FILES_MODIFIED"
 
-if [ "${RUST_FILES_MODIFIED}" = "0" ]
-then
-  echo "__________Skipping Rust tests since no Rust files modified__________";
-  exit 0
-fi
+# if [ "x${RUST_FILES_MODIFIED}" = "x0" ]
+# then
+#   echo "__________Skipping Rust tests since no Rust files modified__________";
+#   exit 0
+# fi
 
 rustup default $1
 
