7cd45327072fe5b0801ddcbc84c7fc9e42319cca
david-yan <david.yan@berkeley.edu>
2018-10-29 09:55:13 -0700

    Display debug statements using println (#4)
    * Display debug messages using println
    
    * Fix bug

--- ethcore/wasm/src/runtime.rs
+++ ethcore/wasm/src/runtime.rs
@@ -629,7 +629,7 @@ impl<'a> Runtime<'a> {
 
 	fn debug(&mut self, args: RuntimeArgs) -> Result<()>
 	{
-		trace!(target: "wasm", "Contract debug message: {}", {
+		println!("Contract debug message: {}", {
 			let msg_ptr: u32 = args.nth_checked(0)?;
 			let msg_len: u32 = args.nth_checked(1)?;
 
