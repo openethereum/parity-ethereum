4d696366486f2b6e364f6f439b45ae1b05385944
gabriel <gabriel@parity.io>
2018-11-06 19:24:22 +0100

    make android build succeed

--- scripts/gitlab/build-unix.sh
+++ scripts/gitlab/build-unix.sh
@@ -23,6 +23,7 @@ else
   cat .cargo/config
 fi
 
+
 echo "_____ Building target: "$CARGO_TARGET" _____"
 time cargo build --target $CARGO_TARGET --release --features final
 time cargo build --target $CARGO_TARGET --release -p evmbin
@@ -36,15 +37,31 @@ mkdir -p artifacts
 cd artifacts
 mkdir -p $CARGO_TARGET
 cd $CARGO_TARGET
-cp ../../target/$CARGO_TARGET/release/parity ./parity
-cp ../../target/$CARGO_TARGET/release/parity-evm ./parity-evm
-cp ../../target/$CARGO_TARGET/release/ethstore ./ethstore
-cp ../../target/$CARGO_TARGET/release/ethkey ./ethkey
-cp ../../target/$CARGO_TARGET/release/whisper ./whisper
-strip -v ./*
+cp -v ../../target/$CARGO_TARGET/release/parity ./parity
+cp -v ../../target/$CARGO_TARGET/release/parity-evm ./parity-evm
+cp -v ../../target/$CARGO_TARGET/release/ethstore ./ethstore
+cp -v ../../target/$CARGO_TARGET/release/ethkey ./ethkey
+cp -v ../../target/$CARGO_TARGET/release/whisper ./whisper
+
+
+# stripping can also be done on release build time
+# export RUSTFLAGS="${RUSTFLAGS} -C link-arg=-s"
+if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
+then
+  arm-linux-androideabi-strip -v ./*
+else
+  strip -v ./*
+fi
+
 echo "_____ Calculating checksums _____"
 for binary in $(ls)
 do
   rhash --sha256 $binary -o $binary.sha256
-  ./parity tools hash $binary > $binary.sha3
+  if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
+  then
+    echo "> ${binary} cannot be hashed with cross-compiled binary"
+  else
+    ./parity tools hash $binary > $binary.sha3
+  fi
 done
+
