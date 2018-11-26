dedffe2d7cec9e569f7d167a2114ccb1ac7a5315
Nicolas Gotchac <ngotchac@gmail.com>
2018-11-26 12:05:02 +0100

    Fix light client informant while syncing (#9932)
    * Add `is_idle` to LightSync to check importing status
    
    * Use SyncStateWrapper to make sure is_idle gets updates
    
    * Update is_major_import to use verified queue size as well
    
    * Add comment for `is_idle`
    
    * Add Debug to `SyncStateWrapper`
    
    * `fn get` -> `fn into_inner`

--- parity/informant.rs
+++ parity/informant.rs
@@ -184,7 +184,7 @@ impl InformantData for LightNodeInformantData {
 	fn executes_transactions(&self) -> bool { false }
 
 	fn is_major_importing(&self) -> bool {
-		self.sync.is_major_importing_no_sync()
+		self.sync.is_major_importing()
 	}
 
 	fn report(&self) -> Report {
