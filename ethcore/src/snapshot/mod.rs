61e8baee0cbb3efa879433cc3eea197bf014edee
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2018-12-11 12:30:21 +0000

    Bump crossbeam. (#10048)

--- ethcore/src/snapshot/mod.rs
+++ ethcore/src/snapshot/mod.rs
@@ -192,11 +192,11 @@ pub fn take_snapshot<W: SnapshotWriter + Send>(
 			state_guards.push(state_guard);
 		}
 
-		let block_hashes = block_guard.join()?;
+		let block_hashes = block_guard.join().expect("Sub-thread never panics; qed")?;
 		let mut state_hashes = Vec::new();
 
 		for guard in state_guards {
-			let part_state_hashes = guard.join()?;
+			let part_state_hashes = guard.join().expect("Sub-thread never panics; qed")?;
 			state_hashes.extend(part_state_hashes);
 		}
 
