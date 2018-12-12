02bb363be7f42ea092088509c65d4c1769ca4cbd
5chdn <5chdn@users.noreply.github.com>
2018-12-12 14:20:23 +0100

    ci: use absolute paths to end this confusion

--- clique.sh
+++ clique.sh
@@ -2,18 +2,17 @@
 
 set -e
 
-cd $TRAVIS_BUILD_DIR/..
-git clone https://github.com/jwasinger/parity-deploy -b clique
-ln -s $TRAVIS_BUILD_DIR/parity-goerli $TRAVIS_BUILD_DIR/parity-ethereum
-cd parity-deploy
+git clone https://github.com/jwasinger/parity-deploy -b clique /home/travis/build/goerli/parity-deploy
+ln -s /home/travis/build/goerli/parity-goerli /home/travis/build/goerli/parity-ethereum
+cd /home/travis/build/goerli/parity-deploy
 ./build.sh
 ./parity-deploy.sh -c clique -n 2 -gn 2
 
-sed -i '/volumes\:/d' docker-compose.yaml
-sed -i '/myapp\:/d' docker-compose.yaml
-sed -i '/\#driver\:\ local/d' docker-compose.yaml
-sed -i '/driver\_opts\:/d' docker-compose.yaml
-sed -i '/o\:\ uid\=1000\,gid\=1000/d' docker-compose.yaml
+sed -i '/volumes\:/d' ./docker-compose.yaml
+sed -i '/myapp\:/d' ./docker-compose.yaml
+sed -i '/\#driver\:\ local/d' ./docker-compose.yaml
+sed -i '/driver\_opts\:/d' ./docker-compose.yaml
+sed -i '/o\:\ uid\=1000\,gid\=1000/d' ./docker-compose.yaml
 
 docker-compose up
 
