0b0087893821b8a3ba1b5e9cb7cb1841d158028b
5chdn <5chdn@users.noreply.github.com>
2018-12-12 11:30:02 +0100

    ci: fix paths in clique deploy script

--- clique.sh
+++ clique.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+cd $TRAVIS_BUILD_DIR
 git clone https://github.com/jwasinger/parity-deploy -b clique
-ln -s $TRAVIS_BUILD_DIR parity-ethereum
+ln -s $TRAVIS_BUILD_DIR/parity-goerli $TRAVIS_BUILD_DIR/parity-ethereum
 cd parity-deploy
 ./build.sh
 ./parity-deploy.sh -c clique -n 2 -gn 2
