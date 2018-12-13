02b56b87a036368ef59c9ad30d5264763ccb9f4c
joshua-mir <43032097+joshua-mir@users.noreply.github.com>
2018-12-13 14:33:14 +0100

    Change back to throwing error

--- rpc/src/v1/impls/personal.rs
+++ rpc/src/v1/impls/personal.rs
@@ -128,7 +128,10 @@ impl<D: Dispatcher + 'static> Personal for PersonalClient<D> {
 
 		let r = match (self.allow_perm_unlock, duration) {
 			(false, None) => store.unlock_account_temporarily(account, account_pass.into()),
-			(false, _) => store.unlock_account_temporarily(account, account_pass.into()),
+			(false, _) => return Err(errors::unsupported(				(false, _) => store.unlock_account_temporarily(account, account_pass.into()),
+				"Time-unlocking is not supported when permanent unlock is disabled.",	
+				Some("Use personal_sendTransaction or enable permanent unlocking, instead."),	
+			)),
 			(true, Some(0)) => store.unlock_account_permanently(account, account_pass.into()),
 			(true, Some(d)) => store.unlock_account_timed(account, account_pass.into(), Duration::from_secs(d.into())),
 			(true, None) => store.unlock_account_timed(account, account_pass.into(), Duration::from_secs(300)),
