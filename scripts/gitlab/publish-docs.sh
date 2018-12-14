4da8b2942a8673aca7b77ef62809f224b85c1972
Joshua Mir <joshua@parity.io>
2018-12-14 15:29:24 +0100

    publish docs changes for autogen config docs

--- scripts/gitlab/publish-docs.sh
+++ scripts/gitlab/publish-docs.sh
@@ -7,6 +7,7 @@ clone_repos() {
     echo "__________Clone repos__________"
     git clone https://github.com/parity-js/jsonrpc.git jsonrpc
     git clone https://github.com/paritytech/wiki.git wiki
+    git clone https://github.com/paritytech/parity-config-generator
 }
 
 build_docs() {
@@ -15,12 +16,20 @@ build_docs() {
     npm run build:markdown
 }
 
+build_config() {
+    echo "_______Build config docs______"
+    npm install
+    npm run generate-data
+    npm run generate-docs
+}
+
 update_wiki_docs() {
     echo "__________Update WIKI docs__________"
     for file in $(ls jsonrpc/docs); do
         module_name=${file:0:-3}
         mv jsonrpc/docs/$file wiki/JSONRPC-$module_name-module.md
     done
+    mv parity-config-generator/docs/config.md wiki/Configuring-Parity-Ethereum.md
 }
 
 setup_git() {
@@ -38,7 +47,7 @@ commit_files() {
     git checkout -b rpcdoc-update-${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}
     git add .
     git commit -m "Update docs to ${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}"
-    git tag -a "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" -m "Update RPC docs to ${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}"
+    git tag -a "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" -m "Update RPC and config docs to ${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}"
 }
 
 upload_files() {
@@ -55,6 +64,9 @@ mkdir -p "jsonrpc/.parity/$RPC_TRAITS_DIR"
 cp $RPC_TRAITS_DIR/*.rs "jsonrpc/.parity/$RPC_TRAITS_DIR"
 cd jsonrpc
 build_docs
+cd ..
+cd parity-config-generator
+build_config
 cd ..
 update_wiki_docs
 cd wiki
