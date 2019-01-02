9164b327b97faf6c1d240dccef833b3ab7ad36b4
niklasad1 <niklasadolfsson1@gmail.com>
2019-01-02 15:39:01 +0100

    fix(move examples to parity-clib/examples)

--- parity-clib/examples/java/run.sh
+++ parity-clib/examples/java/run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 FLAGS="-Xlint:deprecation"
-PARITY_JAVA="../../parity-clib/Parity.java"
-PARITY_LIB=".:../../target/debug/"
+PARITY_JAVA="../../Parity.java"
+# parity-clib must be built with feature `jni` in debug-mode to work
+PARITY_LIB=".:../../../target/debug/"
 
 # build
+cd ..
+cargo build --features jni
+cd -
 javac $FLAGS -d $PWD $PARITY_JAVA
 javac $FLAGS *.java
 # Setup the path `libparity.so` and run
