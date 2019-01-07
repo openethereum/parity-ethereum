dbea690234f83907b094db4663cd270e935e6987
Denis P <pisarevden@gmail.com>
2019-01-07 23:12:44 +0100

    vars

--- scripts/gitlab/publish-snap.sh
+++ scripts/gitlab/publish-snap.sh
@@ -5,13 +5,25 @@ set -u # treat unset variables as error
 
 echo CI_COMMIT_REF NAME: $CI_COMMIT_REF_NAME
 echo VERSION: $VERSION
+echo SCHEDULE_TAG-CI_COMMIT_REF_NAME: ${SCHEDULE_TAG-${CI_COMMIT_REF_NAME}}" = "nightly" && VERSION="${VERSION}-${ID_SHORT}-${DATE_STR}"
+test "$SCHEDULE_TAG-${CI_COMMIT_REF_NAME}}" = "nightly" 
+echo VERSION-ID_SHORT-DATE_STR: "${VERSION}-${ID_SHORT}-${DATE_STR}"
+test VERSION="${VERSION}-${ID_SHORT}-${DATE_STR}"
+test "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" = "nightly" && VERSION="${VERSION}-${ID_SHORT}-${DATE_STR}"
+
+case $ {track} in
+  nightly) export VERSION_NIGHTLY=vn;;
+  beta) export VERSION_BETA=vb;;
+  stable) export VERSION_STABLE=vs;;
+  *) echo "Non releasable track" exit 0;;
+esac
 
 case ${CI_COMMIT_REF_NAME} in
   nightly|*v2.3*) export GRADE="devel" CHANNEL="edge";;
   beta|*v2.2*) export GRADE="stable" CHANNEL="beta";;
   stable|*v2.1*) export GRADE="stable" CHANNEL="stable";;
   pr|*10142*) export GRADE="debug_grade" CHANNEL="debug_channel";; # delme
-  *) echo "No release";; # exit 0;;
+  *) echo "No release" exit 0;;
 esac
 
 SNAP_PACKAGE="parity_"$VERSION"_"$BUILD_ARCH".snap"
