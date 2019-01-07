7c335e87642b848f61ab3ee7bbff5964b53d6f11
Afri Schoedon <5chdn@users.noreply.github.com>
2019-01-07 11:33:07 +0100

    misc: bump license header to 2019 (#10135)
    * misc: bump license header to 2019
    
    * misc: remove_duplicate_empty_lines.sh
    
    * misc: run license header script
    
    * commit cargo lock

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -1,18 +1,18 @@
-// Copyright 2015-2018 Parity Technologies (UK) Ltd.
-// This file is part of Parity.
+// Copyright 2015-2019 Parity Technologies (UK) Ltd.
+// This file is part of Parity Ethereum.
 
-// Parity is free software: you can redistribute it and/or modify
+// Parity Ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Parity is distributed in the hope that it will be useful,
+// Parity Ethereum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+// along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::cmp;
 use std::collections::{HashSet, BTreeMap, VecDeque};
@@ -2563,7 +2563,6 @@ mod tests {
 		assert_eq!(receipts[1].block_number, 2);
 		assert_eq!(receipts[1].cumulative_gas_used, 106_000.into());
 		assert_eq!(receipts[1].gas_used, 53_000.into());
-
 
 		let receipt = client.transaction_receipt(TransactionId::Hash(receipts[0].transaction_hash));
 		assert_eq!(receipt, Some(receipts[0].clone()));
