ea0c4def6a50cae4048f6f7aa2496f51d3b4d8c5
Denis P <pisarevden@gmail.com>
2019-01-07 23:36:30 +0100

    revert

--- scripts/gitlab/publish-snap.sh
+++ scripts/gitlab/publish-snap.sh
@@ -3,26 +3,10 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-echo CI_COMMIT_REF NAME: $CI_COMMIT_REF_NAME
-echo VERSION: $VERSION
-echo SCHEDULE_TAG-CI_COMMIT_REF_NAME: "${SCHEDULE_TAG-${CI_COMMIT_REF_NAME}}"
-test "$SCHEDULE_TAG-${CI_COMMIT_REF_NAME}}" = "nightly" 
-echo VERSION-ID_SHORT-DATE_STR: "${VERSION}-${ID_SHORT}-${DATE_STR}"
-test VERSION="${VERSION}-${ID_SHORT}-${DATE_STR}"
-test "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" = "nightly" && VERSION="${VERSION}-${ID_SHORT}-${DATE_STR}"
-
-# case $ {track} in
-#   nightly) export VERSION_NIGHTLY=vn;;
-#   beta) export VERSION_BETA=vb;;
-#   stable) export VERSION_STABLE=vs;;
-#   *) echo "Non releasable track" exit 0;;
-# esac
-
 case ${CI_COMMIT_REF_NAME} in
   nightly|*v2.3*) export GRADE="devel" CHANNEL="edge";;
   beta|*v2.2*) export GRADE="stable" CHANNEL="beta";;
   stable|*v2.1*) export GRADE="stable" CHANNEL="stable";;
-  pr|*10142*) export GRADE="debug_grade" CHANNEL="debug_channel";; # delme
   *) echo "No release" exit 0;;
 esac
 
@@ -43,8 +27,8 @@ cat $SNAP_PACKAGE".sha256"
 echo "__________Releasing snap package__________"
 echo "Release channel :" $CHANNEL " Branch/tag: " $CI_COMMIT_REF_NAME
 
-# echo $SNAPCRAFT_LOGIN_PARITY_BASE64 | base64 --decode > snapcraft.login
-# snapcraft login --with snapcraft.login
-# snapcraft push --release $CHANNEL $SNAP_PACKAGE
-# snapcraft status parity
-# snapcraft logout
+echo $SNAPCRAFT_LOGIN_PARITY_BASE64 | base64 --decode > snapcraft.login
+snapcraft login --with snapcraft.login
+snapcraft push --release $CHANNEL $SNAP_PACKAGE
+snapcraft status parity
+snapcraft logout
