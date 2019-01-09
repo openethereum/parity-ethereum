e7e29954d48f97f243430dc31095784766a7fb79
Afri Schoedon <5chdn@users.noreply.github.com>
2019-01-09 14:39:21 +0100

    ci: re-enable snap publishing (#10142)
    * ci: enable snap publishing~
    
    * ci: add publish snap script
    
    * ci: add snapcraft skeleton
    
    * ci: group export statements
    
    * ci: enable snaps on pr branch
    
    * ci: enable snaps on pr branch
    
    * ci: set default BUILD_ARCH
    
    * ci: enable snaps on pr branch
    
    * ci: enable snaps on pr branch
    
    * ci: add libdb to snap
    
    * ci: reinitiate gitlabci
    
    * ci: reinitiate publish-snap script
    
    * ci: fix yaml syntax
    
    * cargo/gitlab env vars
    
    * debug, revert me
    
    * version?
    
    * debug vars
    
    * vars
    
    * vars fix
    
    * vars fix
    
    * revert
    
    * Update scripts/gitlab/publish-snap.sh
    
    Co-Authored-By: 5chdn <5chdn@users.noreply.github.com>
    
    * ci: read track from cargo toml

---
+++ scripts/gitlab/publish-snap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e # fail on any error
+set -u # treat unset variables as error
+
+TRACK=`awk -F '=' '/^track/ {print $2}' ./util/version/Cargo.toml`
+
+case ${TRACK} in
+  nightly) export GRADE="devel" CHANNEL="edge";;
+  beta) export GRADE="stable" CHANNEL="beta";;
+  stable) export GRADE="stable" CHANNEL="stable";;
+  *) echo "No release" exit 0;;
+esac
+
+SNAP_PACKAGE="parity_"$VERSION"_"$BUILD_ARCH".snap"
+
+echo "__________Create snap package__________"
+echo "Release channel :" $GRADE " Branch/tag: " $CI_COMMIT_REF_NAME
+echo $VERSION:$GRADE:$BUILD_ARCH
+cat scripts/snap/snapcraft.template.yaml | envsubst '$VERSION:$GRADE:$BUILD_ARCH:$CARGO_TARGET' > snapcraft.yaml
+cat snapcraft.yaml
+snapcraft --target-arch=$BUILD_ARCH
+ls *.snap
+
+echo "__________Calculating checksums__________"
+rhash --sha256 $SNAP_PACKAGE -o $SNAP_PACKAGE".sha256"
+cat $SNAP_PACKAGE".sha256"
+
+echo "__________Releasing snap package__________"
+echo "Release channel :" $CHANNEL " Branch/tag: " $CI_COMMIT_REF_NAME
+
+echo $SNAPCRAFT_LOGIN_PARITY_BASE64 | base64 --decode > snapcraft.login
+snapcraft login --with snapcraft.login
+snapcraft push --release $CHANNEL $SNAP_PACKAGE
+snapcraft status parity
+snapcraft logout
