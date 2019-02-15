03b74320631d5c7c684001e17b44f855c9a677d1
Fernando Herrero Carrón <fernando@parity.io>
2019-02-15 09:34:59 +0100

    Parse "protocol" attribute arguments

--- util/syncpacketid_derive/src/lib.rs
+++ util/syncpacketid_derive/src/lib.rs
@@ -7,6 +7,33 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 
+fn parse_protocol_arguments(args: syn::MetaList) -> proc_macro2::Ident {
+	if args.nested.len() != 1 {
+		panic!("protocol attribute should have exactly one argument");
+	}
+
+	match args.nested.first().expect("protocol attribute without value").value() {
+		// Meta argument
+		syn::NestedMeta::Meta(meta) => match meta {
+			syn::Meta::Word(ident) => ident.clone(),
+			_ => panic!("nested arguments to protocol are not allowed"),
+		},
+		// Quoted string
+		_ => panic!("protocol argument must be an unquoted identifier")
+	}
+}
+
+/// Helper function to parse arguments to the protocol attribute.
+/// Syntax should be #[protocol(P)] PacketName = 0xNN,
+fn parse_protocol_attribute(input: syn::Meta) -> proc_macro2::Ident {
+	// Arguments to invocation attributes are delivered as a list
+	match input {
+		syn::Meta::Word(_) => panic!("protocol attribute without argument"),
+		syn::Meta::List(args) => parse_protocol_arguments(args),
+		_ => panic!("unsupported syntax")
+	}
+}
+
 /// The SyncPackets derive-macro will provide an enum with this attribute:
 ///
 /// * With a method "from_u8" which will optionally convert a u8 value to
@@ -16,24 +43,9 @@ use quote::quote;
 ///   the protocol from instances of the enum.
 #[proc_macro_derive(SyncPackets, attributes(protocol))]
 pub fn sync_packets(input: TokenStream) -> TokenStream {
-	let ast = syn::parse(input).unwrap();
+	let ast = syn::parse(input).expect("invalid enum syntax");
 	let gen = impl_sync_packets(&ast);
 	gen.into()
-}
-
-fn parse_protocol_attribute(input: proc_macro2::TokenStream) -> proc_macro2::Ident {
-	let groups: Vec<_> = input.into_iter().take(1).collect();
-
-	let group: Vec<_> = match &groups[0] {
-		proc_macro2::TokenTree::Group(g) => g.stream().into_iter().take(1).collect(),
-		_ => panic!()
-	};
-
-	if let proc_macro2::TokenTree::Ident(ref i) = group[0] {
-		proc_macro2::Ident::new(i.to_string().as_ref(), i.span())
-	} else {
-		panic!("Should be an Ident");
-	}
 }
 
 fn impl_sync_packets(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
@@ -44,14 +56,39 @@ fn impl_sync_packets(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 
 	let enum_name = &ast.ident;
 
-	let idents1: Vec<_> = body.variants.iter().map(|v| &v.ident).collect();
-	let idents2 = idents1.clone();
+	if body.variants.is_empty() {
+		panic!("enum {} has no variants defined", enum_name);
+	}
 
-	let prots:Vec<_> = body.variants.iter()
-		.filter(|v| v.attrs.get(0).expect("attribute is missing; annotate your enum patterns with #[eth] or #[par]").path.is_ident("protocol"))
-		.map(|v| parse_protocol_attribute(v.attrs.get(0).unwrap().tts.clone())).collect();
+	// Apparently quote! consumes interpolated variables. Clone ids
+	// to use them twice.
+	let idents_from_u8: Vec<_> = body.variants.iter().map(|v| &v.ident).collect();
+	let idents_enum = idents_from_u8.clone();
 
-	let values: Vec<_> = body.variants.iter().map(|v| v.discriminant.clone().expect("enum pattern is not discriminant; should have assigned unique value such as #[eth] Foo = 1").1).collect();
+	// Within each variant of the enum find the first "protocol" attribute
+	// and extract its argument
+	let protocols:Vec<_> = body.variants.iter()
+		.filter_map(
+			|v| v.attrs
+				.iter()
+				.find(|&x| x.path.is_ident("protocol"))
+				.or_else(|| panic!("enum variant without protocol attribute in {}", &enum_name))
+		).map(|a| parse_protocol_attribute(a
+										   .parse_meta()
+										   .clone()
+										   .expect("unknown arguments passed to protocol"))
+		).collect();
+
+
+	// Values asigned to the variants in the enum
+	let values: Vec<_> = body.variants.iter()
+		.map(
+			|v| v.discriminant
+				.clone()
+				.expect("enum pattern is not discriminant; should have assigned unique value such as Foo = 1")
+				.1
+		)
+		.collect();
 
 	quote!{
 		use network::{PacketId, ProtocolId};
@@ -59,7 +96,7 @@ fn impl_sync_packets(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 		impl #enum_name {
 			pub fn from_u8(id: u8) -> Option<SyncPacket> {
 				match id {
-					#(#values => Some(#idents1)),*,
+					#(#values => Some(#idents_from_u8)),*,
 					_ => None
 
 				}
@@ -78,7 +115,7 @@ fn impl_sync_packets(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 		impl PacketInfo for #enum_name {
 			fn protocol(&self) -> ProtocolId {
 				match self {
-					#(#idents2 => #prots),*
+					#(#idents_enum => #protocols),*
 				}
 			}
 
