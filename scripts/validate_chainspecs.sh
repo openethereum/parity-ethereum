87ab7ff113e64143a8d139424309c5530a6084d8
Denis P <pisarevden@gmail.com>
2019-02-19 15:48:05 +0100

    no --release requires changing the artifacts dir

--- scripts/validate_chainspecs.sh
+++ scripts/validate_chainspecs.sh
@@ -5,11 +5,11 @@ ERR=0
 cargo build -p chainspec
 
 for spec in ethcore/res/*.json; do
-    if ! ./target/release/chainspec "$spec"; then ERR=1; fi
+    if ! ./target/debug/chainspec "$spec"; then ERR=1; fi
 done
 
 for spec in ethcore/res/ethereum/*.json; do
-    if ! ./target/release/chainspec "$spec"; then ERR=1; fi
+    if ! ./target/debug/chainspec "$spec"; then ERR=1; fi
 done
 
 exit $ERR
