e53c22f4fa7c8e5f5511094e2117d568f58ba67c
Yucong Sun <sunyucong@gmail.com>
2019-02-24 00:27:54 -0500

    addressing more comments

--- json/src/spec/clique.rs
+++ json/src/spec/clique.rs
@@ -51,7 +51,7 @@ mod tests {
 		}"#;
 
 		let deserialized: Clique = serde_json::from_str(s).unwrap();
-		assert_eq!(deserialized.params.period, Some(5u64)));
+		assert_eq!(deserialized.params.period, Some(5u64));
 		assert_eq!(deserialized.params.epoch, Some(30000u64));
 	}
 }
