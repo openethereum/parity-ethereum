0d63c932afcbba31b494d96805dce3d77bab05d5
Hernando Castano <HCastano@users.noreply.github.com>
2019-02-25 15:24:44 +0100

    Backports for Stable 2.3.5 (#10414)
    * no-git for publish jobs, empty artifacts dir (#10393)
    
    * no-git for publish jobs, empty artifacts dir
    
    * fix syntax
    
    * prettiness
    
    * fix prettiness
    
    * should get rid of git in publishing
    
    * snap: reenable i386, arm64, armhf architecture publishing (#10386)
    
    * snap: reenable i386, arm64, armhf architecture publishing
    
    * gitlab: fix indent
    
    * gitlab: fix yml syntax
    
    * Linker for crosscomile
    
    * fix target to linker
    
    * new docker image
    
    * fix lint, add build to this PR
    
    * calc SHA3 using rhash
    
    * add new images for i386, armhf
    
    * show snap target & artifacts
    
    * set CARGO_TARGET for publish snap
    
    * move detect Version to publish snap
    
    * rm libc6 dep from snap-template up pub-snap script
    
    * clean up cargo config before add linker
    
    * move linker config to docker images
    
    * tx pool: always accept local transactions (#10375)
    
    * tx pool: always accept local transactions
    
    * tx pool: `choose` local txs with same sender and nonce
    
    * Fix to_pod storage trie value decoding (#10368)

--- scripts/gitlab/build-unix.sh
+++ scripts/gitlab/build-unix.sh
@@ -11,6 +11,9 @@ echo "CC:               " $CC
 echo "CXX:              " $CXX
 #strip ON
 export RUSTFLAGS=" -C link-arg=-s"
+# Linker for crosscomile
+echo "_____ Linker _____"
+cat .cargo/config
 
 echo "_____ Building target: "$CARGO_TARGET" _____"
 if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
@@ -25,6 +28,7 @@ else
 fi
 
 echo "_____ Post-processing binaries _____"
+rm -rf artifacts/*
 mkdir -p artifacts/$CARGO_TARGET
 cd artifacts/$CARGO_TARGET
 
@@ -42,11 +46,6 @@ fi
 echo "_____ Calculating checksums _____"
 for binary in $(ls)
 do
-  rhash --sha256 $binary -o $binary.sha256
-  if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
-  then
-    echo "> ${binary} cannot be hashed with cross-compiled binary"
-  else
-    ./parity tools hash $binary > $binary.sha3
-  fi
+  rhash --sha256 $binary -o $binary.sha256 #do we still need this hash (SHA2)?
+  rhash --sha3-256 $binary -o $binary.sha3
 done
