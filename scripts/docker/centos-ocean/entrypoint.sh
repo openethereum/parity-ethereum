c4215d352affae32daaa71d63fd45fff83785b57
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-03-01 08:26:57 +0100

    Compatibility with keeper 0.8

--- scripts/docker/centos-ocean/entrypoint.sh
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -5,10 +5,17 @@ if [ "${CONFIGURE_ACL}" = "true" ]; then
     acl_contract="${ACL_CONTRACT_ADDRESS}"
   else
     echo "Waiting for contracts to be generated..."
-    while [ ! -f "/contracts/ready" ] || [ ! -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
+    while [ ! -f "/contracts/ready" ]; do
       sleep 2
     done
-    acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+    while [ ! -f "/contracts/AccessSecretStoreCondition.${NETWORK}.json" ] && [ ! -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
+      sleep 2
+    done
+    if [ -f "/contracts/AccessSecretStoreCondition.${NETWORK}.json" ]; then
+      acl_contract=$(cat /contracts/AccessSecretStoreCondition.${NETWORK}.json | jq -r .address)
+    else
+      acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+    fi
   fi
   echo "ACL Contract Address: ${acl_contract}"
   sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
