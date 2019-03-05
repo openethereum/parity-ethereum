8247777adf9406c2981acf56a3ee9331c33f2f5e
Denis P <pisarevden@gmail.com>
2019-03-05 17:59:05 +0100

    microfix

--- scripts/gitlab/publish-aws.sh
+++ scripts/gitlab/publish-aws.sh
@@ -6,10 +6,10 @@ set -u # treat unset variables as error
 echo "__________Push binaries to AWS S3____________"
 case "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" in
     (beta|stable|nightly)
-      export BUCKET=releases.parity.io/ethereum;
+      BUCKET=releases.parity.io/ethereum;
       ;;
     (*)
-      export BUCKET=builds-parity;
+      BUCKET=builds-parity;
       ;;
   esac
 aws s3 sync ./artifacts s3://${BUCKET}/${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}/
