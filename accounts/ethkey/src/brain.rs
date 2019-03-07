03d8fd4c03c869afd283b725d47eec5c19c7e46e
Denis P <pisarevden@gmail.com>
2019-03-07 20:09:47 +0100

    pipeline must go 3

--- accounts/ethkey/src/brain.rs
+++ accounts/ethkey/src/brain.rs
@@ -1,7 +1,6 @@
 // Copyright 2015-2019 Parity Technologies (UK) Ltd.
 // This file is part of Parity Ethereum.
 
-
 // Parity Ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
