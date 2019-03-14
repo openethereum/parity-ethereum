4e8a741548bcbd82781dcc9f79cf78bfb27a5483
Wei Tang <accounts@that.world>
2019-03-14 14:41:59 +0100

    Remove ci-skip-tests flag for stable

--- scripts/gitlab/test-linux.sh
+++ scripts/gitlab/test-linux.sh
@@ -3,7 +3,7 @@ echo "________Running test-linux.sh________"
 set -e # fail on any error
 set -u # treat unset variables as error
 
-FEATURES="json-tests,ci-skip-issue"
+FEATURES="json-tests"
 OPTIONS="--release"
 #use nproc `linux only
 THREADS=$(nproc)
