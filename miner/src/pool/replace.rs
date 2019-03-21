00f07c3a686714b9795cba83f893925257f9e3ff
Andrew Jones <ascjones@gmail.com>
2019-03-21 15:46:31 +0000

    Make tests compile

--- miner/src/pool/replace.rs
+++ miner/src/pool/replace.rs
@@ -15,10 +15,9 @@
 // along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::cmp;
-use std::collections::HashMap;
 
 use ethereum_types::U256;
-use txpool::{self, scoring::{Choice, Scoring}, Transaction, Readiness, ReplaceTransaction};
+use txpool::{self, scoring::{Choice, Scoring}, ReplaceTransaction};
 use txpool::{VerifiedTransaction as PoolVerifiedTransaction};
 use super::{client, ScoredTransaction, VerifiedTransaction};
 
@@ -39,16 +38,14 @@ impl<S, C> txpool::ShouldReplace<VerifiedTransaction> for NonceAndGasPriceAndRea
 
 	fn should_replace(
 		&mut self,
-		old_replace: txpool::ReplaceTransaction<VerifiedTransaction>,
-		new_replace: txpool::ReplaceTransaction<VerifiedTransaction>,
+		old: &ReplaceTransaction<VerifiedTransaction>,
+		new: &ReplaceTransaction<VerifiedTransaction>,
 	) -> Choice {
-		let old = old_replace.transaction;
-		let new = new_replace.transaction;
 		let both_local = old.priority().is_local() && new.priority().is_local();
 		if old.sender() == new.sender() {
 			// prefer earliest transaction
 			match new.nonce().cmp(&old.nonce()) {
-				cmp::Ordering::Equal => self.scoring.choose(old, new),
+				cmp::Ordering::Equal => self.scoring.choose(&old, &new),
 				_ if both_local => Choice::InsertNew,
 				cmp::Ordering::Less => Choice::ReplaceOld,
 				cmp::Ordering::Greater => Choice::RejectNew,
@@ -61,8 +58,8 @@ impl<S, C> txpool::ShouldReplace<VerifiedTransaction> for NonceAndGasPriceAndRea
 			if new_score > old_score {
 				let state = &self.client;
 				// calculate readiness based on state nonce + pooled txs from same sender
-				let is_ready = |replace: ReplaceTransaction<VerifiedTransaction>| {
-					let mut state_nonce = state.account_nonce(replace.transaction.sender());
+				let is_ready = |replace: &ReplaceTransaction<VerifiedTransaction>| {
+					let state_nonce = state.account_nonce(replace.transaction.sender());
 					let nonce =
 						replace.pooled_by_sender.map_or(state_nonce, |txs| {
 							txs.iter().fold(state_nonce, |nonce, tx| {
@@ -76,7 +73,7 @@ impl<S, C> txpool::ShouldReplace<VerifiedTransaction> for NonceAndGasPriceAndRea
 					nonce == replace.transaction.transaction.nonce()
 				};
 
-				if !is_ready(new_replace) && is_ready(old_replace) {
+				if !is_ready(new) && is_ready(old) {
 					// prevent a ready transaction being replace by a non-ready transaction
 					Choice::RejectNew
 				} else {
@@ -93,6 +90,7 @@ impl<S, C> txpool::ShouldReplace<VerifiedTransaction> for NonceAndGasPriceAndRea
 mod tests {
 	use super::*;
 
+	use std::sync::Arc;
 	use ethkey::{Random, Generator, KeyPair};
 	use pool::tests::tx::{Tx, TxExt};
 	use pool::tests::client::TestClient;
@@ -101,10 +99,18 @@ mod tests {
 	use txpool::scoring::Choice::*;
 	use txpool::ShouldReplace;
 
-	fn local_tx_verified(tx: Tx, keypair: &KeyPair) -> VerifiedTransaction {
+	fn local_tx_verified(tx: Tx, keypair: &KeyPair) -> ReplaceTransaction<VerifiedTransaction> {
 		let mut verified_tx = tx.unsigned().sign(keypair.secret(), None).verified();
 		verified_tx.priority = ::pool::Priority::Local;
-		verified_tx
+		replace_tx(verified_tx)
+	}
+
+	fn replace_tx<'a>(tx: VerifiedTransaction, ) -> ReplaceTransaction<'a, VerifiedTransaction> {
+		let tx = txpool::Transaction {
+			insertion_id: 0,
+			transaction: Arc::new(tx),
+		};
+		ReplaceTransaction::new(tx, Default::default())
 	}
 
 	#[test]
@@ -134,27 +140,29 @@ mod tests {
 		}, &keypair);
 
 		// different sender txs
+		let sender1 = Random.generate().unwrap();
 		let different_sender_tx1 = local_tx_verified(Tx {
 			nonce: 2,
 			gas_price: 1,
 			..Default::default()
-		}, &Random.generate().unwrap());
+		}, &sender1);
 
+		let sender2 = Random.generate().unwrap();
 		let different_sender_tx2 = local_tx_verified(Tx {
 			nonce: 1,
 			gas_price: 10,
 			..Default::default()
-		}, &Random.generate().unwrap());
+		}, &sender2);
 
-		assert_eq!(replace.should_replace(&same_sender_tx1, &same_sender_tx2, None), InsertNew);
-		assert_eq!(replace.should_replace(&same_sender_tx2, &same_sender_tx1, None), InsertNew);
+		assert_eq!(replace.should_replace(&same_sender_tx1, &same_sender_tx2), InsertNew);
+		assert_eq!(replace.should_replace(&same_sender_tx2, &same_sender_tx1), InsertNew);
 
-		assert_eq!(replace.should_replace(&different_sender_tx1, &different_sender_tx2, None), InsertNew);
-		assert_eq!(replace.should_replace(&different_sender_tx2, &different_sender_tx1, None), InsertNew);
+		assert_eq!(replace.should_replace(&different_sender_tx1, &different_sender_tx2), InsertNew);
+		assert_eq!(replace.should_replace(&different_sender_tx2, &different_sender_tx1), InsertNew);
 
 		// txs with same sender and nonce
-		assert_eq!(replace.should_replace(&same_sender_tx2, &same_sender_tx3, None), ReplaceOld);
-		assert_eq!(replace.should_replace(&same_sender_tx3, &same_sender_tx2, None), RejectNew);
+		assert_eq!(replace.should_replace(&same_sender_tx2, &same_sender_tx3), ReplaceOld);
+		assert_eq!(replace.should_replace(&same_sender_tx3, &same_sender_tx2), RejectNew);
 	}
 
 	#[test]
@@ -185,17 +193,17 @@ mod tests {
 
 		let keypair = Random.generate().unwrap();
 		let txs = vec![tx1, tx2, tx3, tx4].into_iter().map(|tx| {
-			tx.unsigned().sign(keypair.secret(), None).verified()
+			replace_tx(tx.unsigned().sign(keypair.secret(), None).verified())
 		}).collect::<Vec<_>>();
 
-		assert_eq!(replace.should_replace(&txs[0], &txs[1], None), RejectNew);
-		assert_eq!(replace.should_replace(&txs[1], &txs[0], None), ReplaceOld);
+		assert_eq!(replace.should_replace(&txs[0], &txs[1]), RejectNew);
+		assert_eq!(replace.should_replace(&txs[1], &txs[0]), ReplaceOld);
 
-		assert_eq!(replace.should_replace(&txs[1], &txs[2], None), RejectNew);
-		assert_eq!(replace.should_replace(&txs[2], &txs[1], None), RejectNew);
+		assert_eq!(replace.should_replace(&txs[1], &txs[2]), RejectNew);
+		assert_eq!(replace.should_replace(&txs[2], &txs[1]), RejectNew);
 
-		assert_eq!(replace.should_replace(&txs[1], &txs[3], None), ReplaceOld);
-		assert_eq!(replace.should_replace(&txs[3], &txs[1], None), RejectNew);
+		assert_eq!(replace.should_replace(&txs[1], &txs[3]), ReplaceOld);
+		assert_eq!(replace.should_replace(&txs[3], &txs[1]), RejectNew);
 	}
 
 	#[test]
@@ -210,7 +218,7 @@ mod tests {
 				gas_price: 1,
 				..Default::default()
 			};
-			tx.signed().verified()
+			replace_tx(tx.signed().verified())
 		};
 		let tx_regular_high_gas = {
 			let tx = Tx {
@@ -218,7 +226,7 @@ mod tests {
 				gas_price: 10,
 				..Default::default()
 			};
-			tx.signed().verified()
+			replace_tx(tx.signed().verified())
 		};
 		let tx_local_low_gas = {
 			let tx = Tx {
@@ -228,7 +236,7 @@ mod tests {
 			};
 			let mut verified_tx = tx.signed().verified();
 			verified_tx.priority = ::pool::Priority::Local;
-			verified_tx
+			replace_tx(verified_tx)
 		};
 		let tx_local_high_gas = {
 			let tx = Tx {
@@ -238,16 +246,39 @@ mod tests {
 			};
 			let mut verified_tx = tx.signed().verified();
 			verified_tx.priority = ::pool::Priority::Local;
-			verified_tx
+			replace_tx(verified_tx)
 		};
 
-		assert_eq!(replace.should_replace(&tx_regular_low_gas, &tx_regular_high_gas, None), ReplaceOld);
-		assert_eq!(replace.should_replace(&tx_regular_high_gas, &tx_regular_low_gas, None), RejectNew);
+		assert_eq!(replace.should_replace(&tx_regular_low_gas, &tx_regular_high_gas), ReplaceOld);
+		assert_eq!(replace.should_replace(&tx_regular_high_gas, &tx_regular_low_gas), RejectNew);
 
-		assert_eq!(replace.should_replace(&tx_regular_high_gas, &tx_local_low_gas, None), ReplaceOld);
-		assert_eq!(replace.should_replace(&tx_local_low_gas, &tx_regular_high_gas, None), RejectNew);
+		assert_eq!(replace.should_replace(&tx_regular_high_gas, &tx_local_low_gas), ReplaceOld);
+		assert_eq!(replace.should_replace(&tx_local_low_gas, &tx_regular_high_gas), RejectNew);
 
-		assert_eq!(replace.should_replace(&tx_local_low_gas, &tx_local_high_gas, None), InsertNew);
-		assert_eq!(replace.should_replace(&tx_local_high_gas, &tx_regular_low_gas, None), RejectNew);
+		assert_eq!(replace.should_replace(&tx_local_low_gas, &tx_local_high_gas), InsertNew);
+		assert_eq!(replace.should_replace(&tx_local_high_gas, &tx_regular_low_gas), RejectNew);
 	}
+
+//	#[test]
+//	fn should_not_replace_ready_transaction_with_future_transaction() {
+//		let scoring = NonceAndGasPrice(PrioritizationStrategy::GasPriceOnly);
+//		let tx_ready_low_score = {
+//			let tx = Tx {
+//				nonce: 1,
+//				gas_price: 1,
+//				..Default::default()
+//			};
+//			tx.signed().verified()
+//		};
+//		let tx_future_high_score = {
+//			let tx = Tx {
+//				nonce: 3, // future nonce
+//				gas_price: 10,
+//				..Default::default()
+//			};
+//			tx.signed().verified()
+//		};
+//
+//		assert_eq!(should_replace(&scoring, &tx_ready_low_score, &tx_future_high_score), RejectNew);
+//	}
 }
