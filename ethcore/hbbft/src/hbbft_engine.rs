6e90906596b049487a7ecf254710b58a4d12230e
davidf <dforsten@gmail.com>
2019-03-30 17:37:44 +0100

    Implemented a minimal Parity Consensus Engine to implement HoneyBadgerBFT

---
+++ ethcore/hbbft/src/hbbft_engine.rs
@@ -0,0 +1,35 @@
+use std::sync::Arc;
+
+use ethcore::engines::{total_difficulty_fork_choice, Engine, ForkChoice};
+use ethcore::error::Error;
+use ethcore::header::{ExtendedHeader, Header};
+use ethcore::machine::EthereumMachine;
+
+pub struct HoneyBadgerBFT {
+	machine: EthereumMachine,
+}
+
+impl HoneyBadgerBFT {
+	pub fn new(machine: EthereumMachine) -> Result<Arc<Self>, Error> {
+		let engine = Arc::new(HoneyBadgerBFT { machine: machine });
+		Ok(engine)
+	}
+}
+
+impl Engine<EthereumMachine> for HoneyBadgerBFT {
+	fn name(&self) -> &str {
+		"HoneyBadgerBFT"
+	}
+
+	fn machine(&self) -> &EthereumMachine {
+		&self.machine
+	}
+
+	fn verify_local_seal(&self, _header: &Header) -> Result<(), Error> {
+		Ok(())
+	}
+
+	fn fork_choice(&self, new: &ExtendedHeader, current: &ExtendedHeader) -> ForkChoice {
+		total_difficulty_fork_choice(new, current)
+	}
+}
--- ethcore/hbbft/src/lib.rs
+++ ethcore/hbbft/src/lib.rs
@@ -1,13 +1,25 @@
 extern crate ethcore;
 
+pub mod hbbft_engine;
+
 #[cfg(test)]
 mod tests {
 	use ethcore::client::TestBlockChainClient;
+	use ethcore::machine::EthereumMachine;
+	use ethcore::spec::CommonParams;
+	use hbbft_engine::HoneyBadgerBFT;
 
 	#[test]
 	fn test_nodes_p2p() {
+		// create machine
+		let c_params = CommonParams::default();
+		let machine = EthereumMachine::regular(c_params, Default::default());
+
+		// create engine
+		let _engine = HoneyBadgerBFT::new(machine);
+
+		// create test clients (which also creates the miner)
 		let _node_0 = TestBlockChainClient::default();
 		let _node_1 = TestBlockChainClient::default();
-    }
+	}
 }
-
