dc6d4e066377fcc8ddb170be48ea8f13e949fc0f
IntegralTeam <repo@sfxdx.ru>
2019-04-03 15:58:12 +0700

    replace spaces with tabs and remove params

--- rpc/src/v1/impls/transactions_pool.rs
+++ rpc/src/v1/impls/transactions_pool.rs
@@ -5,7 +5,7 @@ use jsonrpc_core::Result;
 use jsonrpc_core::futures::Future;
 use jsonrpc_pubsub::{SubscriptionId, typed::{Sink, Subscriber}};
 
-use v1::helpers::{errors, Subscribers};
+use v1::helpers::Subscribers;
 use v1::metadata::Metadata;
 use v1::traits::TransactionsPool;
 use v1::types::pubsub;
@@ -18,86 +18,76 @@ type Client = Sink<pubsub::Result>;
 
 /// Transactions pool PubSub implementation.
 pub struct TransactionsPoolClient {
-    handler: Arc<TransactionsNotificationHandler>,
-    transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>,
+	handler: Arc<TransactionsNotificationHandler>,
+	transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>,
 }
 
 impl TransactionsPoolClient {
-    /// Creates new `TransactionsPoolClient`.
-    pub fn new(executor: Executor) -> Self {
-        let transactions_pool_subscribers = Arc::new(RwLock::new(Subscribers::default()));
-        let handler = Arc::new(
-            TransactionsNotificationHandler::new(
-                executor,
-                transactions_pool_subscribers.clone(),
-                )
-        );
+	/// Creates new `TransactionsPoolClient`.
+	pub fn new(executor: Executor) -> Self {
+		let transactions_pool_subscribers = Arc::new(RwLock::new(Subscribers::default()));
+		let handler = Arc::new(
+			TransactionsNotificationHandler::new(
+				executor,
+				transactions_pool_subscribers.clone(),
+			)
+		);
 
-        TransactionsPoolClient {
-            handler,
-            transactions_pool_subscribers,
-        }
-    }
-    pub fn run(&self, pool_receiver: mpsc::Receiver<(H256, String)>) {
-        let handler = self.handler.clone();
-        thread::spawn(move || loop {
-            let res = pool_receiver.recv();
-            if let Ok(res) = res {
-                handler.notify_transaction(res)
-            }
-        });
-    }
+		TransactionsPoolClient {
+			handler,
+			transactions_pool_subscribers,
+		}
+	}
 
+	pub fn run(&self, pool_receiver: mpsc::Receiver<(H256, String)>) {
+		let handler = self.handler.clone();
+		thread::spawn(move || loop {
+			let res = pool_receiver.recv();
+			if let Ok(res) = res {
+				handler.notify_transaction(res)
+			}
+		});
+	}
 }
 
+/// Transactions pool PubSub Notification handler.
 pub struct TransactionsNotificationHandler {
-    executor: Executor,
-    transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>,
+	executor: Executor,
+	transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>,
 }
 
 impl TransactionsNotificationHandler {
-    fn new(executor: Executor, transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>) -> Self {
-        TransactionsNotificationHandler {
-            executor,
-            transactions_pool_subscribers,
-        }
-    }
+	fn new(executor: Executor, transactions_pool_subscribers: Arc<RwLock<Subscribers<Client>>>) -> Self {
+		TransactionsNotificationHandler {
+			executor,
+			transactions_pool_subscribers,
+		}
+	}
 
-    fn notify(executor: &Executor, subscriber: &Client, result: pubsub::Result) {
-        executor.spawn(subscriber
-            .notify(Ok(result))
-            .map(|_| ())
-            .map_err(|e| warn!(target: "rpc", "Unable to send notification: {}", e))
-        );
-    }
+	fn notify(executor: &Executor, subscriber: &Client, result: pubsub::Result) {
+		executor.spawn(subscriber
+			.notify(Ok(result))
+			.map(|_| ())
+			.map_err(|e| warn!(target: "rpc", "Unable to send notification: {}", e))
+		);
+	}
 
-    pub fn notify_transaction(&self, status: (H256, String)) {
-        for subscriber in self.transactions_pool_subscribers.read().values() {
-            let status = (status.0.clone(), status.1.clone());
-            Self::notify(&self.executor, subscriber, pubsub::Result::TransactionStatus(status));
-        }
-    }
+	pub fn notify_transaction(&self, status: (H256, String)) {
+		for subscriber in self.transactions_pool_subscribers.read().values() {
+			Self::notify(&self.executor, subscriber, pubsub::Result::TransactionStatus(status.clone()));
+		}
+	}
 }
 
 impl TransactionsPool for TransactionsPoolClient {
-    type Metadata = Metadata;
+	type Metadata = Metadata;
 
-    fn subscribe(&self, _meta: Metadata, subscriber: Subscriber<pubsub::Result>, params: Option<pubsub::Params>) {
-        let error = match params {
-            None => {
-                self.transactions_pool_subscribers.write().push(subscriber);
-                return;
-            }
-            _ => {
-                errors::invalid_params("parity_watchTransactionsPool", "Expected no parameters.")
-            }
-        };
+	fn subscribe(&self, _meta: Metadata, subscriber: Subscriber<pubsub::Result>) {
+		self.transactions_pool_subscribers.write().push(subscriber);
+	}
 
-        let _ = subscriber.reject(error);
-    }
-
-    fn unsubscribe(&self, _meta: Option<Metadata>, id: SubscriptionId) -> Result<bool> {
-        let res = self.transactions_pool_subscribers.write().remove(&id).is_some();
-        Ok(res)
-    }
+	fn unsubscribe(&self, _meta: Option<Metadata>, id: SubscriptionId) -> Result<bool> {
+		let res = self.transactions_pool_subscribers.write().remove(&id).is_some();
+		Ok(res)
+	}
 }
--- rpc/src/v1/traits/transactions_pool.rs
+++ rpc/src/v1/traits/transactions_pool.rs
@@ -7,14 +7,14 @@ use v1::types::pubsub;
 /// Transactions Pool PUB-SUB rpc interface.
 #[rpc]
 pub trait TransactionsPool {
-    /// Pub/Sub Metadata
-    type Metadata;
+	/// Pub/Sub Metadata
+	type Metadata;
 
-    /// Subscribe to Transactions Pool subscription.
-    #[pubsub(subscription = "parity_watchTransactionsPool", subscribe, name = "parity_watchTransactionsPool")]
-    fn subscribe(&self, Self::Metadata, typed::Subscriber<pubsub::Result>, Option<pubsub::Params>);
+	/// Subscribe to Transactions Pool subscription.
+	#[pubsub(subscription = "parity_watchTransactionsPool", subscribe, name = "parity_watchTransactionsPool")]
+	fn subscribe(&self, Self::Metadata, typed::Subscriber<pubsub::Result>);
 
-    /// Unsubscribe from existing Transactions Pool subscription.
-    #[pubsub(subscription = "parity_watchTransactionsPool", unsubscribe, name = "parity_unwatchTransactionsPool")]
-    fn unsubscribe(&self, Option<Self::Metadata>, SubscriptionId) -> Result<bool>;
+	/// Unsubscribe from existing Transactions Pool subscription.
+	#[pubsub(subscription = "parity_watchTransactionsPool", unsubscribe, name = "parity_unwatchTransactionsPool")]
+	fn unsubscribe(&self, Option<Self::Metadata>, SubscriptionId) -> Result<bool>;
 }
