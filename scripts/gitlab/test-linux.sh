099c73a967f6623f5cecc3c82c288bbcc9104e67
Denis P <pisarevden@gmail.com>
2019-04-09 18:07:01 +0200

    Rust nightly and beta test runs reintroduced

--- scripts/gitlab/test-linux.sh
+++ scripts/gitlab/test-linux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# ARGUMENT $1 Rust flavor to run test with (stable/beta/nightly)
+
 echo "________Running test-linux.sh________"
 set -e # fail on any error
 set -u # treat unset variables as error
@@ -7,6 +9,9 @@ FEATURES="json-tests,ci-skip-tests"
 OPTIONS="--release"
 #use nproc `linux only
 THREADS=$(nproc)
+
+rustup default $1
+rustup show
 
 echo "________Running Parity Full Test Suite________"
 time cargo test $OPTIONS --features "$FEATURES" --locked --all --target $CARGO_TARGET --verbose -- --test-threads $THREADS
