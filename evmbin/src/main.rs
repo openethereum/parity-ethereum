b65215f222437b485cc0f62afeed613ced693179
Luke Schoen <ltfschoen@gmail.com>
2019-05-14 17:18:43 +0200

    restore: Restore original Rustdocs of evmbin

--- evmbin/src/main.rs
+++ evmbin/src/main.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Parity EVM interpreter binary.
+
 #![warn(missing_docs)]
 
 extern crate common_types as types;
