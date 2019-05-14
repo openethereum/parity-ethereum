714b9b28ca8bee265d18c505cd1005a3f385fb7b
Denis P <denis.pisarev@parity.io>
2019-05-14 13:34:07 +0200

    some new tags

--- scripts/docker/hub/publish-docker.sh
+++ scripts/docker/hub/publish-docker.sh
@@ -2,6 +2,9 @@
 
 set -e # fail on any error
 
+VERSION="$(cat ./artifacts/VERSION)"
+echo "Parity Ethereum version = ${VERSION}"
+
 # we stopped pushing nightlies to dockerhub, will push to own registry prb.
 case "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" in
     $SCHEDULE_TAG)
@@ -9,25 +12,38 @@ case "${SCHEDULE_TAG:-${CI_COMMIT_REF_NAME}}" in
         docker build --no-cache \
             --build-arg VCS_REF="${CI_COMMIT_SHORT_SHA}" \
             --build-arg BUILD_DATE="$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
-            --tag ${CONTAINER_IMAGE}:$SCHEDULE_TAG \
+            --tag ${CONTAINER_IMAGE}:${SCHEDULE_TAG} \
             --file artifacts/Dockerfile .;
-        docker push ${CONTAINER_IMAGE}:$SCHEDULE_TAG;;
+        docker push ${CONTAINER_IMAGE}:${SCHEDULE_TAG};;
     beta)
-        echo "Docker TAGs - ${CONTAINER_IMAGE}:beta, ${CONTAINER_IMAGE}:latest";
+        echo "Docker TAGs - ${CONTAINER_IMAGE}:beta, ${CONTAINER_IMAGE}:latest, \
+            ${CONTAINER_IMAGE}:${VERSION}";
         docker build --no-cache \
             --build-arg VCS_REF="${CI_COMMIT_SHORT_SHA}" \
             --build-arg BUILD_DATE="$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
             --tag ${CONTAINER_IMAGE}:beta \
             --tag ${CONTAINER_IMAGE}:latest \
+            --tag ${CONTAINER_IMAGE}:${VERSION} \
             --file artifacts/Dockerfile .;
         docker push ${CONTAINER_IMAGE}:beta;
-        docker push ${CONTAINER_IMAGE}:latest ;;
-    stable|*)
-        echo "Docker TAGs - ${CONTAINER_IMAGE}:${CI_COMMIT_REF_NAME}";
+        docker push ${CONTAINER_IMAGE}:latest;
+        docker push ${CONTAINER_IMAGE}:${VERSION};;
+    stable)
+        echo "Docker TAGs - ${CONTAINER_IMAGE}:${VERSION}, ${CONTAINER_IMAGE}:stable";
         docker build --no-cache \
             --build-arg VCS_REF="${CI_COMMIT_SHORT_SHA}" \
             --build-arg BUILD_DATE="$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
-            --tag ${CONTAINER_IMAGE}:$CI_COMMIT_REF_NAME \
+            --tag ${CONTAINER_IMAGE}:${VERSION} \
+            --tag ${CONTAINER_IMAGE}:stable \
             --file artifacts/Dockerfile .;
-        docker push ${CONTAINER_IMAGE}:$CI_COMMIT_REF_NAME ;;
+        docker push ${CONTAINER_IMAGE}:${VERSION};
+        docker push ${CONTAINER_IMAGE}:stable;;
+    *)
+        echo "Docker TAG - ${CONTAINER_IMAGE}:${VERSION}"
+        docker build --no-cache \
+            --build-arg VCS_REF="${CI_COMMIT_SHORT_SHA}" \
+            --build-arg BUILD_DATE="$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
+            --tag ${CONTAINER_IMAGE}:${VERSION}-${CI_COMMIT_SHORT_SHA} \
+            --file artifacts/Dockerfile .;
+        docker push ${CONTAINER_IMAGE}:${VERSION}-${CI_COMMIT_SHORT_SHA};;
 esac
