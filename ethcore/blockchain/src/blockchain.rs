b6800262179b4331c2b8d9d61ffd69bee4742ba7
David Palm <dvdplm@gmail.com>
2019-06-10 15:19:23 +0200

    Extensive logging on unexpected non-canonical hash

--- ethcore/blockchain/src/blockchain.rs
+++ ethcore/blockchain/src/blockchain.rs
@@ -1010,7 +1010,14 @@ impl BlockChain {
 						.take_while(|t| t.block_number <= details.number)
 						.last()
 				},
-				Some(h) => trace!(target: "blockchain", "Block #{}: Found non-canonical block hash {} (expected {})", details.number, h, hash),
+				Some(h) => {
+					warn!(target: "blockchain", "Block #{}: Found non-canonical block hash {} (expected {})", details.number, h, hash);
+
+					trace!(target: "blockchain", "#{} {} != {} HASH UNEQUAL, #{}", details.number, hash, h, self.block_number(&h).unwrap_or_default() );
+					trace!(target: "blockchain", "      Version A {}: #{:#?}", hash, self.block_details(&hash));
+					trace!(target: "blockchain", "      Version B {}: #{:#?}", h, self.block_details(&h));
+
+				},
 				None => trace!(target: "blockchain", "Block #{}: hash {} not found in cache or DB", details.number, hash),
 			}
 		}
