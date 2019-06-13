79d7611ca0a298e537f0791b67d82c20eaa54616
Andrew Jones <ascjones@gmail.com>
2019-06-13 16:50:05 +0100

    Replace error chain for network error

--- util/network/src/error.rs
+++ util/network/src/error.rs
@@ -18,7 +18,7 @@
 // https://github.com/paritytech/parity-ethereum/issues/10302
 #![allow(deprecated)]
 
-use std::{io, net, fmt};
+use std::{error, io, net, fmt};
 use libc::{ENFILE, EMFILE};
 use io::IoError;
 use {rlp, ethkey, crypto, snappy};
@@ -85,84 +85,60 @@ impl fmt::Display for DisconnectReason {
 	}
 }
 
-error_chain! {
-	foreign_links {
-		SocketIo(IoError) #[doc = "Socket IO error."];
-		Decompression(snappy::InvalidInput) #[doc = "Decompression error."];
-		Rlp(rlp::DecoderError) #[doc = "Rlp decoder error."];
-	}
+/// Queue error
+#[derive(Debug, derive_more::Display)]
+pub enum Error {
+	/// Socket IO error.
+	SocketIo(IoError),
+	/// Decompression error.
+	Decompression(snappy::InvalidInput),
+	/// Rlp decoder error.
+	Rlp(rlp::DecoderError),
+	/// Error concerning the network address parsing subsystem.
+	#[display(fmt ="Failed to parse network address")]
+	AddressParse,
+	/// Error concerning the network address resolution subsystem.
+	#[display(fmt ="Failed to resolve network address {}", "_0.as_ref().map_or("".to_string(), |e| e.to_string())")]
+	AddressResolve(Option<io::Error>),
+	/// Authentication failure
+	#[display(fmt ="Authentication failure")]
+	Auth,
+	/// Unrecognised protocol
+	#[display(fmt ="Bad protocol")]
+	BadProtocol,
+	/// Expired message
+	#[display(fmt ="Expired message")]
+	Expired,
+	/// Peer not found
+	#[display(fmt ="Peer not found")]
+	PeerNotFound,
+	/// Peer is disconnected
+	#[display(fmt ="Peer disconnected: {}", _0)]
+	Disconnect(DisconnectReason),
+	/// Invalid node id
+	#[display(fmt ="Invalid node id")]
+	InvalidNodeId,
+	/// Packet size is over the protocol limit
+	#[display(fmt ="Packet is too large")]
+	OversizedPacket,
+	/// Reached system resource limits for this process
+	#[display(fmt ="Too many open files in this process. Check your resource limits and restart parity")]
+	ProcessTooManyFiles,
+	/// Reached system wide resource limits
+	#[display(fmt ="Too many open files on system. Consider closing some processes/release some file handlers or increas the system-wide resource limits and restart parity.")]
+	SystemTooManyFiles,
+	/// An unknown IO error occurred.
+	#[display(fmt ="Unexpected IO error: {}", _0)]
+	Io(io::Error),
+}
 
-	errors {
-		#[doc = "Error concerning the network address parsing subsystem."]
-		AddressParse {
-			description("Failed to parse network address"),
-			display("Failed to parse network address"),
-		}
-
-		#[doc = "Error concerning the network address resolution subsystem."]
-		AddressResolve(err: Option<io::Error>) {
-			description("Failed to resolve network address"),
-			display("Failed to resolve network address {}", err.as_ref().map_or("".to_string(), |e| e.to_string())),
-		}
-
-		#[doc = "Authentication failure"]
-		Auth {
-			description("Authentication failure"),
-			display("Authentication failure"),
-		}
-
-		#[doc = "Unrecognised protocol"]
-		BadProtocol {
-			description("Bad protocol"),
-			display("Bad protocol"),
-		}
-
-		#[doc = "Expired message"]
-		Expired {
-			description("Expired message"),
-			display("Expired message"),
-		}
-
-		#[doc = "Peer not found"]
-		PeerNotFound {
-			description("Peer not found"),
-			display("Peer not found"),
-		}
-
-		#[doc = "Peer is disconnected"]
-		Disconnect(reason: DisconnectReason) {
-			description("Peer disconnected"),
-			display("Peer disconnected: {}", reason),
-		}
-
-		#[doc = "Invalid node id"]
-		InvalidNodeId {
-			description("Invalid node id"),
-			display("Invalid node id"),
-		}
-
-		#[doc = "Packet size is over the protocol limit"]
-		OversizedPacket {
-			description("Packet is too large"),
-			display("Packet is too large"),
-		}
-
-		#[doc = "Reached system resource limits for this process"]
-		ProcessTooManyFiles {
-			description("Too many open files in process."),
-			display("Too many open files in this process. Check your resource limits and restart parity"),
-		}
-
-		#[doc = "Reached system wide resource limits"]
-		SystemTooManyFiles {
-			description("Too many open files on system."),
-			display("Too many open files on system. Consider closing some processes/release some file handlers or increas the system-wide resource limits and restart parity."),
-		}
-
-		#[doc = "An unknown IO error occurred."]
-		Io(err: io::Error) {
-			description("IO Error"),
-			display("Unexpected IO error: {}", err),
+impl error::Error for Error {
+	fn source(&self) -> Option<&(error::Error + 'static)> {
+		match self {
+			Error::SocketIo(e) => Some(e),
+			Error::Decompression(e) => Some(e),
+			Error::Rlp(e) => Some(e),
+			_ => None,
 		}
 	}
 }
@@ -170,33 +146,33 @@ error_chain! {
 impl From<io::Error> for Error {
 	fn from(err: io::Error) -> Self {
 		match err.raw_os_error() {
-			Some(ENFILE) => ErrorKind::ProcessTooManyFiles.into(),
-			Some(EMFILE) => ErrorKind::SystemTooManyFiles.into(),
-			_ => Error::from_kind(ErrorKind::Io(err))
+			Some(ENFILE) => Error::ProcessTooManyFiles,
+			Some(EMFILE) => Error::SystemTooManyFiles,
+			_ => Error::Io(err)
 		}
 	}
 }
 
 impl From<ethkey::Error> for Error {
 	fn from(_err: ethkey::Error) -> Self {
-		ErrorKind::Auth.into()
+		Error::Auth
 	}
 }
 
 impl From<ethkey::crypto::Error> for Error {
 	fn from(_err: ethkey::crypto::Error) -> Self {
-		ErrorKind::Auth.into()
+		Error::Auth
 	}
 }
 
 impl From<crypto::error::SymmError> for Error {
 	fn from(_err: crypto::error::SymmError) -> Self {
-		ErrorKind::Auth.into()
+		Error::Auth
 	}
 }
 
 impl From<net::AddrParseError> for Error {
-	fn from(_err: net::AddrParseError) -> Self { ErrorKind::AddressParse.into() }
+	fn from(_err: net::AddrParseError) -> Self { Error::AddressParse }
 }
 
 #[test]
