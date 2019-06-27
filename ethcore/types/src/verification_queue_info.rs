7dc279c3b4cd2d74e73840e3dee538f6ca9f8bb0
debris <marek.kotewicz@gmail.com>
2019-06-27 19:28:34 +0800

    removed redundant fmt::Display implementations for Account and State Diff

--- ethcore/types/src/verification_queue_info.rs
+++ ethcore/types/src/verification_queue_info.rs
@@ -37,15 +37,6 @@ impl VerificationQueueInfo {
 	/// The total size of the queues.
 	pub fn total_queue_size(&self) -> usize { self.unverified_queue_size + self.verified_queue_size + self.verifying_queue_size }
 
-	/// The size of the unverified and verifying queues.
-	pub fn incomplete_queue_size(&self) -> usize { self.unverified_queue_size + self.verifying_queue_size }
-
-	/// Indicates that queue is full
-	pub fn is_full(&self) -> bool {
-		self.unverified_queue_size + self.verified_queue_size + self.verifying_queue_size > self.max_queue_size ||
-			self.mem_used > self.max_mem_use
-	}
-
 	/// Indicates that queue is empty
 	pub fn is_empty(&self) -> bool {
 		self.unverified_queue_size + self.verified_queue_size + self.verifying_queue_size == 0
