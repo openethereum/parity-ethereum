120eb4ce775bf751cbbf19bfcf12629eacddd3e8
Luke Schoen <ltfschoen@gmail.com>
2019-07-03 13:59:03 +0200

    review-fix: Fix indentation

--- whisper/cli/src/config/logger.rs
+++ whisper/cli/src/config/logger.rs
@@ -19,76 +19,76 @@ use rlog::{LevelFilter};
 use std::io;
 
 fn setup_logger(verbosity: u32, logging_to_file: bool) -> Result<(), fern::InitError> {
-  let colors_line = ColoredLevelConfig::new()
-    .error(Color::Red)
-    .warn(Color::Yellow)
-    .info(Color::BrightBlack)
-    .debug(Color::BrightBlack)
-    .trace(Color::BrightBlack);
+	let colors_line = ColoredLevelConfig::new()
+		.error(Color::Red)
+		.warn(Color::Yellow)
+		.info(Color::BrightBlack)
+		.debug(Color::BrightBlack)
+		.trace(Color::BrightBlack);
 
-  let colors_level = colors_line.clone()
-    .info(Color::Green);
+	let colors_level = colors_line.clone()
+		.info(Color::Green);
 
-  let mut base_config = fern::Dispatch::new();
+	let mut base_config = fern::Dispatch::new();
 
-  base_config = match verbosity {
-    0 => {
-      base_config
-      .level(LevelFilter::Error)
-      .level_for("pretty_colored", LevelFilter::Error)
-    }
-    1 => {
-      base_config
-      .level(LevelFilter::Warn)
-      .level_for("pretty_colored", LevelFilter::Warn)
-    },
-    2 => {
-      base_config
-      .level(LevelFilter::Info)
-      .level_for("pretty_colored", LevelFilter::Info)
-    },
-    3 => {
-      base_config
-      .level(LevelFilter::Debug)
-      .level_for("pretty_colored", LevelFilter::Debug)
-    },
-    _ => {
-      base_config
-      .level(LevelFilter::Trace)
-      .level_for("pretty_colored", LevelFilter::Trace)
-    },
-  };
+	base_config = match verbosity {
+		0 => {
+			base_config
+			.level(LevelFilter::Error)
+			.level_for("pretty_colored", LevelFilter::Error)
+		}
+		1 => {
+			base_config
+			.level(LevelFilter::Warn)
+			.level_for("pretty_colored", LevelFilter::Warn)
+		},
+		2 => {
+			base_config
+			.level(LevelFilter::Info)
+			.level_for("pretty_colored", LevelFilter::Info)
+		},
+		3 => {
+			base_config
+			.level(LevelFilter::Debug)
+			.level_for("pretty_colored", LevelFilter::Debug)
+		},
+		_ => {
+			base_config
+			.level(LevelFilter::Trace)
+			.level_for("pretty_colored", LevelFilter::Trace)
+		},
+	};
 
-  let format_config = fern::Dispatch::new()
-    .format(move |out, message, record| {
-      out.finish(format_args!(
-        "{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
-        color_line = format_args!("\x1B[{}m", colors_line.get_color(&record.level()).to_fg_str()),
-        date = chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-        level = colors_level.color(record.level()),
-        target = record.target(),
-        message = message
-      ))
-    })
-    .chain(io::stdout());
+	let format_config = fern::Dispatch::new()
+		.format(move |out, message, record| {
+			out.finish(format_args!(
+				"{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
+				color_line = format_args!("\x1B[{}m", colors_line.get_color(&record.level()).to_fg_str()),
+				date = chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+				level = colors_level.color(record.level()),
+				target = record.target(),
+				message = message
+			))
+		})
+		.chain(io::stdout());
 
-  if logging_to_file {
-    let file_config = fern::Dispatch::new()
-      .format(|out, message, record| {
-        out.finish(format_args!(
-          "{}[{}][{}] {}",
-          chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-          record.level(),
-          record.target(),
-          message
-        ))
-      })
-      .chain(fern::log_file("output.log")?);
+	if logging_to_file {
+		let file_config = fern::Dispatch::new()
+			.format(|out, message, record| {
+				out.finish(format_args!(
+				"{}[{}][{}] {}",
+				chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+				record.level(),
+				record.target(),
+				message
+				))
+			})
+			.chain(fern::log_file("output.log")?);
 
-    base_config.chain(file_config).chain(format_config).apply()?;
-  } else {
-    base_config.chain(format_config).apply()?;
-  }
+		base_config.chain(file_config).chain(format_config).apply()?;
+	} else {
+		base_config.chain(format_config).apply()?;
+	}
 
   Ok(())
 }
@@ -102,13 +102,13 @@ fn setup_logger(verbosity: u32, logging_to_file: bool) -> Result<(), fern::InitE
 /// are configured in Cargo.toml. In production the max logging level may differ from in development.
 /// - Output to output.log when logging_to_file is true.
 pub fn init_logger(logging: &str, logging_to_file: bool) -> () {
-  let verbosity: u32 = logging.parse::<u32>().expect("parsing cannot fail; qed");
+	let verbosity: u32 = logging.parse::<u32>().expect("parsing cannot fail; qed");
 
-  match setup_logger(verbosity, logging_to_file) {
-    Ok(_) => {
-      println!("Success initializing logger. Verbosity: {:?}. Log to file: {}", verbosity, &logging_to_file);
-      ()
-    }
-    Err(e) => { println!("Error initializing logger: {}", e); }
-  }
+	match setup_logger(verbosity, logging_to_file) {
+		Ok(_) => {
+			println!("Success initializing logger. Verbosity: {:?}. Log to file: {}", verbosity, &logging_to_file);
+			()
+		}
+		Err(e) => { println!("Error initializing logger: {}", e); }
+	}
 }
