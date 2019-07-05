67332d74c694b08b279c775c6073458acdfae7f9
David Palm <dvdplm@gmail.com>
2019-07-05 09:00:16 +0200

    some notes

--- ethcore/state-account/src/state.rs
+++ ethcore/state-account/src/state.rs
@@ -216,7 +216,30 @@ impl AccountEntry {
 //	transaction: &SignedTransaction,
 //	machine: &Machine,
 //	env_info: &EnvInfo,
-//) -> ProvedExecution
+//) -> ProvedExecution {
+//	let backend = self::backend::ProofCheck::new(proof);
+//	let mut factories = Factories::default();
+//	factories.accountdb = ::account_db::Factory::Plain;
+//
+//	let res = State::from_existing(
+//		backend,
+//		root,
+//		machine.account_start_nonce(env_info.number),
+//		factories
+//	);
+//
+//	let mut state = match res {
+//		Ok(state) => state,
+//		Err(_) => return ProvedExecution::BadProof,
+//	};
+//
+//	let options = TransactOptions::with_no_tracing().save_output_from_contract();
+//	match state.execute(env_info, machine, transaction, options, true) {
+//		Ok(executed) => ProvedExecution::Complete(Box::new(executed)),
+//		Err(ExecutionError::Internal(_)) => ProvedExecution::BadProof,
+//		Err(e) => ProvedExecution::Failed(e),
+//	}
+//}
 
 // TODO: needs Machine
 ///// Prove a `virtual` transaction on the given state.
@@ -230,7 +253,31 @@ impl AccountEntry {
 //	env_info: &EnvInfo,
 //	factories: Factories,
 //) -> Option<(Bytes, Vec<DBValue>)> {
-
+//	use state_account::backend::Proving;
+//
+//	let backend = Proving::new(db);
+//	let res = State::from_existing(
+//		backend,
+//		root,
+//		machine.account_start_nonce(env_info.number),
+//		factories,
+//	);
+//
+//	let mut state = match res {
+//		Ok(state) => state,
+//		Err(_) => return None,
+//	};
+//
+//	let options = TransactOptions::with_no_tracing().dont_check_nonce().save_output_from_contract();
+//	match state.execute(env_info, machine, transaction, options, true) {
+//		Err(ExecutionError::Internal(_)) => None,
+//		Err(e) => {
+//			trace!(target: "state", "Proved call failed: {}", e);
+//			Some((Vec::new(), state.drop().1.extract_proof()))
+//		}
+//		Ok(res) => Some((res.output, state.drop().1.extract_proof())),
+//	}
+//}
 
 /// Representation of the entire state of all accounts in the system.
 ///
@@ -548,7 +595,7 @@ impl<B: Backend> State<B> {
 		Ok(())
 	}
 
-	// TODO: Needs Machine and TransactionOptions
+	// TODO: Needs Machine and TransactOptions
 	/// Execute a given transaction, producing a receipt and an optional trace.
 	/// This will change the state accordingly.
 //	pub fn apply(&mut self, env_info: &EnvInfo, machine: &Machine, t: &SignedTransaction, tracing: bool) -> ApplyResult<FlatTrace, VMTrace> {
