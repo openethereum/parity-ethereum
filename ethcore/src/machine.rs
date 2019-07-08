5a28340a0a6dd9b90e70b08088909beedadb9ad6
David Palm <dvdplm@gmail.com>
2019-07-08 10:48:29 +0200

    cosmetics

--- ethcore/src/machine.rs
+++ ethcore/src/machine.rs
@@ -83,9 +83,9 @@ impl Machine {
 	pub fn regular(params: CommonParams, builtins: BTreeMap<Address, Builtin>) -> Machine {
 		let tx_filter = TransactionFilter::from_params(&params).map(Arc::new);
 		Machine {
-			params: params,
+			params,
 			builtins: Arc::new(builtins),
-			tx_filter: tx_filter,
+			tx_filter,
 			ethash_extensions: None,
 			schedule_rules: None,
 		}
