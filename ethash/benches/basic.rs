5a131175e8e5a99ab65570e6af2b9d2a008a6d60
Andronik Ordian <write@reusable.software>
2019-07-12 10:04:41 +0200

    [ethash] remove mem::uninitialized (#10861)
    * [ethash] replace mem::uninitialized with MaybeUninit
    
    * [ethash] replace another occurence of mem::uninitialized
    
    * [ethash] remove last mem::uninitialized
    
    * [ethash] clean up benches
    
    * [ethash] update outdated comment
    
    * [ethash] compile error on big endian targets
    
    * [ethash] extract 32 into a constant
    
    * [ethash] rename the constant to KECCAK_LEN
    
    * [ethash] bench quick_get_difficulty
    
    * [ethash] remove MaybeUninit completely
    
    * [ethash] replace ptr::copy_nonoverlapping with copy_from_slice
    
    * [ethash] s/header_len/hash_len
    
    * [ethash] remove duplication in bench
    
    * [ethash] add a config for basic benches
    
    * [ethash] fix a typo in bench fn name
    
    * [ethash] remove needless cast

--- ethash/benches/basic.rs
+++ ethash/benches/basic.rs
@@ -21,21 +21,38 @@ extern crate ethash;
 use criterion::Criterion;
 use ethash::{NodeCacheBuilder, OptimizeFor};
 
-const HASH: [u8; 32] = [0xf5, 0x7e, 0x6f, 0x3a, 0xcf, 0xc0, 0xdd, 0x4b, 0x5b, 0xf2, 0xbe,
-	                    0xe4, 0x0a, 0xb3, 0x35, 0x8a, 0xa6, 0x87, 0x73, 0xa8, 0xd0, 0x9f,
-	                    0x5e, 0x59, 0x5e, 0xab, 0x55, 0x94, 0x05, 0x52, 0x7d, 0x72];
+const HASH: [u8; 32] = [
+	0xf5, 0x7e, 0x6f, 0x3a, 0xcf, 0xc0, 0xdd, 0x4b,
+	0x5b, 0xf2, 0xbe, 0xe4, 0x0a, 0xb3, 0x35, 0x8a,
+	0xa6, 0x87, 0x73, 0xa8, 0xd0, 0x9f, 0x5e, 0x59,
+	0x5e, 0xab, 0x55, 0x94, 0x05, 0x52, 0x7d, 0x72,
+];
+const MIX_HASH: [u8; 32] = [
+	0x1f, 0xff, 0x04, 0xce, 0xc9, 0x41, 0x73, 0xfd,
+	0x59, 0x1e, 0x3d, 0x89, 0x60, 0xce, 0x6b, 0xdf,
+	0x8b, 0x19, 0x71, 0x04, 0x8c, 0x71, 0xff, 0x93,
+	0x7b, 0xb2, 0xd3, 0x2a, 0x64, 0x31, 0xab, 0x6d,
+];
 const NONCE: u64 = 0xd7b3ac70a301a249;
 
-criterion_group!(
-	basic,
-	bench_light_compute_memmap,
-	bench_light_compute_memory,
-	bench_light_new_round_trip_memmap,
-	bench_light_new_round_trip_memory,
-	bench_light_from_file_round_trip_memory,
-	bench_light_from_file_round_trip_memmap
-);
+criterion_group! {
+	name = basic;
+	config = dont_take_an_eternity_to_run();
+	targets = bench_light_compute_memmap,
+		bench_light_compute_memory,
+		bench_light_new_round_trip_memmap,
+		bench_light_new_round_trip_memory,
+		bench_light_from_file_round_trip_memory,
+		bench_light_from_file_round_trip_memmap,
+		bench_quick_get_difficulty,
+}
 criterion_main!(basic);
+
+fn dont_take_an_eternity_to_run() -> Criterion {
+	Criterion::default().nresamples(1_000)
+		.without_plots()
+		.sample_size(10)
+}
 
 fn bench_light_compute_memmap(b: &mut Criterion) {
 	use std::env;
@@ -52,13 +69,13 @@ fn bench_light_compute_memory(b: &mut Criterion) {
 	let builder = NodeCacheBuilder::new(OptimizeFor::Cpu, u64::max_value());
 	let light = builder.light(&env::temp_dir(), 486382);
 
-	b.bench_function("bench_light_compute_memmap", move |b| b.iter(|| light.compute(&HASH, NONCE, u64::max_value())));
+	b.bench_function("bench_light_compute_memory", move |b| b.iter(|| light.compute(&HASH, NONCE, u64::max_value())));
 }
 
 fn bench_light_new_round_trip_memmap(b: &mut Criterion) {
 	use std::env;
 
-	b.bench_function("bench_light_compute_memmap", move |b| b.iter(|| {
+	b.bench_function("bench_light_new_round_trip_memmap", move |b| b.iter(|| {
 		let builder = NodeCacheBuilder::new(OptimizeFor::Memory, u64::max_value());
 		let light = builder.light(&env::temp_dir(), 486382);
 		light.compute(&HASH, NONCE, u64::max_value());
@@ -68,7 +85,7 @@ fn bench_light_new_round_trip_memmap(b: &mut Criterion) {
 fn bench_light_new_round_trip_memory(b: &mut Criterion) {
 	use std::env;
 
-	b.bench_function("bench_light_compute_memmap", move |b| b.iter(|| {
+	b.bench_function("bench_light_new_round_trip_memory", move |b| b.iter(|| {
 		let builder = NodeCacheBuilder::new(OptimizeFor::Cpu, u64::max_value());
 		let light = builder.light(&env::temp_dir(), 486382);
 		light.compute(&HASH, NONCE, u64::max_value());
@@ -86,7 +103,7 @@ fn bench_light_from_file_round_trip_memory(b: &mut Criterion) {
 		dummy.to_file().unwrap();
 	}
 
-	b.bench_function("bench_light_compute_memmap", move |b| b.iter(|| {
+	b.bench_function("bench_light_from_file_round_trip_memory", move |b| b.iter(|| {
 		let builder = NodeCacheBuilder::new(OptimizeFor::Cpu, u64::max_value());
 		let light = builder.light_from_file(&dir, 486382).unwrap();
 		light.compute(&HASH, NONCE, u64::max_value());
@@ -105,9 +122,21 @@ fn bench_light_from_file_round_trip_memmap(b: &mut Criterion) {
 		dummy.to_file().unwrap();
 	}
 
-	b.bench_function("bench_light_compute_memmap", move |b| b.iter(|| {
+	b.bench_function("bench_light_from_file_round_trip_memmap", move |b| b.iter(|| {
 		let builder = NodeCacheBuilder::new(OptimizeFor::Memory, u64::max_value());
 		let light = builder.light_from_file(&dir, 486382).unwrap();
 		light.compute(&HASH, NONCE, u64::max_value());
+	}));
+}
+
+fn bench_quick_get_difficulty(b: &mut Criterion) {
+	b.bench_function("bench_quick_get_difficulty", move |b| b.iter(|| {
+		let d = ethash::quick_get_difficulty(&HASH, NONCE, &MIX_HASH, false);
+		let boundary_good = [
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x3e, 0x9b, 0x6c, 0x69, 0xbc, 0x2c, 0xe2, 0xa2,
+			0x4a, 0x8e, 0x95, 0x69, 0xef, 0xc7, 0xd7, 0x1b, 0x33, 0x35, 0xdf, 0x36, 0x8c, 0x9a,
+			0xe9, 0x7e, 0x53, 0x84,
+		];
+		assert_eq!(d[..], boundary_good[..]);
 	}));
 }
