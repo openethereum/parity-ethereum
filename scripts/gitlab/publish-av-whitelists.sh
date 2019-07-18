b1b120699f1c9b5cadb72e66d7b41f3140fcd459
Martin Pugh <pugh@s3kr.it>
2019-07-18 15:20:16 +0200

    remove debug print

--- scripts/gitlab/publish-av-whitelists.sh
+++ scripts/gitlab/publish-av-whitelists.sh
@@ -6,7 +6,6 @@ echo "__________Publish Windows binaries to Avast Whitelisting program__________
 target_filename="parity-${CI_COMMIT_TAG:-${CI_COMMIT_REF_NAME}}.exe"
 apt -y update
 apt -y install ftp
-ls ./artifacts
 ftp -pinv whitelisting.avast.com <<EOF
 quote USER ftp_parityio
 quote PASS $avast_ftp_password
