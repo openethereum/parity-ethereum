ec90fc47bc540a215539d7d4c2753f9f103a5a52
s3krit <pugh@s3kr.it>
2019-07-29 16:44:19 +0200

    Kaspersky AV whitelisting (#10919)
    * Add Kaspersky's FTP to AV Whitelisting release script
    
    * Uncommented lines commented for debugging

--- scripts/gitlab/publish-av-whitelists.sh
+++ scripts/gitlab/publish-av-whitelists.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "__________Publish Windows binaries to Avast Whitelisting program__________"
-
 target_filename="parity-${CI_COMMIT_TAG:-${CI_COMMIT_REF_NAME}}.exe"
 apt -y update
 apt -y install ftp
+
+echo "__________Publish Windows binaries to Avast Whitelisting program__________"
+
 ftp -pinv whitelisting.avast.com <<EOF
 quote USER ftp_parityio
 quote PASS $avast_ftp_password
@@ -14,3 +15,11 @@ put ./artifacts/x86_64-pc-windows-msvc/parity.exe $target_filename
 bye
 EOF
 
+echo "__________Publish Windows binaries to Kaspersky Whitelisting program__________"
+
+ftp -pinv whitelist1.kaspersky-labs.com <<EOF
+quote USER wl-ParityTech
+quote PASS $kaspersky_ftp_password
+put ./artifacts/x86_64-pc-windows-msvc/parity.exe $target_filename
+bye
+EOF
