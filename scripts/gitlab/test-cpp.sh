892918517f0277a299c526e74aa619a920571107
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-08-09 14:01:05 +0200

    fix: `test-cpp.sh`

--- scripts/gitlab/test-cpp.sh
+++ scripts/gitlab/test-cpp.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 echo "________Running test-cpp.sh________"
 set -e # fail on any error
 set -u # treat unset variables as error
@@ -10,7 +9,10 @@ echo "________Running the C++ example________"
 DIR=parity-clib/examples/cpp/build
 mkdir -p $DIR
 cd $DIR
-cmake --build . -- -j $THREADS
-./parity-example
+# TODO(niklasad1): could be `cmake --build . -- -j $THREADS`
+cmake ..
+make VERBOSE=1 -j $THREADS
+# Note: we don't try to run the example because it tries to sync Kovan, and we don't want
+#       that to happen on CI
 cd -
 rm -rf $DIR
