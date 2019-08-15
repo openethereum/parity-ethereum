e50eafe6e161301188c51d2d305b8e44e580c134
David <dvdplm@gmail.com>
2019-08-15 15:36:07 +0200

    [blooms-db] Fix benchmarks (#10974)
    * [blooms-db] Fix benchmarks
    
    * Use Bloom::zero()

--- util/blooms-db/benches/blooms.rs
+++ util/blooms-db/benches/blooms.rs
@@ -31,8 +31,8 @@ use ethbloom::Bloom;
 fn blooms_filter_1_million_ok(b: &mut Bencher) {
 	let tempdir = TempDir::new("").unwrap();
 	let database = Database::open(tempdir.path()).unwrap();
-	database.insert_blooms(999_999, iter::once(&Bloom::from(0))).unwrap();
-	let bloom = Bloom::from(0x001);
+	database.insert_blooms(999_999, iter::once(&Bloom::zero())).unwrap();
+	let bloom = Bloom::from_low_u64_be(0x001);
 	database.insert_blooms(200_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(400_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(600_000, iter::once(&bloom)).unwrap();
@@ -48,9 +48,9 @@ fn blooms_filter_1_million_ok(b: &mut Bencher) {
 fn blooms_filter_1_million_miss(b: &mut Bencher) {
 	let tempdir = TempDir::new("").unwrap();
 	let database = Database::open(tempdir.path()).unwrap();
-	database.insert_blooms(999_999, iter::once(&Bloom::from(0))).unwrap();
-	let bloom = Bloom::from(0x001);
-	let bad_bloom = Bloom::from(0x0001);
+	database.insert_blooms(999_999, iter::once(&Bloom::zero())).unwrap();
+	let bloom = Bloom::from_low_u64_be(0x001);
+	let bad_bloom = Bloom::from_low_u64_be(0x0001);
 	database.insert_blooms(200_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(400_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(600_000, iter::once(&bloom)).unwrap();
@@ -66,9 +66,9 @@ fn blooms_filter_1_million_miss(b: &mut Bencher) {
 fn blooms_filter_1_million_miss_and_ok(b: &mut Bencher) {
 	let tempdir = TempDir::new("").unwrap();
 	let database = Database::open(tempdir.path()).unwrap();
-	database.insert_blooms(999_999, iter::once(&Bloom::from(0))).unwrap();
-	let bloom = Bloom::from(0x001);
-	let bad_bloom = Bloom::from(0x0001);
+	database.insert_blooms(999_999, iter::once(&Bloom::zero())).unwrap();
+	let bloom = Bloom::from_low_u64_be(0x001);
+	let bad_bloom = Bloom::from_low_u64_be(0x0001);
 	database.insert_blooms(200_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(400_000, iter::once(&bloom)).unwrap();
 	database.insert_blooms(600_000, iter::once(&bloom)).unwrap();
