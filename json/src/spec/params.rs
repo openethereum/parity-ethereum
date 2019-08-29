916b7cb44f476ca12950e6708a3172c3fd7de857
Wei Tang <hi@that.world>
2019-08-29 21:31:52 +0200

    Implement EIP-1283 reenable transition, EIP-1706 and EIP-2200 (#10191)
    * Add reentry protection for EIP-1283
    
    * typo: should use <=
    
    * Put things behind flag eip1706
    
    * Fix compile
    
    * Fix missing config in json and add eip1283_reenable_transition

--- json/src/spec/params.rs
+++ json/src/spec/params.rs
@@ -92,7 +92,11 @@ pub struct Params {
 	/// See `CommonParams` docs.
 	pub eip1283_disable_transition: Option<Uint>,
 	/// See `CommonParams` docs.
+	pub eip1283_reenable_transition: Option<Uint>,
+	/// See `CommonParams` docs.
 	pub eip1014_transition: Option<Uint>,
+	/// See `CommonParams` docs.
+	pub eip1706_transition: Option<Uint>,
 	/// See `CommonParams` docs.
 	pub eip1344_transition: Option<Uint>,
 	/// See `CommonParams` docs.
