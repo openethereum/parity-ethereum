680807e601be936e21086573e28189200f1c7167
Toby Dimmick <tobydimmick@pm.me>
2019-09-05 18:28:01 +0100

    Convert `std::test` benchmarks to use Criterion (#10999)
    * Convert std::test benches to Criterion
    
    * Required feature for ethash benches
    
    * gitlab-ci: Check all bench targets
    
    * check ethash benches
    
    * evmbin/benches/mod.rs: Update bench sample command
    
    Co-Authored-By: Andronik Ordian <write@reusable.software>
    
    * Upgrade to criterion 0.3

--- evmbin/benches/mod.rs
+++ evmbin/benches/mod.rs
@@ -17,12 +17,11 @@
 //! benchmarking for EVM
 //! should be started with:
 //! ```bash
-//! rustup run nightly cargo bench
+//! cargo bench
 //! ```
 
-#![feature(test)]
-
-extern crate test;
+#[macro_use]
+extern crate criterion;
 extern crate ethcore;
 extern crate evm;
 extern crate ethereum_types;
@@ -30,7 +29,7 @@ extern crate rustc_hex;
 extern crate vm;
 
 use std::sync::Arc;
-use self::test::{Bencher, black_box};
+use criterion::{Criterion, black_box};
 
 use ethereum_types::U256;
 use evm::Factory;
@@ -38,54 +37,63 @@ use rustc_hex::FromHex;
 use vm::tests::FakeExt;
 use vm::{ActionParams, Ext};
 
-#[bench]
-fn simple_loop_usize(b: &mut Bencher) {
-	simple_loop(U256::from(::std::usize::MAX), b)
+criterion_group!(
+	evmbin,
+	bench_simple_loop_usize,
+	bench_simple_loop_u256,
+	bench_rng_usize,
+	bench_rng_u256
+);
+criterion_main!(evmbin);
+
+fn bench_simple_loop_usize(c: &mut Criterion) {
+	simple_loop(U256::from(::std::usize::MAX), c, "simple_loop_usize")
 }
 
-#[bench]
-fn simple_loop_u256(b: &mut Bencher) {
-	simple_loop(!U256::zero(), b)
+fn bench_simple_loop_u256(c: &mut Criterion) {
+	simple_loop(!U256::zero(), c, "simple_loop_u256")
 }
 
-fn simple_loop(gas: U256, b: &mut Bencher) {
+fn simple_loop(gas: U256, c: &mut Criterion, bench_id: &str) {
 	let code = black_box(
 		"606060405260005b620042408112156019575b6001016007565b600081905550600680602b6000396000f3606060405200".from_hex().unwrap()
 	);
 
-	b.iter(|| {
-		let mut params = ActionParams::default();
-		params.gas = gas;
-		params.code = Some(Arc::new(code.clone()));
+	c.bench_function(bench_id, move |b| {
+		b.iter(|| {
+			let mut params = ActionParams::default();
+			params.gas = gas;
+			params.code = Some(Arc::new(code.clone()));
 
-		let mut ext = FakeExt::new();
-		let evm = Factory::default().create(params, ext.schedule(), ext.depth());
-		let _ = evm.exec(&mut ext);
+			let mut ext = FakeExt::new();
+			let evm = Factory::default().create(params, ext.schedule(), ext.depth());
+			let _ = evm.exec(&mut ext);
+		})
 	});
 }
 
-#[bench]
-fn rng_usize(b: &mut Bencher) {
-	rng(U256::from(::std::usize::MAX), b)
+fn bench_rng_usize(c: &mut Criterion) {
+	rng(U256::from(::std::usize::MAX), c, "rng_usize")
 }
 
-#[bench]
-fn rng_u256(b: &mut Bencher) {
-	rng(!U256::zero(), b)
+fn bench_rng_u256(c: &mut Criterion) {
+	rng(!U256::zero(), c, "rng_u256")
 }
 
-fn rng(gas: U256, b: &mut Bencher) {
+fn rng(gas: U256, c: &mut Criterion, bench_id: &str) {
 	let code = black_box(
 		"6060604052600360056007600b60005b62004240811215607f5767ffe7649d5eca84179490940267f47ed85c4b9a6379019367f8e5dd9a5c994bba9390930267f91d87e4b8b74e55019267ff97f6f3b29cda529290920267f393ada8dd75c938019167fe8d437c45bb3735830267f47d9a7b5428ffec019150600101600f565b838518831882186000555050505050600680609a6000396000f3606060405200".from_hex().unwrap()
 	);
 
-	b.iter(|| {
-		let mut params = ActionParams::default();
-		params.gas = gas;
-		params.code = Some(Arc::new(code.clone()));
+	c.bench_function(bench_id, move |b| {
+		b.iter(|| {
+			let mut params = ActionParams::default();
+			params.gas = gas;
+			params.code = Some(Arc::new(code.clone()));
 
-		let mut ext = FakeExt::new();
-		let evm = Factory::default().create(params, ext.schedule(), ext.depth());
-		let _ = evm.exec(&mut ext);
+			let mut ext = FakeExt::new();
+			let evm = Factory::default().create(params, ext.schedule(), ext.depth());
+			let _ = evm.exec(&mut ext);
+		})
 	});
 }
