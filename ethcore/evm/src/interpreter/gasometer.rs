7c7b181ca07e7a2f76737a2115c8e6cafe940a80
s3krit <pugh@s3kr.it>
2019-09-13 15:46:03 +0200

    v2.5.8-stable (rev2) (#11051)
    * EIP 1884 Re-pricing of trie-size dependent operations  (#10992)
    * Implement EIP-1283 reenable transition, EIP-1706 and EIP-2200  (#10191)

--- ethcore/evm/src/interpreter/gasometer.rs
+++ ethcore/evm/src/interpreter/gasometer.rs
@@ -121,6 +121,9 @@ impl<Gas: evm::CostType> Gasometer<Gas> {
 				Request::Gas(Gas::from(1))
 			},
 			instructions::SSTORE => {
+				if schedule.eip1706 && self.current_gas <= Gas::from(schedule.call_stipend) {
+					return Err(vm::Error::OutOfGas);
+				}
 				let address = H256::from(stack.peek(0));
 				let newval = stack.peek(1);
 				let val = U256::from(&*ext.storage_at(&address)?);
