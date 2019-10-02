ca20ad9d8ac48e6ab418721fe8feac7cfed62e6e
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2019-10-02 22:16:12 +0300

    statisticss after stop sccache server sccache CC&CXX

--- scripts/gitlab/test-linux.sh
+++ scripts/gitlab/test-linux.sh
@@ -4,7 +4,8 @@
 echo "________Running test-linux.sh________"
 set -e # fail on any error
 set -u # treat unset variables as error
-
+export CC="sccache gcc"
+export CXX="sccache g++"
 FEATURES="json-tests,ci-skip-tests"
 OPTIONS="--release"
 #use nproc `linux only
@@ -16,3 +17,6 @@ rustup show
 echo "________Running Parity Full Test Suite________"
 # Why are we using RUSTFLAGS? See https://github.com/paritytech/parity-ethereum/pull/10719
 CARGO_INCREMENTAL=0 RUSTFLAGS="-C opt-level=3 -C overflow-checks=on -C debuginfo=2 -Ctarget-feature=+aes,+sse2,+ssse3" time cargo test $OPTIONS --features "$FEATURES" --locked --all --target $CARGO_TARGET --verbose --color=never -- --test-threads $THREADS
+
+#show sccache statistics
+sccache --stop-server
