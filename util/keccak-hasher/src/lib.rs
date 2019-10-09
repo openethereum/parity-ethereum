79f91c04e4d7df6be1ea862d11111a1cebecb46c
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-10-09 13:56:02 +0200

    [keccak-hasher]: rust2018

--- util/keccak-hasher/src/lib.rs
+++ util/keccak-hasher/src/lib.rs
@@ -15,15 +15,14 @@
 // along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Hasher implementation for the Keccak-256 hash
-extern crate hash_db;
-extern crate ethereum_types;
-extern crate tiny_keccak;
-extern crate plain_hasher;
+
+#![warn(missing_docs)]
 
 use hash_db::Hasher;
 use ethereum_types::H256;
 use tiny_keccak::Keccak;
 use plain_hasher::PlainHasher;
+
 /// Concrete `Hasher` impl for the Keccak-256 hash
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct KeccakHasher;
@@ -32,7 +31,7 @@ impl Hasher for KeccakHasher {
 	type StdHasher = PlainHasher;
 	const LENGTH: usize = 32;
 	fn hash(x: &[u8]) -> Self::Out {
-		let mut out = [0;32];
+		let mut out = [0; 32];
 		Keccak::keccak256(x, &mut out);
 		out.into()
 	}
