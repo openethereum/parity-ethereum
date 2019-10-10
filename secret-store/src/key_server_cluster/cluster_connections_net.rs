85ca659c350499137cb6d9c2fa9e3265dbd050aa
Svyatoslav Nikolsky <svyatonik@gmail.com>
2019-10-10 10:45:29 +0300

    fix compilation

--- secret-store/src/key_server_cluster/cluster_connections_net.rs
+++ secret-store/src/key_server_cluster/cluster_connections_net.rs
@@ -471,7 +471,7 @@ fn net_keep_alive(data: Arc<NetConnectionsData>) {
 	for connection in active_connections {
 		// the last_message_time could change after active_connections() call
 		// => we always need to call Instant::now() after getting last_message_time
-		let last_message_time = connection.last_message_time()
+		let last_message_time = connection.last_message_time();
 		let now = Instant::now();
 		let last_message_diff = now - last_message_time;
 		if last_message_diff > KEEP_ALIVE_DISCONNECT_INTERVAL {
