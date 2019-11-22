a36028bab839b8f25fe464404d4adb6db05cb71f
Demi M. Obenour <demi@parity.io>
2019-11-22 10:38:16 -0500

    Switch C++ compiler to Clang

--- scripts/gitlab/test-cpp.sh
+++ scripts/gitlab/test-cpp.sh
@@ -5,7 +5,7 @@ set -u # treat unset variables as error
 #use nproc `linux only
 THREADS=$(nproc)
 export CC="sccache gcc"
-export CXX="sccache g++"
+export CXX="sccache clang++"
 
 echo "________Running the C++ example________"
 DIR=parity-clib/examples/cpp/build
