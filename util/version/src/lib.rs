9c008339923285f26d44e9357b12662473c8aaa8
Andronik Ordian <write@reusable.software>
2019-12-02 15:26:32 +0100

    version: upgrade vergen to 3.0

--- util/version/src/lib.rs
+++ util/version/src/lib.rs
@@ -24,11 +24,6 @@ use target_info::Target;
 use bytes::Bytes;
 use rlp::RlpStream;
 
-mod vergen {
-	#![allow(unused)]
-	include!(concat!(env!("OUT_DIR"), "/version.rs"));
-}
-
 mod generated {
 	include!(concat!(env!("OUT_DIR"), "/meta.rs"));
 }
@@ -50,11 +45,16 @@ pub fn platform() -> String {
 
 /// Get the standard version string for this software.
 pub fn version() -> String {
-	let sha3 = vergen::short_sha();
-	let sha3_dash = if sha3.is_empty() { "" } else { "-" };
-	let commit_date = vergen::commit_date().replace("-", "");
-	let date_dash = if commit_date.is_empty() { "" } else { "-" };
-	format!("Parity-Ethereum/v{}-{}{}{}{}{}/{}/rustc{}", env!("CARGO_PKG_VERSION"), THIS_TRACK, sha3_dash, sha3, date_dash, commit_date, platform(), generated::rustc_version())
+	let commit_date = format!("{}", env!("VERGEN_COMMIT_DATE")).replace("-", "");
+	format!(
+		"Parity-Ethereum/v{}-{}-{}-{}/{}/rustc{}",
+		env!("CARGO_PKG_VERSION"),
+		THIS_TRACK,
+		env!("VERGEN_SHA_SHORT"),
+		commit_date,
+		platform(),
+		generated::rustc_version(),
+	)
 }
 
 /// Get the standard version data for this software.
@@ -73,5 +73,5 @@ pub fn version_data() -> Bytes {
 
 /// Provide raw information on the package.
 pub fn raw_package_info() -> (&'static str, &'static str, &'static str) {
-	(THIS_TRACK, env!["CARGO_PKG_VERSION"], vergen::sha())
+	(THIS_TRACK, env!["CARGO_PKG_VERSION"], env!["VERGEN_SHA"])
 }
