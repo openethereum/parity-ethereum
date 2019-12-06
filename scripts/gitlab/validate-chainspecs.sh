36c917eb7a0312bdd19733126dc1762d0ebdd0cd
Andronik Ordian <write@reusable.software>
2019-12-06 13:12:08 +0100

    validate-chainspecs: check istanbul eips are in the foundation spec (#11305)
    * validate-chainspecs: check istanbul eips are in foundation spec
    
    * foundation: specify 1283 disable/reenable transitions

--- scripts/gitlab/validate-chainspecs.sh
+++ scripts/gitlab/validate-chainspecs.sh
@@ -15,6 +15,15 @@ done
 for spec in ethcore/res/ethereum/*.json; do
     if ! ./target/release/chainspec "$spec"; then ERR=1; fi
 done
+
+echo "________Mainnet contains Istanbul EIPs________"
+for eip in $(grep --only-matching "eip.*Transition" ethcore/res/ethereum/istanbul_test.json); do
+    if ! grep -q $eip ethcore/res/ethereum/foundation.json; then
+        echo "ERROR: $eip is missing in the foundation json spec"
+        ERR=1
+    fi
+done
+
 #show sccache statistics
 sccache --show-stats
 exit $ERR
