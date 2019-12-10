f1f64930cfcd5311bc5dece78321efbd683013d4
Boqin Qin <bobbqqin@bupt.edu.cn>
2019-12-10 01:46:32 -0500

    Fix atomicity violation in network-devp2p (#11277)

--- util/network-devp2p/src/connection.rs
+++ util/network-devp2p/src/connection.rs
@@ -220,14 +220,13 @@ impl Connection {
 
 	/// Register this connection with the IO event loop.
 	pub fn register_socket<Host: Handler>(&self, reg: Token, event_loop: &mut EventLoop<Host>) -> io::Result<()> {
-		if self.registered.load(AtomicOrdering::SeqCst) {
+		if self.registered.compare_and_swap(false, true, AtomicOrdering::SeqCst) {
 			return Ok(());
         }
 		trace!(target: "network", "connection register; token={:?}", reg);
 		if let Err(e) = event_loop.register(&self.socket, reg, self.interest, PollOpt::edge() /* | PollOpt::oneshot() */) { // TODO: oneshot is broken on windows
 			trace!(target: "network", "Failed to register {:?}, {:?}", reg, e);
 		}
-		self.registered.store(true, AtomicOrdering::SeqCst);
 		Ok(())
 	}
 
