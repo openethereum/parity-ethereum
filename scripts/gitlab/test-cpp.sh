5930c94f3828ed3b534578ae6a6393a690c1892c
Niklas <niklasadolfsson1@gmail.com>
2019-12-19 12:24:35 +0100

    test clang again

--- scripts/gitlab/test-cpp.sh
+++ scripts/gitlab/test-cpp.sh
@@ -6,15 +6,18 @@ set -B # enable brace expansion
 #use nproc `linux only
 THREADS=$(nproc)
 
+export CC="clang"
+export CXX="clang++"
+
 echo "________Running the C++ example________"
 DIR=parity-clib/examples/cpp/build
 mkdir -p $DIR
 cd $DIR
-cmake .. -DCMAKE_C{,XX}_COMPILER_LAUNCHER=sccache
+cmake ..
 make VERBOSE=1 -j $THREADS
 # Note: we don't try to run the example because it tries to sync Kovan, and we don't want
 #       that to happen on CI
 cd -
 rm -rf $DIR
 #show sccache statistics
-sccache --show-stats
+# sccache --show-stats
