eeecfe647dc795b14b3191c40fb346a798dbf74b
Max Johansen <macsj200@gmail.com>
2020-01-02 11:54:29 -0700

    Fix error message typo (#11363)

--- parity/main.rs
+++ parity/main.rs
@@ -285,7 +285,7 @@ fn main_direct(force_can_restart: bool) -> i32 {
 					let e = exit.clone();
 					let exiting = exiting.clone();
 					move |panic_msg| {
-						warn!("Panic occured, see stderr for details");
+						warn!("Panic occurred, see stderr for details");
 						eprintln!("{}", panic_msg);
 						if !exiting.swap(true, Ordering::SeqCst) {
 							*e.0.lock() = ExitStatus {
