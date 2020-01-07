b8d9b6f826f0bb4752b148c362a6a0e36e000f66
Demi Obenour <48690212+DemiMarie-parity@users.noreply.github.com>
2020-01-07 15:53:02 +0000

    Rip out the C and Java bindings (#11346)
    * Remove the C and Java bindings
    
    * update Cargo.lock
    
    * [ci]: don't build removed `parity-clib`
    
    * [ci]: fix grumbles
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

--- scripts/gitlab/build-linux.sh
+++ scripts/gitlab/build-linux.sh
@@ -6,45 +6,33 @@ set -u # treat unset variables as error
 export CC="sccache "$CC
 export CXX="sccache "$CXX
 echo "__________Show ENVIROMENT__________"
-echo "CI_SERVER_NAME:   " $CI_SERVER_NAME
-echo "CARGO_HOME:       " $CARGO_HOME
 echo "CARGO_TARGET:     " $CARGO_TARGET
 echo "CC:               " $CC
 echo "CXX:              " $CXX
 #strip ON
-export RUSTFLAGS=" -C link-arg=-s"
+export RUSTFLAGS+=" -C link-arg=-s"
 
 echo "_____ Building target: "$CARGO_TARGET" _____"
-if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
+if [ "${CARGO_TARGET}" = "x86_64-unknown-linux-gnu" ] || [ "${CARGO_TARGET}" = "x86_64-apple-darwin" ]
 then
-  time cargo build --target $CARGO_TARGET --verbose --color=always --release -p parity-clib --features final
-else
-  if [ "${CARGO_TARGET}" = "x86_64-unknown-linux-gnu" ] || [ "${CARGO_TARGET}" = "x86_64-apple-darwin" ]
-  then
-    # NOTE: Enables the aes-ni instructions for RustCrypto dependency.
-    # If you change this please remember to also update .cargo/config
-    export RUSTFLAGS="$RUSTFLAGS -Ctarget-feature=+aes,+sse2,+ssse3"
-  fi
-  time cargo build --target $CARGO_TARGET --verbose --color=always --release --features final
-  time cargo build --target $CARGO_TARGET --verbose --color=always --release -p evmbin
-  time cargo build --target $CARGO_TARGET --verbose --color=always --release -p ethstore-cli
-  time cargo build --target $CARGO_TARGET --verbose --color=always --release -p ethkey-cli
+  # NOTE: Enables the aes-ni instructions for RustCrypto dependency.
+  # If you change this please remember to also update .cargo/config
+  export RUSTFLAGS+=" -C target-feature=+aes,+sse2,+ssse3"
 fi
+time cargo build --target $CARGO_TARGET --verbose --color=always --release --features final
+time cargo build --target $CARGO_TARGET --verbose --color=always --release -p evmbin
+time cargo build --target $CARGO_TARGET --verbose --color=always --release -p ethstore-cli
+time cargo build --target $CARGO_TARGET --verbose --color=always --release -p ethkey-cli
 
 echo "_____ Post-processing binaries _____"
 rm -rf artifacts/*
 mkdir -p artifacts/$CARGO_TARGET
 cd artifacts/$CARGO_TARGET
 
-if [ "${CARGO_TARGET}" = "armv7-linux-androideabi" ]
-then
- cp -v ../../target/$CARGO_TARGET/release/libparity.so ./libparity.so
-else
- cp -v ../../target/$CARGO_TARGET/release/parity ./parity
- cp -v ../../target/$CARGO_TARGET/release/parity-evm ./parity-evm
- cp -v ../../target/$CARGO_TARGET/release/ethstore ./ethstore
- cp -v ../../target/$CARGO_TARGET/release/ethkey ./ethkey
-fi
+cp -v ../../target/$CARGO_TARGET/release/parity ./parity
+cp -v ../../target/$CARGO_TARGET/release/parity-evm ./parity-evm
+cp -v ../../target/$CARGO_TARGET/release/ethstore ./ethstore
+cp -v ../../target/$CARGO_TARGET/release/ethkey ./ethkey
 
 echo "_____ Calculating checksums _____"
 for binary in $(ls)
