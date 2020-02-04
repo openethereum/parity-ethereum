71d489dd29097d8a13bea80b38ffadd1b5562e0a
Artem Vorotnikov <artem@vorotnikov.me>
2020-02-04 14:54:16 +0300

    warn on missing docs in ethkey

--- accounts/ethkey/src/lib.rs
+++ accounts/ethkey/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
+#![warn(missing_docs)]
+
 #![warn(
 	clippy::all,
 	clippy::pedantic,
