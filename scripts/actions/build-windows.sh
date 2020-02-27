3ca4540117c46a4466f3ce0365ae5233e372cf2c
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-02-27 12:59:15 +0300

    remove ARM builds PLATFORM for build scripts

--- scripts/actions/build-windows.sh
+++ scripts/actions/build-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # fail on any error
 set -u # treat unset variables as error
-
+PLATFORM="$1"
   # NOTE: Enables the aes-ni instructions for RustCrypto dependency.
   # If you change this please remember to also update .cargo/config
 export RUSTFLAGS=" -Ctarget-feature=+aes,+sse2,+ssse3 -Ctarget-feature=+crt-static  -Clink-arg=-s"
