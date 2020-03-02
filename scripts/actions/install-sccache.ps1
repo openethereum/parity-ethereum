bd58f3c35eac5a5f2226e1edb9439c8d040c975d
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-03-02 16:13:39 +0300

    echo sccache url

--- scripts/actions/install-sccache.ps1
+++ scripts/actions/install-sccache.ps1
@@ -2,16 +2,15 @@
 $os=$args[0]
 $SCCACHE_CACHE_SIZE="1G"
 $SCCACHE_IDLE_TIMEOUT=0
-$version = "0.2.12"
+$version="0.2.12"
 $platform =
   @{ "macOS"   = "x86_64-apple-darwin"
      "Linux"   = "x86_64-unknown-linux-musl"
      "Windows" = "x86_64-pc-windows-msvc"
    }.$os
 $basename = "sccache-$version-$platform"
-$url = "https://github.com/mozilla/sccache/releases/download/" +
-       "$version/$basename.tar.gz"
-echo $url
+$url = "https://github.com/mozilla/sccache/releases/download/"+"$version/$basename.tar.gz"
+echo "Download sccache from "+$url
 curl -LO $url
 tar -xzvf "$basename.tar.gz"
 ls $basename/
