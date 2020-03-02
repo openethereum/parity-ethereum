99eb482d8ef3ea29072da061823a8a2b89f92539
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-03-02 16:29:23 +0300

    add swith for select platform

--- scripts/actions/install-sccache.ps1
+++ scripts/actions/install-sccache.ps1
@@ -3,11 +3,13 @@ $os=$args[0]
 $SCCACHE_CACHE_SIZE="1G"
 $SCCACHE_IDLE_TIMEOUT=0
 $version="0.2.12"
-$platform =
-  @{ "macOS"   = "x86_64-apple-darwin"
-     "Linux"   = "x86_64-unknown-linux-musl"
-     "Windows" = "x86_64-pc-windows-msvc"
-   }.$os
+echo "Current OS:" $os
+switch ($os){
+   "macOS" {$platform = "x86_64-apple-darwin"}
+   "Linux" {$platform = "x86_64-unknown-linux-musl"}
+   "Windows"  {$platform ="x86_64-pc-windows-msvc"}
+}
+echo "Target arch: " $platform
 $basename = "sccache-$version-$platform"
 $url = "https://github.com/mozilla/sccache/releases/download/"+"$version/$basename.tar.gz"
 echo "Download sccache from "+$url
