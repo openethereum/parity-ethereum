fb98ae60e5e954f5669f265761e1bd32011b834f
David Palm <dvdplm@gmail.com>
2020-03-10 19:52:30 +0100

    Increase default pruning history to 128 Increase default pruning memory to 48Mb

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -737,8 +737,7 @@ impl Client {
 		debug!(target: "client", "Cleanup journal: DB Earliest = {:?}, Latest = {:?}", state_db.journal_db().earliest_era(), state_db.journal_db().latest_era());
 
 		let history = if config.history < MIN_HISTORY_SIZE {
-			info!(target: "client", "Ignoring pruning history parameter of {}\
-				, falling back to minimum of {}",
+			info!(target: "client", "Ignoring pruning history parameter of {} , falling back to minimum of {}",
 				config.history, MIN_HISTORY_SIZE);
 			MIN_HISTORY_SIZE
 		} else {
