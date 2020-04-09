f2b0869b0146059cb931fe3d24e2e9bab987e95e
Andronik Ordian <write@reusable.software>
2020-04-09 23:13:45 +0200

    quickfix for ethash

--- ethash/src/keccak.rs
+++ ethash/src/keccak.rs
@@ -21,39 +21,27 @@ pub type H256 = [u8; 32];
 pub mod keccak_512 {
 	use super::hash;
 
-	pub use self::hash::keccak_512 as unchecked;
+	pub use self::hash::{keccak_512 as write, keccak512 as inplace};
 
-	pub fn write(input: &[u8], output: &mut [u8]) {
-		hash::keccak_512(input, output);
-	}
+    pub unsafe fn unchecked(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) {
+        // TODO(ordian): is this UB?
+        let src = core::slice::from_raw_parts(input, inputlen);
+        let dst = core::slice::from_raw_parts_mut(out, outlen);
 
-	pub fn inplace(input: &mut [u8]) {
-		hash::keccak_512(input, input);
-	}
-	pub fn inplace_old(input: &mut [u8]) {
-		// This is safe since `keccak_*` uses an internal buffer and copies the result to the output. This
-		// means that we can reuse the input buffer for both input and output.
-		unsafe {
-			hash::keccak_512(input.as_mut_ptr(), input.len(), input.as_ptr(), input.len());
-		}
+		self::hash::keccak_512(src, dst);
 	}
 }
 
 pub mod keccak_256 {
 	use super::hash;
 
-	pub use self::hash::keccak_256_unchecked as unchecked;
+	pub use self::hash::{keccak_256 as write, keccak256 as inplace};
 
-	#[allow(dead_code)]
-	pub fn write(input: &[u8], output: &mut [u8]) {
-		hash::keccak_256(input, output);
-	}
+    pub unsafe fn unchecked(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) {
+        // TODO(ordian): is this UB?
+        let src = core::slice::from_raw_parts(input, inputlen);
+        let dst = core::slice::from_raw_parts_mut(out, outlen);
 
-	pub fn inplace(input: &mut [u8]) {
-		// This is safe since `keccak_*` uses an internal buffer and copies the result to the output. This
-		// means that we can reuse the input buffer for both input and output.
-		unsafe {
-			hash::keccak_256_unchecked(input.as_mut_ptr(), input.len(), input.as_ptr(), input.len());
-		}
+		self::hash::keccak_256(src, dst);
 	}
 }
