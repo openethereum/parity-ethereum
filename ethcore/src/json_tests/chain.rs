ff6c4150e24ea5386964d36bdb826bf925793a1d
adria0 <adria@codecontext.io>
2020-04-09 17:55:52 +0200

    verify post_state in BlockchainTests

--- ethcore/src/json_tests/chain.rs
+++ ethcore/src/json_tests/chain.rs
@@ -17,15 +17,24 @@
 use std::path::Path;
 use std::sync::Arc;
 use client::{Client, ClientConfig};
-use client_traits::{ImportBlock, ChainInfo};
+use client_traits::{ImportBlock, ChainInfo, StateOrBlock, Balance, Nonce, BlockChainClient};
 use spec::Genesis;
-use ethjson::test_helpers::blockchain;
+use ethjson::{
+	test_helpers::blockchain,
+	spec::State
+};
 use miner::Miner;
 use io::IoChannel;
 use test_helpers::{self, EvmTestClient};
-use types::verification::Unverified;
+use types::{
+	verification::Unverified,
+	ids::BlockId,
+	client_types::StateResult
+};
 use verification::{VerifierType, queue::kind::BlockLike};
 use super::{HookType, SKIP_TESTS};
+use rustc_hex::ToHex;
+use ethereum_types::{U256, H256}; 
 
 #[allow(dead_code)]
 fn skip_test(name: &String, is_legacy: bool) -> bool {
@@ -36,6 +45,80 @@ fn skip_test(name: &String, is_legacy: bool) -> bool {
 	};
 	skip_set.iter()
 		.any(|block_test|block_test.subtests.contains(name))
+}
+
+fn check_poststate(client: &Arc<Client>, test_name: &str, post_state: State) -> bool {
+	let mut success = true;
+
+	for (address,expected) in post_state {
+ 
+		if let Some(expected_balance) = expected.balance {
+			let expected_balance : U256 = expected_balance.into();
+			let current_balance = client.balance(&address.into(),StateOrBlock::Block(BlockId::Latest)).unwrap();
+			if expected_balance != current_balance {
+				warn!(target: "json-tests", "{} – Poststate {:?} balance mismatch current={} expected={}",
+					test_name, address, current_balance, expected_balance);
+				success = false;
+			}											
+		}
+
+		if let Some(expected_nonce) = expected.nonce {
+			let expected_nonce : U256 = expected_nonce.into();
+			let current_nonce = client.nonce(&address.into(), BlockId::Latest).unwrap();
+			if expected_nonce != current_nonce {
+				warn!(target: "json-tests", "{} – Poststate {:?} nonce mismatch current={} expected={}",
+					test_name, address, current_nonce, expected_nonce);
+				success = false;
+			}											
+		}
+
+		if let Some(expected_code) = expected.code {
+			let expected_code : String = expected_code.as_ref().to_hex();
+			let current_code = match client.code(&address.into(),StateOrBlock::Block(BlockId::Latest)) {
+				StateResult::Some(Some(code)) => code.to_hex(), 
+				_ => "".to_string(),
+			};
+			if current_code != expected_code {
+				warn!(target: "json-tests", "{} – Poststate {:?} code mismatch current={} expected={}",
+					test_name, address, current_code, expected_code);
+				success = false;
+			}
+		}
+
+		if let Some(expected_storage) = expected.storage {
+			for (uint_position,uint_expected_value) in expected_storage.iter() {
+				
+				let mut position = H256::default();
+				uint_position.0.to_big_endian(position.as_fixed_bytes_mut());
+				
+				let mut expected_value = H256::default();
+				uint_expected_value.0.to_big_endian(expected_value.as_fixed_bytes_mut());
+
+				let current_value = client.storage_at(&address.into(), &position, StateOrBlock::Block(BlockId::Latest)).unwrap();
+
+				if current_value != expected_value {
+					warn!(target: "json-tests", "{} – Poststate {:?} state {} mismatch actual={} expected={}",
+						test_name, address, position.as_bytes().to_hex::<String>(), current_value.as_bytes().to_hex::<String>(),
+						expected_value.as_bytes().to_hex::<String>());
+					success = false;
+				}
+			}
+		}
+
+		if expected.builtin.is_some() {
+			warn!(target: "json-tests", "{} – Poststate {:?} builtin not supported", test_name, address);
+			success = false;
+		}
+		if expected.constructor.is_some() {
+		    warn!(target: "json-tests", "{} – Poststate {:?} constructor not supported", test_name, address);
+			success = false;
+		}
+		if expected.version.is_some() {
+			warn!(target: "json-tests", "{} – Poststate {:?} version not supported", test_name, address);
+			success = false;
+		}
+	}
+	success
 }
 
 #[allow(dead_code)]
@@ -119,7 +202,17 @@ pub fn json_chain_test<H: FnMut(&str, HookType)>(path: &Path, json_data: &[u8], 
 						}
 					}
 				}
-				fail_unless(client.chain_info().best_block_hash == blockchain.best_block.into());
+
+				let post_state_success = if let Some(post_state) = blockchain.post_state.clone() {
+					check_poststate(&client,&name, post_state)
+				} else {
+					true
+				};
+
+				fail_unless(
+					client.chain_info().best_block_hash == blockchain.best_block.into() 
+					&& post_state_success
+				);
 			}
 		}
 
