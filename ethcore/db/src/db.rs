3e12a954753772dd07c097f79fac525c57d30b3f
David <dvdplm@gmail.com>
2020-04-14 13:29:39 +0200

    Update ethcore/db/src/db.rs
    Co-Authored-By: Andronik Ordian <write@reusable.software>

--- ethcore/db/src/db.rs
+++ ethcore/db/src/db.rs
@@ -36,7 +36,7 @@ pub const COL_EXTRA: u32 = 3;
 /// Column for Traces
 pub const COL_TRACE: u32 = 4;
 /// Column for the empty accounts bloom filter.
-/// **DEPRECATED**
+#[deprecated(since = "3.0.0", note = "Bloom column is deprecated")]
 pub const COL_ACCOUNT_BLOOM: u32 = 5;
 /// Column for general information from the local node which can persist.
 pub const COL_NODE_INFO: u32 = 6;
