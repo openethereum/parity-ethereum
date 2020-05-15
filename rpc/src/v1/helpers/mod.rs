8fbea5d6b5636056f392fc14f0764ca8759b563e
Artem Vorotnikov <artem@vorotnikov.me>
2020-05-15 09:08:56 +0300

    Fix naming in license headers

--- rpc/src/v1/helpers/mod.rs
+++ rpc/src/v1/helpers/mod.rs
@@ -1,18 +1,18 @@
 // Copyright 2015-2020 Parity Technologies (UK) Ltd.
-// This file is part of Open Ethereum.
+// This file is part of OpenEthereum.
 
-// Open Ethereum is free software: you can redistribute it and/or modify
+// OpenEthereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Open Ethereum is distributed in the hope that it will be useful,
+// OpenEthereum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Open Ethereum.  If not, see <http://www.gnu.org/licenses/>.
+// along with OpenEthereum.  If not, see <http://www.gnu.org/licenses/>.
 
 #[macro_use]
 pub mod errors;
