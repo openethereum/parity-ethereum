91c7a7d296a511823241d89e01f6aaf8c7c1896c
adria0 <adria@codecontext.io>
2020-05-29 21:10:11 +0200

    ECD#88 update

--- ethcore/evm/src/instructions.rs
+++ ethcore/evm/src/instructions.rs
@@ -321,11 +321,11 @@ enum_with_from_u8! {
 		LOG4 = 0xa4,
 
 		#[doc = "Marks the entry point to a subroutine."]
-		BEGINSUB = 0xb2,
-		#[doc = "Jumps to a defined BEGINSUB subroutine."]
-		JUMPSUB = 0xb3,
+		BEGINSUB = 0x5c,
 		#[doc = "Returns from a subroutine."]
-		RETURNSUB = 0xb7,
+		RETURNSUB = 0x5d,
+		#[doc = "Jumps to a defined BEGINSUB subroutine."]
+		JUMPSUB = 0x5e,
 
 		#[doc = "create a new account with associated code"]
 		CREATE = 0xf0,
@@ -593,8 +593,8 @@ lazy_static! {
 		arr[LOG3 as usize] = Some(InstructionInfo::new("LOG3", 5, 0, GasPriceTier::Special));
 		arr[LOG4 as usize] = Some(InstructionInfo::new("LOG4", 6, 0, GasPriceTier::Special));
 		arr[BEGINSUB as usize] = Some(InstructionInfo::new("BEGINSUB", 0, 0, GasPriceTier::Base));
-		arr[JUMPSUB as usize] = Some(InstructionInfo::new("JUMPSUB", 1, 0, GasPriceTier::Mid));
-		arr[RETURNSUB as usize] = Some(InstructionInfo::new("RETURNSUB", 0, 0, GasPriceTier::Base));
+		arr[JUMPSUB as usize] = Some(InstructionInfo::new("JUMPSUB", 1, 0, GasPriceTier::High));
+		arr[RETURNSUB as usize] = Some(InstructionInfo::new("RETURNSUB", 0, 0, GasPriceTier::Low));
 		arr[CREATE as usize] = Some(InstructionInfo::new("CREATE", 3, 1, GasPriceTier::Special));
 		arr[CALL as usize] = Some(InstructionInfo::new("CALL", 7, 1, GasPriceTier::Special));
 		arr[CALLCODE as usize] = Some(InstructionInfo::new("CALLCODE", 7, 1, GasPriceTier::Special));
